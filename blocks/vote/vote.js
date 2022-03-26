b3c82cccd67d5c78cca2583ce34de59ef3d8e38d
Chris Millar <cmillar@adobe.com>
2022-03-26 11:48:27 -0600

    IMS

--- blocks/vote/vote.js
+++ blocks/vote/vote.js
@@ -1,3 +1,7 @@
+import { loadScript } from '../../scripts/scripts.js';
+
+const data = {};
+
 function decorateRatings(el) {
   const ratingsEl = el.querySelector('h2 + p');
   const ratings = ratingsEl.textContent.split(' ');
@@ -15,86 +19,126 @@ function decorateRatings(el) {
   return ratingsHeader;
 }
 
-export default async function init(el) {
-  const intro = el.querySelector('p');
-  const heading = el.querySelector('h2');
+async function imsReady() {
+  const accessToken = window.adobeIMS.getAccessToken();
+    if (accessToken) {
+      const profile = await adobeIMS.getProfile();
+      data.id = profile.userId;
+      document.querySelector('.login').classList.add('hide');
+      document.querySelector('.vote').classList.remove('hide');
+    } else {
+      window.adobeIMS.signIn();
+    }
+}
 
-  // Setup Ratings Header
-  const ratingsHeader = decorateRatings(el);
+function loadIms(el) {
+  window.adobeid = {
+    client_id: 'wp4',
+    scope: 'AdobeID,openid',
+    locale: 'en_US',
+    autoValidateToken: true,
+    environment: 'stg1',
+    useLocalStorage: false,
+    onReady: () => { imsReady(el) },
+  };
+  loadScript('https://auth.services.adobe.com/imslib/imslib.min.js');
+}
 
-  const form = document.createElement('form');
-  const resp = await fetch('names.json');
-  const json = await resp.json();
+function buildRow(choice) {
+  const { name, description } = choice;
 
-  const data = {};
+  // Card
+  const card = document.createElement('div');
+  card.className = 'vote-card';
 
-  const choices = json.data.map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
+  // Text Container
+  const text = document.createElement('div');
+  text.className = 'text';
 
-  choices.forEach((choice) => {
-    const { name, description } = choice;
+  // Label
+  const title = document.createElement('h3');
+  title.innerHTML = name;
 
-    // Card
-    const card = document.createElement('div');
-    card.className = 'vote-card';
+  // Description
+  const desc = document.createElement('p');
+  desc.innerHTML = description;
 
-    // Text Container
-    const text = document.createElement('div');
-    text.className = 'text';
+  const sliderContainer = document.createElement('div');
+  sliderContainer.className = 'slider-container';
+  const slider = document.createElement('input');
+  slider.className = 'slider';
+  slider.type = 'range';
+  slider.setAttribute('min', -1);
+  slider.setAttribute('value', 0);
+  slider.setAttribute('max', 1);
+  slider.id = name;
 
-    // Label
-    const title = document.createElement('h3');
-    title.innerHTML = name;
+  data[name] = 0;
 
-    // Description
-    const desc = document.createElement('p');
-    desc.innerHTML = description;
-
-    const sliderContainer = document.createElement('div');
-    sliderContainer.className = 'slider-container';
-    const slider = document.createElement('input');
-    slider.className = 'slider';
-    slider.type = 'range';
-    slider.setAttribute('min', -1);
-    slider.setAttribute('value', 0);
-    slider.setAttribute('max', 1);
-    slider.id = name;
-
-    data[name] = 0;
-
-    slider.addEventListener('change', (e) => {
-      slider.setAttribute('value', e.target.value);
-      data[name] = e.target.value;
-    });
-    sliderContainer.append(slider);
-
-    text.append(title, desc);
-    card.append(text, sliderContainer);
-    form.append(card);
+  slider.addEventListener('change', (e) => {
+    slider.setAttribute('value', e.target.value);
+    data[name] = e.target.value;
   });
+  sliderContainer.append(slider);
 
+  text.append(title, desc);
+  card.append(text, sliderContainer);
+  return card;
+}
+
+function hasVoted() {
+  return localStorage.getItem('voted');
+}
+
+function buildSend() {
   const send = document.createElement('button');
   send.textContent = 'Send';
   send.className = 'send-vote';
 
   send.addEventListener('click', async (e) => {
-    const resp = await fetch('/names', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ data }),
-    });
-    if (resp.ok) {
-      form.remove();
-      ratingsHeader.remove();
-      send.remove();
-      heading.textContent = 'Thank you!';
-      heading.after(intro);
-      intro.textContent = 'Your vote has been counted.';
-    }
+    e.preventDefault();
+    console.log(data);
+    // const resp = await fetch('/names', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ data }),
+    // });
+    // if (resp.ok) {
+    //   localStorage.setItem('voted', true);
+    //   document.querySelector('.vote').classList.add('hide');
+    //   document.querySelector('.voted').classList.remove('hide');
+    // }
   });
+  return send;
+}
 
-  el.append(ratingsHeader, form, send);
+export default async function init(el) {
+  el.classList.add('contained', 'hide');
+  if (hasVoted()) {
+    document.querySelector('.login').classList.add('hide');
+    document.querySelector('.voted').classList.remove('hide');
+  } else {
+    loadIms(el);
+
+    // Setup Ratings Header
+    const ratingsHeader = decorateRatings(el);
+
+    const form = document.createElement('form');
+    const resp = await fetch('names.json');
+    const json = await resp.json();
+
+    // Randomize choices
+    const choices = json.data.map(value =>
+      ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    choices.forEach((choice) => {
+      const row = buildRow(choice);
+      form.append(row);
+    });
+
+    form.append(buildSend());
+    el.append(ratingsHeader, form);
+  }
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -84,6 +84,22 @@ export function loadCSS(href, callback) {
   }
 }
 
+export function loadScript(url, callback, type) {
+  let script = document.querySelector(`head > script[src="${url}"]`);
+  if (!script) {
+    const { head } = document;
+    script = document.createElement('script');
+    script.setAttribute('src', url);
+    if (type) {
+      script.setAttribute('type', type);
+    }
+    script.onload = () => { if (callback) callback(); };
+    script.onerror = () => { if (callback) callback(); };
+    head.append(script);
+  }
+  return script;
+}
+
 /**
  * Retrieves the content of a metadata tag.
  * @param {string} name The metadata name (or property)
