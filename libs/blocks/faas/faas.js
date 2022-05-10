310dfe5ab400341a742edda6705a4d6b7ae8ebc2
Sean Choi <seanchoi@adobe.com>
2022-05-10 12:28:09 -0600

    minor update.

--- libs/blocks/faas/faas.js
+++ libs/blocks/faas/faas.js
@@ -1,9 +1,8 @@
 import { parseEncodedConfig } from '/libs/utils/utils.js';
 import { initFaas, loadFaasFiles } from './utils.js';
 
-export default function init(el) {
-  loadFaasFiles().then(()=>{
-    const a = el  ;
+export default function init(a) {
+  loadFaasFiles().then(() => {
     const encodedConfig = a.href.split('#')[1];
     initFaas(parseEncodedConfig(encodedConfig), a);
   });
