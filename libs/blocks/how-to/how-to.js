8b737200a55b32b16945a71bbc68b507b2442e34
Brad Johnson <fullcolorcoder@gmail.com>
2022-05-25 10:56:48 -0500

    Updated theme, fixed valid JSON

--- libs/blocks/how-to/how-to.js
+++ libs/blocks/how-to/how-to.js
@@ -1,74 +1,42 @@
 function loadJsonLd(el) {
-  // Check for image in first div of el.
-  const firstImage = el.querySelectorAll(':scope > div > div')[0].querySelector('img');
-  const firstImageJson = [];
-  if (firstImage) {
-    const firstImageWidth = firstImage.width;
-    const firstImageHeight = firstImage.height;
-    const imageUrl = firstImage.src;
-    firstImageJson.push({
-      '@type': 'ImageObject',
-      url: imageUrl,
-      height: firstImageWidth,
-      width: firstImageHeight,
-    });
-  }
   let howToSteps;
-  let description;
-  // Check for inital image being used in how-to-steps
-  if (firstImage) {
-    // This has image and description.
-    howToSteps = [...el.querySelectorAll(':scope > div > div')].slice(1);
-    description = howToSteps[0].innerHTML;
-  } else {
-    // This has only description.
-    howToSteps = [...el.querySelectorAll(':scope > div > div')].slice();
-    description = howToSteps[0].innerHTML;
-    howToSteps = howToSteps.slice(1);
-  }
+  howToSteps = [...el.querySelectorAll(':scope > div > div')].slice();
+  const h1Content = howToSteps[0].innerHTML;
+  const description = howToSteps[1].innerHTML;
 
-  // If the how-to-steps have images, filter the images out of the steps.
-  if (el.querySelectorAll(':scope > div + div img')) {
-    howToSteps = howToSteps.filter((element, index) => index % 2 === 0);
-  }
+  const h1 = document.createElement('h1');
+  h1.innerHTML = h1Content;
+  el.appendChild(h1);
+  // give div an id of its h1 content
+  const divId = h1Content.replace(/\s+/g, '-').toLowerCase();
+  h1.id = divId;
+  howToSteps[0].remove();
+  howToSteps = howToSteps.slice(2);
+
   let stepsCount = 1;
-  const steps = [];
-  howToSteps.forEach((step) => {
-    if (step.nextSibling !== null) {
-      // Get current url.
-      const stepImageSrc = step.nextSibling.querySelector('img').src;
-      const stepImageHeight = step.nextSibling.querySelector('img').height;
-      const stepImageWidth = step.nextSibling.querySelector('img').width;
+  const step = [];
+  const orderedList = document.createElement('ol');
 
-      const currentUrl = window.location.href;
-      const stepId = `step-${stepsCount}`;
-      const stepUrl = `${currentUrl}#${stepId}`;
-      if (stepImageSrc) {
-        steps.push(
-          {
-            '@type': 'HowToStep',
-            url: stepUrl,
-            name: `Step ${stepsCount}`,
-            text: step.innerHTML,
-            image: {
-              '@type': 'ImageObject',
-              url: stepImageSrc,
-              height: stepImageHeight,
-              width: stepImageWidth,
-            },
-          },
-        );
-      }
-    } else {
-      steps.push({
-        '@type': 'HowToStep',
-        name: `Step ${stepsCount}`,
-        text: step.innerHTML,
-      });
-    }
+  howToSteps.forEach((steps) => {
+    const stepListItem = document.createElement('li');
+    const stepListDiv = document.createElement('div');
+    stepListDiv.innerHTML = steps.innerHTML;
+    stepListItem.innerHTML = stepListDiv.outerHTML;
+    orderedList.appendChild(stepListItem);
+    step.push({
+      '@type': 'HowToStep',
+      url: `${window.location.href}#${divId}`,
+      name: `Step ${stepsCount}`,
+      text: step.innerHTML,
+      itemListElement: [{
+        '@type': 'HowToDirection',
+        text: steps.innerHTML,
+      }],
+    });
     stepsCount += 1;
+    steps.parentNode.remove();
   });
-
+  el.appendChild(orderedList);
   const jsonLd = {
     '@context': 'http://schema.org',
     '@type': 'HowTo',
@@ -82,14 +50,12 @@ function loadJsonLd(el) {
         url: 'https://www.adobe.com/content/dam/cc/icons/Adobe_Corporate_Horizontal_Red_HEX.svg',
       },
     },
-    image: firstImageJson,
-    mainEntity: {
-      '@type': 'HowToSection',
-      name: 'How to',
-      description,
-      itemListElement: steps,
-    },
+    step,
   };
+
+  if (description) {
+    el.querySelector(':scope > div > div').parentNode.remove();
+  }
 
   const jsonLdString = JSON.stringify(jsonLd);
   const jsonLdScript = document.createElement('script');
