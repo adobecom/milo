f05fcec32ad3d500877ec6506bc0034f69ad7463
Sean Choi <seanchoi@adobe.com>
2022-05-26 10:05:28 -0600

    bug fix from merge.

--- libs/blocks/faas-config/faas-config.js
+++ libs/blocks/faas-config/faas-config.js
@@ -8,7 +8,7 @@ import {
   useState,
 } from '../../deps/htm-preact.js';
 import { loadStyle, getHashConfig, utf8ToB64 } from '../../utils/utils.js';
-import { Accordion } from '../../ui/controls/controls.js';
+import Accordion from '../../ui/controls/Accordion.js';
 import { faasHostUrl, defaultState, initFaas, loadFaasFiles } from '../faas/utils.js';
 
 let faasEl;
