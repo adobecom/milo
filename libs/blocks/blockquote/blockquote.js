641a28592a30210858f310d84bfd657524671e2f
thi64146 <thi64146@adobe.com>
2022-07-01 16:02:52 -0600

    initial blockquote js/css

---
+++ libs/blocks/blockquote/blockquote.js
@@ -0,0 +1,51 @@
+/**
+ * loads and decorates a blockquote
+ * @param {Element} blockquote block element
+ *  ex...
+ *   <figure>
+ *    <blockquote cite="https://www.huxley.net/bnw/four.html">
+ *      <p>Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.</p>
+ *    </blockquote>
+ *    <figcaption>—Aldous Huxley, <cite>Brave New World</cite></figcaption>
+ *   </figure>
+ */
+
+import { decorateBlockBg } from '../../utils/decorate.js';
+
+export default async function init(el) {
+  const rows = el.querySelectorAll(':scope > div');
+  decorateBlockBg(el, rows[0]);
+  const lastRow = rows[rows.length - 1];
+  const imageRow = rows[1];
+  imageRow.classList.add('image');
+
+  const copyNodes = lastRow.querySelectorAll('h1, h2, h3, h4, h5, h6, p');
+  const quoteCopy = copyNodes[0];
+  quoteCopy?.classList.add('quote');
+  const figcaptionCopy = copyNodes[1];
+  figcaptionCopy?.classList.add('figcaption');
+  const citeCopy = copyNodes[2];
+  citeCopy?.classList.add('cite');
+  lastRow.remove();
+
+  const figure = document.createElement('figure');
+  const blockquote = document.createElement('blockquote');
+  const figcaption = document.createElement('figcaption');
+  const cite = document.createElement('cite');
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('quote-wrapper');
+
+  figure.insertAdjacentElement('afterbegin', wrapper);
+  figure.insertAdjacentElement('afterbegin', blockquote);
+  el.insertAdjacentElement('afterbegin', figure);
+
+  figcaption.insertAdjacentElement('afterbegin', figcaptionCopy);
+  cite.insertAdjacentElement('afterbegin', citeCopy)
+  figcaption.insertAdjacentElement('beforeend', cite);
+  blockquote.insertAdjacentElement('afterend', figcaption);
+  blockquote.insertAdjacentElement('afterbegin', quoteCopy);
+  blockquote.insertAdjacentElement('beforebegin', imageRow);
+  wrapper.insertAdjacentElement('beforeend', figcaption);
+  wrapper.insertAdjacentElement('afterbegin', blockquote);
+
+}
