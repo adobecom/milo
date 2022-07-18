ee14ee137687ff6c0693e8a00b9cad4b4ae1bae0
Megan Thomas <mnthomas15@gmail.com>
2022-07-18 16:40:03 -0700

    init bar chart MWPW-112993 (#69)
    * init bar chart MWPW-112993
    
    * add todo, cleanup chart theme
    
    Co-authored-by: Megan Thomas <methomas@adobe.com>

--- libs/blocks/chart/chart.js
+++ libs/blocks/chart/chart.js
@@ -1,4 +1,12 @@
-import { makeRelative } from '../../utils/utils.js';
+import { makeRelative, loadScript } from '../../utils/utils.js';
+import getTheme from './chartLightTheme.js';
+
+// const SMALL = 'small';
+// const MEDIUM = 'medium';
+const LARGE = 'large';
+const chartTypes = [
+  'bar',
+];
 
 function processDataset(data) {
   const dataset = {};
@@ -48,20 +56,111 @@ async function fetchData(link) {
   return data;
 }
 
-export default async function init(el) {
-  const link = el.querySelector('a[href$="json"]');
-  if (!link) return;
+const barTooltipFormatter = ({
+  seriesName,
+  marker,
+  value,
+  encode: { x = [] },
+  name,
+} = {}, unit = '') => (
+  `${seriesName}<br />${marker} ${value[x[0]]}${unit} ${name}<i class="tooltip_icon"></i>`
+);
 
-  const data = await fetchData(link);
+const barSeriesOptions = (seriesData, colors, unit) => (
+  seriesData.map((value, index) => ({
+    type: 'bar',
+    label: {
+      show: true,
+      formatter: `{@[${index + 1}]}${unit}`,
+    },
+    showBackground: true,
+    backgroundStyle: { color: colors[index] },
+  }))
+);
+
+export const getChartOptions = (chartType, dataset, colors, unit = '') => {
+  const source = dataset?.source;
+  const seriesData = (source && source[1]) ? source[1].slice() : [];
+
+  seriesData.shift();
+
+  return {
+    dataset,
+    color: colors,
+    legend: {
+      show: true,
+      type: 'scroll',
+    },
+    tooltip: {
+      show: true,
+      formatter: (params) => barTooltipFormatter(params, unit),
+    },
+    xAxis: {
+      type: chartType === 'bar' ? 'value' : 'category',
+      axisLabel: { show: chartType !== 'bar' },
+      axisTick: { show: chartType !== 'bar' },
+      max: (value) => {
+        if (chartType !== 'bar') return null;
+        // This adds extra space on xAxis so labels will fit
+        const extraSpace = 1;
+        return Math.ceil((value.max + (value.max * extraSpace)) / 10) * 10;
+      },
+    },
+    yAxis: {
+      type: chartType === 'bar' ? 'category' : 'value',
+      axisLabel: { show: chartType !== 'bar' },
+      axisTick: { show: chartType !== 'bar' },
+    },
+    series: barSeriesOptions(seriesData, colors, unit),
+  };
+};
+
+const init = async (el) => {
+  const children = el?.querySelectorAll(':scope > div');
+  children[0]?.classList.add('chart_title');
+  children[1]?.classList.add('chart_subTitle');
+  children[3]?.classList.add('chart_footnote');
+
+  // ToDo: Replace hardcoded size MWPW-112994
+  const size = LARGE;
+  const chartRow = children[2];
+
+  if (chartRow) {
+    chartRow.style.width = '600px';
+    chartRow.style.height = '600px';
+  }
+
+  const chart = chartRow?.querySelector(':scope > div');
+
+  if (chart) {
+    chart.style.width = '600px';
+    chart.style.height = '600px';
+  }
+
+  const chartType = chartTypes?.find((type) => el?.className?.indexOf(type));
+  const dataLink = chart?.querySelector('a[href$="json"]');
+  dataLink?.remove();
+
+  if (!chartType || !chart || !dataLink) return;
+
+  const data = await fetchData(dataLink);
   if (!data) return;
 
-  const dataSet = processDataset(data.data);
-  const series = processSeries(data);
+  const dataset = processDataset(data.data);
   const unit = data?.data[0]?.Unit;
+  // ToDo: Replace colors MWPW-112994
+  const colors = ['red', 'blue', 'green'];
 
-  // TODO: Create Chart
-  const chart = document.createElement('pre');
-  chart.textContent = JSON.stringify(dataSet, null, 2);
+  if (chartType !== 'oversizedNumber') {
+    loadScript('/libs/deps/echarts.min.js')
+      .then(() => {
+        const themeName = getTheme(size);
+        const barChart = window.echarts?.init(chart, themeName, { renderer: 'svg' });
 
-  link.replaceWith(chart);
-}
+        barChart?.setOption(getChartOptions(chartType, dataset, colors, unit));
+      })
+      .catch((error) => console.log('Error loading script:', error));
+  }
+};
+
+export default init;
