cc1b1bd01cf6d10f03ff010416b2b7335b5fa758
Megan Thomas <mnthomas15@gmail.com>
2022-07-20 16:03:58 -0700

    Clean up chart theme and add tests MWPW-112997 (#71)
    * Clean up chart theme and add tests MWPW-112997
    
    * Add comments

--- test/blocks/chart/chart.test.js
+++ test/blocks/chart/chart.test.js
@@ -3,6 +3,7 @@
 
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
 
 const { getChartOptions } = await import('../../../libs/blocks/chart/chart.js');
 
@@ -11,5 +12,27 @@ describe('chart', () => {
     document.body.innerHTML = await readFile({ path: './mocks/bar.html' });
     const options = getChartOptions('bar', {}, []);
     expect(options).to.exist;
+  });
+});
+
+const { default: getTheme, lightLarge } = await import('../../../libs/blocks/chart/chartLightTheme.js');
+
+describe('chartLightTheme', async () => {
+  it('returns null if echarts does not exist', () => {
+    expect(getTheme()).to.be.null;
+  });
+
+  it('registers theme if it does not exist', async () => {
+    window.dataViz = { lightLarge: false };
+    window.echarts = { registerTheme: sinon.spy() };
+    expect(getTheme('large')).to.equal(lightLarge);
+    expect(window.echarts.registerTheme.called).to.be.true;
+  });
+
+  it('returns name if theme already exists', async () => {
+    window.dataViz = { lightLarge: true };
+    window.echarts = { registerTheme: sinon.spy() };
+    expect(typeof getTheme('large')).to.equal('string');
+    expect(window.echarts.registerTheme.called).to.be.false;
   });
 });
