6192761d5672b8b912e79d4f77aa97df90304c7d
bhagwath <bhagwath@adobe.com>
2022-07-24 23:12:29 -0700

    Adding Helix md2mdast and mdast2docx web versions POC for round trip.

--- tools/loc/generatePatch.js
+++ tools/loc/generatePatch.js
@@ -1,3 +1,10 @@
+import parseMarkdown from './helix/parseMarkdown.bundle.js';
+import mdast2docx from './helix/mdast2docx.bundle.js';
+import {
+  connect as connectToSP,
+  saveFile,
+} from '../translation/sharepoint.js';
+
 const MAX_RETRIES = 5;
 
 async function fetchFile(url, retryAttempt) {
@@ -9,20 +16,20 @@ async function fetchFile(url, retryAttempt) {
 }
 
 async function init() {
-  const first = await fetchFile('https://diffpoc--milo--adobecom.hlx.page/drafts/bhagwath/validatemd.md', 1);
-  const second = await fetchFile('https://diffpoc--milo--adobecom.hlx.page/drafts/bhagwath/validatemd1.md', 1);
-  const third = await fetchFile('https://diffpoc--milo--adobecom.hlx.page/drafts/bhagwath/validatemd2.md', 1);
+  const first = await fetchFile('http://localhost:3000/drafts/bhagwath/validatemd.md', 1);
+  const second = await fetchFile('http://localhost:3000/drafts/bhagwath/validatemd1.md', 1);
+  const third = await fetchFile('http://localhost:3000/drafts/bhagwath/validatemd2.md', 1);
   const firstText = await first.text();
-  const secondText = await second.text();
+  let secondText = await second.text();
   const thirdText = await third.text();
   // Char Diff
-  const diffChars = Diff.diffWords(firstText, secondText);
+  /*const diffChars = Diff.diffWords(firstText, secondText);
   const diffCharsContainer = document.getElementById('md-diff-chars');
   diffChars.forEach((part) => {
     const paragraph = document.createElement('p');
     paragraph.textContent = JSON.stringify(part, undefined, 2);
     diffCharsContainer.append(paragraph);
-  });
+  });*/
   // Line Diff
   const diffLines = Diff.diffLines(firstText, secondText);
   const diffLinesContainer = document.getElementById('md-diff-lines');
@@ -36,13 +43,37 @@ async function init() {
   const diffContainer = document.getElementById('md-diff');
   diffContainer.innerText = standardPatch;
   // Structured Patch
-  const structuredPatch = Diff.structuredPatch('firstmd', 'secondmd', firstText, secondText);
+  /*const structuredPatch = Diff.structuredPatch('firstmd', 'secondmd', firstText, secondText);
   const diffContainer2 = document.getElementById('md-diff-json');
-  diffContainer2.textContent = JSON.stringify(structuredPatch, undefined, 2);
+  diffContainer2.textContent = JSON.stringify(structuredPatch, undefined, 2);*/
   // eslint-disable-next-line max-len
-  const appliedPatch = Diff.applyPatch(thirdText, Diff.parsePatch(standardPatch), { fuzzFactor: 10 });
+  /*const appliedPatch = Diff.applyPatch(thirdText, Diff.parsePatch(standardPatch), { fuzzFactor: 10 });
   const appliedPatchContainer = document.getElementById('md-generated-patch');
-  appliedPatchContainer.textContent = appliedPatch;
+  appliedPatchContainer.textContent = appliedPatch;*/
+  // markdown parsing
+  const state = { content: { data: firstText }, log: '' };
+  parseMarkdown(state);
+  const { mdast } = state.content;
+  const mdastContainer = document.getElementById('md-syntax-tree');
+  const mdastStr = JSON.stringify(mdast, undefined, 2);
+  mdastContainer.textContent = mdastStr;
+
+  // Line Diff for MDAST
+  const state2 = { content: { data: secondText }, log: '' };
+  parseMarkdown(state2);
+  const mdast2 = state2.content.mdast;
+  const mdast2Str = JSON.stringify(mdast2, undefined, 2);
+  const mdastDiffContainer = document.getElementById('md-syntax-tree-diff');
+  const diffLinesMdast = Diff.diffLines(mdastStr, mdast2Str);
+  diffLinesMdast.forEach((part) => {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = JSON.stringify(part, undefined, 2);
+    mdastDiffContainer.append(paragraph);
+  });
+  const docx = await mdast2docx(mdast2);
+  await connectToSP(async () => {
+    await saveFile(docx, '/drafts/bhagwath/ValidateMD2AutoSavedV2.docx');
+  });
 }
 
 export default init;
