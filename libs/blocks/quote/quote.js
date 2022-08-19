d11ec9a0c9f7ea3bb8277401b50fce39f14d97a8
thi64146 <thi64146@adobe.com>
2022-08-19 14:14:09 -0600

    removed blockquote, cleaned up quote

--- libs/blocks/quote/quote.js
+++ libs/blocks/quote/quote.js
@@ -17,46 +17,26 @@
 import { createTag } from '../../utils/utils.js';
 
 export default function init(el) {
-
-  // const elContainer = el.closest('.container');
-  // const elFullWidth = el.classList.contains('full-width');
-  // if (!elFullWidth) el.classList.add('contained');
-  // if (!elFullWidth && elContainer === null) el.classList.add('contained');
-  // console.log('!elFullWidth && elContainer === null', !elFullWidth && elContainer === null);
-  // console.log('elContainer === null', elContainer === null);
-  // console.log('elFullWidth', elFullWidth);
-
   const allRows = el.querySelectorAll(':scope > div');
   const lastRow = allRows[allRows.length - 1];
-  lastRow.classList.add('last-row');
-  const rows = el.querySelectorAll(':scope > div:not([class])');
-  rows.forEach((row) => {
-    if (row.querySelector(':scope img')) {
-      row.classList.add('quote-image');
-    } else {
-      console.log('row', row);
-      el.classList.add('background');
-      el.style.background = row.textContent.trim();
-      row.remove();
-    }
-  });
-  const copyNodes = lastRow.querySelectorAll('h1, h2, h3, h4, h5, h6, p');
-  const quoteCopy = copyNodes[0];
-  let figcaptionCopy = copyNodes[1];
-  let citeCopy = copyNodes[2];
-  const blockquote = createTag('blockquote', {}, quoteCopy);
-  const figcaption = createTag('figcaption', {}, figcaptionCopy);
-  const cite = createTag('cite', {}, citeCopy);
+  let imageRow = null;
+  if (allRows.length > 1) {
+    imageRow = allRows[0];
+    imageRow.classList.add('quote-image');
+  }
+  const copyNodes = lastRow.querySelectorAll('p');
+  const blockquote = createTag('blockquote', {}, copyNodes[0]);
+  const figcaption = createTag('figcaption', {}, copyNodes[1]);
+  const cite = createTag('cite', {}, copyNodes[2]);
   const wrapper = createTag('div', { class: 'quote-wrapper' });
   const figure = createTag('figure', {}, wrapper);
-  quoteCopy?.classList.add('quote-copy');
-  figcaptionCopy?.classList.add('figcaption');
+  copyNodes[0]?.classList.add('quote-copy');
+  copyNodes[1]?.classList.add('figcaption');
   lastRow.remove();
   figure.insertAdjacentElement('afterbegin', blockquote);
   el.insertAdjacentElement('afterbegin', figure);
-  if (citeCopy) figcaption.insertAdjacentElement('beforeend', cite);
+  if (copyNodes[2]) figcaption.insertAdjacentElement('beforeend', cite);
   blockquote.insertAdjacentElement('afterend', figcaption);
-  const imageRow = el.querySelector(':scope > div.quote-image');
   if (imageRow) blockquote.insertAdjacentElement('beforebegin', imageRow);
   wrapper.insertAdjacentElement('beforeend', figcaption);
   wrapper.insertAdjacentElement('afterbegin', blockquote);
