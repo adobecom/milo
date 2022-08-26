68acc10bde82cd5dad8af1604a8021acc3d80c4a
prathyushanallapati <prathyusha.nallapati@gmail.com>
2022-08-26 21:47:00 +0530

    MWPW-108925 [Blog Support] Article Ratings (#97)
    * MWPW-108925 [Blog Support] Article Ratings
    
    * MWPW-108925 [Blog Support] Article Ratings
    
    * MWPW-108925 [Blog Support] Article Ratings
    
    * MWPW-108925 [Blog Support] Article Ratings
    
    * MWPW-108925 Fixed linting issues
    
    * MWPW-108925 More Functional Fixes
    
    * MWPW-108925 Handle GET review's response being an empty array
    
    * Remove wrong test file
    
    * MWPW-108925 [Blog Support] Article Ratings clean up css
    
    * Addressed review comments
    
    * Addressed review comments
    
    * MWPW-108925 Added UTs
    
    * retrigger tests
    
    * MWPW-108925 Added more UTs
    
    * MWPW-108925 Added more tests for sub-components
    
    * MWPW-108925- Expose actual block to be consumed by blog.adobe.com
    
    * MWPW-108925 [Blog Support] Article Ratings Improve UT
    
    * MWPW-108925 Added README for Review component
    
    * MWPW-108925 Minor formatting in README for Review component
    
    Co-authored-by: Prathyusha Boayapti <boyapati@adobe.com>
    Co-authored-by: Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
    Co-authored-by: Chris Millar <cmillar@adobe.com>

---
+++ test/blocks/review/components/helixReview/HelixReview.test.js
@@ -0,0 +1,112 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it beforeEach */
+import { expect } from '@esm-bundle/chai';
+import { stub } from 'sinon';
+import { html, render } from '../../../../../libs/deps/htm-preact.js';
+import { waitForElement, delay } from '../../../../helpers/selectors.js';
+
+import HelixReview from '../../../../../libs/blocks/review/components/helixReview/HelixReview.js';
+import {
+  stubFetch,
+  stubFetchError,
+  stubEmptyResponse,
+} from './mockHelixData.js';
+
+describe('HelixReview', () => {
+  stubFetch();
+  beforeEach(() => {
+    const strings = {
+      tooltipdelay: 5,
+      postUrl: '',
+    };
+    window.localStorage.setItem('/data/review', JSON.stringify({ rating: 5 }));
+    const helixReview = html`<${HelixReview}
+      clickTimeout="5000"
+      commentThreshold="3"
+      hideTitleOnReload="true"
+      lang="en"
+      reviewTitle="Review"
+      strings=${strings}
+      tooltipDelay=${strings.tooltipdelay}
+      postUrl=${strings.postUrl}
+      visitorId=""
+      reviewPath="/data/review"
+      onRatingSet=${({ rating, comment }) => {}}
+      onRatingHover=${({ rating }) => {}}
+      onReviewLoad=${({ hasRated, rating }) => {}}
+      productJson="{}"
+    />`;
+    render(helixReview, document.body);
+  });
+
+  it('should display review', async () => {
+    const reviewElement = await waitForElement('.hlx-ReviewWrapper');
+    const titleElement = reviewElement.querySelector('.hlx-reviewTitle');
+    expect(reviewElement).to.exist;
+    expect(titleElement).to.exist;
+  });
+
+  it('should test handle submit', async () => {
+    const reviewElement = await waitForElement('.hlx-ReviewWrapper');
+    const formElement = reviewElement.querySelector('.hlx-Review');
+    formElement.dispatchEvent(new Event('submit'));
+    await delay(100);
+    const thankYouElement = reviewElement.querySelector('.hlx-submitResponse');
+    expect(thankYouElement).to.exist;
+  });
+
+  it('should test the review without review path', async () => {
+    stubEmptyResponse();
+    const strings = {
+      tooltipdelay: 5,
+      postUrl: '',
+    };
+    const helixReview = html`<${HelixReview}
+      clickTimeout="5000"
+      commentThreshold="3"
+      hideTitleOnReload="true"
+      lang="en"
+      reviewTitle="Review"
+      strings=${strings}
+      tooltipDelay=${strings.tooltipdelay}
+      postUrl=${strings.postUrl}
+      visitorId=""
+      reviewPath="/data/review"
+      onRatingSet=${({ rating, comment }) => {}}
+      onRatingHover=${({ rating }) => {}}
+      onReviewLoad=${({ hasRated, rating }) => {}}
+      productJson="{}"
+    />`;
+    render(helixReview, document.body);
+    const reviewElement = await waitForElement('.hlx-ReviewWrapper');
+    expect(reviewElement).to.exist;
+  });
+
+  it('should test the review without review path', async () => {
+    stubFetchError();
+    window.localStorage.removeItem('/data/review');
+    const strings = {
+      tooltipdelay: 5,
+      postUrl: '',
+    };
+    const helixReview = html`<${HelixReview}
+      clickTimeout="5000"
+      commentThreshold="3"
+      hideTitleOnReload="true"
+      lang="en"
+      reviewTitle="Review"
+      strings=${strings}
+      tooltipDelay=${strings.tooltipdelay}
+      postUrl=${strings.postUrl}
+      visitorId=""
+      reviewPath="/data/review"
+      onRatingSet=${({ rating, comment }) => {}}
+      onRatingHover=${({ rating }) => {}}
+      onReviewLoad=${({ hasRated, rating }) => {}}
+      productJson="{}"
+    />`;
+    render(helixReview, document.body);
+    const reviewElement = await waitForElement('.hlx-ReviewWrapper');
+    expect(reviewElement).to.exist;
+  });
+});
