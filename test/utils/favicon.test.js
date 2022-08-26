99a5957d5febe08cc915759376fa9d15099a9de0
Chris Millar <cmillar@adobe.com>
2022-08-25 18:35:33 -0600

    MWPW-115472 - Favicon (#127)
    * Ability to have a default / no-touch favicon
    * Ability to spec a favicon using metadata
    * Support for app icons are also built in
    * Extensible for consumers and specific content trees
    
    Resolves: [MWPW-115472](https://jira.corp.adobe.com/browse/MWPW-115472)

---
+++ test/utils/favicon.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it */
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import { setConfig, createTag, getMetadata } from '../../libs/utils/utils.js';
+import loadFavIcons from '../../libs/utils/favicon.js';
+
+document.head.innerHTML = await readFile({ path: './mocks/head.html' });
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+
+describe('Favicon', () => {
+  it('sets all default favicon props', async () => {
+    const config = setConfig({});
+    const favicon = document.head.querySelector('link[rel="icon"]');
+    loadFavIcons(createTag, config, getMetadata);
+    expect(favicon.href).to.equal('http://localhost:2000/img/favicons/favicon.ico');
+  });
+
+  it('sets metadata favicon', async () => {
+    const meta = createTag('meta', { name: 'favicon', content: 'otis' });
+    document.head.append(meta);
+    const config = setConfig({});
+    const favicon = document.head.querySelector('link[rel="icon"]');
+    loadFavIcons(createTag, config, getMetadata);
+    expect(favicon.href).to.equal('http://localhost:2000/img/favicons/otis.ico');
+  });
+});
