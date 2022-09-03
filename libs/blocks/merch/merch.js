a53fda3b0a3334f5881a5349e56bdea02ce3a624
Chris Millar <cmillar@adobe.com>
2022-09-03 17:13:23 -0600

    MWPW-116458 - Merch at Scale (#162)
    * New Merch block
    * Add an offer id and you get a price
    * Add a link and you get a checkout button
    * Link / button can be localized
    * Supports all M@S price templates: strikethrough, with tax, optical, etc.
    
    Resolves: [MWPW-116458](https://jira.corp.adobe.com/browse/MWPW-116458)

---
+++ libs/blocks/merch/merch.js
@@ -0,0 +1,55 @@
+import { loadScript, getConfig, createTag } from '../../utils/utils.js';
+
+const { miloLibs, codeRoot } = getConfig();
+const base = miloLibs || codeRoot;
+
+await loadScript(`${base}/deps/tacocat-index.js`);
+
+function buildButton(a, osi) {
+  if (!a) return null;
+  a.href = '#';
+  a.className = 'con-button blue button-M';
+  a.dataset.checkoutClientid = 'mini_plans';
+  a.dataset.checkoutWorkflow = 'UCv3';
+  a.dataset.checkoutWorkflowStep = 'email';
+  a.dataset.wcsOsi = osi;
+  a.dataset.template = 'checkoutUrl';
+  return a;
+}
+
+function buildPrice(osi, type) {
+  return createTag('span', { 'data-wcs-osi': osi, 'data-template': type });
+}
+
+function getPriceType(name) {
+  switch (name) {
+    case 'price': { return 'price'; }
+    case 'optical': { return 'priceOptical'; }
+    case 'strikethrough': { return 'priceStrikethrough'; }
+    case 'with-tax': { return 'priceWithTax'; }
+    case 'with-strikethrough-tax': { return 'priceWithTaxStrikethrough'; }
+    default: return null;
+  }
+}
+
+export default async function init(el) {
+  const osi = el.querySelector(':scope > div:first-of-type > div').textContent;
+  if (!osi) return;
+  const priceType = getPriceType([...el.classList][1]);
+  if (priceType) {
+    const price = buildPrice(osi, priceType);
+    el.append(price);
+  }
+  const button = buildButton(el.querySelector('a'), osi);
+  if (button) {
+    el.append(button);
+  }
+
+  // Show *something* if there's just an OSI and nothing else.
+  if (!priceType && !button) {
+    const price = buildPrice(osi, 'price');
+    el.append(price);
+  }
+
+  window.tacocat();
+}
