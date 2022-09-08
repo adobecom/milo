ddd32f22408630f6a93acc148f22ba453ff64e30
Sean Choi <seanchoi@adobe.com>
2022-09-08 12:17:28 -0600

    update 404.js

--- libs/templates/404/404.js
+++ libs/templates/404/404.js
@@ -1,21 +1,6 @@
-import {
-  getConfig, setConfig, createTag, getMetadata, loadHeader, loadFooter,
-} from '../utils/utils.js';
-
-const locales = {
-  '': { ietf: 'en-US', tk: 'hah7vzn.css' },
-  de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
-  cn: { ietf: 'zh-CN', tk: 'tav4wnu' },
-  kr: { ietf: 'ko-KR', tk: 'zfo3ouc' },
-};
-const config = {
-  imsClientId: 'milo',
-  codeRoot: '/libs',
-  locales,
-};
+import { getConfig, createTag } from '../../utils/utils.js';
 
 export default async function load404() {
-  setConfig(config);
   const { locale } = getConfig();
   const resp = await fetch(`${locale.contentRoot}/404.plain.html`);
   if (resp.ok) {
@@ -55,11 +40,3 @@ export default async function load404() {
     main.append(...doc.querySelectorAll('body > *'));
   }
 }
-
-(async function init() {
-  load404();
-  loadHeader();
-  loadFooter();
-  const { default: loadFavIcon } = await import('../utils/favicon.js');
-  loadFavIcon(createTag, getConfig(), getMetadata);
-}());
