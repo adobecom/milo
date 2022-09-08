bd904edfff05b40b2d71ef75ef0fdb59569a5dd0
Elan Bartholomew <elan@thinkingbox.com>
2022-09-08 16:27:39 -0600

    simplify styles and js, align with section metadata

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -19,19 +19,16 @@ import { decorateBlockBg, decorateBlockText } from '../../utils/decorate.js';
 export default function init(el) {
   const children = el.querySelectorAll(':scope > div');
   const [background, ...cols] = children;
-  // basic background handling
   decorateBlockBg(el, background);
 
-  // create foreground
   const container = document.createElement('div');
   container.classList.add('foreground', 'container', 'grid');
   el.appendChild(container);
   el.classList.add('block');
 
-  // process columns
   cols.forEach((col, idx) => {
     let headingClass = 'medium';
-    if (idx === 0 && (el.classList.contains('full-width') || el.classList.contains('has-intro'))) {
+    if (idx === 0 && (el.classList.contains('full-width'))) {
       col.children[0].classList.add('full-width');
       headingClass = el.classList.contains('large') ? 'large' : 'medium';
     }
