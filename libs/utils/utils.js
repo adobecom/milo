a82efd9341f785f91c96c7ab8db083067f838d52
Chris Millar <cmillar@adobe.com>
2022-09-08 13:57:18 -0600

    Update utils.js

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -2,6 +2,7 @@ const PROJECT_NAME = 'milo--adobecom';
 const PRODUCTION_DOMAINS = ['milo.adobe.com'];
 const MILO_TEMPLATES = [];
 const MILO_BLOCKS = [
+  'accordion',
   'adobetv',
   'aside',
   'caas',
