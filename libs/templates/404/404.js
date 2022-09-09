8679e47b14f1a062b6b6acf7a3b491b6a84388c3
Sean Choi <seanchoi@adobe.com>
2022-09-09 12:52:56 -0600

    MWPW-116364 - 404 (#172)

---
+++ libs/templates/404/404.js
@@ -0,0 +1,46 @@
+import { getConfig, createTag } from '../../utils/utils.js';
+
+async function load404() {
+  const { locale } = getConfig();
+  const main = document.body.querySelector('main');
+  main.innerHTML = '';
+  let resp = await fetch(`${locale.contentRoot}/404.plain.html`);
+  if (!resp || !resp.ok) {
+    resp = await fetch('/404.plain.html');
+  }
+  const columns = createTag('div', { class: 'columns-404' });
+  const html = await resp.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+
+  // picture for background img
+  const picture = doc.querySelector('picture');
+  picture.classList.add('bg-img');
+
+  // sections
+  const sections = doc.querySelectorAll('body > div');
+  const header = [...sections].shift();
+  header.classList.add('header-404', 'section');
+  sections.forEach((section) => {
+    if (section.querySelector('ul')) {
+      if (section.querySelectorAll('li').length > 5) {
+        section.classList.add('split-items');
+      }
+      section.classList.add('column');
+      columns.append(section);
+    } else {
+      section.classList.add('section');
+    }
+  });
+
+  // appending background img, header, columns
+  main.append(picture, header, columns);
+  header.querySelector('p').remove();
+
+  // appending other sections to main
+  main.append(...doc.querySelectorAll('body > *'));
+}
+
+(async function init() {
+  load404();
+}());
