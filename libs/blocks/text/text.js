c8c03ad6b32d24c8d49d6b23385d740b2d88eadb
Elan Bartholomew <79870969+elan-tbx@users.noreply.github.com>
2022-09-14 10:53:01 -0600

    [MWPW-105625] - Consonant Text block (#174)
    * migrate text block files
    
    * remove analytics call
    
    * add grid class to container
    
    * simplify styles and js, align with section metadata
    
    * add test files
    
    * remove unnecessary line-height rules, account for 3+ CTAs
    
    * tweak to how spacing is handled with multiple buttons, don’t wrap text links
    
    * revert triple button styles
    
    Co-authored-by: Chris Millar <cmillar@adobe.com>

---
+++ libs/blocks/text/text.js
@@ -0,0 +1,41 @@
+/*
+* Copyright 2022 Adobe. All rights reserved.
+* This file is licensed to you under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License. You may obtain a copy
+* of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under
+* the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+* OF ANY KIND, either express or implied. See the License for the specific language
+* governing permissions and limitations under the License.
+*/
+
+import { decorateBlockBg, decorateBlockText } from '../../utils/decorate.js';
+
+/*
+ * Text Block - v1.0
+ */
+
+export default function init(el) {
+  const children = el.querySelectorAll(':scope > div');
+  const [background, ...cols] = children;
+  decorateBlockBg(el, background);
+
+  const container = document.createElement('div');
+  container.classList.add('foreground', 'container', 'grid');
+  el.appendChild(container);
+  el.classList.add('block');
+
+  cols.forEach((col, idx) => {
+    let headingClass = 'medium';
+    if (idx === 0 && (el.classList.contains('full-width'))) {
+      col.children[0].classList.add('full-width');
+      headingClass = el.classList.contains('large') ? 'large' : 'medium';
+    }
+    col.children[0].classList.add('text');
+    decorateBlockText(el, headingClass);
+    col.querySelector('a + a')?.closest('p').classList.add('action-area');
+    container.insertAdjacentElement('beforeend', col.children[0]);
+    col.remove();
+  });
+}
