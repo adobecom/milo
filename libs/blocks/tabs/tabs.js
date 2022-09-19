21be9e86688fb34082b246e1f776088292e463c4
thi64146 <thi64146@adobe.com>
2022-09-19 17:01:30 -0600

    optimization feedback from pr review

--- libs/blocks/tabs/tabs.js
+++ libs/blocks/tabs/tabs.js
@@ -8,21 +8,16 @@ function initTabs(e) {
   const tabs = e.querySelectorAll('[role="tab"]');
   const tabLists = e.querySelectorAll('[role="tablist"]');
   tabLists.forEach( tabList => {
-    // Enable arrow navigation between tabs in the tab list
     let tabFocus = 0;
     tabList.addEventListener("keydown", (e) => {
-      // Move right
-      if (e.keyCode === 39 || e.keyCode === 37) {
+      if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
         tabs[tabFocus].setAttribute("tabindex", -1);
-        if (e.keyCode === 39) {
+        if (e.key === 'ArrowRight') {
           tabFocus++;
-          // If we're at the end, go to the start
           /* c8 ignore next */
           if (tabFocus >= tabs.length) tabFocus = 0;
-          // Move left
-        } else if (e.keyCode === 37) {
+        } else if (e.key === 'ArrowLeft') {
           tabFocus--;
-          // If we're at the start, move to the end
           /* c8 ignore next */
           if (tabFocus < 0) tabFocus = tabs.length - 1;
         }
@@ -56,24 +51,14 @@ function changeTabs(e) {
   const target = e.target;
   const parent = target.parentNode;
   const grandparent = parent.parentNode.nextElementSibling;
-
-  // Remove all current selected tabs
   parent
     .querySelectorAll('[aria-selected="true"]')
     .forEach(t => t.setAttribute("aria-selected", false));
-
-  // Set this tab as selected
   target.setAttribute("aria-selected", true);
-
-  // Scroll tab into view if off Viewport
   scrollTabIntoView(target);
-
-  // Hide all tab panels
   grandparent
     .querySelectorAll('[role="tabpanel"]')
     .forEach(p => p.setAttribute("hidden", true));
-
-  // Show the selected panel
   grandparent.parentNode
     .querySelector(`#${target.getAttribute("aria-controls")}`)
     .removeAttribute("hidden");
@@ -86,11 +71,9 @@ const init = (element) => {
   if(!rows.length) return;
   const tabList = createTag('div', {role: 'tablist'});
   const tabListContainer = createTag('div', {class: 'tabList-container container'});
-  const tabContentContainerContainer = createTag('div', {class: 'container'});
-  const tabContentContainer = createTag('div', {class: 'tabContent-container'}, tabContentContainerContainer);
+  const containerWrapper = createTag('div', {class: 'container'});
+  const tabContentContainer = createTag('div', {class: 'tabContent-container'}, containerWrapper);
   let btnClass = [...element.classList].includes('quiet') ? 'heading-XS' : 'heading-XS'; // tabList size
-
-  // title row
   let singleColRows = 0;
   rows.forEach((row) => { if (row.childElementCount === 1) singleColRows += 1; });
   if (singleColRows) {
@@ -101,7 +84,6 @@ const init = (element) => {
   const tabRows = element.querySelectorAll(':scope > div:not([class])');
   tabRows.forEach((row, i) => {
     const rowTitle = row.querySelector(':scope > div:nth-child(1)');
-    const rowContent = row.querySelector(':scope > div:nth-child(2)');
     const tabBtnAttributes = {
       role: 'tab',
       class: btnClass,
@@ -112,18 +94,16 @@ const init = (element) => {
     tabBtn.setAttribute('aria-selected', (i === 0) ? 'true' : 'false');
     tabBtn.setAttribute('aria-controls', `panel-${initCount}-${i}`);
     tabBtn.innerText = rowTitle.textContent;
-    rowTitle.remove();
     tabListContainer.append(tabBtn);
-
-    const rowContentParent = rowContent.parentNode;
+    const rowContent = row.querySelector(':scope > div:nth-child(2)');
+    const rowContentParent = rowContent.innerText !== '' ? rowContent.parentNode : createTag('div', {}, `<div data-failed="true">Data failed</label>`);
     rowContentParent.id = `panel-${initCount}-${i}`;
     rowContentParent.setAttribute('role', 'tabpanel');
     rowContentParent.setAttribute('tabindex', '0');
     rowContentParent.setAttribute('aria-labelledby', `tab-${initCount}-${i}`);
-    if(i > 0) {
-      rowContentParent.setAttribute('hidden', '');
-    }
-    tabContentContainerContainer.append(rowContentParent);
+    if(i > 0) rowContentParent.setAttribute('hidden', '');
+    containerWrapper.append(rowContentParent);
+    rowTitle.remove();
   });
   tabList.append(tabListContainer);
   element.append(tabList);
