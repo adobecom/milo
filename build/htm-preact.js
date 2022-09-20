29d5deaa1229c9bfb55ef452d512128d5344439e
Chris Peyer <chrischrischris@users.noreply.github.com>
2022-09-20 17:42:03 -0400

    Update preact (#189)

---
+++ build/htm-preact.js
@@ -0,0 +1,10 @@
+// Used to create custom htm-preact dependency file
+
+import { h, Component, createContext, createRef, render } from 'preact';
+import { signal } from '@preact/signals';
+import { useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle, useMemo, useCallback, useContext, useDebugValue, useErrorBoundary, useId } from 'preact/hooks';
+import htm from 'htm';
+
+const html = htm.bind(h);
+
+export { h, html, signal, render, Component, createContext, createRef, useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle, useMemo, useCallback, useContext, useDebugValue, useErrorBoundary, useId };
