dc70208116fe2739b6906a59b828c3ff4944ec1b
Nino Aguilar <12245781+ninoaguilar@users.noreply.github.com>
2022-11-08 15:46:42 -0700

    Moved shared methods to helper file

--- libs/blocks/article-feed/helpers.js
+++ libs/blocks/article-feed/helpers.js
@@ -9,8 +9,23 @@ import * as taxonomyLibrary from '../../scripts/taxonomy.js';
 
 let taxonomyModule;
 
-export function getTaxonomyModule() {
-  return taxonomyModule;
+/**
+ * number case, coming from Excel
+ * 1/1/1900 is day 1 in Excel, so:
+ *  - add this
+ *  - add days between 1/1/1900 and 1/1/1970
+ *  - add one more day for Excel's leap year bug
+ *
+ * @param {number} date The date to format
+ * @returns {string} The formatted date
+ */
+function calculateExcelDate(date) {
+  return new Date(Math.round((date - (1 + 25567 + 1)) * 86400 * 1000));
+}
+
+// Safari won't accept '-' as a date separator
+function replaceSeparator(date) {
+  date.replace(/-/g, '/');
 }
 
 /**
@@ -55,6 +70,59 @@ function computeTaxonomyFromTopics(taxonomy, topics, path) {
     return { category, topics, visibleTopics, allTopics };
   }
   return { category, topics };
+}
+
+/**
+ * Loads (i.e. sets on object) the taxonomy properties for the given article.
+ * @param {Object} article The article to enhance with the taxonomy data
+ */
+function loadArticleTaxonomy(article) {
+  const clonedArticle = { ...article };
+
+  if (clonedArticle.allTopics) {
+    return clonedArticle;
+  }
+
+  // for now, we can only compute the category
+  const { tags, path } = clonedArticle;
+
+  if (tags) {
+    const topics = tags
+      .replace(/[["\]]/gm, '')
+      .split(',')
+      .map((t) => t.trim())
+      .filter((t) => t && t !== '');
+
+    const articleTax = computeTaxonomyFromTopics(topics, path);
+
+    clonedArticle.category = articleTax.category;
+
+    // topics = tags as an array
+    clonedArticle.topics = topics;
+
+    // visibleTopics = visible topics including parents
+    clonedArticle.visibleTopics = articleTax.allVisibleTopics;
+
+    // allTopics = all topics including parents
+    clonedArticle.allTopics = articleTax.allTopics;
+  } else {
+    clonedArticle.category = 'News';
+    clonedArticle.topics = [];
+    clonedArticle.visibleTopics = [];
+    clonedArticle.allTopics = [];
+  }
+
+  return clonedArticle;
+}
+
+/**
+ * Returns the language dependent root path
+ * @returns {string} The computed root path
+ */
+export const getPrefix = () => (getConfig().locale?.prefix ? `/${getConfig().locale?.prefix}` : '');
+
+export function getTaxonomyModule() {
+  return taxonomyModule;
 }
 
 export async function loadTaxonomy() {
@@ -112,13 +180,29 @@ export async function loadTaxonomy() {
   });
 }
 
-export const getLocaleIetf = () => getConfig().locale?.ietf;
-
 /**
- * Returns the language dependent root path
- * @returns {string} The computed root path
+ * Format date to locale.
+ *
+ * @param {number} date The date to format
+ * @returns {string} The formatted card date
  */
-export const getPrefix = () => (getConfig().locale?.prefix ? `/${getConfig().locale?.prefix}` : '');
+export function formatCardLocaleDate(date) {
+  const jsDate = !date.includes('-') ? calculateExcelDate(date) : replaceSeparator(date);
+  const dateLocale = getConfig().locale?.ietf;
+
+  let dateString = new Date(jsDate).toLocaleDateString(dateLocale, {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    timeZone: 'UTC',
+  });
+
+  if (dateLocale === 'en-US') {
+    // stylize US date format with dashes instead of slashes
+    dateString = dateString.replace(/\//g, '-');
+  }
+  return dateString;
+}
 
 /**
  * fetches the string variables.
@@ -135,6 +219,121 @@ export async function fetchPlaceholders() {
       }));
   }
   return window.placeholders;
+}
+
+/**
+ * Returns a picture element with webp and fallbacks
+ * @param {string} src The image URL
+ * @param {boolean} eager load image eager
+ * @param {Array} breakpoints breakpoints and corresponding params (eg. width)
+ */
+
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 400px)', width: '2000' }, { width: '750' }]) {
+  const url = new URL(src, window.location.href);
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+    }
+  });
+
+  return picture;
+}
+
+/**
+ * Get the taxonomy of the given article. Object can be composed of:
+ * - category: main topic
+ * - topics: tags as an array
+ * - visibleTopics: list of visible topics, including parents
+ * - allTopics: list of all topics, including parents
+ * Note: to get the full object, taxonomy must be loaded
+ * @param {Object} article The article
+ * @returns The taxonomy object
+ */
+export function getArticleTaxonomy(article) {
+  const {
+    category,
+    topics,
+    visibleTopics,
+    allTopics,
+  } = article.allTopics ? article : loadArticleTaxonomy(article);
+
+  return { category, topics, visibleTopics, allTopics };
+}
+
+/**
+ * Returns a link tag with the proper href for the given topic.
+ * If the taxonomy is not yet available, the tag is decorated with the topicLink
+ * data attribute so that the link can be fixed later.
+ * @param {string} topic The topic name
+ * @returns {string} A link tag as a string
+ */
+function getLinkForTopic(topic, path) {
+  const titleSubs = { 'Transformation digitale': 'Transformation numérique' };
+
+  const catLink = [getTaxonomyModule().get(topic)].map((tax) => tax?.link ?? '#');
+
+  if (catLink === '#') {
+    // eslint-disable-next-line no-console
+    console.warn(`Trying to get a link for an unknown topic: ${topic} ${path ? `on page ${path}` : '(current page)'}`);
+  }
+
+  return `<a href="${catLink ?? ''}" ${catLink ?? `data-topic-link="${topic}"`}>${titleSubs[topic] ?? topic}</a>`;
+}
+
+/**
+ * Build article card
+ * @param {Element} article The article data to be placed in card.
+ * @returns card Generated card
+ */
+export function buildArticleCard(article, type = 'article', eager = false) {
+  const { title, description, image, imageAlt, date } = article;
+
+  const path = article.path.split('.')[0];
+
+  const picture = createOptimizedPicture(image, imageAlt || title, eager, [{ width: '750' }]);
+  const pictureTag = picture.outerHTML;
+  const card = document.createElement('a');
+  card.className = `${type}-card`;
+  card.href = path;
+
+  const articleTax = getArticleTaxonomy(article);
+  const categoryTag = getLinkForTopic(articleTax.category, path);
+
+  card.innerHTML = `<div class="${type}-card-image">
+      ${pictureTag}
+    </div>
+    <div class="${type}-card-body">
+      <p class="${type}-card-category">
+        ${categoryTag}
+      </p>
+      <h3>${title}</h3>
+      <p class="${type}-card-description">${description}</p>
+      <p class="${type}-card-date">${formatCardLocaleDate(date)}
+    </div>`;
+  return card;
 }
 
 export function stamp(message) {
