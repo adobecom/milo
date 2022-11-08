8207d8313f9c2fcf5d451697232c9779b3b459fc
thi64146 <thi64146@adobe.com>
2022-11-08 09:13:44 -0700

    Feedback on design review

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -3,14 +3,22 @@ import { decorateBlockBg, decorateIconArea, decorateButtons, getBlockSize, setDa
 import { decorateLinkAnalytics } from '../../martech/attributes.js';
 import { createTag } from '../../utils/utils.js';
 
+function getSizeClassName(s, t = 'body') {
+  if (s.startsWith('xs') || s.startsWith('xl')) {
+    return `${t}_${s[0]+s[1].toUpperCase()}`;
+  } else {
+    return `${t}_${s[0].toUpperCase()}`;
+  }
+}
+
 export function decorateText(el, size = 'medium') {
-  decorateButtons(el);
-  const isInset = el.classList.contains('inset');
-  if (!isInset) decorateIconArea(el);
+  decorateButtons(el, size);
+  const inset = el.classList.contains('inset');
+  if (!inset) decorateIconArea(el, size);
   const headings = el.querySelectorAll('h1, h2, h3, h4, h5, h6');
   const decorate = (headingEl, headingSize, detailSize) => {
     headingEl.classList.add(`heading-${headingSize}`);
-    if (!isInset) headingEl.previousElementSibling?.classList.add(`detail-${detailSize}`);
+    if (!inset) headingEl.previousElementSibling?.classList.add(`detail-${detailSize}`);
   };
   headings.forEach((heading) => {
     if (size === 'small') {
@@ -23,8 +31,17 @@ export function decorateText(el, size = 'medium') {
       decorate(heading, 'M', 'M');
     }
   });
-  const emptyPs = el.querySelectorAll(":scope p:not([class])");
-  emptyPs.forEach((p) => { p.classList.add(`body-M`) });
+  const emptyPs = el.querySelectorAll(':scope p:not([class])');
+  const lists = el.querySelectorAll('ul, ol');
+  if (inset) {
+    emptyPs.forEach((p) => { p.classList.add(`body-L`) });
+    if (!lists) return;
+    lists.forEach((list) => { list.classList.add('body-M') });
+  } else {
+    emptyPs.forEach((p) => { p.classList.add(`body-M`) });
+  }
+  const actionArea = el.querySelectorAll('.action-area');
+  if (size && size === 'small') actionArea.forEach((area) => { area.classList.add('body-S'); });
   decorateLinkAnalytics(el, headings);
 }
 
