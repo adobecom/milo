cbbe204820125c4805124b6aababe90400377c64
thi64146 <thi64146@adobe.com>
2022-11-09 10:49:06 -0700

    update to text block to support grid

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -49,7 +49,12 @@ export default function init(el) {
   setDataConBlockAttribute(el);
   el.classList.add('text-block');
   const rows = el.querySelectorAll(':scope > div');
-  const classList = ['foreground']; if (el.classList.contains('contained')) classList.push('container');
+  let classList = 'foreground';
+  if (el.classList.contains('contained')) {
+    classList = classList + ' container';
+    // const spanClass = el.classList.contains('span-8', 'span-10');
+    // if (spanClass) classList = classList + spanClass;
+  };
   const container = createTag('div', { class: classList });
   if (rows.length > 1) {
     decorateBlockBg(el, rows[0]);
