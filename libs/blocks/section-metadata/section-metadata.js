e298058f0c88516e676550cca5fd326b81557f82
Sartxi <sean.archibeque@gmail.com>
2022-11-08 18:27:20 -0700

    auto offset with custom items

--- libs/blocks/section-metadata/section-metadata.js
+++ libs/blocks/section-metadata/section-metadata.js
@@ -23,19 +23,29 @@ function handleStyle(div, section, keyDivs) {
   }
 }
 
-function colsOffset(cols) {
-  return cols.reduce((a, b) => parseInt(a) + parseInt(b), 0) == 12 ? null : cols.length;
+function colsAutoOffset(cols) {
+  let offset = cols.length;
+  const total = cols.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  if (total > 12) {
+    let i = 0;
+    for (const col of cols) {
+      const addCol = i + parseInt(col);
+      if (addCol < 12) i = addCol;
+      else offset = cols.indexOf(col) + 1;
+    }
+  }
+  return total == 12 ? null : offset;
 }
 
 function handleColumns(value, values, section) {
-  if (!value.includes('up')) return { columns: values, offset: colsOffset(values) };
+  if (!value.includes('up')) return { columns: values, offset: colsAutoOffset(values) };
   let upConfig = { 'two': 6, 'three': 4, 'four': 3, 'five': 2, 2: 6, 3: 4, 4: 3, 5: 2 };
   const up = values.find(i => i.includes('up'));
   const colSpan = upConfig[up.replace(' up', '')];
   const sectionClass = Object.keys(upConfig).filter(key => upConfig[key] === colSpan);
   section.classList.add(`${sectionClass[1]}-up`);
   const spans = Array(parseInt(sectionClass[0])).fill(colSpan);
-  return { columns: [colSpan], offset: colsOffset(spans) };
+  return { columns: [colSpan], offset: colsAutoOffset(spans) };
 }
 
 function handleGridColumns(div, section) {
