0e751e06dad6fe3645e00538217bdf3cf113f7b5
Chris Peyer <chrischrischris@users.noreply.github.com>
2022-11-22 12:22:58 -0500

    MWPW-106279 - Add Franklin RUM to Milo (#329)
    Add SampleRUM to Milo

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -452,12 +452,18 @@ async function loadPostLCP(config) {
   loadFonts(config.locale, loadStyle);
 }
 
-export async function loadDeferred(area) {
+export async function loadDeferred(area, blocks) {
   if (getMetadata('nofollow-links') === 'on') {
     const path = getMetadata('nofollow-path') || '/seo/nofollow.json';
     const { default: nofollow } = await import('../features/nofollow.js');
     nofollow(path, area);
   }
+
+  import('./samplerum.js').then(({ sampleRUM }) => {
+    sampleRUM('lazy');
+    sampleRUM.observe(blocks);
+    sampleRUM.observe(area.querySelectorAll('picture > img'));
+  });
 }
 
 function loadPrivacy() {
@@ -493,12 +499,19 @@ export async function loadArea(area = document) {
 
   if (isDoc) {
     decorateHeader();
+
+    import('./samplerum.js').then(({ addRumListeners }) => {
+      addRumListeners();
+    });
   }
 
   const sections = decorateSections(area, isDoc);
+
+  const areaBlocks = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const section of sections) {
     const loaded = section.blocks.map((block) => loadBlock(block));
+    areaBlocks.push(...section.blocks);
 
     // Only move on to the next section when all blocks are loaded.
     // eslint-disable-next-line no-await-in-loop
@@ -529,7 +542,7 @@ export async function loadArea(area = document) {
   }
 
   // Load everything that can be deferred until after all blocks load.
-  await loadDeferred(area);
+  await loadDeferred(area, areaBlocks);
 }
 
 // Load everything that impacts performance later.
@@ -544,6 +557,7 @@ export function loadDelayed(delay = 3000) {
       } else {
         resolve(null);
       }
+      import('./samplerum.js').then(({ sampleRUM }) => sampleRUM('cwv'));
     }, delay);
   });
 }
