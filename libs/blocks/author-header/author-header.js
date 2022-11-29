a011e71c44d21d6e7ad0ffcd79787021361cefe4
Saugat <saugat013@gmail.com>
2022-11-29 14:41:42 -0500

    add support for social links

--- libs/blocks/author-header/author-header.js
+++ libs/blocks/author-header/author-header.js
@@ -1,8 +1,12 @@
 export default function init(blockEl) {
+  const blockWrapper = blockEl.parentElement;
+  blockWrapper.classList.add('author-header-wrapper');
   const title = blockEl.querySelector('div:nth-child(1)');
   if (title) { title.classList.add('author-header-title'); }
   const img = blockEl.querySelector('div:nth-child(2)');
   if (img) { img.classList.add('author-header-img'); }
   const bio = blockEl.querySelector('div:nth-child(3)');
   if (bio) { bio.classList.add('author-header-bio'); }
+  const social = blockEl.querySelector('div:nth-child(4)');
+  if (social) { social.classList.add('author-header-socials'); }
 }
