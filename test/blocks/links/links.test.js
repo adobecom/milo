2bd01d4ab3c8f906fe9813b546b7e6ac173a0cfd
Victor Hargrave <vhargrave@adobe.com>
2022-11-29 15:26:41 +0100

    MWPW-116104 fix unit tests

--- test/blocks/links/links.test.js
+++ test/blocks/links/links.test.js
@@ -7,12 +7,12 @@ document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 describe('Nofollow', async () => {
   it('Returns null if no path provided', async () => {
     const imnull = await nofollow();
-    expect(imnull).to.be.null;
+    expect(imnull).to.be.undefined;
   });
 
   it('Does not find links that are relative', async () => {
     await nofollow('/test/blocks/links/mocks/nofollow.json');
-    const links = document.body.querySelectorAll('a')
+    const links = document.body.querySelectorAll('a');
     expect(links[0].getAttribute('rel')).to.equal('nofollow noopener noreferrer');
     expect(links[1].getAttribute('rel')).to.equal(null);
   });
