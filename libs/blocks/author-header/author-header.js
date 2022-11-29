544cc681bfb9a1b509f1584e46e0f9de8496a6f0
Saugat <saugat013@gmail.com>
2022-11-29 14:50:53 -0500

    add test

--- libs/blocks/author-header/author-header.js
+++ libs/blocks/author-header/author-header.js
@@ -1,6 +1,4 @@
 export default function init(blockEl) {
-  const blockWrapper = blockEl.parentElement;
-  blockWrapper.classList.add('author-header-wrapper');
   const title = blockEl.querySelector('div:nth-child(1)');
   if (title) { title.classList.add('author-header-title'); }
   const img = blockEl.querySelector('div:nth-child(2)');
@@ -9,4 +7,7 @@ export default function init(blockEl) {
   if (bio) { bio.classList.add('author-header-bio'); }
   const social = blockEl.querySelector('div:nth-child(4)');
   if (social) { social.classList.add('author-header-socials'); }
+
+  const blockWrapper = blockEl.parentElement;
+  blockWrapper.classList.add('author-header-wrapper');
 }
--- test/blocks/author-header/author-header.test.js
+++ test/blocks/author-header/author-header.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* global describe it */
-
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 
@@ -15,8 +12,10 @@ describe('renders author header block', () => {
     const title = block.querySelector('div:nth-child(1)');
     const img = block.querySelector('div:nth-child(2)');
     const bio = block.querySelector('div:nth-child(3)');
+    const social = block.querySelector('div:nth-child(4)');
     if (title) expect(document.querySelector('.author-header-title')).to.exist;
     if (img) expect(document.querySelector('.author-header-img')).to.exist;
     if (bio) expect(document.querySelector('.author-header-bio')).to.exist;
+    if (social) expect(document.querySelector('.author-header-socials')).to.exist;
   });
 });
