ddb2e4df3740a5684f993aaf40204f6ccb7bcbf3
Chris Millar <cmillar@adobe.com>
2022-11-30 16:14:02 -0700

    Update featured-article.js

--- libs/blocks/featured-article/featured-article.js
+++ libs/blocks/featured-article/featured-article.js
@@ -37,28 +37,3 @@ export default async function init(el) {
 
   a.append(featuredImg, body);
 }
-
-/* <div class="featured-article">
-  <a class="featured-article-card" href="/published/2022/11/15/voices-of-our-community">
-    <div class="featured-article-card-image">
-      <picture>
-        <source type="image/webp"
-          srcset="/published/2022/11/15/media_1cd7a8c416aaff1e70d71679b74441e46c4c545f3.png?width=750&amp;format=webply&amp;optimize=medium">
-        <img
-          src="/published/2022/11/15/media_1cd7a8c416aaff1e70d71679b74441e46c4c545f3.png?width=750&amp;format=png&amp;optimize=medium"
-          loading="lazy" alt="Still from 'New York Minute'">
-      </picture>
-    </div>
-    <div class="featured-article-card-body">
-      <p class="featured-article-card-category">
-        <a href="/topics/creativity" topics="" creativity="">Creativity</a>
-      </p>
-      <h3>"In a New York Minute” with Maliyamungu Muhande and Imani Dennison </h3>
-      <p class="featured-article-card-description">Filmmakers Maliyamungu Muhande and Imani Dennison collaborated
-        together with Muhande creating an original short film around the theme of “community”, and Dennison documenting
-        and filming Muhande’s creative process behind the scenes.</p>
-      <p class="featured-article-card-date">08-01-2022
-      </p>
-    </div>
-  </a>
-</div> */
