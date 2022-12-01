4c6e9328188558f409b96004b53bac96d266a383
Saugat <saugat013@gmail.com>
2022-12-01 17:26:53 -0500

    refactor code

--- libs/blocks/author-header/author-header.js
+++ libs/blocks/author-header/author-header.js
@@ -1,13 +1,34 @@
-export default function init(blockEl) {
-  const title = blockEl.querySelector('div:nth-child(1)');
-  if (title) { title.classList.add('author-header-title'); }
-  const img = blockEl.querySelector('div:nth-child(2)');
-  if (img) { img.classList.add('author-header-img'); }
-  const bio = blockEl.querySelector('div:nth-child(3)');
-  if (bio) { bio.classList.add('author-header-bio'); }
-  const social = blockEl.querySelector('div:nth-child(4)');
-  if (social) { social.classList.add('author-header-socials'); }
+import { createTag } from '../../utils/utils.js'
 
-  const blockWrapper = blockEl.parentElement;
+export default function init(blockElement) {
+  const block = blockElement.querySelector('div');
+  blockElement.innerHTML = '';
+
+  const titleText = block.querySelector('h1').textContent;
+  const titleEl = createTag('h1', { class: 'author-header-title' }, titleText);
+
+  let pictureElement;
+  let bioElement;
+  const paragraphs = block.querySelectorAll('p');
+  paragraphs.forEach((el) => {
+    if (el.querySelector('picture')) {
+      pictureElement = el;
+    } else {
+      bioElement = el;
+    }
+  });
+
+  const pictureWrapper = createTag('div', { class: 'author-header-img' }, pictureElement);
+  const bioWrapper = createTag('div', { class: 'author-header-bio' }, bioElement);
+
+  const social = block.querySelector('h2');
+  const socialWrapper = createTag('div', { class: 'author-header-socials' }, social);
+
+  const socialLinksList = block.querySelector('ul');
+  socialWrapper.append(socialLinksList);
+
+  blockElement.append(titleEl, pictureWrapper, bioWrapper, socialWrapper);
+
+  const blockWrapper = blockElement.parentElement;
   blockWrapper?.classList.add('author-header-wrapper');
 }
