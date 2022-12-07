b99516da7871ad69c404d9c86f067e842eeca255
Chris Peyer <chrischrischris@users.noreply.github.com>
2022-12-07 11:26:51 -0500

    Revert "MWPW-116104 set target blank on links containing #_blank (#331)"
    This reverts commit 8f6a274592ca5791b6bae0aef701c648c640559a.

--- test/blocks/nofollow/nofollow.test.js
+++ test/blocks/nofollow/nofollow.test.js
@@ -1,19 +1,17 @@
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
-import nofollow from '../../../libs/features/links.js';
+import nofollow from '../../../libs/features/nofollow.js';
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
 describe('Nofollow', async () => {
   it('Returns null if no path provided', async () => {
     const imnull = await nofollow();
-    expect(imnull).to.be.undefined;
+    expect(imnull).to.be.null;
   });
 
   it('Does not find links that are relative', async () => {
-    await nofollow('/test/blocks/links/mocks/nofollow.json');
-    const links = document.body.querySelectorAll('a');
+    const links = await nofollow('/test/blocks/nofollow/mocks/nofollow.json');
     expect(links[0].getAttribute('rel')).to.equal('nofollow noopener noreferrer');
-    expect(links[1].getAttribute('rel')).to.equal(null);
   });
 });
