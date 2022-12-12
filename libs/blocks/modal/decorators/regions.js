1ecea299e5eae31a62969b5dbf7094ec9fcf2936
Victor Hargrave <vhargrave@adobe.com>
2022-12-12 14:28:43 +0100

    MWPW-97638 refactor

--- libs/blocks/modal/decorators/regions.js
+++ libs/blocks/modal/decorators/regions.js
@@ -1,31 +1,34 @@
 import { getConfig } from '../../../utils/utils.js';
 
+function handleEvent(e, prefix, link, config) {
+  e.preventDefault();
+  document.cookie = `international=${prefix};path=/`;
+  sessionStorage.setItem('international', prefix);
+  fetch(link.href, { method: 'HEAD' }).then((resp) => {
+    if (resp.ok) {
+      window.location = link.href;
+    } else {
+      const prefixUrl = prefix ? `/${prefix}` : '';
+      window.location = `${prefixUrl}${config.contentRoot || ''}/`;
+    }
+  });
+}
+
+function decorateLink(link, config, path) {
+  const linkParts = link.getAttribute('href').split('/');
+  const prefix = linkParts[1] || 'us';
+  console.log(prefix);
+  link.href += `${config.contentRoot || ''}${path}`;
+  link.addEventListener('click', (e) => handleEvent(e, prefix, link, config));
+}
+
 export default function decorate(block) {
   const config = getConfig();
   const regionSelectorBlock = block.querySelector('.region-selector');
-  if (!regionSelectorBlock) return;
-  const links = regionSelectorBlock.querySelectorAll('a');
-  if (!links || links.length === 0) return;
+  const links = regionSelectorBlock?.querySelectorAll('a');
+  if (!links || !links.length) return;
   const { contentRoot } = config.locale;
   const path = window.location.href.replace(`${contentRoot}/`, '').replace('#langnav', '');
 
-  links.forEach((l) => {
-    const linkParts = l.getAttribute('href').split('/');
-    const prefix = linkParts[1] || 'us';
-    console.log(prefix);
-    l.href += `${config.contentRoot || ''}${path}`;
-    l.addEventListener('click', (e) => {
-      e.preventDefault();
-      document.cookie = `international=${prefix};path=/`;
-      sessionStorage.setItem('international', prefix);
-      fetch(l.href, { method: 'HEAD' }).then((resp) => {
-        if (resp.ok) {
-          window.location = l.href;
-        } else {
-          const prefixUrl = prefix ? `/${prefix}` : '';
-          window.location = `${prefixUrl}${config.contentRoot || ''}/`;
-        }
-      });
-    });
-  });
+  links.map((l) => decorateLink(l, config, path));
 }
