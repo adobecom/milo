bb4e4839ce2df2f62016ba349a94129f9069b30b
Saugat <saugat013@gmail.com>
2022-12-12 11:24:08 -0500

    add vimeo

---
+++ libs/blocks/vimeo/vimeo.js
@@ -0,0 +1,29 @@
+import { createIntersectionObserver, createTag } from '../../utils/utils.js';
+
+export default function init(a) {
+  const embedVimeo = () => {
+    if (!a.origin?.includes('vimeo')) return;
+
+    const url = new URL(a.href);
+    let src;
+    if (!url.href.includes('player.vimeo.com')) {
+      const video = url.pathname.split('/')[1];
+      src = `https://player.vimeo.com/video/${video}?app_id=122963`;
+    }
+    const iframe = createTag('iframe', {
+      src,
+      style: 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;',
+      frameborder: '0',
+      allow: 'autoplay; fullscreen; picture-in-picture',
+      allowfullscreen: 'true',
+      title: 'Content from Vimeo',
+      loading: 'lazy',
+    });
+    const wrapper = createTag('div', { class: 'embed-vimeo' }, iframe);
+
+    a.insertAdjacentElement('afterend', wrapper);
+    a.remove();
+  };
+
+  createIntersectionObserver({ el: a, callback: embedVimeo });
+}
