173e2879968ed25d6bc8b7157801ed0fed0fd112
Victor Hargrave <vhargrave@adobe.com>
2022-12-16 19:12:21 +0100

    MWPW-97638 cleanup after review

--- libs/blocks/modal/decorators/langnav.js
+++ libs/blocks/modal/decorators/langnav.js
@@ -1,9 +1,9 @@
 import { getConfig } from '../../../utils/utils.js';
 
-export async function handleEvent(prefix, link, config) {
+function handleEvent(prefix, link, config) {
   document.cookie = `international=${prefix};path=/`;
   sessionStorage.setItem('international', prefix);
-  return fetch(link.href, { method: 'HEAD' }).then((resp) => {
+  fetch(link.href, { method: 'HEAD' }).then((resp) => {
     if (!resp.ok) throw new Error('request failed');
     window.location.assign(link.href);
   }).catch(() => {
@@ -30,7 +30,7 @@ export default function decorate(block) {
   const config = getConfig();
   const regionSelectorBlock = block.querySelector('.region-selector');
   const links = regionSelectorBlock?.querySelectorAll('a');
-  if (!links || !links.length) return;
+  if (!links?.length) return;
   const { contentRoot } = config.locale;
   const path = window.location.href.replace(`${contentRoot}`, '').replace('#langnav', '');
   links.forEach((l) => decorateLink(l, config, path));
