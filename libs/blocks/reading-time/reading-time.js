32621253fbbde1df5df89ba70de534a2c9a43f10
Solène Duprat <solene.duprat@gmail.com>
2022-12-16 10:13:06 -0500

    added placeholders for localization of the '5 min read' text

--- libs/blocks/reading-time/reading-time.js
+++ libs/blocks/reading-time/reading-time.js
@@ -1,4 +1,5 @@
-import { createTag } from '../../utils/utils.js';
+import { createTag, getConfig } from '../../utils/utils.js';
+import { replaceKey } from '../../features/placeholders.js';
 
 function getTextLength(node) {
   let textLength = 0;
@@ -15,14 +16,16 @@ function getTextLength(node) {
   return textLength;
 }
 
-
-export default function init(el) {
+export default async function init(el) {
   el.innerHTML = '';
   el.classList.add('content');
   const span = createTag('span');
   const readingSpeed = 200; // Assume that the average reader reads 200 words per minute
   const textLength = getTextLength(document.body);
   const readingTime = Math.ceil(textLength / readingSpeed);
-  span.innerHTML = `${readingTime} min read`;
+  const config = getConfig();
+  const format = await replaceKey('5-min-read', config);
+  const readingTimeText = format.replace('5', readingTime);
+  span.innerHTML = readingTimeText;
   el.append(span);
 }
