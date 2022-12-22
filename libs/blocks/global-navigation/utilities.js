bb40db71ee2d20025a5f8ce516c4d303472fde87
Okan Sahin <osahin@adobe.com>
2022-12-22 13:45:23 +0100

    Rename gnav to global-navigation and add toFragment utility

---
+++ libs/blocks/global-navigation/utilities.js
@@ -0,0 +1,29 @@
+export function toFragment(htmlStrings, ...values) {
+  const templateStr = htmlStrings.reduce((acc, htmlString, index) => {
+    if (values[index] instanceof HTMLElement) {
+      return acc + htmlString + `<elem ref="${index}"></elem>`;
+    } else {
+      return acc + htmlString + (values[index] || '');
+    }
+  }, '');
+
+  const fragment = document.createRange().createContextualFragment(templateStr).children[0];
+
+  Array.prototype.map.call(fragment.querySelectorAll('elem'), (replaceable) => {
+    const ref = replaceable.getAttribute('ref');
+    replaceable.replaceWith(values[ref]);
+  });
+
+  return fragment;
+};
+
+export function debounceCallback(callback, time = 200) {
+  if (typeof callback !== 'function') return undefined;
+
+  let timeout = null;
+
+  return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => callback(...args), time);
+  };
+};
