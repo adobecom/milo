4334c3ce017a213310a0513a35604cdd7ea9c7c6
Saugat <saugat013@gmail.com>
2023-01-12 17:09:05 -0500

    pr feedback

--- libs/blocks/sticky-header/sticky-header.js
+++ libs/blocks/sticky-header/sticky-header.js
@@ -1,24 +1,23 @@
 import { createTag, getConfig } from '../../utils/utils.js';
 
-export default function decorate($block) {
-  const $header = document.querySelector('header');
-  const $div = $block.firstElementChild;
-  const $a = $block.querySelector('a');
-  const $close = createTag('a', { class: 'sticky-header-close' });
+export default function decorate(block) {
+  const header = document.querySelector('header');
+  const div = block.firstElementChild;
+  const a = block.querySelector('div > :last-child > a');
+  if (a) a.classList.add('button');
+
+  const closeButton = createTag('a', { class: 'sticky-header-close' });
 
   const { miloLibs, codeRoot } = getConfig();
   const base = miloLibs || codeRoot;
-  const $closeIcon = createTag('img', { class: 'sticky-header-close-icon', src: `${base}/blocks/sticky-header/close.svg` });
+  const closeIcon = createTag('img', { class: 'sticky-header-close-icon', src: `${base}/blocks/sticky-header/close.svg` });
 
-  $header.parentNode.insertBefore($block, $header.nextElementSibling);
+  header.parentNode.insertBefore(block, header.nextElementSibling);
 
-  $a.classList.add('button');
-  $a.target = '_blank';
-
-  $close.addEventListener('click', () => {
-    $block.remove();
+  closeButton.addEventListener('click', () => {
+    block.remove();
   });
 
-  $div.append($close);
-  $close.append($closeIcon);
+  div.append(closeButton);
+  closeButton.append(closeIcon);
 }
