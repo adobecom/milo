15a74551ccb8294a9629f0765d4931f0c7f8970d
Saugat <saugat013@gmail.com>
2023-01-27 16:26:00 -0500

    -pr feedback: CLS fix

--- libs/blocks/promo/promo.js
+++ libs/blocks/promo/promo.js
@@ -5,11 +5,11 @@ export default function decorate(block) {
   const a = block.querySelector('div > :last-child > a');
   if (a) a.classList.add('button');
 
-  const closeButton = createTag('a', { class: 'promo-close' });
+  const closeButton = createTag('span', { class: 'promo-close' });
 
   const { miloLibs, codeRoot } = getConfig();
   const base = miloLibs || codeRoot;
-  const closeIcon = createTag('img', { class: 'promo-close-icon', src: `${base}/blocks/promo/close.svg` });
+  const closeIcon = createTag('img', { class: 'promo-close-icon', src: `${base}/blocks/promo/close.svg`, alt: 'promo close icon' });
 
   closeButton.addEventListener('click', () => {
     const section = block.closest('.section');
