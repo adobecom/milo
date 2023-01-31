ebeb74721f1b525da18820c2c04f6cccd070ef02
Megan Thomas <methomas@adobe.com>
2023-01-31 13:42:00 -0800

    MWPW-124262 PDF Viewer Analytics

--- libs/blocks/pdf-viewer/pdf-viewer.js
+++ libs/blocks/pdf-viewer/pdf-viewer.js
@@ -55,20 +55,20 @@ const init = async (a) => {
         metaData: { fileName },
       },
     );
+
     adobeDCView.registerCallback(
-      AdobeDC.Enum.CallbackType.EVENT_LISTENER,
-      (event) => {
-        console.log(event);
-      },
+      AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+      () => undefined,
       {
         listenOn: [
+          AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_OPEN,
           AdobeDC.View.Enum.PDFAnalyticsEvents.PAGE_VIEW,
-          AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_DOWNLOAD,
-          AdobeDC.View.Enum.PDFAnalyticsEvents.ZOOM_LEVEL,
-          AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_SEARCH,
           AdobeDC.View.Enum.PDFAnalyticsEvents.BOOKMARK_ITEM_CLICK,
-          AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_COPY,
+          AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_DOWNLOAD,
           AdobeDC.View.Enum.PDFAnalyticsEvents.DOCUMENT_PRINT,
+          AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_SEARCH,
+          AdobeDC.View.Enum.PDFAnalyticsEvents.TEXT_COPY,
+          AdobeDC.View.Enum.PDFAnalyticsEvents.ZOOM_LEVEL,
         ],
         enablePDFAnalytics: true,
       },
