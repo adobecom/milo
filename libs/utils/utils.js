a94453c4a317f69057f54f03527efa5c4d92fb67
Brad Johnson <fullcolorcoder@gmail.com>
2023-01-31 15:05:12 -0600

    MWPW-124483 included footer consideration

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -132,21 +132,17 @@ export const [setConfig, getConfig] = (() => {
       if (Array.isArray(config.contentRoot)) {
         const pathSplit = pathname.split('/'); 
         const contentRoots = config.contentRoot;   
-        // page is in contentRoot array, is in en-US.
         if (contentRoots.includes(pathSplit[1])) {
           config.contentRoot = `/${pathSplit[1]}`;
         } else if (contentRoots.includes(pathSplit[2])) {
           if (config.locale.prefix.length > 0) {
             config.contentRoot = `/${pathSplit[2]}`;
           } else {
-            // second split is present, but en-US prefix ie: /au/sub-cat.
             config.contentRoot = `/${pathSplit[1]}/${pathSplit[2]}`;
           }
         } else if (!config.locale.prefix.length > 0 && !contentRoots.includes(pathSplit[1]) && pathSplit[2]) {
-          // page is not in contentRoot array, has no prefix (us-en), and isn't root index. ie: root page of /au/ site.
           config.contentRoot = `/${pathSplit[1]}`;
         } else {
-          // Fallback - ie: "/".
           config.contentRoot = '';
         }
       }
