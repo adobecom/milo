0087023be59528e4321dcd2cba348e2175e41992
Rares Munteanu <overmyheadandbody@gmail.com>
2023-02-03 10:28:52 +0100

    [MWPW-123840] Gnav centralised placeholders & main style refactor (#438)
    [MWPW-123840] Gnav centralised placeholders
    
    Co-authored-by: Rares Munteanu <ramuntea@adobe.com>

--- test/features/placeholders/placeholders.test.js
+++ test/features/placeholders/placeholders.test.js
@@ -1,6 +1,6 @@
 import { expect } from '@esm-bundle/chai';
 import { setConfig, getConfig } from '../../../libs/utils/utils.js';
-import { replaceText, replaceKey } from '../../../libs/features/placeholders.js';
+import { replaceText, replaceKey, replaceKeyArray } from '../../../libs/features/placeholders.js';
 
 const locales = { '': { ietf: 'en-US', tk: 'hah7vzn.css' } };
 const conf = { locales };
@@ -16,14 +16,19 @@ describe('Placeholders', () => {
   it('Replaces text', async () => {
     config.locale.contentRoot = '/test/features/placeholders';
     const regex = /{{(.*?)}}/g;
-    let text = 'Hello world {{recommended-for-you}}';
-    text = await replaceText(config, regex, text);
-    expect(text).to.equal('Hello world Recommended for you');
+    let text = 'Hello world {{recommended-for-you}} and {{no-results}}';
+    text = await replaceText(text, config, regex);
+    expect(text).to.equal('Hello world Recommended for you and No results found');
   });
 
   it('Replaces key', async () => {
     const text = await replaceKey('recommended-for-you', config);
     expect(text).to.equal('Recommended for you');
+  });
+
+  it('Replaces a key array', async () => {
+    const labelArray = await replaceKeyArray(['recommended-for-you', 'no-results'], config);
+    expect(labelArray).to.eql(['Recommended for you', 'No results found']);
   });
 
   it('Gracefully falls back', async () => {
