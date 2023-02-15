5dfedccda98712f8dc60639c24d6ac7a73735070
Saugat <saugat013@gmail.com>
2023-02-15 15:16:04 -0500

    remove preload

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -415,7 +415,7 @@ export function decorateVideo(a) {
 
   const isAutoplay = !!(hash?.includes('autoplay'));
 
-  const attrs = isAutoplay ? 'playsinline autoplay loop muted' : 'playsinline controls preload="metadata"';
+  const attrs = isAutoplay ? 'playsinline autoplay loop muted' : 'playsinline controls';
   const video = `<video ${attrs}>
         <source src=".${pathname}" type="video/mp4" />
       </video>`;
