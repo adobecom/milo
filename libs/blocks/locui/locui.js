e640ff2263ed31c429c8d4c4cef22aec1ed2b599
Chris Millar <cmillar@adobe.com>
2023-02-17 10:23:26 -0700

    Add mocks. Minor UX polish

--- libs/blocks/locui/locui.js
+++ libs/blocks/locui/locui.js
@@ -3,15 +3,18 @@ import { getProjectHeading, getProjectDetails } from './utils/utils.js';
 
 const LOC_NAME = 'Milo Localization';
 
-const heading = signal({ name: 'Getting Information' });
+const status = signal('Getting project name');
+const heading = signal({ name: '' });
 const subProjects = signal([]);
 const urls = signal([]);
 
 async function setDetails() {
   heading.value = await getProjectHeading();
+  status.value = 'Getting sub-projects';
   const { languages, paths } = await getProjectDetails();
   subProjects.value = languages;
   urls.value = paths;
+  status.value = null;
 }
 
 function UrlItem({ item }) {
@@ -19,12 +22,15 @@ function UrlItem({ item }) {
 }
 
 function SubProject({ item }) {
+  console.log(item);
   return html`
     <li class=locui-subproject>
-      <p class=locui-subproject-label>Language</p>
+      <p class=locui-project-label>Language</p>
       <h3 class=locui-subproject-name>${item.Language}</h3>
-      <p class=locui-subproject-label>Action</p>
+      <p class=locui-project-label>Action</p>
       <h3 class=locui-subproject-name>${item.Action}</h3>
+      <p class=locui-project-label>Items</p>
+      <h3 class=locui-subproject-name>${item.size}</h3>
     </li>
   `;
 }
@@ -39,17 +45,27 @@ function Localization() {
 
   return html`
     <h1>${LOC_NAME}</h1>
-    <p class=locui-subproject-label>Project</p>
-    <div class=locui-project-details>
-      <span class=locui-project-details-name>
-        ${heading.value.name}
-      </span>
-      ${heading.value.editUrl
-        && html`<a class=locui-project-details-edit href="${heading.value.editUrl}" target="_blank">Edit</a>`}
+    <div class=locui-project-heading>
+      <div class=locui-project-heading-column>
+        <h2 class=locui-project-label>Project</h2>
+        <div class=locui-project-details-name>
+          <span>${heading.value.name}</span>
+          ${heading.value.editUrl
+            && html`<a class=locui-project-details-edit href="${heading.value.editUrl}" target="_blank">Edit</a>`}
+        </div>
+      </div>
+      ${status.value && html`
+        <div class=locui-project-heading-column>
+          <h2 class=locui-project-label>Status</h2>
+          <div class=locui-project-details>
+            <span>${status.value}</span>
+          </div>
+        </div>
+      `}
     </div>
 
     ${hasProjects && html`
-      <h2>Sub Projects</h2>
+      <h2 class=locui-project-label>Sub Projects</h2>
       <ul class=locui-subprojects>
         ${subProjects.value.map((proj) => html`<${SubProject} item=${proj} />`)}
       </ul>
--- libs/blocks/locui/utils/utils.js
+++ libs/blocks/locui/utils/utils.js
@@ -1,14 +1,17 @@
+const LOC_CONFIG = 'https://main--milo--adobecom.hlx.page/drafts/localization/configs/config.json';
 const ADMIN = 'https://admin.hlx.page';
 const LANG_ACTIONS = ['Translate', 'English Copy', 'Rollout'];
+
+const MOCK_REFERRER = 'https%3A%2F%2Fadobe.sharepoint.com%2F%3Ax%3A%2Fr%2Fsites%2Fadobecom%2F_layouts%2F15%2FDoc.aspx%3Fsourcedoc%3D%257B94460FAC-CDEE-4B31-B8E0-AA5E3F45DCC5%257D%26file%3Dwesco-demofoo.xlsx';
 
 let resourcePath;
 
 export async function getStatus() {
   const urlParams = new URLSearchParams(window.location.search);
-  const owner = urlParams.get('owner');
-  const repo = urlParams.get('repo');
-  const ref = urlParams.get('ref');
-  const editUrl = urlParams.get('referrer');
+  const owner = urlParams.get('owner') || 'adobecom';
+  const repo = urlParams.get('repo') || 'milo';
+  const ref = urlParams.get('ref') || 'main';
+  const editUrl = urlParams.get('referrer') || MOCK_REFERRER;
   const resp = await fetch(`${ADMIN}/status/${owner}/${repo}/${ref}?editUrl=${editUrl}`);
   const json = await resp.json();
   resourcePath = json.resourcePath;
@@ -27,7 +30,13 @@ export async function getProjectDetails() {
   if (!resp.ok) return null;
   const json = await resp.json();
   const paths = getPaths(json.urls);
-  const languages = json.languages.data.filter((lang) => LANG_ACTIONS.includes(lang.Action));
+  const languages = json.languages.data.reduce((rdx, lang) => {
+    if (LANG_ACTIONS.includes(lang.Action)) {
+      lang.size = paths.length;
+      rdx.push(lang);
+    }
+    return rdx;
+  }, []);
   return { languages, paths };
 }
 
