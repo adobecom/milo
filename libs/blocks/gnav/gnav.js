1be81204b6ac1df321ac8951c6f773a3c6f48afb
Megan Thomas <methomas@adobe.com>
2023-02-28 14:44:58 -0800

    MWPW-126915 Fix gnav link order

--- libs/blocks/gnav/gnav.js
+++ libs/blocks/gnav/gnav.js
@@ -67,10 +67,6 @@ class Gnav {
 
     const mainNav = this.decorateMainNav();
     if (mainNav) {
-      const cta = this.decorateCta();
-      if (cta) {
-        mainNav.append(cta);
-      }
       scrollWrapper.append(mainNav);
     }
 
@@ -167,7 +163,7 @@ class Gnav {
 
   decorateMainNav = () => {
     const mainNav = createTag('div', { class: 'gnav-mainnav' });
-    const mainLinks = this.body.querySelectorAll('h2 > a');
+    const mainLinks = this.body.querySelectorAll('h2 > a, strong a');
     if (mainLinks.length > 0) {
       this.buildMainNav(mainNav, mainLinks);
     }
@@ -176,6 +172,11 @@ class Gnav {
 
   buildMainNav = (mainNav, navLinks) => {
     navLinks.forEach((navLink, idx) => {
+      if (navLink.parentElement.tagName === 'STRONG') {
+        const cta = this.decorateCta(navLink);
+        mainNav.append(cta);
+        return;
+      }
       navLink.href = localizeLink(navLink.href);
       const navItem = createTag('div', { class: 'gnav-navitem' });
       const navBlock = navLink.closest('.large-menu');
@@ -330,8 +331,7 @@ class Gnav {
     });
   };
 
-  decorateCta = () => {
-    const cta = this.body.querySelector('strong a');
+  decorateCta = (cta) => {
     if (cta) {
       const { origin } = new URL(cta.href);
       if (origin !== window.location.origin) {
