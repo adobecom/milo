da14da40b3af69339b3d4dcfe5acf34080385ff3
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2023-03-02 14:17:22 -0500

    Use optional chaining for lana calls
    If a Milo consumer does not have lana setup (or setup wrong) then the `window.lana.log` call can fail and stop js execution.  Using optional chaining means it will fail gracefully.
    
    Note that ALL `lana.log` calls in Milo need to use this.

--- test/blocks/fragment/fragment.test.js
+++ test/blocks/fragment/fragment.test.js
@@ -18,13 +18,13 @@ document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 const { default: getFragment } = await import('../../../libs/blocks/fragment/fragment.js');
 
 describe('Fragments', () => {
-  beforeEach(() => {
-    sinon.spy(console, 'log');
-  });
+  // beforeEach(() => {
+  //   sinon.spy(lana, 'log');
+  // });
 
-  afterEach(() => {
-    console.log.restore();
-  });
+  // afterEach(() => {
+  //   lana.log.restore();
+  // });
 
   it('Loads a fragment', async () => {
     const a = document.querySelector('a');
@@ -36,20 +36,20 @@ describe('Fragments', () => {
   it('Doesnt load a fragment', async () => {
     const a = document.querySelector('a.bad');
     await getFragment(a);
-    expect(console.log.args[0][0]).to.equal('Could not get fragment');
+    expect(window.lana.log.args[0][0]).to.equal('Could not get fragment: http://localhost:2000/test/blocks/fragment/mocks/fragments/bad.plain.html');
   });
 
   it('Doesnt create a malformed fragment', async () => {
     const a = document.querySelector('a.malformed');
     await getFragment(a);
     console.log(window.lana.log.args);
-    expect(window.lana.log.args[0][0]).to.equal('Could not make fragment');
+    expect(window.lana.log.args[1][0]).to.equal('Could not make fragment: http://localhost:2000/test/blocks/fragment/mocks/fragments/malform.plain.html');
   });
 
   it('Doesnt infinitely load circular references', async () => {
     const a = document.querySelector('a.frag-a');
     await getFragment(a);
     expect(document.querySelector('h4')).to.exist;
-    expect(console.log.args[0][0]).to.equal('ERROR: Fragment Circular Reference loading http://localhost:2000/test/blocks/fragment/mocks/fragments/frag-a');
+    expect(window.lana.log.args[2][0]).to.equal('ERROR: Fragment Circular Reference loading http://localhost:2000/test/blocks/fragment/mocks/fragments/frag-a');
   });
 });
