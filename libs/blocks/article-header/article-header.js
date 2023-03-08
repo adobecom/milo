d55f3f570b04651a18f58000db7238ee007465c8
Saugat <saugat013@gmail.com>
2023-03-08 12:02:34 -0500

    add test article-header

--- libs/blocks/article-header/article-header.js
+++ libs/blocks/article-header/article-header.js
@@ -140,6 +140,8 @@ export default async function init(blockEl) {
     await loadTaxonomy();
   }
 
+  console.log('test')
+
   const childrenEls = Array.from(blockEl.children);
 
   const categoryContainer = childrenEls[0];
