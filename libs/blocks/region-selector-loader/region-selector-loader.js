53786d47f5532128c0e2f4adc09396d4dc8cbf05
Sean Choi <seanchoi@adobe.com>
2023-03-10 03:21:50 -0700

    wip

--- libs/blocks/region-selector-loader/region-selector-loader.js
+++ libs/blocks/region-selector-loader/region-selector-loader.js
@@ -1,9 +1,22 @@
 import { createTag, getConfig } from '../../utils/utils.js';
 
-const init = () => {
-  const config = getConfig();
-  const { contentRoot } = config.locale;
-  console.log(contentRoot);
+const LOC_CONFIG = 'https://main--milo--adobecom.hlx.page/drafts/localization/configs/config.json';
+const HELIX_ADMIN = 'https://admin.hlx.page';
+
+const getLivecopies = async () => {
+  const livecopies = [];
+  const res = await fetch(LOC_CONFIG);
+  const json = await res.json();
+  json.locales.data.forEach((d) => {
+    livecopies.push(...d.livecopies.split(','));
+  });
+  return livecopies;
+};
+
+const init = async () => {
+  const livecopies = getLivecopies();
+  const { origin, pathname } = window.location;
+  console.log(origin, pathname);
 }
 
 export default init;
