a4aa9fb7ead7d337e1c951d76370e51546b2172d
Sean Choi <seanchoi@adobe.com>
2023-03-10 04:12:15 -0700

    for testing.

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -108,6 +108,20 @@ function hasSchema(host) {
           },
         },
       },
+      {
+        id: 'region-selector',
+        condition: (s) => s.isEditor(),
+        button: {
+          text: 'Region Selector',
+          action: (_, s) => {
+            const { config } = s;
+            // Change this for local development
+            const domain = `https://${config.innerHost}`;
+            console.log(config);
+            console.log(s);
+          },
+        },
+      },
     ],
   });
 })();
