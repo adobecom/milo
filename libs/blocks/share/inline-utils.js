d6ec1441c7b99bcbacbf12fb370791cd0790ce98
Solène Duprat <solene.duprat@gmail.com>
2023-03-20 11:50:51 -0400

    update inline-utils.js to account for PR comments

--- libs/blocks/share/inline-utils.js
+++ libs/blocks/share/inline-utils.js
@@ -2,16 +2,26 @@ import { createTag } from '../../utils/utils.js';
 
 export function inlineBlock(block) {
   if (!block.classList.contains('inline')) return;
-
+  
   const section = block.closest('.section');
-  const inlineSiblinggs = section.querySelectorAll('.inline');
-  if (inlineSiblinggs.length > 1) {
-    let inlineContainer = section.querySelector('.inline-wrapper');
-    if (!inlineContainer) {
-      inlineContainer = createTag('div', { class: 'inline-wrapper content' });
-      block.after(inlineContainer);
-    }
-    inlineSiblinggs.forEach((el) => inlineContainer.append(el));
+  const blocks = Array.from(section.children);
+  const blockIndex = blocks.findIndex((el) => el === block);
+  
+  let inlineSiblings = [block];
+  let nextSibling = blocks[blockIndex + 1];
+  
+  while (nextSibling && nextSibling.classList.contains('inline')) {
+    inlineSiblings.push(nextSibling);
+    nextSibling = blocks[blockIndex + inlineSiblings.length];
+  }
+  
+  if (inlineSiblings.length > 1) {
+    const wrapper = createTag('div', { class: 'inline-wrapper content' });
+    section.insertBefore(wrapper, inlineSiblings[0]);
+    
+    inlineSiblings.forEach((el) => {
+      wrapper.appendChild(el);
+    });
   } else {
     block.classList.add('content');
   }
