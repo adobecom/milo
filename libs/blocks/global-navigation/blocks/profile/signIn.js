6e8513e4d6d25c1920eac4d9cd7f260cb2b102ae
Rares Munteanu <overmyheadandbody@gmail.com>
2023-03-21 17:00:42 +0100

    [MWPW-128550] Profile block fixes (#581)
    * [MWPW-128550] Profile block fixes
    
    * [MWPW-128550] Profile block fixes PR feedback implementation
    
    ---------
    
    Co-authored-by: Rares Munteanu <ramuntea@adobe.com>

--- libs/blocks/global-navigation/blocks/profile/signIn.js
+++ libs/blocks/global-navigation/blocks/profile/signIn.js
@@ -1,35 +1,49 @@
 import { toFragment, getFedsPlaceholderConfig } from '../../utilities/utilities.js';
 import { replaceKey } from '../../../../features/placeholders.js';
 
-const decorateSignIn = async ({ blockEl, decoratedEl }) => {
-  const dropDown = blockEl.querySelector(':scope > div:nth-child(2)');
+const decorateSignIn = async ({ rawElem, decoratedElem }) => {
+  const dropdownElem = rawElem.querySelector(':scope > div:nth-child(2)');
   const signInLabel = await replaceKey('sign-in', getFedsPlaceholderConfig());
-  if (!dropDown) {
-    const signIn = toFragment`<a href="#" daa-ll="${signInLabel}" class="feds-signin">${signInLabel}</a>`;
-    signIn.addEventListener('click', (e) => {
+  let signInElem;
+
+  if (!dropdownElem) {
+    signInElem = toFragment`<a href="#" daa-ll="${signInLabel}" class="feds-signIn">${signInLabel}</a>`;
+
+    signInElem.addEventListener('click', (e) => {
       e.preventDefault();
       // TODO confirm adobeIMS is always set
       // e.g. when it's consumed as NPM package or when we are on a consumer page
       window.adobeIMS.signIn();
     });
-    return signIn;
-  }
+  } else {
+    signInElem = toFragment`<a href="#" daa-ll="${signInLabel}" class="feds-signIn" role="button" aria-expanded="false" aria-haspopup="true">${signInLabel}</a>`;
 
-  const signInWithDropDown = toFragment`<a href="#" daa-ll="${signInLabel}" class="feds-signin" role="button" aria-expanded="false" aria-controls="navmenu-profile" daa-lh="header|Close" aria-haspopup="true">${signInLabel}</a>`;
-  dropDown.classList.add('feds-signin-dropdown');
-  decoratedEl.insertAdjacentElement('beforeend', dropDown);
+    signInElem.addEventListener('click', () => {
+      const isOpen = signInElem.getAttribute('aria-expanded') === 'true';
 
-  // TODO we don't have a good way of adding attributes to links
-  const dropDownSignIn = dropDown.querySelector('[href="https://adobe.com?sign-in=true"]');
-  if (dropDownSignIn) {
-    dropDownSignIn.addEventListener('click', (e) => {
-      e.preventDefault();
-      window.adobeIMS.signIn();
+      if (isOpen) {
+        signInElem.setAttribute('aria-expanded', 'false');
+      } else {
+        signInElem.setAttribute('aria-expanded', 'true');
+      }
     });
+
+    dropdownElem.classList.add('feds-signIn-dropdown');
+
+    // TODO we don't have a good way of adding attributes to links
+    const dropdownSignIn = dropdownElem.querySelector('[href="https://adobe.com?sign-in=true"]');
+
+    if (dropdownSignIn) {
+      dropdownSignIn.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.adobeIMS.signIn();
+      });
+    }
+
+    decoratedElem.append(dropdownElem);
   }
 
-  signInWithDropDown.addEventListener('click', () => window.dispatchEvent(new Event('feds:profileSignIn:clicked')));
-  return signInWithDropDown;
+  decoratedElem.prepend(signInElem);
 };
 
 export default decorateSignIn;
