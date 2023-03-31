54211f34b9d9ab29c21e8cce7a156b42a18a73ff
Solène Duprat <solene.duprat@gmail.com>
2023-03-31 12:55:50 -0400

    updates to `libs/utils/inline.js`,  `libs/utils/inline.css` and `libs/blocks/reading-time.css`

--- libs/utils/inline.js
+++ libs/utils/inline.js
@@ -1,24 +1,24 @@
-import { createTag } from '../../utils/utils.js';
+import { createTag } from './utils.js';
 
-export function inlineBlock(block) {
+export default function inlineBlock(block) {
   if (!block.classList.contains('inline')) return;
-  
+
   const section = block.closest('.section');
   const blocks = Array.from(section.children);
   const blockIndex = blocks.findIndex((el) => el === block);
-  
-  let inlineSiblings = [block];
+
+  const inlineSiblings = [block];
   let nextSibling = blocks[blockIndex + 1];
-  
+
   while (nextSibling && nextSibling.classList.contains('inline')) {
     inlineSiblings.push(nextSibling);
     nextSibling = blocks[blockIndex + inlineSiblings.length];
   }
-  
+
   if (inlineSiblings.length > 1) {
     const wrapper = createTag('div', { class: 'inline-wrapper content' });
     section.insertBefore(wrapper, inlineSiblings[0]);
-    
+
     inlineSiblings.forEach((el) => {
       wrapper.appendChild(el);
     });
