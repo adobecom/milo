9e8c8eea3de9bd9e5e4e9d6a52d9ed319af37f87
Jason Slavin <slavin@adobe.com>
2023-04-05 08:00:14 -0700

    Updating to rely on single hash param as opposed to an additional param

--- libs/blocks/video/video.js
+++ libs/blocks/video/video.js
@@ -2,7 +2,7 @@ export default function init(a) {
   const { pathname, hash } = a;
 
   const isAutoplay = !!(hash?.includes('autoplay'));
-  const isNotLooped = !!(hash?.includes('noloop'));
+  const isNotLooped = !!(hash?.includes('autoplay1'));
 
   const attrs = isAutoplay ? `playsinline autoplay ${isNotLooped ? '' : 'loop'} muted` : 'playsinline controls';
   const video = `<video ${attrs}>
