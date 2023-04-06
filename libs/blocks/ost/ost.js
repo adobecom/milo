305632199c2ceffc667da2b2c04799167de19d44
Axel Cureno Basurto <axel@adobe.com>
2023-04-06 13:35:21 -0700

    ost ims authentication

--- libs/blocks/ost/ost.js
+++ libs/blocks/ost/ost.js
@@ -1,24 +1,41 @@
-import { loadScript, getConfig } from '../../utils/utils.js';
 
-const { miloLibs, codeRoot } = getConfig();
-const base = miloLibs || codeRoot;
+import { loadScript, loadStyle } from '../../utils/utils.js';
+
+const IMS_COMMERCE_CLIENT_ID = 'aos_milo_commerce';
+const IMS_PROD_URL = 'https://auth.services.adobe.com/imslib/imslib.min.js';
+const OST_SCRIPT_URL = 'https://www.stage.adobe.com/special/tacocat/ost/lib/index.js';
+const OST_STYLE_URL = 'https://www.stage.adobe.com/special/tacocat/ost/lib/index.css';
+
+const getImsToken = async () => {
+  window.adobeid = {
+    client_id: IMS_COMMERCE_CLIENT_ID,
+    environment: 'prod',
+    scope: 'AdobeID,openid',
+  };
+  if (!window.adobeIMS) {
+    await loadScript(IMS_PROD_URL);
+  }
+  if (!window.adobeIMS.isSignedInUser()) {
+    window.adobeIMS.signIn();
+  }
+  return window.adobeIMS?.getAccessToken()?.token;
+};
 
 export default async function init() {
-  if (!window.ost) {
-    await loadScript(`https://www.stage.adobe.com/special/tacocat/ost/offer-selector-tool.js`);
-  }
-
+  const ostAosAccessToken = await getImsToken();
   const ostCountry = 'US';
   const ostLanguage = 'en';
-  const ostEnvironment = 'STAGE';
+  const ostEnvironment = 'PROD';
   const ostWcsApiKey = 'wcms-commerce-ims-ro-user-cc';
-  const ostAosApiKey = 'dexter-commerce-offers';
+  const ostAosApiKey = 'wcms-commerce-ims-user-prod';
   const checkoutClientId = 'creative';
-  const ostAosAccessToken = sessionStorage.getItem('AOS_ACCESS_TOKEN') || localStorage.getItem('AOS_ACCESS_TOKEN');
-  const ostRootContainer = document.querySelector('.ost');
-  let url = new URL(window.location);
-  let searchParameters = url.searchParams;
-  ostRootContainer.removeChild(ostRootContainer.firstElementChild);
+  const rootContainer = document.querySelector('.ost');
+  const searchParameters = new URLSearchParams(window.location.search);
+  rootContainer.removeChild(rootContainer.firstElementChild);
+  if (!window.ost) {
+    loadStyle(OST_STYLE_URL);
+    await loadScript(OST_SCRIPT_URL);
+  }
   window.ost.openOfferSelectorTool({
     ostCountry,
     ostLanguage,
