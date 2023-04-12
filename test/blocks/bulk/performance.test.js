57c04d328bc04844d2e52e202f019e42addc63af
JC Kautzmann <jkautzma@adobe.com>
2023-04-12 16:59:27 +0200

    MWPW-125675 - [Core] Milo Bulk Publish
    - track necessary change

--- test/blocks/bulk/performance.test.js
+++ test/blocks/bulk/performance.test.js
@@ -9,6 +9,7 @@ import {
 
 const TEST_TIMEOUT_MS = 3 * 60 * 1000;
 const ORIGIN = 'https://main--milo--adobecom.hlx.page';
+// TODO: use a generic page that is always available
 const PAGE_PATH = '/drafts/jck/seo-event/test-seo-event';
 const PAGE_URL = `${ORIGIN}${PAGE_PATH}`;
 
