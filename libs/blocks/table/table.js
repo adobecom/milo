6ea61f16f7df560ee0cec5e13a26db05af2a8153
Sean Choi <seanchoi@adobe.com>
2023-04-17 15:02:06 -0600

    For heading and highlight color / bg color.

--- libs/blocks/table/table.js
+++ libs/blocks/table/table.js
@@ -1,11 +1,11 @@
 export default function init(el) {
   const rows = el.querySelectorAll(':scope > div');
-  console.log(rows);
   rows.forEach((row, rdx) => {
     row.className = `row row-${rdx + 1}`;
     const cols = row.querySelectorAll(':scope > div');
     cols.forEach((col, cdx) => {
       col.className = `col col-${cdx + 1}`;
+      if (rdx === 1) col.classList.add('col-heading');
     });
   });
 }
