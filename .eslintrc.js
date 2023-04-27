541bde4d991e2f725e8e9c0bad8cd1addeafe202
vivgoodrich <vivian.goodrich@gmail.com>
2023-04-26 18:44:42 -0600

    improved highlight and add sticky

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': process.platform === 'win32' ? 0 : ['error', 'unix'],
     'import/extensions': ['error', { js: 'always' }],
     'object-curly-newline': ['error', {
       ObjectExpression: { multiline: true, minProperties: 6 },
