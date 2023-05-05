a58149645684441865be817f8567a73ac854a2d2
Sunil Kamat <sukamat@adobe.com>
2023-05-05 11:18:48 -0700

    compares langstore latest vs livecopy

--- tools/loc/rollout.js
+++ tools/loc/rollout.js
@@ -1,65 +1,30 @@
 import parseMarkdown from './helix/parseMarkdown.bundle.js';
 import { mdast2docx } from './helix/mdast2docx.bundle.js';
-import { docx2md } from './helix/docx2md.bundle.js';
 
 import {
   connect as connectToSP,
   copyFileAndUpdateMetadata,
   getFileMetadata,
-  getFileVersionInfo,
-  getVersionOfFile,
-  saveFileAndUpdateMetadata, updateFile,
+  saveFileAndUpdateMetadata,
 } from './sharepoint.js';
 import { getUrlInfo, loadingON, simulatePreview, stripExtension } from './utils.js';
 
-const types = new Set();
 const hashToContentMap = new Map();
 
-function getBlockName(node) {
-  if (node.type === 'gridTable') {
-    let blockNameNode = node;
-    try {
-      while (blockNameNode.type !== 'text' && blockNameNode?.children.length > 0) {
-        // eslint-disable-next-line prefer-destructuring
-        blockNameNode = blockNameNode.children[0];
-      }
-      const fullyQualifiedBlockName = blockNameNode.value;
-      if (fullyQualifiedBlockName) {
-        const blockClass = fullyQualifiedBlockName.indexOf('(');
-        let blockName = fullyQualifiedBlockName;
-        if (blockClass !== -1) {
-          blockName = fullyQualifiedBlockName.substring(0, blockClass).trim();
-        }
-        return blockName.toLowerCase().trim();
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log('Unable to get component name', error);
-    }
-  }
-  return undefined;
-}
-
-function getNodeType(node) {
-  return getBlockName(node) || node.type;
-}
-
 function processMdast(nodes) {
-  const hashToIndex = new Map();
-  const arrayWithTypeAndHash = [];
-  let index = 0;
-  let structure = '';
+  const arrayWithContentHash = [];
   nodes.forEach((node) => {
-    const nodeType = getNodeType(node);
-    types.add(nodeType);
-    structure = `${structure}-${nodeType}`;
     const hash = objectHash.sha1(node);
-    arrayWithTypeAndHash.push({ type: nodeType, hash });
-    hashToIndex.set(hash, index);
+    arrayWithContentHash.push(hash);
     hashToContentMap.set(hash, node);
-    index += 1;
   });
-  return { hashToIndex, arrayWithTypeAndHash, structure };
+  return arrayWithContentHash;
+}
+
+async function getMdastFromMd(mdContent) {
+  const state = { content: { data: mdContent }, log: '' };
+  await parseMarkdown(state);
+  return state.content.mdast;
 }
 
 async function getMd(path) {
@@ -67,12 +32,6 @@ async function getMd(path) {
   await simulatePreview(mdPath);
   const mdFile = await fetch(`${getUrlInfo().origin}${mdPath}`);
   return mdFile.text();
-}
-
-function getMdastFromMd(mdContent) {
-  const state = { content: { data: mdContent }, log: '' };
-  parseMarkdown(state);
-  return state.content.mdast;
 }
 
 async function getMdast(path) {
@@ -111,68 +70,8 @@ async function persist(srcPath, mdast, dstPath) {
   }
 }
 
-function updateChangesMap(changesMap, key, value) {
-  if (!changesMap.has(key)) {
-    changesMap.set(key, value);
-  }
-}
-
-function getChanges(left, right) {
-  const leftArray = left.arrayWithTypeAndHash;
-  const leftHashToIndex = left.hashToIndex;
-  const rightArray = right.arrayWithTypeAndHash;
-  const rightHashToIndex = right.hashToIndex;
-  const leftLimit = leftArray.length - 1;
-  const rightLimit = rightArray.length - 1;
-  const changesMap = new Map();
-  const editSet = new Set();
-  let rightPointer = 0;
-  for (let leftPointer = 0; leftPointer <= leftLimit; leftPointer += 1) {
-    const leftElement = leftArray[leftPointer];
-    if (leftPointer <= rightLimit) {
-      rightPointer = leftPointer;
-      const rightElement = rightArray[rightPointer];
-      if (leftElement.hash === rightElement.hash) {
-        updateChangesMap(changesMap, leftElement.hash, { op: 'nochange' });
-      } else if (rightHashToIndex.has(leftElement.hash)) {
-        updateChangesMap(changesMap, rightElement.hash, { op: 'added' });
-      } else if (leftHashToIndex.has(rightElement.hash)) {
-        updateChangesMap(changesMap, leftElement.hash, { op: 'deleted' });
-        updateChangesMap(changesMap, rightElement.hash, { op: 'added' });
-      } else if (leftElement.type === rightElement.type) {
-        updateChangesMap(changesMap, leftElement.hash, { op: 'edited', newHash: rightElement.hash });
-        editSet.add(rightElement.hash);
-      } else {
-        updateChangesMap(changesMap, leftElement.hash, { op: 'deleted' });
-        updateChangesMap(changesMap, rightElement.hash, { op: 'added' });
-      }
-    } else {
-      updateChangesMap(changesMap, leftElement.hash, { op: 'deleted' });
-    }
-  }
-  for (let pointer = 0; pointer <= rightLimit; pointer += 1) {
-    const rightElement = rightArray[pointer];
-    if (!changesMap.has(rightElement.hash) && !editSet.has(rightElement.hash)) {
-      changesMap.set(rightElement.hash, { op: 'added' });
-    }
-  }
-  return changesMap;
-}
-
-function updateMergedMdast(mergedMdast, node, author) {
-  const hash = node.opInfo.op === 'edited' ? node.opInfo.newHash : node.hash;
-  const content = hashToContentMap.get(hash);
-  if (node.opInfo.op !== 'edited' && node.opInfo.op !== 'nochange') {
-    content.author = `${author} ${node.opInfo.op}`;
-    content.action = node.opInfo.op;
-  }
-  mergedMdast.children.push(content);
-}
-
-function getMergedMdast(left, right) {
+function getMergedMdast(langstoreNowProcessedMdast, livecopyProcessedMdast) {
   const mergedMdast = { type: 'root', children: [] };
-  let leftPointer = 0;
-  const leftArray = Array.from(left.entries());
 
   function addTrackChangesInfo(author, action, root) {
     root.author = author;
@@ -196,75 +95,64 @@ function getMergedMdast(left, right) {
     addTrackChangesInfoToChildren(root);
   }
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [rightHash, rightOpInfo] of right) {
-    if (left.has(rightHash)) {
-      for (leftPointer; leftPointer < leftArray.length; leftPointer += 1) {
-        const leftArrayElement = leftArray[leftPointer];
-        const leftHash = leftArrayElement[0];
-        const leftOpInfo = leftArrayElement[1];
-        if (leftHash === rightHash) {
-          const leftContent = hashToContentMap.get(leftOpInfo.op === 'edited' ? leftOpInfo.newHash : leftHash);
-          if (rightOpInfo.op === 'nochange') {
-            mergedMdast.children.push(leftContent);
-          } else if (rightOpInfo.op === 'edited') {
-            addTrackChangesInfo('Langstore Version', 'deleted', leftContent);
-            mergedMdast.children.push(leftContent);
-            const rightContent = hashToContentMap.get(rightOpInfo.newHash);
-            addTrackChangesInfo('Regional Version', 'edited', rightContent);
-            mergedMdast.children.push(rightContent);
-          }
-          leftPointer += 1;
-          break;
-        } else {
-          updateMergedMdast(mergedMdast, { hash: leftHash, opInfo: leftOpInfo }, 'Langstore');
-        }
-      }
+  // Iterate and insert content in mergedMdast as long as both arrays have content
+  const length = Math.min(langstoreNowProcessedMdast.length, livecopyProcessedMdast.length);
+  let index;
+  for (index = 0; index < length; index += 1) {
+    if (langstoreNowProcessedMdast[index] === livecopyProcessedMdast[index]) {
+      const content = hashToContentMap.get(langstoreNowProcessedMdast[index]);
+      mergedMdast.children.push(content);
     } else {
-      updateMergedMdast(mergedMdast, { hash: rightHash, opInfo: rightOpInfo }, 'Region');
+      const langstoreContent = hashToContentMap.get(langstoreNowProcessedMdast[index]);
+      addTrackChangesInfo('Langstore Version', 'deleted', langstoreContent);
+      mergedMdast.children.push(langstoreContent);
+      const livecopyContent = hashToContentMap.get(livecopyProcessedMdast[index]);
+      addTrackChangesInfo('Regional Version', 'added', livecopyContent);
+      mergedMdast.children.push(livecopyContent);
     }
   }
-  // Check if there are excess elements in left array - if yes update them into the mergedMdast.
-  if (leftPointer < leftArray.length) {
-    // eslint-disable-next-line no-plusplus
-    for (leftPointer; leftPointer < leftArray.length; leftPointer++) {
-      const leftArrayElement = leftArray[leftPointer];
-      updateMergedMdast(
-        mergedMdast,
-        { hash: leftArrayElement[0], opInfo: leftArrayElement[1] },
-        'Langstore',
-      );
+
+  // Insert the leftover content in langstore if any
+  if (index < langstoreNowProcessedMdast.length) {
+    for (; index < langstoreNowProcessedMdast.length; index += 1) {
+      const langstoreContent = hashToContentMap.get(langstoreNowProcessedMdast[index]);
+      addTrackChangesInfo('Langstore Version', 'deleted', langstoreContent);
+      mergedMdast.children.push(langstoreContent);
     }
   }
+
+  // Insert the leftover content in livecopy if any
+  if (index < livecopyProcessedMdast.length) {
+    for (; index < livecopyProcessedMdast.length; index += 1) {
+      const livecopyContent = hashToContentMap.get(livecopyProcessedMdast[index]);
+      mergedMdast.children.push(livecopyContent);
+    }
+  }
+
   return mergedMdast;
 }
 
+// If modified timestamp and rollout timestamp is less than 10seconds for livecopy, return true.
 function noRegionalChanges(fileMetadata) {
   const lastModified = new Date(fileMetadata.Modified);
   const lastRollout = new Date(fileMetadata.Rollout);
   const diffBetweenRolloutAndModification = Math.abs(lastRollout - lastModified) / 1000;
-  return diffBetweenRolloutAndModification < 10;
-}
-
-async function safeGetVersionOfFile(filePath, version) {
-  let versionFile;
-  try {
-    versionFile = await getVersionOfFile(filePath, version);
-  } catch (error) {
-    // Do nothing
-  }
-  return versionFile;
+  // TODO: temporarily commenting out for the ease of testing
+  // return diffBetweenRolloutAndModification < 10;
+  return false;
 }
 
 async function rollout(file, targetFolders, skipMerge = false) {
+  // path to the doc that is being rolled out
   const filePath = file.path;
   await connectToSP();
   const filePathWithoutExtension = stripExtension(filePath);
   const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
 
   async function rolloutPerFolder(targetFolder) {
-    let langstorePrevMd;
+    // live copy path of where the file is getting rolled out to
     const livecopyFilePath = `${targetFolder}/${fileName}`;
+    // holds the status of whether rollout was successful
     const status = { success: true, path: livecopyFilePath };
 
     function udpateErrorStatus(errorMessage) {
@@ -275,17 +163,25 @@ async function rollout(file, targetFolders, skipMerge = false) {
 
     try {
       loadingON(`Rollout to ${livecopyFilePath} started`);
+      // gets the metadata info of the live copy file
       const fileMetadata = await getFileMetadata(livecopyFilePath);
+      // does the live copy file exist?
       const isfileNotFound = fileMetadata?.status === 404;
+      // get langstore file prev version value from the rolled out live copy file's RolloutVersion value.
+      // the RolloutVersion basically gives which version of langstore file was previously rolled out
       const langstorePrevVersion = fileMetadata.RolloutVersion;
+      // get RolloutStatus value - eg: 'Merged'
       const previouslyMerged = fileMetadata.RolloutStatus;
-      const langstoreCurrentVersion = await getFileVersionInfo(filePath);
+
+      // if regional file does not exist, just copy the langstore file to region
       if (isfileNotFound) {
         // just copy since regional document does not exist
         await copyFileAndUpdateMetadata(filePath, targetFolder);
         loadingON(`Rollout to ${livecopyFilePath} complete`);
         return status;
       }
+
+      // if regional file exists but there are no changes in regional doc or if we are skipping merge altogether
       if (skipMerge || (noRegionalChanges(fileMetadata) && !previouslyMerged)) {
         await saveFileAndUpdateMetadata(
           filePath,
@@ -295,44 +191,29 @@ async function rollout(file, targetFolders, skipMerge = false) {
         loadingON(`Rollout to ${livecopyFilePath} complete`);
         return status;
       }
+
+      // if regional file exists AND there are changes in the regional file, then merge process kicks in.
       if (!langstorePrevVersion) {
+        // if for some reason the RolloutVersion metadata info is not available in region, rollout is NOT performed
         // Cannot merge since we don't have rollout version info.
         // eslint-disable-next-line no-console
         loadingON(`Cannot rollout to ${livecopyFilePath} since last rollout version info is unavailable`);
-      } else if (langstoreCurrentVersion === langstorePrevVersion) {
-        langstorePrevMd = await getMd(filePathWithoutExtension);
       } else {
-        const langstoreBaseFile = await safeGetVersionOfFile(filePath, langstorePrevVersion);
-        if (langstoreBaseFile) {
-          langstorePrevMd = await docx2md(langstoreBaseFile, { gridtables: true });
-        }
-      }
-      if (langstorePrevMd != null) {
-        const langstorePrev = await getMdastFromMd(langstorePrevMd);
-        const langstoreBaseProcessed = await getProcessedMdast(langstorePrev);
+        // get MDAST of the current langstore file that needs to be rolled out
         const langstoreNow = await getMdast(filePathWithoutExtension);
-        const langstoreNowProcessed = await getProcessedMdast(langstoreNow);
+        // get processed data Map for the current langstore file that needs to be rolled out
+        const langstoreNowProcessedMdast = await getProcessedMdast(langstoreNow);
+
         const liveCopyPath = `${targetFolder}/${fileName}`;
-        if (langstoreBaseProcessed.structure !== langstoreNowProcessed.structure) {
-          await updateFile(liveCopyPath, null, { RolloutStatus: 'Skipped' });
-          return status;
-        }
+        // get MDAST of the livecopy file
         const livecopy = await getMdast(liveCopyPath.substring(0, liveCopyPath.lastIndexOf('.')));
-        const livecopyProcessed = await getProcessedMdast(livecopy);
-        if (langstoreBaseProcessed.structure !== livecopyProcessed.structure) {
-          await updateFile(liveCopyPath, null, { RolloutStatus: 'Skipped' });
-          return status;
-        }
-        const langstoreBaseToNowChanges = getChanges(langstoreBaseProcessed, langstoreNowProcessed);
-        const langstoreBaseToRegionChanges = getChanges(langstoreBaseProcessed, livecopyProcessed);
-        const livecopyMergedMdast = getMergedMdast(
-          langstoreBaseToNowChanges,
-          langstoreBaseToRegionChanges,
-        );
+        // get processed data Map for the livecopy file
+        const livecopyProcessedMdast = await getProcessedMdast(livecopy);
+        // get merged mdast
+        const livecopyMergedMdast = getMergedMdast(langstoreNowProcessedMdast, livecopyProcessedMdast);
+        // save the merged livecopy file
         await persist(filePath, livecopyMergedMdast, livecopyFilePath);
         loadingON(`Rollout to ${livecopyFilePath} complete`);
-      } else {
-        udpateErrorStatus(`Rollout to ${livecopyFilePath} did not succeed. Missing langstore file`);
       }
     } catch (error) {
       udpateErrorStatus(`Rollout to ${livecopyFilePath} did not succeed. Error ${error.message}`);
