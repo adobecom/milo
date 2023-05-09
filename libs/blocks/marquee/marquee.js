201fb7306b4f7c4bf34afb8280b271efb6695605
Suhani Jain <suhjain@adobe.com>
2023-05-09 08:35:35 +0530

    MWPW-127384 removing unwanted function

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -65,16 +65,6 @@ const decorateBlockBg = (block, node, metaNode) => {
   }
 };
 
-export const getMetadata = (el) => [...el.rows].reduce((obj, row) => {
-  if (row.children) {
-    const key = row.children[0].textContent.trim().toLowerCase();
-    const content = row.children[1];
-    const text = content.textContent.trim().toLowerCase();
-    if (key && content) obj[key] = { content, text };
-  }
-  return obj;
-}, {});
-
 function decorateText(el, size) {
   const headings = el.querySelectorAll('h1, h2, h3, h4, h5, h6');
   const heading = headings[headings.length - 1];
