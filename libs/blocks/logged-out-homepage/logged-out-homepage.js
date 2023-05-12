858e21edd6a67ad863b428f869810c05880b2f78
vivgoodrich <vivian.goodrich@gmail.com>
2023-05-12 17:54:55 -0600

    after working session

--- libs/blocks/logged-out-homepage/logged-out-homepage.js
+++ libs/blocks/logged-out-homepage/logged-out-homepage.js
@@ -1,11 +1,20 @@
-import { decorateButtons, decorateBlockBg, decorateBlockText, getBlockSize } from '../../utils/decorate.js';
+import { decorateButtons, decorateBlockBg, decorateBlockText } from '../../utils/decorate.js';
 
 // size: [heading, body, ...detail]
+// blockTypeSizes array order: heading, body, detail, button, link
 const blockTypeSizes = {
-  small: ['m', 's', 's'],
-  medium: ['l', 'm', 'm'],
-  large: ['xl', 'm', 'l'],
-  xlarge: ['xxl', 'l', 'xl'],
+  small: ['m', 's', 's', 'l', 's'],
+  medium: ['l', 'm', 'm', 'l', 'm'],
+  large: ['xl', 'm', 'l', 'l', 'm'],
+  xlarge: ['xxl', 'l', 'xl', 'l', 'l'],
+
+  /* TODO: add this to CSS */
+  podFullSizePodDesktop: ['xl', 'm', 'l', 'm'],
+
+  'link-pod': ['m', 'xs', 'm', 's', 'xs'],
+  'news-pod': ['s', 'm', 'm', 's', 'xs'],
+  'quick-tools-bar': ['l', 'l', 'm', 's', 'xs'],
+  default: ['m', 'm', 'l', 's', 'xs'],
 };
 
 function goToDataHref() {
@@ -48,12 +57,28 @@ function parseBlockMetaData(el) {
   return results;
 }
 
+function getBlockSize(el) {
+  const sizes = ['small', 'medium', 'large', 'xlarge', 'link-pod', 'news-pod', 'quick-tools-bar', 'default'];
+  return sizes.find((size) => el.classList.contains(size)) || sizes[7];
+}
+
+function decorateLinks(el, size) {
+  const links = el.querySelectorAll('a:not(.con-button)');
+  if (links.length === 0) return;
+  links.forEach((link) => {
+    const parent = link.closest('p, div');
+    parent.setAttribute('class', `body-${size}`);
+  });
+}
+
 export default function init(el) {
-  decorateButtons(el, 'button-l');
+  const blockSize = getBlockSize(el);
+  decorateButtons(el, `button-${blockTypeSizes[blockSize][3]}`);
+  decorateLinks(el, blockTypeSizes[blockSize][4]);
   const parsedBlock = parseBlockMetaData(el);
   let { rows, metaData } = parsedBlock;
 
-  if (!el.classList.contains('no-bg')) {
+  if (rows.length > 1) {
     el.classList.add('has-bg');
     const [head, ...tail] = rows;
     decorateBlockBg(el, head);
@@ -78,7 +103,7 @@ export default function init(el) {
     }
   }
 
-  const config = blockTypeSizes[getBlockSize(el)];
+  const config = blockTypeSizes[blockSize];
   const overrides = ['-heading', '-body', '-detail'];
   overrides.forEach((override, index) => {
     const hasClass = [...el.classList].filter((listItem) => listItem.includes(override));
@@ -89,7 +114,6 @@ export default function init(el) {
 
   if (el.classList.contains('link-pod') || el.classList.contains('click-pod') || el.classList.contains('news-pod')) {
     const links = el.querySelectorAll('a');
-    links.forEach((link) => { link.classList.add('pod-link'); });
     if (el.classList.contains('click-pod') && links.length) {
       const link = links[0];
       el.dataset.href = link.href;
