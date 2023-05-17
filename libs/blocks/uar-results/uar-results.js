9ef303b7131515f27276c2e7494298471f7e48f9
Cody Lloyd <colloyd@adobe.com>
2023-05-17 07:54:54 -0600

    Finalized UAR Results
    * Lana logging  and a debug param to stop redirects (MWPW-125185)
    * Set a value for pageload analytics (MWPW-128939)
    * Unit tests (MWPW-130870)
    
    Resolves: [MWPW-125185](https://jira.corp.adobe.com/browse/MWPW-125185)
    
    Resolves: [MWPW-128939](https://jira.corp.adobe.com/browse/MWPW-128939)
    
    Resolves: [MWPW-130870](https://jira.corp.adobe.com/browse/MWPW-130870)
    
    **Test URLs:**
    - Before: https://main--milo--adobecom.hlx.page/?martech=off
    - After: https://<branch>--milo--adobecom.hlx.page/?martech=off

--- libs/blocks/uar-results/uar-results.js
+++ libs/blocks/uar-results/uar-results.js
@@ -1,10 +1,10 @@
-/* eslint-disable no-await-in-loop */
 import { createTag } from '../../utils/utils.js';
 import { getMetadata, handleStyle } from '../section-metadata/section-metadata.js';
 
 async function loadFragments(el, experiences) {
   // eslint-disable-next-line no-restricted-syntax
   for (const href of experiences) {
+    /* eslint-disable no-await-in-loop */
     const a = createTag('a', { href });
     el.append(a);
     const { default: createFragment } = await import('../fragment/fragment.js');
@@ -12,48 +12,83 @@ async function loadFragments(el, experiences) {
   }
 }
 
-function redirectPage(quizUrl) {
+function redirectPage(quizUrl, debug, message) {
   const url = (quizUrl) ? quizUrl.text : 'https://adobe.com';
-  console.log(`Invalid Result, redirecting to: ${url}`);
+  window.lana.log(message);
+
+  if (debug === 'uar') {
+    // eslint-disable-next-line no-console
+    console.log(`${message}, redirecting to: ${url}`);
+  } else {
+    window.location = url;
+  }
 }
 
-export default async function init(el) {
+function setAnalytics(hashValue, debug) {
+  /* eslint-disable no-underscore-dangle */
+  window.alloy_load ??= {};
+  window.alloy_load.data ??= {};
+  window.alloy_all ??= {};
+  window.alloy_all.data ??= {};
+  window.alloy_all.data._adobe_corpnew ??= {};
+  window.alloy_all.data._adobe_corpnew.digitalData ??= {};
+  window.alloy_all.data._adobe_corpnew.digitalData.page ??= {};
+  window.alloy_all.data._adobe_corpnew.digitalData.page.pageInfo ??= {};
+  window.alloy_all.data._adobe_corpnew.digitalData.page.pageInfo.customHash = hashValue;
+  if (debug === 'uar') {
+    // eslint-disable-next-line no-console
+    console.log(`Setting a custom hash for pageload to: ${window.alloy_all.data._adobe_corpnew.digitalData.page.pageInfo.customHash}`);
+  }
+}
+
+export default async function init(el, debug = null, localStoreKey = null) {
   const data = getMetadata(el);
   const metaData = el.querySelectorAll('div');
   const params = new URL(document.location).searchParams;
-  const localStoreKey = params.get('quizKey');
   const quizUrl = data['quiz-url'];
   const BASIC_KEY = 'basicFragments';
   const NESTED_KEY = 'nestedFragments';
+  const HASH_KEY = 'pageloadHash';
+
+  /* eslint-disable no-param-reassign */
+  // handle these two query param values in this way to facilitate unit tests
+  localStoreKey ??= params.get('quizKey');
+  debug ??= params.get('debug');
 
   let results = localStorage.getItem(localStoreKey);
-
-  for( const div of metaData){
-    div.remove();
-  }
-
   if (!results) {
-    redirectPage(quizUrl);
-    return
+    redirectPage(quizUrl, debug, 'Could not load UAR results: local storage missing');
+    return;
   }
-
   results = JSON.parse(results);
 
-  if(data['nested-fragments'] && el.classList.contains('nested')) {
+  if (data['nested-fragments'] && el.classList.contains('nested')) {
     const nested = results[NESTED_KEY][data['nested-fragments'].text];
     if (nested) loadFragments(el, nested);
   } else if (el.classList.contains('basic')) {
-    const structure = results[BASIC_KEY];
-    if (!structure) { 
-      redirectPage(quizUrl);
-      return
+    const basic = results[BASIC_KEY];
+    const pageloadHash = results[HASH_KEY];
+
+    if (!basic) {
+      redirectPage(quizUrl, debug, 'Could not load UAR results: Basic fragments are missing');
+      return;
     }
-    loadFragments(el, structure);
+
+    if (pageloadHash) {
+      setAnalytics(pageloadHash, debug);
+    }
+
+    loadFragments(el, basic);
   } else {
-    return
+    return;
   }
-  
-  if(data.style) {
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const div of metaData) {
+    div.remove();
+  }
+
+  if (data.style) {
     el.classList.add('section');
     handleStyle(data.style.text, el);
   }
