3b3d35b8b3b64b7aa6c0f47160917f6e9f788111
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2023-05-24 16:29:45 +0200

    standard table and merch table - iteration 2

--- libs/blocks/table/table.js
+++ libs/blocks/table/table.js
@@ -1,5 +1,3 @@
-import {decorateBlockText, decorateButtons, getBlockSize} from '../../utils/decorate.js';
-
 export default function init(el) {
   const rows = Array.from(el.children);
   rows.forEach((row, rdx) => {
@@ -10,14 +8,20 @@ export default function init(el) {
       if (rdx === 1) {
         col.classList.add('col-heading');
       }
+      if (rdx === 0) {
+        col.classList.add('col-highlight');
+      }
     });
   });
+
+  const isMerchTable = el.classList.contains('merch');
 
   handleSectionHead(el);
   handleHeading(el);
   handleHovering(el);
   handleHighlight(el);
   handleScrollEffect(el);
+  if (isMerchTable) formatMerchTable(el);
 
   applyStylesBasedOnScreenSize(el);
   window.addEventListener('resize', () => applyStylesBasedOnScreenSize(el));
@@ -32,7 +36,7 @@ function handleHighlight(table) {
     const hasText = headingCols[i].innerText && col.innerText;
 
     if (hasText) {
-      col.classList.add('highlight');
+      col.classList.add('col-highlight');
       headingCols[i].classList.add('no-rounded');
     } else {
       col.classList.add('hidden');
@@ -41,25 +45,38 @@ function handleHighlight(table) {
 }
 
 function handleSectionHead(table) {
-  const rows = table.querySelectorAll('.row');
-  rows.forEach((row, i) => {
-    if (i > 1) {
-      const title = row.querySelector('.col-2');
-      const infoIcon = title.querySelector('.icon-info');
-      const firstElementChild = title.firstElementChild;
-      const isSectionHead = firstElementChild && firstElementChild === row.getElementsByTagName('strong')[0];
+  const isMerchTable = table.classList.contains('merch');
 
-      if (isSectionHead) {
-        title.classList.add('sectionTitle');
-        row.classList.add('sectionHead');
+  if (isMerchTable) {
+    const merchCols =  Array.from(table.querySelectorAll('.col'));
+    const merhContentCols = merchCols.filter(col => !col.parentElement.classList.contains('row-1') && !col.parentElement.classList.contains('row-2'));
+
+    merhContentCols.forEach(e => {
+      if (e.firstElementChild && e.firstElementChild.tagName === 'STRONG') {
+        e.classList.add('sectionTitle');
       } else {
-        title.classList.add('subSectionTitle');
-        row.classList.add('subSection');
+        e.classList.add('col-merch');
       }
-    }
-  });
-}
+    });
+  } else {
+    const rows = table.querySelectorAll('.row');
 
+    rows.forEach((row, i) => {
+      if (i > 1) {
+        const title = row.querySelector('.col-1');
+        const firstElementChild = title.firstElementChild;
+
+        if (firstElementChild && firstElementChild === row.getElementsByTagName('strong')[0]) {
+          title.classList.add('sectionTitle');
+          row.classList.add('sectionHead');
+        } else {
+          title.classList.add('subSectionTitle');
+          row.classList.add('subSection');
+        }
+      }
+    });
+  }
+}
 
 function applyStylesBasedOnScreenSize(table) {
   if (!(table instanceof Element)) {
@@ -68,24 +85,41 @@ function applyStylesBasedOnScreenSize(table) {
 
   const screenWidth = window.innerWidth;
   const subSectionRows = Array.from(table.getElementsByClassName('subSection'));
+  if (subSectionRows.length >0 ) {
+    const colsForTablet = subSectionRows[0].children.length - 1;
+    const percentage = 100 / colsForTablet;
+    const templateColumnsValue = `repeat(auto-fit, ${percentage}%)`;
 
-  const colsForTablet = subSectionRows[0].children.length - 2;
-  const percentage = 100 / colsForTablet;
-  const templateColumnsValue = `repeat(auto-fit, ${percentage}%)`;
+    subSectionRows.forEach(row => {
+      if (screenWidth > 859) {
+        row.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, 1fr))`;
+      } else if (screenWidth < 860) {
+        row.style.gridTemplateColumns = templateColumnsValue;
+      }
+    });
+  }
+}
 
-  subSectionRows.forEach(row => {
-    if (screenWidth > 859) {
-      row.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, 1fr))`;
-    } else if (screenWidth < 860) {
-      row.style.gridTemplateColumns = templateColumnsValue;
+function formatMerchTable(table) {
+  const rows = table.querySelectorAll('.row');
+  const rowsNum = rows.length;
+
+  const firstRow = rows[0];
+  const colsInRow = firstRow.querySelectorAll('.col');
+  const colsInRowNum = colsInRow.length;
+
+  for (let i = colsInRowNum; i > 0; i--) {
+    const cols = table.querySelectorAll(`.col-${i}`);
+    for (let j = rowsNum - 1; j >= 0; j--) {
+      let currentCol = cols[j];
+      if (!currentCol.innerText && currentCol.children.length === 0) {
+        currentCol.classList.add('no-borders');
+      } else {
+        currentCol.classList.add('border-bottom');
+        break;
+      }
     }
-    // to do: for mobile, this is hardcoded just for test
-    // if (screenWidth < 500) {
-    //   let toRemove = table.querySelectorAll('.col-6, .col-5');
-    //   toRemove.forEach(e => e.style.display = 'none');
-    //   row.style.gridTemplateColumns = `repeat(auto-fit, 50%)`;
-    // }
-  });
+  }
 }
 
 function handleHeading(table) {
@@ -93,7 +127,6 @@ function handleHeading(table) {
   const row2 = table.querySelector('.row-2');
   const cols1 = row1.querySelectorAll('.col');
   const cols2 = row2.querySelectorAll('.col');
-  const size = getBlockSize(table);
 
   cols2.forEach((col, i) => {
     const isEmpty = col.innerHTML === "";
@@ -117,7 +150,7 @@ function handleHeading(table) {
           elements[1].classList.add('pricing');
         }
 
-        decorateButtons(col, size === 'large' ? 'button-xl' : 'button-l');
+        decorateButtons(col, 'button-l');
       }
     }
   });
@@ -127,34 +160,42 @@ function handleHovering(table) {
   const row1 = table.querySelector('.row-1');
   const colsInRowNum = row1.childElementCount;
 
-  for (let i = 3; i <= colsInRowNum; i++) {
+  const isMerchTable = table.classList.contains('merch');
+  const startValue = isMerchTable ? 1 : 2;
+
+  for (let i = startValue; i <= colsInRowNum; i++) {
     const elements = table.querySelectorAll(`.col-${i}`);
     elements.forEach(e => {
-      e.addEventListener('mouseover', () => handleMouseOver(elements, table));
+      e.addEventListener('mouseover', () => handleMouseOver(elements, table, i));
       e.addEventListener('mouseout', () => handleMouseOut(elements));
     })
   }
 }
 
-function handleMouseOver(elements, table) {
+function handleMouseOver(elements, table, colNum) {
   handleMouseOut(elements);
+
+  const secondRow = table.querySelector('.row-2');
+  const colClass = 'col-' + colNum;
+
   elements.forEach(e => {
-    if (e.classList.contains('highlight')) {
-      e.classList.forEach(classname => {
-        if (classname.startsWith('col-')) {
-          let secondRow = table.querySelector('.row-2');
-          let noTopBorderCol = secondRow.querySelector('.' + classname);
-          noTopBorderCol.classList.add('no-top-border');
-        }
-      })
+    if (e.classList.contains('col-highlight') && e.innerText) {
+      const matchingCols = Array.from(e.classList).filter(className => className.startsWith(colClass));
+
+      matchingCols.forEach(className => {
+        const noTopBorderCol = secondRow.querySelector('.' + className);
+        noTopBorderCol.classList.add('no-top-border');
+      });
     }
+
     e.classList.add('hover');
-  })
+  });
 }
 
 function handleMouseOut(elements) {
   elements.forEach(e => {
     e.classList.remove('hover');
+    e.classList.remove('no-top-border');
   })
 }
 
@@ -167,6 +208,24 @@ function handleScrollEffect(table) {
     header.classList.toggle('active', !entry.isIntersecting);
   });
   observer.observe(intercept);
+}
+
+export function decorateButtons(el, size) {
+  const buttons = el.querySelectorAll('em a, strong a');
+  if (buttons.length === 0) return;
+  buttons.forEach((button) => {
+    const parent = button.parentElement;
+    const buttonType = parent.nodeName === 'STRONG' ? 'blue' : 'outline';
+    button.classList.add('con-button', buttonType);
+    if (size) button.classList.add(size); /* button-l, button-xl */
+    parent.insertAdjacentElement('afterend', button);
+    parent.remove();
+  });
+  const actionArea = buttons[0].closest('p, div');
+  if (actionArea) {
+    actionArea.classList.add('action-area');
+    actionArea.nextElementSibling?.classList.add('supplemental-text', 'body-xl');
+  }
 }
 
 window.addEventListener('resize', applyStylesBasedOnScreenSize);
