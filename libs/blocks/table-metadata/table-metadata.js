658ba05ca827143246696e8b3d5f11bbb1d34264
draganatrajkovic <65951679+draganatrajkovic@users.noreply.github.com>
2023-05-25 11:25:30 +0200

    Table block (#769)
    * standard table svgs
    
    * standard table - iteration 1
    
    * standard table and merch table - iteration 2
    
    * uncomment css for sticky header
    
    ---------
    
    Co-authored-by: Dragana Trajkovic <dragana.trajkovic@lilly021.com>

--- libs/blocks/table-metadata/table-metadata.js
+++ libs/blocks/table-metadata/table-metadata.js
@@ -14,22 +14,41 @@ function handleSectionHead(text, table) {
 const handleColumnColor = (text, table, columnType) => {
   if (!text) return;
   const colors = getIndexedValues(text);
-  colors.forEach((color) => {
-    const colorIndex = color[0];
-    const colorValue = color[1]?.replace('white', 'light');
-    table.querySelector(`.col-${colorIndex}.col-${columnType}`)?.classList.add(colorValue);
-  });
+
+  if (colors.length === 1 && colors[0].length === 1) {
+    const color = colors[0][0]?.replace('white', 'light');
+    const allClassCols = Array.from(table.getElementsByClassName(`col-${columnType}`));
+    allClassCols.forEach(element => {
+      element.classList.add(color);
+    });
+  } else {
+    colors.forEach((color) => {
+      const colorIndex = color[0];
+      const colorValue = color[1]?.replace('white', 'light');
+      table.querySelector(`.col-${colorIndex}.col-${columnType}`)?.classList.add(colorValue);
+    });
+  }
 };
 
 const handleColumnBgColor = (text, table, columnType) => {
   if (!text) return;
   const bgColors = getIndexedValues(text);
-  bgColors.forEach((color) => {
-    const bgColorIndex = color[0];
-    const bgColorValue = color[1];
-    const col = table.querySelector(`.col-${bgColorIndex}.col-${columnType}`);
-    if (col) col.style.background = bgColorValue;
-  });
+
+  if (bgColors.length === 1 && bgColors[0].length === 1) {
+    const color = bgColors[0][0];
+    const allClassCols = Array.from(table.getElementsByClassName(`col-${columnType}`));
+    allClassCols.forEach(element => {
+      element.style.backgroundColor = color;
+    });
+  } else {
+    bgColors.forEach((color) => {
+      const [bgColorIndex, bgColorValue] = color;
+      const col = table.querySelector(`.col-${bgColorIndex}.col-${columnType}`);
+      if (col) {
+        col.style.background = bgColorValue;
+      }
+    });
+  }
 };
 
 function handleCompare(text, table) {
@@ -42,15 +61,68 @@ function handleCompare(text, table) {
   });
 }
 
+function handleCollapse(collapsRows, expandDefault, table) {
+  if (!collapsRows) return;
+
+  const rows = getIndexedValues(collapsRows);
+
+  rows.forEach((group) => {
+    const el = group[0];
+    if (!el.includes('-')) return;
+
+    const [rowStartStr, rowEndStr] = el.trim().split('-');
+    const rowStart = Number(rowStartStr) + 1;
+    const rowEnd = Number(rowEndStr);
+
+    if (!rowStart || !rowEnd) return;
+
+    const range = Array.from({ length: rowEnd - rowStart + 1 }, (_, index) => index + rowStart);
+
+    const collapsHeader = table.querySelector(`.row-${rowStart - 1}`);
+    collapsHeader.classList.add('sectionHead');
+
+    const iconTag = document.createElement('span');
+    iconTag.classList.add('icon', 'expand');
+
+    const collapsHeaderTitle = collapsHeader.querySelector('.col-1');
+    collapsHeaderTitle.appendChild(iconTag);
+    iconTag.setAttribute('aria-expanded', el === expandDefault ? 'true' : 'false');
+
+    range.forEach((row) => {
+      const rowElement = table.querySelector(`.row-${row}`);
+      if (el !== expandDefault) {
+        rowElement.setAttribute('hidden', '');
+      }
+    });
+
+    iconTag.addEventListener('click', (e) => {
+      handleExpand(e.target, range, table);
+    });
+  });
+}
+
+function handleExpand(el, rows, table) {
+  const expanded = el.getAttribute('aria-expanded') === 'false';
+  el.setAttribute('aria-expanded', expanded.toString());
+  rows.forEach((row) => {
+    const rowElement = table.querySelector(`.row-${row}`);
+    if (expanded) {
+      rowElement.removeAttribute('hidden');
+    } else {
+      rowElement.setAttribute('hidden', '');
+    }
+  });
+}
+
 export default function init(el) {
   const table = el.closest('.section').querySelector('.table');
   if (!table) return;
-
   const metadata = getMetadata(el);
   if (metadata.section) handleSectionHead(metadata.section.text, table);
   if (metadata.compare) handleCompare(metadata.compare.text, table);
   if (metadata['heading color']) handleColumnColor(metadata['heading color'].text, table, 'heading');
   if (metadata['heading background color']) handleColumnBgColor(metadata['heading background color'].text, table, 'heading');
-  if (metadata['highlight color']) handleColumnColor(metadata['heading color'].text, table, 'highlight');
+  if (metadata['highlight color']) handleColumnColor(metadata['highlight color'].text, table, 'highlight');
   if (metadata['highlight background color']) handleColumnBgColor(metadata['highlight background color'].text, table, 'highlight');
+  if (metadata['collapse rows'] && metadata['expand default']) handleCollapse(metadata['collapse rows'].text, metadata['expand default'].text, table);
 }
--- libs/blocks/table/table.js
+++ libs/blocks/table/table.js
@@ -70,30 +70,253 @@ const addStickyListeners = (table, highlightOn) => {
 
 export default function init(el) {
   // remove top row if empty
-  const firstRow = el.querySelector(':scope > div:first-child');
-  if (firstRow.innerText.trim() === '') firstRow.remove();
+  // const firstRow = el.querySelector(':scope > div:first-child');
+  // if (firstRow.innerText.trim() === '') firstRow.remove();
 
   const highlightOn = el.classList.contains('highlight');
-  const rows = el.querySelectorAll(':scope > div');
+
+  const rows = Array.from(el.children);
   rows.forEach((row, rdx) => {
-    row.className = `row row-${rdx + 1}`;
+    row.classList.add(`row`, `row-${rdx + 1}`);
     if (highlightOn && rdx === 0) {
       row.classList.add('row-highlight');
     } else if ((highlightOn && rdx === 1) || (!highlightOn && rdx === 0)) {
       row.classList.add('row-header');
     }
-    const cols = row.querySelectorAll(':scope > div');
+
+    const cols = Array.from(row.children);
     cols.forEach((col, cdx) => {
-      col.className = `col col-${cdx + 1}`;
-      if (row.classList.contains('row-header')) {
+      col.classList.add(`col`, `col-${cdx + 1}`);
+      // if (row.classList.contains('row-header')) {
+      //   col.classList.add('col-heading');
+      // } else if (
+      //   row.classList.contains('row-highlight')
+      //   && col.innerText !== ''
+      // ) {
+      //   col.classList.add('col-highlight');
+      // }
+      if (rdx === 1) {
         col.classList.add('col-heading');
-      } else if (
-        row.classList.contains('row-highlight')
-        && col.innerText !== ''
-      ) {
+      }
+      if (rdx === 0) {
         col.classList.add('col-highlight');
       }
     });
   });
+
+  const isMerchTable = el.classList.contains('merch');
+
+  handleSectionHead(el);
+  handleHeading(el);
+  handleHovering(el);
+  handleHighlight(el);
+  handleScrollEffect(el);
+  if (isMerchTable) formatMerchTable(el);
+
+  applyStylesBasedOnScreenSize(el);
+  window.addEventListener('resize', () => applyStylesBasedOnScreenSize(el));
   if (el.classList.contains('sticky-top')) addStickyListeners(el, highlightOn);
 }
+
+function handleHighlight(table) {
+  const rows = table.querySelectorAll('.row');
+  const highlightCols = rows[0].querySelectorAll('.col');
+  const headingCols = rows[1].querySelectorAll('.col');
+
+  highlightCols.forEach((col, i) => {
+    const hasText = headingCols[i].innerText && col.innerText;
+
+    if (hasText) {
+      col.classList.add('col-highlight');
+      headingCols[i].classList.add('no-rounded');
+    } else {
+      col.classList.add('hidden');
+    }
+  });
+}
+
+function handleSectionHead(table) {
+  const isMerchTable = table.classList.contains('merch');
+
+  if (isMerchTable) {
+    const merchCols =  Array.from(table.querySelectorAll('.col'));
+    const merhContentCols = merchCols.filter(col => !col.parentElement.classList.contains('row-1') && !col.parentElement.classList.contains('row-2'));
+
+    merhContentCols.forEach(e => {
+      if (e.firstElementChild && e.firstElementChild.tagName === 'STRONG') {
+        e.classList.add('sectionTitle');
+      } else {
+        e.classList.add('col-merch');
+      }
+    });
+  } else {
+    const rows = table.querySelectorAll('.row');
+
+    rows.forEach((row, i) => {
+      if (i > 1) {
+        const title = row.querySelector('.col-1');
+        const firstElementChild = title.firstElementChild;
+
+        if (firstElementChild && firstElementChild === row.getElementsByTagName('strong')[0]) {
+          title.classList.add('sectionTitle');
+          row.classList.add('sectionHead');
+        } else {
+          title.classList.add('subSectionTitle');
+          row.classList.add('subSection');
+        }
+      }
+    });
+  }
+}
+
+function applyStylesBasedOnScreenSize(table) {
+  if (!(table instanceof Element)) {
+    return;
+  }
+
+  const screenWidth = window.innerWidth;
+  const subSectionRows = Array.from(table.getElementsByClassName('subSection'));
+  if (subSectionRows.length >0 ) {
+    const colsForTablet = subSectionRows[0].children.length - 1;
+    const percentage = 100 / colsForTablet;
+    const templateColumnsValue = `repeat(auto-fit, ${percentage}%)`;
+
+    subSectionRows.forEach(row => {
+      if (screenWidth > 859) {
+        row.style.gridTemplateColumns = `repeat(auto-fit, minmax(100px, 1fr))`;
+      } else if (screenWidth < 860) {
+        row.style.gridTemplateColumns = templateColumnsValue;
+      }
+    });
+  }
+}
+
+function formatMerchTable(table) {
+  const rows = table.querySelectorAll('.row');
+  const rowsNum = rows.length;
+
+  const firstRow = rows[0];
+  const colsInRow = firstRow.querySelectorAll('.col');
+  const colsInRowNum = colsInRow.length;
+
+  for (let i = colsInRowNum; i > 0; i--) {
+    const cols = table.querySelectorAll(`.col-${i}`);
+    for (let j = rowsNum - 1; j >= 0; j--) {
+      let currentCol = cols[j];
+      if (!currentCol.innerText && currentCol.children.length === 0) {
+        currentCol.classList.add('no-borders');
+      } else {
+        currentCol.classList.add('border-bottom');
+        break;
+      }
+    }
+  }
+}
+
+function handleHeading(table) {
+  const row1 = table.querySelector('.row-1');
+  const row2 = table.querySelector('.row-2');
+  const cols1 = row1.querySelectorAll('.col');
+  const cols2 = row2.querySelectorAll('.col');
+
+  cols2.forEach((col, i) => {
+    const isEmpty = col.innerHTML === "";
+
+    if (isEmpty) {
+      const nextCol = cols2[i + 1];
+      nextCol.classList.add('left-top-rounded');
+      col.classList.add('no-borders');
+      cols1[i].classList.add('no-borders');
+    } else {
+      const elements = col.children;
+      const hasElements = elements.length > 0;
+
+      if (!hasElements) {
+        const innerText = col.innerHTML;
+        col.innerHTML = `<p class="heading-title">${innerText}</p>`;
+      } else {
+        elements[0].classList.add('heading-title');
+
+        if (elements[1]) {
+          elements[1].classList.add('pricing');
+        }
+
+        decorateButtons(col, 'button-l');
+      }
+    }
+  });
+}
+
+function handleHovering(table) {
+  const row1 = table.querySelector('.row-1');
+  const colsInRowNum = row1.childElementCount;
+
+  const isMerchTable = table.classList.contains('merch');
+  const startValue = isMerchTable ? 1 : 2;
+
+  for (let i = startValue; i <= colsInRowNum; i++) {
+    const elements = table.querySelectorAll(`.col-${i}`);
+    elements.forEach(e => {
+      e.addEventListener('mouseover', () => handleMouseOver(elements, table, i));
+      e.addEventListener('mouseout', () => handleMouseOut(elements));
+    })
+  }
+}
+
+function handleMouseOver(elements, table, colNum) {
+  handleMouseOut(elements);
+
+  const secondRow = table.querySelector('.row-2');
+  const colClass = 'col-' + colNum;
+
+  elements.forEach(e => {
+    if (e.classList.contains('col-highlight') && e.innerText) {
+      const matchingCols = Array.from(e.classList).filter(className => className.startsWith(colClass));
+
+      matchingCols.forEach(className => {
+        const noTopBorderCol = secondRow.querySelector('.' + className);
+        noTopBorderCol.classList.add('no-top-border');
+      });
+    }
+
+    e.classList.add('hover');
+  });
+}
+
+function handleMouseOut(elements) {
+  elements.forEach(e => {
+    e.classList.remove('hover');
+    e.classList.remove('no-top-border');
+  })
+}
+
+function handleScrollEffect(table) {
+  const header = table.querySelector('.row-2');
+  const intercept = document.createElement('div');
+  intercept.setAttribute('data-observer-intercept', '');
+  header.insertAdjacentElement('beforebegin', intercept);
+  const observer = new IntersectionObserver(([entry]) => {
+    header.classList.toggle('active', !entry.isIntersecting);
+  });
+  observer.observe(intercept);
+}
+
+export function decorateButtons(el, size) {
+  const buttons = el.querySelectorAll('em a, strong a');
+  if (buttons.length === 0) return;
+  buttons.forEach((button) => {
+    const parent = button.parentElement;
+    const buttonType = parent.nodeName === 'STRONG' ? 'blue' : 'outline';
+    button.classList.add('con-button', buttonType);
+    if (size) button.classList.add(size); /* button-l, button-xl */
+    parent.insertAdjacentElement('afterend', button);
+    parent.remove();
+  });
+  const actionArea = buttons[0].closest('p, div');
+  if (actionArea) {
+    actionArea.classList.add('action-area');
+    actionArea.nextElementSibling?.classList.add('supplemental-text', 'body-xl');
+  }
+}
+
+window.addEventListener('resize', applyStylesBasedOnScreenSize);
