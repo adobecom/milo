50d34bd8ed6599f4ac7d8b99bc0100dd36cbebc4
glo77801 <glo77801@adobe.com>
2023-05-26 13:30:40 +0530

    updated code

--- libs/utils/sidekick.js
+++ libs/utils/sidekick.js
@@ -83,8 +83,4 @@ export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
   sk.addEventListener('custom:check-schema', checkSchemaListener);
   sk.addEventListener('custom:preflight', preflightListener);
   sk.addEventListener('published', addVersion);
-
-  window.skevent = {
-    addVersion
-  }
 }
