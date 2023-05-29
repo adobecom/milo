b413bedf4396fb4515f7dbc5bd3df7c13e342fa3
glo77801 <glo77801@adobe.com>
2023-05-29 10:59:01 +0530

    added some changes

--- libs/utils/sidekick.js
+++ libs/utils/sidekick.js
@@ -65,9 +65,12 @@ export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
     const headers = getAuthorizedRequestOptionSP({
       method: 'POST'
     });
-    await fetch(`${url}/Publish('Last Published version')`, {...headers});
-    event.detail.resolveMethod();
-    // navigator.sendBeacon(`${url}/Publish('Last Published version')`);
+    // await fetch(`${url}/Publish('Last Published version')`, {...headers});
+    const data = '';
+    console.log(headers);
+
+    const blob = new Blob([data], { type: 'text/plain' });
+    await navigator.sendBeacon(`${url}/Publish('Last Published version')`, blob, )
   };
 
   // Support for legacy manifest v2 - Delete once everyone is migrated to v3
