c79683a440853da571150e06851e2ae09f25059c
glo77801 <glo77801@adobe.com>
2023-05-30 15:45:59 +0530

    added some changes

--- libs/utils/sidekick.js
+++ libs/utils/sidekick.js
@@ -1,4 +1,5 @@
-import { connectWithSPRest, getAuthorizedRequestOptionSP } from '../../tools/loc/sharepoint.js';
+import { getConfig } from '../../tools/loc/config.js';
+import '../../tools/loc/lib/msal-browser.js';
 // loadScript and loadStyle are passed in to avoid circular dependencies
 export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
   let accessToken = '';
@@ -24,15 +25,40 @@ export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
     getModal(null, { id: 'preflight', content, closeEvent: 'closeModal' });
   };
 
-  // Support for legacy manifest v2 - Delete once everyone is migrated to v3
-  document.addEventListener('send-to-caas', async (e) => {
-    const { host, project, branch, repo, owner } = e.detail;
-    const { sendToCaaS } = await import('../../tools/send-to-caas/send-to-caas.js');
-    sendToCaaS({ host, project, branch, repo, owner }, loadScript, loadStyle);
-  });
+  const fetchAccessToken = async () => {
+    const { sprest } = await getConfig();
+      const msalClient = new msal.PublicClientApplication(sprest);
+      const loginRequest = {
+        scopes: ["https://adobe.sharepoint.com/.default"] // SharePoint API scope
+      };
+      const response = await msalClient.loginPopup(loginRequest);
+      accessToken = response.accessToken;
+  };
 
-  // connect with sharepoint
-  connectWithSPRest();
+  const getAuthorizedRequestOptionSP = ({
+    body = null,
+    json = true,
+    method = 'GET',
+  } = {}) => {
+    const bearer = `Bearer ${accessToken}`;
+    const headers = new Headers();
+    headers.append('Authorization', bearer);
+    if (json) {
+      headers.append('Accept', 'application/json; odata=nometadata');
+      headers.append('Content-Type', 'application/json;odata=verbose');
+    }
+  
+    const options = {
+      method,
+      headers,
+    };
+  
+    if (body) {
+      options.body = typeof body === 'string' ? body : JSON.stringify(body);
+    }
+  
+    return options;
+  };
 
   const addVersion = async (event) => {
     const url = `https://adobe.sharepoint.com/sites/adobecom/_api/web/GetFileByServerRelativeUrl('/sites/adobecom/CC/www${event.detail.data}.docx')`;
@@ -42,11 +68,24 @@ export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
     await fetch(`${url}/Publish('Last Published version')`, {...options, keepalive: true});
   };
 
+  // Support for legacy manifest v2 - Delete once everyone is migrated to v3
+  document.addEventListener('send-to-caas', async (e) => {
+    const { host, project, branch, repo, owner } = e.detail;
+    const { sendToCaaS } = await import('../../tools/send-to-caas/send-to-caas.js');
+    sendToCaaS({ host, project, branch, repo, owner }, loadScript, loadStyle);
+  });
+
+  // fetch sharepoint access token
+  fetchAccessToken();
+
+  const 
+
   const sk = document.querySelector('helix-sidekick');
 
   // Add plugin listeners here
   sk.addEventListener('custom:send-to-caas', sendToCaasListener);
   sk.addEventListener('custom:check-schema', checkSchemaListener);
   sk.addEventListener('custom:preflight', preflightListener);
+  sk.addEventListener('custom:addVersion', addVersion);
   sk.addEventListener('published', addVersion);
 }
