cdb2732f4cf2f27218b77fa88b894a67f8c3bd5f
Okan Sahin <osahin@adobe.com>
2023-05-31 14:18:13 +0200

    Unit tests - surface external requests

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -39,4 +39,35 @@ export default {
     defaultReporter({ reportTestResults: true, reportTestProgress: true }),
     customReporter(),
   ],
+  testRunnerHtml: (testFramework) => `
+    <html>
+      <head>
+        <script type="module">
+          const oldFetch = window.fetch;
+          window.fetch = async (resource, options) => {
+            if (!resource.startsWith("/") && !resource.startsWith('http://localhost')) {
+              console.error(
+                '** External resource fetch is disallowed in unit tests, please find a way to mock!',
+                resource
+              );
+            }
+            return oldFetch.call(window, resource, options);
+          };
+          const oldXHROpen = XMLHttpRequest.prototype.open;
+          XMLHttpRequest.prototype.open = async function (...args) {
+            let [method, url, asyn] = args;
+            if (!resource.startsWith("/") && url.startsWith('http://localhost')) {
+              console.error(
+                '** XMLHttpRequest request is disallowed in unit tests, please find a way to mock!',
+                url
+              );
+            }
+            return oldXHROpen.apply(this, args);
+          };
+        </script>
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
 };
