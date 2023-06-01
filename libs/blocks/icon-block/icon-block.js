6c04b2cd2bc74e8621df84fd82f15a63ecce4afa
Sonja Popovic <sonja.popovic@lilly021.com>
2023-06-01 13:55:31 +0200

    MWPW-129909-renamed variation to inline, added padding for action area, unit test

--- libs/blocks/icon-block/icon-block.js
+++ libs/blocks/icon-block/icon-block.js
@@ -17,7 +17,7 @@
 import { decorateBlockText, getBlockSize } from '../../utils/decorate.js';
 import { createTag } from '../../utils/utils.js';
 
-const variants = ['fullwidth', 'vertical', 'bio', 'icon-inline'];
+const variants = ['fullwidth', 'vertical', 'bio', 'inline'];
 const iconBlocks = {
   small: {
     [variants[0]]: ['m', 'm'],
@@ -29,11 +29,13 @@ const iconBlocks = {
     [variants[0]]: ['l', 'm'],
     [variants[1]]: ['m', 'm'],
     [variants[2]]: ['s', 's'],
+    [variants[3]]: ['s', 's'],
   },
   large: {
     [variants[0]]: ['xl', 'm'],
     [variants[1]]: ['m', 'm'],
     [variants[2]]: ['s', 's'],
+    [variants[3]]: ['s', 's'],
   },
 };
 
@@ -56,8 +58,7 @@ function decorateContent(el) {
     const size = getBlockSize(el, 2);
     const variant = [...variants].filter((v) => el.classList.contains(v))?.[0] ?? 'fullwidth';
     decorateBlockText(el, iconBlocks[size][variant]);
-
-    if (el.classList.contains('icon-inline')) {
+    if (el.classList.contains('inline')) {
      const textContent = el.querySelectorAll('.text-content > :not(.icon-area)');
       const secondColumn = createTag('div', {class:'second-column'});
       textContent.forEach((el) => {
