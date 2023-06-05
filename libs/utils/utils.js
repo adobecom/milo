ab0936423d15d5662692ec217188fb1c88265719
thi64146 <thi64146@adobe.com>
2023-06-05 10:51:06 -0600

    remove unnecessary check for length value

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -432,7 +432,7 @@ export function decorateImageLinks(a) {
     if (!img.alt || !(hasUrlWithPipe(img.alt))) return;
     const [source, alt] = img.alt.split('|');
     const textUrl = new URL(source.trim());
-    if (alt && alt.trim().length > 0) img.alt = alt.trim();
+    if (alt && alt.trim().length) img.alt = alt.trim();
     const pic = img.closest('picture');
     const picParent = pic.parentElement;
     const aTag = createTag('a', { href: textUrl, class: 'image-link' }, pic);
