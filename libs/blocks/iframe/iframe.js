9c4d415090ada7589e12e6955d9e9032c1c12a08
Jason Slavin <slavin@adobe.com>
2023-06-06 10:20:39 -0700

    MWPW-129094 Iframe Block Classes and Height Issue  (#753)
    * Allows for iframe block to pass classes to rendered counterpart. Adds a class for issue
    
    * Cr change, more specific class name
    
    ---------
    
    Co-authored-by: Honwai Wong <honstar.wong@gmail.com>

--- libs/blocks/iframe/iframe.js
+++ libs/blocks/iframe/iframe.js
@@ -2,11 +2,13 @@ import { createTag } from '../../utils/utils.js';
 
 export default function init(el) {
   const url = el.href ?? el.querySelector('a')?.href;
+  el.classList.remove('iframe');
+  const classes = [...el.classList].join(' ');
 
   if (!url) return;
 
-  const iframe = createTag('iframe', { src: url, allowfullscreen: true })
-  const embed = createTag('div', { class: 'milo-iframe' }, iframe);
+  const iframe = createTag('iframe', { src: url, allowfullscreen: true });
+  const embed = createTag('div', { class: `milo-iframe ${classes}` }, iframe);
 
   el.insertAdjacentElement('afterend', embed);
   el.remove();
