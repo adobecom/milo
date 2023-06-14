138e0fcfc9250599ce86773d81331570d14c4094
Laishram Bandana Devi <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-06-14 15:16:28 +0530

    fix: css fixes and error handling for empty list and creation failure

--- libs/blocks/version-history/version-history.js
+++ libs/blocks/version-history/version-history.js
@@ -4,6 +4,10 @@ import loginToSharePoint from '../../utils/deps/login.js';
 import { createHistoryTag } from './index.js';
 
 export default async function init(el) {
+  if (window.self === window.top) {
+    document.body.classList.add('in-page');
+  }
+
   render(
     html`<${View} loginToSharePoint=${loginToSharePoint} createHistoryTag=${createHistoryTag}/>`, el);
 }
