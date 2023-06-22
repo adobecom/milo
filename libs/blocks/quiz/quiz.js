fc36274be1c4da15262893f96ed0e42ac2428dac
Sabya <sabyasachi.exe@gmail.com>
2023-06-22 14:19:56 -0700

    MWPW-125274 - UAR Analytics (#871)
    * Refactoring question flow and adding analytics data
    
    * Adding button analytics data
    
    * Setting pageLoadHash in local storage
    
    * Refactoring
    
    * Adding guardrail around result parsing

--- libs/blocks/quiz/quiz.js
+++ libs/blocks/quiz/quiz.js
@@ -2,7 +2,7 @@ import { render, html, useEffect, useState } from '../../deps/htm-preact.js';
 import { getConfig, loadStyle } from '../../utils/utils.js';
 import { GetQuizOption } from './quizoption.js';
 import { DecorateBlockBackground, DecorateBlockForeground } from './quizcontainer.js';
-import { initConfigPathGlob, handleResultFlow, handleNext, transformToFlowData, getQuizData } from './utils.js';
+import { initConfigPathGlob, handleResultFlow, handleNext, transformToFlowData, getQuizData, getAnalyticsDataForBtn } from './utils.js';
 import StepIndicator from './stepIndicator.js';
 
 const { codeRoot } = getConfig();
@@ -22,24 +22,27 @@ const App = () => {
   const [currentStep, setCurrentStep] = useState(0);
   const [totalSteps, setTotalSteps] = useState(3);
   const [prevStepIndicator, setPrevStepIndicator] = useState([]);
+  const [nextQuizViewsExist, setNextQuizViewsExist] = useState(true);
+  const [urlParam, setUrlParam] = useState({});
+  const [btnAnalytics, setBtnAnalytics] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const [questions, datastrings] = await getQuizData();
+      const [questions, dataStrings] = await getQuizData();
       const qMap = {};
       questions.questions.data.forEach((question) => {
         qMap[question.questions] = question;
       });
 
       const strMap = {};
-      datastrings.questions.data.forEach((question) => {
+      dataStrings.questions.data.forEach((question) => {
         strMap[question.q] = question;
       });
 
       // initial quesiton to load - picking 1st one
       setUserFlow([questions.questions.data[0].questions]);
 
-      setStringData(datastrings);
+      setStringData(dataStrings);
       setQuestionData(questions);
       setStringQuestionList(strMap);
       setQuestionList(qMap);
@@ -54,48 +57,102 @@ const App = () => {
 
   useEffect(() => {
     if (userFlow.length) {
-      const currentflow = userFlow.shift();
-      if (!currentflow.length) {
-        console.log('No next view so setting select question to empty');
+      const currentFlow = userFlow.shift();
+      if (!currentFlow.length) {
+        return;
       }
-      setSelectedQuestion(questionList[currentflow] || []);
+      setSelectedQuestion(questionList[currentFlow] || []);
     }
   }, [userFlow, questionList]);
 
+  /**
+   * Updates the analytics data for the next button.
+   * Happens with each option click/tap.
+   * @returns {void}
+   */
+  useEffect(() => {
+    setBtnAnalytics(getAnalyticsDataForBtn(selectedQuestion, selectedCards));
+  }, [selectedQuestion, selectedCards]);
+
+  /**
+   * Handling the result flow when user has selected all the options.
+   * nextQuizViewsExist is set to false when the next button is clicked
+   * and there are no more views to show.
+   */
+  useEffect(() => {
+    if (!nextQuizViewsExist && userSelection.length) {
+      handleResultFlow(transformToFlowData(userSelection));
+    }
+  }, [userSelection, nextQuizViewsExist]);
+
+  /**
+   *  Updates the url param when user selects the options.
+   *  Happens with each option click/tap.
+   */
+  useEffect(() => {
+    const cardValues = Object.getOwnPropertyNames(selectedCards);
+    if (selectedQuestion) {
+      setUrlParam((prevUrlParam) => {
+        const newParam = { ...prevUrlParam };
+        if (selectedQuestion && cardValues.length === 0) {
+          delete newParam[selectedQuestion.questions];
+        } else if (!urlParam[selectedQuestion.questions]) {
+          newParam[selectedQuestion.questions] = new Set(
+            [...(urlParam[selectedQuestion.questions] || []), ...cardValues],
+          );
+        } else {
+          newParam[selectedQuestion.questions] = new Set(cardValues);
+        }
+        return newParam;
+      });
+    }
+  }, [selectedQuestion, selectedCards, JSON.stringify(urlParam)]);
+
+  /**
+   * Updates the url when the url param is updated as part of the option click.
+   */
+  useEffect(() => {
+    if (Object.keys(urlParam).length > 0) {
+      const urlParamList = Object.keys(urlParam).map((key) => {
+        const paramList = [...urlParam[key]];
+        if (paramList.length) {
+          return `${key}=${paramList.join(',')}`;
+        }
+      }).filter((item) => !!item);
+      window.history.pushState('', '', `?${urlParamList.join('&')}`);
+    } else {
+      window.history.pushState('', '', '?');
+    }
+  }, [urlParam]);
+
+  /**
+   * Updates the prevStepIndicator when user selects the options.
+   */
   useEffect(() => {
     if (userSelection.length) {
-      window.history.pushState('', '', buildQParam(userSelection));
+      setPrevStepIndicator(userSelection.map((_, index) => index));
     }
-  }, [userSelection.length]);
+  }, [userSelection]);
 
-  const buildQParam = (selection) => {
-    let params = '';
-    selection.forEach((sel) => {
-      if (params) {
-        params = `${params}&${sel.selectedQuestion.questions}=${Object.getOwnPropertyNames(sel.selectedCards)}`;
-      } else {
-        params = `?${sel.selectedQuestion.questions}=${Object.getOwnPropertyNames(sel.selectedCards)}`;
-      }
-    });
-    return params;
-  };
-
+  /**
+   * Handler of the next button click. Checks whether any next view exists or not.
+   * Takes care of the user flow and updates the state accordingly.
+   * @param {Object} selCards - Selected cards
+   * @returns {void}
+   */
   const handleOnNextClick = (selCards) => {
     const { nextQuizViews } = handleNext(questionData, selectedQuestion, selCards, userFlow);
     const nextQuizViewsLen = nextQuizViews.length;
-    setCurrentStep(currentStep + 1);
-    updateUserSelection((userSelection) => {
-      const updatedUserSelection = [...userSelection, { selectedQuestion, selectedCards }];
-      setPrevStepIndicator(updatedUserSelection.map((_, index) => index));
-      return updatedUserSelection;
-    });
 
+    setNextQuizViewsExist(!!nextQuizViewsLen);
+    setCurrentStep(currentStep + 1);
+    updateUserSelection((userSelection) => (
+      [...userSelection, { selectedQuestion, selectedCards }]));
     setSelectedCards({});
+    setSelectedQuestion(null);
     setCountOfSelectedCards(0);
 
-    if (!nextQuizViewsLen) {
-      handleResultFlow(transformToFlowData(userSelection));
-    } else {
+    if (nextQuizViewsLen) {
       setUserFlow(nextQuizViews);
       if (nextQuizViewsLen > 1) {
         setTotalSteps(totalSteps + 1);
@@ -105,6 +162,11 @@ const App = () => {
     }
   };
 
+  /**
+   * Handler of the option click. Updates the selected cards and the count of selected cards.
+   * @param {Object} option - Selected option
+   *  @returns {void}
+   * */
   const onOptionClick = (option) => () => {
     const newState = { ...selectedCards };
 
@@ -164,7 +226,8 @@ const App = () => {
                       selectedCards=${selectedCards}
                       onOptionClick=${onOptionClick}
                       getOptionsIcons=${getOptionsIcons}
-                      handleOnNextClick=${handleOnNextClick} />
+                      handleOnNextClick=${handleOnNextClick}
+                      btnAnalyticsData=${btnAnalytics}/>
                       <${StepIndicator} 
                       currentStep=${currentStep} 
                       totalSteps=${totalSteps} 
--- libs/blocks/quiz/utils.js
+++ libs/blocks/quiz/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { getMetadata } from '../section-metadata/section-metadata.js';
 import { getConfig } from '../../utils/utils.js';
 
@@ -5,7 +6,7 @@ const QUESTIONS_EP_NAME = 'questions.json';
 const STRINGS_EP_NAME = 'strings.json';
 const RESULTS_EP_NAME = 'results.json';
 
-let getConfigPath; let getQuizKey; let getAnalyticsType; let getAnalyticsQuiz; let metaData;
+let configPath; let quizKey; let analyticsType; let analyticsQuiz; let metaData;
 const { locale } = getConfig();
 
 const initConfigPath = (roolElm) => {
@@ -15,8 +16,8 @@ const initConfigPath = (roolElm) => {
 };
 
 const initQuizKey = () => {
-  getQuizKey = metaData.storagepath?.text;
-  return locale.ietf ? `${getQuizKey}-${locale.ietf}` : getQuizKey;
+  quizKey = metaData.storagepath?.text;
+  return locale.ietf ? `${quizKey}-${locale.ietf}` : quizKey;
 };
 
 const initAnalyticsType = () => metaData['analytics-type']?.text;
@@ -24,24 +25,24 @@ const initAnalyticsType = () => metaData['analytics-type']?.text;
 const initAnalyticsQuiz = () => metaData['analytics-quiz']?.text;
 
 async function fetchContentOfFile(path) {
-  const response = await fetch(getConfigPath(path));
+  const response = await fetch(configPath(path));
   return response.json();
 }
 
 export const initConfigPathGlob = (rootElement) => {
   metaData = getMetadata(rootElement);
-  getConfigPath = initConfigPath(rootElement);
-  getQuizKey = initQuizKey(rootElement);
-  getAnalyticsType = initAnalyticsType();
-  getAnalyticsQuiz = initAnalyticsQuiz();
+  configPath = initConfigPath(rootElement);
+  quizKey = initQuizKey(rootElement);
+  analyticsType = initAnalyticsType();
+  analyticsQuiz = initAnalyticsQuiz();
 };
 
 export const getQuizData = async () => {
   try {
-    const [questions, datastrings] = await Promise.all(
+    const [questions, dataStrings] = await Promise.all(
       [fetchContentOfFile(QUESTIONS_EP_NAME), fetchContentOfFile(STRINGS_EP_NAME)],
     );
-    return [questions, datastrings];
+    return [questions, dataStrings];
   } catch (ex) {
     console.log('Error while fetching data : ', ex);
   }
@@ -67,12 +68,25 @@ export const handleResultFlow = async (answers = []) => {
     umbrellaProduct = resultData.matchedResults[0]['umbrella-result'];
   }
 
-  storeResultInLocalStorage(resultData, resultResources, primaryProductCodes, secondaryProductCodes, umbrellaProduct);
-
+  storeResultInLocalStorage(
+    answers,
+    resultData,
+    resultResources,
+    primaryProductCodes,
+    secondaryProductCodes,
+    umbrellaProduct,
+  );
   window.location.href = getRedirectUrl(destinationPage, primaryProductCodes, answers);
 };
 
-const storeResultInLocalStorage = (resultData, resultResources, primaryProducts, secondaryProductCodes, umbrellaProduct) => {
+const storeResultInLocalStorage = (
+  answers,
+  resultData,
+  resultResources,
+  primaryProducts,
+  secondaryProductCodes,
+  umbrellaProduct,
+) => {
   const nestedFrags = resultData.matchedResults[0]['nested-fragments'];
   const structureFrags = resultData.matchedResults[0]['basic-fragments'];
 
@@ -83,13 +97,30 @@ const storeResultInLocalStorage = (resultData, resultResources, primaryProducts,
     primaryProducts,
     secondaryProducts: secondaryProductCodes,
     umbrellaProduct,
-    basicFragments: structuredFragments(structureFragsArray, resultResources, primaryProducts, umbrellaProduct),
-    nestedFragments: nestedFragments(nestedFragsArray, resultResources, primaryProducts, secondaryProductCodes, umbrellaProduct)
+    basicFragments: structuredFragments(
+      structureFragsArray,
+      resultResources,
+      primaryProducts,
+      umbrellaProduct,
+    ),
+    nestedFragments: nestedFragments(
+      nestedFragsArray,
+      resultResources,
+      primaryProducts,
+      secondaryProductCodes,
+      umbrellaProduct,
+    ),
+    pageloadHash: getAnalyticsDataForLocalStorage(answers),
   };
-  localStorage.setItem(getQuizKey, JSON.stringify(resultToDelegate));
+  localStorage.setItem(quizKey, JSON.stringify(resultToDelegate));
 };
 
-const structuredFragments = (structureFragsArray, resultResources, primaryProducts, umbrellaProduct) => {
+const structuredFragments = (
+  structureFragsArray,
+  resultResources,
+  primaryProducts,
+  umbrellaProduct,
+) => {
   let structureFragments = [];
   structureFragsArray.forEach((frag) => {
     frag = frag.trim();
@@ -108,7 +139,13 @@ const structuredFragments = (structureFragsArray, resultResources, primaryProduc
   return structureFragments;
 };
 
-const nestedFragments = (nestedFragsArray, resultResources, primaryProducts, secondaryProductCodes, isUmbrella) => {
+const nestedFragments = (
+  nestedFragsArray,
+  resultResources,
+  primaryProducts,
+  secondaryProductCodes,
+  isUmbrella,
+) => {
   const nestedObject = {};
   nestedFragsArray.forEach((frag) => {
     const fragKey = frag.trim();
@@ -134,18 +171,16 @@ const nestedFragments = (nestedFragsArray, resultResources, primaryProducts, sec
   return nestedObject;
 };
 
-const getRedirectUrl = (destinationPage, primaryProducts) => `${destinationPage}?primary=${primaryProducts}&quizKey=${getQuizKey}`;
-
-const buildQueryParam = (answers) => answers.reduce((str, [questionId, answersFromUser], i) => `${str + (i > 0 ? '&' : '') + questionId}=${answersFromUser.join(',')}`, '');
+const getRedirectUrl = (destinationPage, primaryProducts) => `${destinationPage}?primary=${primaryProducts}&quizKey=${quizKey}`;
 
 const parseResultData = async (answers) => {
-  const results  = await fetchContentOfFile(RESULTS_EP_NAME);
+  const results = await fetchContentOfFile(RESULTS_EP_NAME);
   const filteredResults = results.result.data.reduce(
     (resultObj, resultMap) => {
       let hasMatch = false;
       const resultRow = Object.entries(resultMap);
 
-      for (let i = 0; i < resultRow.length; i++) {
+      for (let i = 0; i < resultRow.length; i += 1) {
         const key = resultRow[i][0];
         const val = resultRow[i][1];
 
@@ -235,21 +270,22 @@ const findMatchForSelections = (results, selections) => {
     return defaultResult;
   }
 
-  const compundResults = results.find((destination) => {
+  const compoundResults = results.find((destination) => {
     if (destination.result.indexOf('&') !== -1 && destination.result.split('&').length === userSelectionLen) {
       return destination;
     }
   });
 
-  const productList = compundResults.result.split('&');
-  const isCompoundProductsMatched = selections.primary.every(
-    (product, index) => productList[index].includes(product),
-  );
+  const productList = compoundResults !== undefined ? compoundResults.result.split('&') : [];
 
-  if (isCompoundProductsMatched) {
-    recommendations.push(compundResults);
-
-    return recommendations;
+  if (productList.length) {
+    const isCompoundProductsMatched = selections.primary.every(
+      (product, index) => productList[index].includes(product),
+    );
+    if (isCompoundProductsMatched) {
+      recommendations.push(compoundResults);
+      return recommendations;
+    }
   }
 
   return defaultResult;
@@ -271,7 +307,7 @@ export const handleNext = (questionsData, selectedQuestion, userInputSelections,
       (nextItem) => nextItem.options === selection,
     );
 
-    getAllSelectedQuestionsRelatedOptions.forEach(( { options, next }) => {
+    getAllSelectedQuestionsRelatedOptions.forEach(({ options, next }) => {
       if (options === selection) {
         const flowStepsList = next.split(',');
 
@@ -315,6 +351,25 @@ export const handleNext = (questionsData, selectedQuestion, userInputSelections,
 };
 
 export const transformToFlowData = (userSelection) => {
-  const flowData = userSelection.map(({ selectedCards, selectedQuestion}) => [selectedQuestion.questions, Object.keys(selectedCards)]);
+  const flowData = userSelection.map(({ selectedCards, selectedQuestion }) => [
+    selectedQuestion.questions, Object.keys(selectedCards)]);
   return flowData;
+};
+
+export const getAnalyticsDataForBtn = (selectedQuestion, selectedCards) => {
+  const selectedCardNames = Object.keys(selectedCards);
+  if (selectedCardNames.length > 0) {
+    const btnAnalytics = `Filters|${analyticsType}|${selectedQuestion.questions}/${selectedCardNames.join('/')}`;
+    return btnAnalytics;
+  }
+};
+
+const getAnalyticsDataForLocalStorage = (answers) => {
+  let formattedAnswerString = '';
+  answers.forEach((answer) => {
+    const eachAnswer = `${answer[0]}/${answer[1].join('/')}`;
+    formattedAnswerString = formattedAnswerString === '' ? eachAnswer : formattedAnswerString.concat('|', eachAnswer);
+  });
+  const analyticsHash = `type=${analyticsType}&quiz=${analyticsQuiz}&selectedOptions=${formattedAnswerString}`;
+  return analyticsHash;
 };
