72f8bb919e3eed07ed093f737d47770639f011a1
Victor Hargrave <vhargrave@adobe.com>
2023-07-11 16:37:23 +0200

    MWPW-133591 rename input to action in action test file

--- test/utils/action.test.js
+++ test/utils/action.test.js
@@ -3,7 +3,7 @@ import { expect } from '@esm-bundle/chai';
 import { debounce } from "../../libs/utils/action.js";
 
 
-describe('Input', () => {
+describe('Action', () => {
     it('Debounces callback correctly', async () => {
         const clock = sinon.useFakeTimers({
             toFake: ['setTimeout'],
