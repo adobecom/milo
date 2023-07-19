3e3dd9beccc4bdb56df3bad7c0dcf11f83f13d98
Sean Choi <seanchoi@adobe.com>
2023-07-19 07:12:26 -0600

    Test update according to the PR review.

--- test/blocks/table/table.test.js
+++ test/blocks/table/table.test.js
@@ -1,6 +1,6 @@
 import { readFile, sendMouse, sendKeys } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
-import { delay } from '../../helpers/waitfor.js';
+import { delay, waitForElement } from '../../helpers/waitfor.js';
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 const { default: init } = await import('../../../libs/blocks/table/table.js');
@@ -19,11 +19,15 @@ describe('table and tablemetadata', () => {
       expect(table.querySelector('.row-heading')).to.exist;
     });
 
-    it('click expand icon', async () => {
+    it('expand icon event by mouse click', () => {
       const expandIcon = table.querySelector('.icon.expand');
       expect(expandIcon.ariaExpanded).to.be.equal('true');
       expandIcon.parentElement.click();
       expect(expandIcon.ariaExpanded).to.be.equal('false');
+    });
+
+    it('expand icon event by keyboard', async () => {
+      const expandIcon = table.querySelector('.icon.expand');
       expandIcon.parentElement.focus();
       await sendKeys({ type: 'Enter' });
       expect(expandIcon.ariaExpanded).to.be.equal('false');
@@ -32,7 +36,11 @@ describe('table and tablemetadata', () => {
     });
 
     it('hovering-test', async () => {
-      const headingCol = document.querySelector('.row-heading .col:not(.hidden)');
+      const headingCol = table.querySelector('.row-heading .col:not(.hidden)');
+      const sectionHeads = table.querySelectorAll('.section-head');
+      const lastSectionHead = sectionHeads[sectionHeads.length - 1];
+      const lastExpandIcon = lastSectionHead.querySelector('.icon.expand');
+      lastExpandIcon.setAttribute('aria-expanded', 'false');
       await sendMouse({
         type: 'move',
         position: [10, (headingCol?.offsetTop ?? 0) + 1],
@@ -43,27 +51,33 @@ describe('table and tablemetadata', () => {
     it('mobile test', async () => {
       window.innerWidth = 760;
       window.dispatchEvent(new Event('resize'));
-      await delay(700);
-      expect(document.querySelector('.filters')).to.be.exist;
+      const filters = await waitForElement('.filters');
+      const col5 = table.querySelector('.col-5');
+      expect(filters).to.be.exist;
+      expect(col5).to.be.null;
     });
 
-    it('filter test', async () => {
+    it('filter test: for case of both filter poiting same options', async () => {
       const filters = document.querySelectorAll('.filters select');
       const options = document.querySelectorAll('.filters select option');
-
-      // for case of both filter poiting same options.
       options[1].selected = true;
       filters[0].dispatchEvent(new Event('change', { bubbles: true }));
       await delay(500);
       expect(filters[0].value).to.equal('2');
+    });
 
-      // for case of order change
+    it('filter test:for case of order change', async () => {
+      const filters = document.querySelectorAll('.filters select');
+      const options = document.querySelectorAll('.filters select option');
       options[3].selected = true;
       filters[0].dispatchEvent(new Event('change', { bubbles: true }));
       await delay(500);
       expect(filters[0].value).to.equal('4');
+    });
 
-      // for merch filter test
+    it('filter test: merch filter test', async () => {
+      const filters = document.querySelectorAll('.filters select');
+      const options = document.querySelectorAll('.filters select option');
       options[10].selected = true;
       filters[2].dispatchEvent(new Event('change', { bubbles: true }));
       await delay(500);
@@ -73,8 +87,8 @@ describe('table and tablemetadata', () => {
     it('back to desktop test', async () => {
       window.innerWidth = 1200;
       window.dispatchEvent(new Event('resize'));
-      await delay(700);
-      expect(document.querySelector('.filters')).to.be.exist;
+      const col5 = await waitForElement('.table .col-5');
+      expect(col5).to.be.exist;
     });
   });
 });
