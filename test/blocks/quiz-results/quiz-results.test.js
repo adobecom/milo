ee8ace5b0ddaf300b6ae81e7f41c09bf08115549
Cody Lloyd <119891065+colloyd@users.noreply.github.com>
2023-07-21 12:03:38 -0600

    MWPW-133268 - Rename uar-results to quiz-results (#962)
    * renamed all block files and references
    * updated unit tests
    * updated debug query params
    
    Resolves: [MWPW-133268](https://jira.corp.adobe.com/browse/MWPW-133268)
    
    **Test URLs:**
    - Before: https://main--milo--adobecom.hlx.page/?martech=off
    - After: https://<branch>--milo--adobecom.hlx.page/?martech=off

--- test/blocks/quiz-results/quiz-results.test.js
+++ test/blocks/quiz-results/quiz-results.test.js
@@ -7,48 +7,48 @@ window.lana = { log: stub() };
 localStorage.clear();
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
-const { default: init, loadingErrorText } = await import('../../../libs/blocks/uar-results/uar-results.js');
-const { default: mockData } = await import('./mocks/uar-results.mock-data.js');
+const { default: init, loadingErrorText } = await import('../../../libs/blocks/quiz-results/quiz-results.js');
+const { default: mockData } = await import('./mocks/quiz-results.mock-data.js');
 
-describe('UAR Results', () => {
+describe('Quiz Results', () => {
   it('Doesnt load data without local storage', async () => {
     const el = document.body.querySelector('.basic');
     localStorage.clear();
 
-    await init(el, 'uar', 'uar-test');
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     expect(window.lana.log.args[0][0]).to.equal(`${loadingErrorText} local storage missing`);
   });
   it('Doesnt load data without basicFragments in local storage', async () => {
     const el = document.body.querySelector('.basic-one');
-    localStorage.setItem('uar-test', JSON.stringify(mockData.mockOne));
+    localStorage.setItem('quiz-result-test', JSON.stringify(mockData.mockOne));
 
-    await init(el, 'uar', 'uar-test');
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     expect(window.lana.log.args[1][0]).to.equal(`${loadingErrorText} Basic fragments are missing`);
   });
   it('Loads basic fragments', async () => {
     const el = document.body.querySelector('.basic-one');
-    localStorage.setItem('uar-test', JSON.stringify(mockData.mockTwo));
+    localStorage.setItem('quiz-result-test', JSON.stringify(mockData.mockTwo));
 
-    await init(el, 'uar', 'uar-test');
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     await delay(700);
     expect(el.querySelector('h1')).to.be.exist;
   });
   it('Loads nested fragments', async () => {
     const el = document.body.querySelector('.nested-one');
-    localStorage.setItem('uar-test', JSON.stringify(mockData.mockTwo));
-    await init(el, 'uar', 'uar-test');
+    localStorage.setItem('quiz-result-test', JSON.stringify(mockData.mockTwo));
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     await delay(700);
     expect(el.querySelector('h2')).to.be.exist;
   });
   it('Sets style values', async () => {
     const el = document.body.querySelector('.nested-two');
-    localStorage.setItem('uar-test', JSON.stringify(mockData.mockTwo));
+    localStorage.setItem('quiz-result-test', JSON.stringify(mockData.mockTwo));
 
-    await init(el, 'uar', 'uar-test');
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     await delay(700);
     expect(el.classList.contains('section')).to.be.true;
@@ -56,9 +56,9 @@ describe('UAR Results', () => {
   });
   it('Sets analytics customHash', async () => {
     const el = document.body.querySelector('.basic-two');
-    localStorage.setItem('uar-test', JSON.stringify(mockData.mockThree));
+    localStorage.setItem('quiz-result-test', JSON.stringify(mockData.mockThree));
 
-    await init(el, 'uar', 'uar-test');
+    await init(el, 'quiz-results', 'quiz-result-test');
 
     /* eslint-disable no-underscore-dangle */
     expect(window.alloy_all.data._adobe_corpnew.digitalData.page.pageInfo.customHash).to.equal('test analytics value');
