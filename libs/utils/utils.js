b5658aeff2dae9f060629d526caab1b07c63837f
Chris Peyer <chrischrischris@users.noreply.github.com>
2023-07-26 07:27:18 -0700

    MWPW-131707 Get correct region for caas autolang (#991)

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -145,7 +145,9 @@ export function getLocale(locales, pathname = window.location.pathname) {
     locale.prefix = `/${localeString}/${split[2]}`;
     return locale;
   }
-  locale.prefix = locale.ietf === 'en-US' ? '' : `/${localeString}`;
+  const isUS = locale.ietf === 'en-US';
+  locale.prefix = isUS ? '' : `/${localeString}`;
+  locale.region = isUS ? 'us' : localeString.split('_')[0];
   return locale;
 }
 
