dd485a0bc85c16628a99e80cd5c4b74f61198c10
Gen Watanabe <gwatanab@adobe.com>
2023-07-26 14:12:53 +0900

    MWPW-134209 Enable excel format localization

--- tools/loc/utils.js
+++ tools/loc/utils.js
@@ -51,6 +51,10 @@ export function getDocPathFromUrl(url) {
   if (!path) {
     return undefined;
   }
+  if (path.endsWith('.json')) {
+    path = path.slice(0, -5);
+    return `${path}.xlsx`;
+  }
   if (path.endsWith('/')) {
     path += 'index';
   } else if (path.endsWith('.html')) {
