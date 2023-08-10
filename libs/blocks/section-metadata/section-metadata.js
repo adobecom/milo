321d7936c3a3c0133ccfd0913e7cae1b4fa3aacb
Sean Archibeque <sean.archibeque@gmail.com>
2023-08-10 14:12:01 -0600

    Update libs/blocks/section-metadata/section-metadata.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- libs/blocks/section-metadata/section-metadata.js
+++ libs/blocks/section-metadata/section-metadata.js
@@ -83,7 +83,7 @@ function handleLayout(text, section) {
 }
 
 export const getMetadata = (el) => [...el.childNodes].reduce((rdx, row) => {
-    if (row.children) {
+  if (row.children) {
       const key = row.children[0].textContent.trim().toLowerCase();
       const content = row.children[1];
     const text = content.textContent.trim().toLowerCase();
