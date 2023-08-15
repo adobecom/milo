a73aa2f1df3090ae540ea3078aaf914ade1bfa29
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2023-08-15 13:00:44 -0700

    incorporate review comments

--- libs/blocks/video/video.js
+++ libs/blocks/video/video.js
@@ -1,20 +1,18 @@
+import { applyHoverPlay } from '../../utils/decorate.js';
+
 function getAttrs(hash) {
   const isAutoplay = hash?.includes('autoplay');
   const isAutoplayOnce = hash?.includes('autoplay1');
   const playOnHover = hash.includes('hoverplay');
   let attrs = '';
-  switch (true) {
-    case isAutoplay === true && isAutoplayOnce === false:
-      attrs = 'playsinline autoplay loop muted';
-      break;
-    case playOnHover === true && isAutoplayOnce === true:
-      attrs = 'playsinline autoplay muted data-hoverplay';
-      break;
-    case isAutoplayOnce === true:
-      attrs = 'playsinline autoplay muted';
-      break;
-    default:
-      attrs = 'playsinline controls';
+  if (isAutoplay && !isAutoplayOnce) {
+    attrs = 'playsinline autoplay loop muted';
+  } else if (playOnHover && isAutoplayOnce) {
+    attrs = 'playsinline autoplay muted data-hoverplay';
+  } else if (isAutoplayOnce) {
+    attrs = 'playsinline autoplay muted';
+  } else {
+    attrs = 'playsinline controls';
   }
   return attrs;
 }
@@ -27,13 +25,7 @@ export default function init(a) {
       </video>`;
   a.insertAdjacentHTML('afterend', video);
   const videoElem = document.querySelector('video');
-  if (attrs.includes('data-hoverplay')) {
-    videoElem.addEventListener('mouseenter', () => {
-      videoElem.play();
-    });
-    videoElem.addEventListener('mouseleave', () => {
-      videoElem.pause();
-    });
-  }
+  applyHoverPlay(videoElem);
   a.remove();
 }
+
