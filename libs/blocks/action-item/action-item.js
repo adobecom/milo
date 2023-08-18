568e645e8ece215fc33503aa7b82a36639fd101f
Sartxi <sean.archibeque@gmail.com>
2023-08-18 11:14:18 -0600

    fix tests and final cleanup

--- libs/blocks/action-item/action-item.js
+++ libs/blocks/action-item/action-item.js
@@ -9,35 +9,35 @@ function floatIcon(picture, icon) {
 function floatButton(picture, content) {
   decorateButtons(content);
   const btn = content.querySelector('.con-button');
-  if (!btn) return;
   picture.classList.add('dark');
-  picture.appendChild(btn);
+  if (btn) picture.appendChild(btn);
 }
 
 function getLinkAttrs(link) {
   const anchor = link.querySelector('a');
-  if (!anchor) return {};
-  return Object.fromEntries([...anchor.attributes].map((attr) => [attr.name, attr.value]));
+  let attrs = {};
+  if (anchor) {
+    attrs = Object.fromEntries([...anchor.attributes].map((attr) => [attr.name, attr.value]));
+  }
+  return attrs;
 }
 
 function getContent(el, variants, link) {
   const pictures = el.querySelectorAll('picture');
-  const mainPic = pictures[0];
-  const picture = mainPic?.parentElement;
+  const picture = pictures[0]?.parentElement;
   picture?.classList.add('main-image');
   const wrapLink = link && !variants.contains('float-button');
   const tag = wrapLink ? 'a' : 'div';
-  let attrs = wrapLink ? getLinkAttrs(link) : {};
+  const attrs = wrapLink ? getLinkAttrs(link) : {};
   if (variants.contains('float-icon') && pictures.length > 1) floatIcon(picture, pictures[1]);
   if (variants.contains('float-button') && link) floatButton(picture, link);
-  if (variants.contains('static-links')) attrs = { ...attrs, class: 'static' };
-  const content = createTag(tag, { ...attrs }, picture.closest('div'));
+  if (variants.contains('static-links')) attrs.class = 'static';
+  const content = createTag(tag, attrs, picture.closest('div'));
   return content;
 }
 
 export default function init(el) {
   const elems = el.querySelectorAll(':scope > div');
-  if (!elems.length) return;
   const link = elems.length > 1 ? elems[elems.length - 1] : null;
   const content = getContent(elems[0], el.classList, link);
   el.replaceChildren(content);
--- test/blocks/action-item/action-item.test.js
+++ test/blocks/action-item/action-item.test.js
@@ -3,45 +3,30 @@ import { expect } from '@esm-bundle/chai';
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 const { default: init } = await import('../../../libs/blocks/action-item/action-item.js');
-const variants = ['float-button', 'float-icon'];
+const variants = ['default', 'float-icon', 'float-button', 'static-links', 'no-link-text'];
 
-describe('action item blocks', () => {
-  const actionItems = document.querySelectorAll('.action-item');
-  actionItems.forEach((actionItem) => {
-    init(actionItem);
+const actionItems = document.querySelectorAll('.action-item');
+actionItems.forEach((actionItem) => {
+  init(actionItem);
+});
+describe('action item', () => {
+  it(`${variants[0]} has an image`, () => {
+    const image = actionItems[0].querySelector('.main-image');
+    expect(image).to.exist;
+  });
 
-    const variantIndex = variants.findIndex((v) => actionItem.classList.contains(v));
-    const variant = variantIndex >= 0 ? variants[variantIndex] : 'default';
+  it(`${variants[1]} has floated icon`, () => {
+    const icon = actionItems[1].querySelector('.floated-icon');
+    expect(icon).to.exist;
+  });
 
-    describe(`action item ${variant}`, () => {
-      it('has an image', () => {
-        const image = actionItem.querySelector('.main-image');
-        expect(image).to.exist;
-      });
+  it(`${variants[2]} has floated button`, () => {
+    const button = actionItems[2].querySelector('.con-button');
+    expect(button).to.exist;
+  });
 
-      it('has an link', () => {
-        const link = actionItem.querySelector('a');
-        expect(link).to.exist;
-      });
-
-      it('has text', () => {
-        const text = actionItem.querySelector('p');
-        expect(text).to.exist;
-      });
-
-      if (variant === variants[0]) {
-        it('has floated button', () => {
-          const button = actionItem.querySelector('.con-button');
-          expect(button).to.exist;
-        });
-      }
-
-      if (variant === variants[1]) {
-        it('has floated icon', () => {
-          const icon = actionItem.querySelector('.floated-icon');
-          expect(icon).to.exist;
-        });
-      }
-    });
+  it(`${variants[3]} has static links`, () => {
+    const link = actionItems[3].querySelector('a.static');
+    expect(link).to.exist;
   });
 });
