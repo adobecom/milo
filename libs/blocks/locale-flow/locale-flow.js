240f5036fd08e5846976ef627a536c38c1a482fd
Raghav Sharma <raghavs@adobe.com>
2023-08-22 23:01:26 +0530

    Add content to localeflow modal

--- libs/blocks/locale-flow/locale-flow.js
+++ libs/blocks/locale-flow/locale-flow.js
@@ -3,4 +3,5 @@ import { createTag } from '../../utils/utils.js';
 export default (block) => {
   const content = createTag('div', {class: 'locale-flow-form'});
   content.textContent = "The locale flow form goes here. We'll take this information and send it off to the localization service"
+  block.insertAdjacentElement('afterbegin', content);
 }; 
