009b68fc35e600a4778b1a473d48e8f3ac53f98f
Raghav Sharma <raghavs@adobe.com>
2023-08-25 00:05:14 +0530

    What to do after a response

--- libs/blocks/locale-flow/locale-flow.js
+++ libs/blocks/locale-flow/locale-flow.js
@@ -43,19 +43,36 @@ const createForm = () => {
 
   submitButton.addEventListener('click', async (event) => {
     event.preventDefault();
+    submitButton.disabled = true;
     const url = 'http://localhost:80/translate';
-    const data = {
+    const body = {
       url: `${window.location.origin}${window.location.pathname}.plain.html`,
       selectedLocales
     };
     const resp = await fetch(url, {
       method: 'POST',
-      body: JSON.stringify(data),
+      body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json"
       }
     });
     const response = resp.json();
+    const { status, data } = response;
+    if (response.status === 200) {
+      const downloads = createTag('div', { class: 'downloads' });
+      const t = createTag('h4', { class: 'title' });
+      t.textContent = "Your translated documents are ready";
+      downloads.appendChild(t);
+      selectedLocales.forEach(loc => {
+        const link = createTag('a');
+        const fail = createTag('span', { class: 'fail' });
+        link.textContent = `Download the ${loc} translation`;
+        link.href = data[loc].translatedDocUrl;
+        fail.textContent = `The ${loc} translation failed`;
+        downloads.appendChild(data[loc].success ? link : fail);
+      });
+      form.replaceChildren(downloads);
+    }
   });
 
   form.appendChild(localesContainer);
