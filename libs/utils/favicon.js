0103f1c3f757ed97c8a0720fb5430bba114d1f97
Hayley Parra <h@hgpa.tv>
2023-08-29 14:01:50 -0700

    Add new behavior

--- libs/utils/favicon.js
+++ libs/utils/favicon.js
@@ -1,11 +1,28 @@
-export default function loadFavicon({ getConfig, getMetadata }) {
-  const { codeRoot } = getConfig();
-  const name = getMetadata('favicon') || 'favicon';
-  const favBase = `${codeRoot}/img/favicons/${name}`;
+export const FAVICON_TYPES = {
+  gif: 'image/gif',
+  ico: 'image/x-icon',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+};
 
+export function getFileExt(str) {
+  return str.toLowerCase().split('.').pop();
+}
+
+export default function loadFavicon({ getConfig, getMetadata }) {
   const favicon = document.head.querySelector('link[rel="icon"]');
-  const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png">
-                <link rel="manifest" href="${favBase}.webmanifest">`;
-  favicon.insertAdjacentHTML('afterend', tags);
-  favicon.href = `${favBase}.ico`;
+  const { codeRoot } = getConfig();
+
+  const faviconVal = getMetadata('favicon') || 'favicon';
+  const ext = getFileExt(faviconVal);
+  if (ext && ext in FAVICON_TYPES) {
+    favicon.href = faviconVal;
+    favicon.type = FAVICON_TYPES[ext];
+  } else {
+    const favBase = `${codeRoot}/img/favicons/${faviconVal}`;
+    const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png">
+                  <link rel="manifest" href="${favBase}.webmanifest">`;
+    favicon.insertAdjacentHTML('afterend', tags);
+    favicon.href = `${favBase}.ico`;
+  }
 }
--- test/utils/favicon.test.js
+++ test/utils/favicon.test.js
@@ -12,21 +12,31 @@ describe('Favicon', () => {
     setConfig({});
   });
 
-  it('sets all default favicon props', async () => {
+  it('should set default favicon', async () => {
     const getMetadata = stub().withArgs('favicon').returns(null);
     const faviconEl = document.head.querySelector('link[rel="icon"]');
     loadFavIcons({ getConfig, getMetadata });
     const expected = 'http://localhost:2000/img/favicons/favicon.ico';
-    waitFor(() => faviconEl.href === expected);
+    await waitFor(() => faviconEl.href === expected);
     expect(faviconEl.href).to.equal(expected);
   });
 
-  it('sets metadata favicon', async () => {
+  it('should set favicon from metadata key (legacy)', async () => {
     const getMetadata = stub().withArgs('favicon').returns('otis');
     const faviconEl = document.head.querySelector('link[rel="icon"]');
     loadFavIcons({ getConfig, getMetadata });
     const expected = 'http://localhost:2000/img/favicons/otis.ico';
-    waitFor(() => faviconEl.href === expected);
+    await waitFor(() => faviconEl.href === expected);
     expect(faviconEl.href).to.equal(expected);
+  });
+
+  it('should set favicon from metadata path', async () => {
+    const getMetadata = stub().withArgs('favicon').returns('/favicon.ico');
+    const faviconEl = document.head.querySelector('link[rel="icon"]');
+    loadFavIcons({ getConfig, getMetadata });
+    const expected = 'http://localhost:2000/favicon.ico';
+    await waitFor(() => faviconEl.href === expected);
+    expect(faviconEl.href).to.equal(expected);
+    expect(faviconEl.type).to.equal('image/x-icon');
   });
 });
