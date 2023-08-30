2b793004cd48b62b96a275032726348a79232b56
Hayley Parra <h@hgpa.tv>
2023-08-30 12:35:49 -0700

    Load js from /libs/deps

--- libs/blocks/code/code.js
+++ libs/blocks/code/code.js
@@ -1,6 +1,6 @@
 import * as utils from '../../utils/utils.js';
 
-export const HIGHLIGHT_JS = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/highlight.min.js';
+export const HIGHLIGHT_JS = '/libs/deps/highlight.min.js';
 export const FONT_CSS = 'https://use.typekit.net/vih2anh.css';
 export const SIZE_VARIANTS = ['small', 'medium', 'large'];
 
