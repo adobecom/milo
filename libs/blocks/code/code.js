22aa6f638fb133ece33399ebbd8f227603a3f0a0
Hayley Parra <h@hgpa.tv>
2023-08-30 16:59:08 -0700

    Fix language allowlist

--- libs/blocks/code/code.js
+++ libs/blocks/code/code.js
@@ -3,6 +3,8 @@ import * as utils from '../../utils/utils.js';
 export const HIGHLIGHT_JS = '/libs/deps/highlight.min.js';
 export const FONT_CSS = 'https://use.typekit.net/vih2anh.css';
 export const SIZE_VARIANTS = ['small', 'medium', 'large'];
+export const MISC_VARIANTS = ['light', 'dark', 'wrap'];
+export const ALL_VARIANTS = [...SIZE_VARIANTS, ...MISC_VARIANTS, 'code'];
 
 function logError(msg) {
   window.lana?.log(`Code: ${msg}`, { tags: 'errorType=code' });
@@ -13,7 +15,7 @@ function getSizeVariant(el) {
 }
 
 function getLanguageVariant(el) {
-  return [...el.classList].find((v) => ![...SIZE_VARIANTS, 'code'].includes(v));
+  return [...el.classList].find((v) => !ALL_VARIANTS.includes(v));
 }
 
 export default async function init(el, { loadScript, loadStyle } = utils) {
