c4af208afec236e0daba51267fcb26b9ad9967c4
Hayley Parra <h@hgpa.tv>
2023-08-30 19:29:14 -0700

    Fix eslint

--- libs/utils/fonts.js
+++ libs/utils/fonts.js
@@ -1,7 +1,7 @@
 // A gently modified version of the dynamic subsetting loader from Adobe Fonts
 function dynamicTypekit(kitId, d = document) {
   const config = { kitId, scriptTimeout: 3000, async: true };
-  /* c8 ignore next 1 */
+  // eslint-disable-next-line
   const h = d.documentElement; const t = setTimeout(() => { h.className = `${h.className.replace(/\bwf-loading\b/g, '')} wf-inactive`; }, config.scriptTimeout); const tk = d.createElement('script'); let f = false; const s = d.getElementsByTagName('script')[0]; let a; h.className += ' wf-loading'; tk.src = `https://use.typekit.net/${config.kitId}.js`; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != 'complete' && a != 'loaded') return; f = true; clearTimeout(t); try { Typekit.load(config); } catch (e) {} }; s.parentNode.insertBefore(tk, s);
   return h;
 }
