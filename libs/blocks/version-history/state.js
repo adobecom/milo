b8377bde1f5c93bb994b1b6e233a8f2829915768
Bandana Laishram <laishrambandanadevi@Laishrams-MacBook-Pro.local>
2023-09-04 19:31:10 +0530

    Eslint fix

--- libs/blocks/version-history/state.js
+++ libs/blocks/version-history/state.js
@@ -7,4 +7,11 @@ const error = signal('');
 const versions = signal([]);
 const showLogin = signal(false);
 
-export { origin, itemId, comment, error, versions, showLogin };
+export {
+  origin,
+  itemId,
+  comment,
+  error,
+  versions,
+  showLogin
+};
