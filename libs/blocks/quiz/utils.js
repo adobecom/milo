4286abde272fa45caea1bea1c1c8fbbf5458fba9
Erich Champion <echampio@adobe.com>
2023-09-04 18:41:54 -0700

    MWPW-135903: Feedback from Okan

--- libs/blocks/quiz/utils.js
+++ libs/blocks/quiz/utils.js
@@ -190,19 +190,12 @@ export const nestedFragments = (
   nestedFragsSecondaryArray?.forEach((frag) => {
     if (!frag) return;
     const fragKey = frag.trim();
-    if (nestedObject[fragKey]) {
-      nestedObject[fragKey].push(...getNestedFragments(
-        resultResources,
-        secondaryProductCodes,
-        fragKey,
-      ));
-    } else {
-      nestedObject[fragKey] = getNestedFragments(
-        resultResources,
-        secondaryProductCodes,
-        fragKey,
-      );
-    }
+    if (!nestedObject[fragKey]) nestedObject[fragKey] = [];
+    nestedObject[fragKey].push(...getNestedFragments(
+      resultResources,
+      secondaryProductCodes,
+      fragKey,
+    ));
   });
 
   return nestedObject;
