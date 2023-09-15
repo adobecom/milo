c63b0f74f41fda716af963979bbc48b1c099f7e4
lukianet <lukianet@adobe.com>
2023-09-15 14:40:20 +0200

    add flags

--- libs/features/personalization/manifest-utils.js
+++ libs/features/personalization/manifest-utils.js
@@ -18,16 +18,40 @@ export const normalizePath = (p) => {
   }
   return path;
 };
-
-export const preloadManifests = ({ targetManifests = [], persManifests = [] }) => {
+function xlSerialToJsDate(xlSerial) {
+  return new Date(-2209075200000 + (xlSerial - (xlSerial < 61 ? 0 : 1)) * 86400000);
+}
+export const preloadManifests = ({ targetManifests = [], persManifests = [], flags = [] }) => {
   let manifests = targetManifests;
 
   manifests = manifests.concat(
-    persManifests.map((manifestPath) => ({ manifestPath: appendJsonExt(manifestPath) })),
+    persManifests.map((persManifest) => {
+      const mmm = { manifestPath: appendJsonExt(persManifest) };
+
+      if (persManifest.includes('|')) {
+        const [flag, manifestPath] = appendJsonExt(persManifest).split('|');
+        let isDisabled = false;
+        const flagData = flags.data.find((f) => f?.flag === flag);
+        if (flagData) {
+          if (flagData.onoff === 'off') {
+            isDisabled = true;
+          } else {
+            const start = xlSerialToJsDate(flagData.start);
+            const end = xlSerialToJsDate(flagData.end);
+            const currentDate = new Date();
+            if (start && end && (currentDate < start || currentDate > end)) {
+              isDisabled = true;
+            }
+          }
+        }
+        return { manifestFlag: flag, manifestPath, disabled: isDisabled };
+      }
+      return mmm;
+    }),
   );
 
   for (const manifest of manifests) {
-    if (!manifest.manifestData && manifest.manifestPath) {
+    if (!manifest.manifestData && manifest.manifestPath && manifest.disabled) {
       manifest.manifestPath = normalizePath(manifest.manifestPath);
       loadLink(
         manifest.manifestPath,
