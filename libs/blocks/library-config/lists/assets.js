fd2d574c99a0d6868e1414dbc8aa2918bd5f2f72
Chris Millar <cmillar@adobe.com>
2023-09-22 14:10:07 -0600

    revert assets

--- libs/blocks/library-config/lists/assets.js
+++ libs/blocks/library-config/lists/assets.js
@@ -2,19 +2,17 @@ import createCopy from '../library-utils.js';
 import { createTag } from '../../../utils/utils.js';
 
 function buildLink(href) {
-  return createTag('a', { href, style: 'background-color: #FF00B9' }, href).outerHTML;
+  return createTag('a', { href }, href).outerHTML;
 }
 
 export default async function assetsList(content, list) {
   content.forEach((href) => {
-    const img = createTag('img', { src: href, style: '', title: 'does this work' });
-    const a = createTag('a', { href: 'https://www.adobe.com', style: `background-image: url('${href}')` }, 'Hello World');
+    const img = createTag('img', { src: href });
     const li = createTag('li', { class: 'asset-item' }, img);
     list.append(li);
-    img.addEventListener('click', async () => {
+    img.addEventListener('click', () => {
       const html = href.endsWith('.svg') ? buildLink(href) : img.outerHTML;
       const blob = new Blob([html], { type: 'text/html' });
-      document.body.querySelector('.section').append(img);
       createCopy(blob);
     });
   });
