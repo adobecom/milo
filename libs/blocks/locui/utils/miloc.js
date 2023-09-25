db12c48f07187af31d9ccfdc32a006271178e5d2
Chris Millar <cmillar@adobe.com>
2023-09-24 20:23:23 -0600

    Update to .live in desperate hopes it will work with loc service

--- libs/blocks/locui/utils/miloc.js
+++ libs/blocks/locui/utils/miloc.js
@@ -74,8 +74,8 @@ export async function createProject() {
   const url = await getMilocUrl();
   setStatus('service', 'info', 'Creating new project.');
   setExcelStatus('Creating new project.', '');
-  const path = heading.value.path.replace('.page', '.live');
-  const body = `${origin}${path}.json`;
+  const liveOrigin = origin.replace('.page', '.live');
+  const body = `${liveOrigin}${heading.value.path}.json`;
   const opts = { method: 'POST', body };
   const resp = await fetch(`${url}create-project`, opts);
   if (resp.status === 201) {
