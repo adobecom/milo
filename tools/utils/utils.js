c4d4f9513ce55d114e1c98999ea074fb98bf9b6c
Raghu A <raghu@Raghus-MacBook-Pro.local>
2023-09-26 18:34:50 +0530

    check updated

--- tools/utils/utils.js
+++ tools/utils/utils.js
@@ -14,7 +14,7 @@ const getImsToken = async (loadScript) => {
   return window.adobeIMS?.getAccessToken()?.token;
 };
 
-const isImsStage = () => window.adobeid?.environment
-  && window.adobeid.environment?.toLowerCase() !== 'prod';
+const isImsStage = () => window.adobeid?.environment?.toLowerCase
+  && window.adobeid.environment.toLowerCase() !== 'prod';
 
 export { getImsToken, isImsStage };
