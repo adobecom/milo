9d4ecf2e672775b6154771cd47d7ff8a5b57e8f2
Chris Millar <cmillar@adobe.com>
2023-09-29 13:46:11 -0600

    meaningless comment (#1376)
    * meaningless comment
    
    * Remove test

--- libs/blocks/locui/locui.js
+++ libs/blocks/locui/locui.js
@@ -2,5 +2,6 @@ import { html, render } from '../../deps/htm-preact.js';
 import Localization from './loc/view.js';
 
 export default function init(el) {
+  // a meaningless comment
   render(html`<${Localization} />`, el);
 }
