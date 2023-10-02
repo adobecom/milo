d11c1ad1c118c070d02fdd35073d8e00c1190bf3
Carlos Miqueo <cmiqueo@adobe.com>
2023-10-02 10:46:56 -0700

    MWPW-126974: updates ubitt-tests

--- test/blocks/caas/send-to-caas.test.js
+++ test/blocks/caas/send-to-caas.test.js
@@ -5,28 +5,17 @@ import { checkUrl } from '../../../tools/send-to-caas/send-utils.js';
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
-describe('Metadata', () => {
-  it('Renders with card-metadata class', async () => {
-    const cardMetadata = document.querySelector('.card-metadata');
-    expect(cardMetadata).to.exist;
+describe('checkUrl function', () => {
+  it('should return a flattened URL when given a URL with "href="', () => {
+    const cardMetadataLinks = document.querySelectorAll('.card-metadata div a');
+    const htmlLink = cardMetadataLinks[0].toString();
+    const result = checkUrl(htmlLink, 'Error message');
+    expect(result).to.equal(htmlLink);
   });
 
-  it('Renders card-metadata elements', async () => {
-    const cardMetadataValues = document.querySelectorAll('.card-metadata div');
-    expect(cardMetadataValues).to.exist;
-  });
-
-  it('Flattens URLs', async () => {
-    const expectedResults = [
-      'https://milo.adobe.com/index.html',
-      'https://www.youtube.com/watch?v=9H5S4Cc5SgM',
-    ];
-    const cardMetadataValues = document.querySelectorAll('.card-metadata a');
-
-    const miloUrl = await checkUrl(cardMetadataValues[0].toString());
-    expect(miloUrl).to.equal(expectedResults[0]);
-
-    const youtubeUrl = await checkUrl(cardMetadataValues[1].toString());
-    expect(youtubeUrl).to.equal(expectedResults[1]);
+  it('should return the original URL when not containing "href="', () => {
+    const inputUrl = 'https://milo.adobe.com/index.html';
+    const result = checkUrl(inputUrl, 'Error message');
+    expect(result).to.equal(inputUrl);
   });
 });
