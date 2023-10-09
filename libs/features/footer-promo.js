e5b5c89c0ae34c4ff3c19bec5bcfd6dc391236e3
Megan Thomas <methomas@adobe.com>
2023-10-09 16:54:46 -0700

    cr

--- libs/features/footer-promo.js
+++ libs/features/footer-promo.js
@@ -1,41 +1,39 @@
 import { createTag } from '../utils/utils.js';
 
 async function getFooterPromoByTag(contentRoot) {
-  const NAME = 'Name';
-  const FOOTER_PROMO_LINK = 'Footer Promo Link';
+  const NAME_KEY = 'Name';
+  const FOOTER_PROMO_LINK_KEY = 'Footer Promo Link';
   const taxonomyUrl = `${contentRoot}/taxonomy.json`;
-  const tagsOnPage = [...document.head.querySelectorAll('meta[property="article:tag"]')].map((el) => el.content);
+  const tags = [...document.head.querySelectorAll('meta[property="article:tag"]')].map((el) => el.content);
 
-  if (!tagsOnPage.length) return false;
+  if (!tags.length) return undefined;
 
   try {
     const resp = await fetch(taxonomyUrl);
-    if (!resp.ok) return false;
+    if (!resp.ok) return undefined;
     const { data } = await resp.json();
     const primaryTag = data.find((tag) => {
-      const name = tag[NAME].split('|').pop().trim();
-      return tagsOnPage.includes(name) && tag[FOOTER_PROMO_LINK];
+      const name = tag[NAME_KEY].split('|').pop().trim();
+      return tags.includes(name) && tag[FOOTER_PROMO_LINK_KEY];
     });
-    if (primaryTag) return primaryTag[FOOTER_PROMO_LINK];
+    if (primaryTag) return primaryTag[FOOTER_PROMO_LINK_KEY];
   } catch (error) {
     window.lana.log(`Footer Promo - Taxonomy error: ${error}`);
-    return false;
+    return undefined;
   }
-  return false;
+  return undefined;
 }
 
 export default async function initFooterPromo(contentRoot, urlBasedPromo, tagBasedPromo) {
-  let href;
-  if (urlBasedPromo && urlBasedPromo !== 'off') href = `${contentRoot}/fragments/footer-promos/${urlBasedPromo}`;
+  let href = urlBasedPromo && urlBasedPromo !== 'off' && `${contentRoot}/fragments/footer-promos/${urlBasedPromo}`;
 
   if (tagBasedPromo === 'on') {
     const linkFromTag = await getFooterPromoByTag(contentRoot);
     if (linkFromTag) href = linkFromTag;
   }
 
-  if (href) {
-    const para = createTag('p', {}, createTag('a', { href }, href));
-    const section = createTag('div', null, para);
-    document.querySelector('main > div:last-of-type').insertAdjacentElement('afterend', section);
-  }
+  if (!href) return;
+  const para = createTag('p', {}, createTag('a', { href }, href));
+  const section = createTag('div', null, para);
+  document.querySelector('main > div:last-of-type').insertAdjacentElement('afterend', section);
 }
