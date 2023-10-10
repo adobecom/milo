5fd9f208235ac2eecd850d9a1808d9dc1163573c
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2023-10-10 11:06:49 +0200

    lazy load videos only when visible

--- libs/blocks/video/video.js
+++ libs/blocks/video/video.js
@@ -1,14 +1,29 @@
 import { applyHoverPlay, getVideoAttrs } from '../../utils/decorate.js';
+import { createTag } from '../../utils/utils.js';
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+      entry.target.removeAttribute('preload');
+      const source = createTag('source', { type: 'video/mp4', src: entry.target.getAttribute('data-src') });
+      entry.target.appendChild(source);
+      entry.target.removeAttribute('data-src');
+    }
+  });
+};
+
+const videoObserver = new IntersectionObserver(callback, {});
 
 export default function init(a) {
   const { pathname, hash } = a;
   const attrs = getVideoAttrs(hash);
-  const video = `<video ${attrs}>
-        <source src=".${pathname}" type="video/mp4" />
+  const video = `<video ${attrs} preload="none" data-src=".${pathname}">
       </video>`;
   if (!a.parentNode) return;
   a.insertAdjacentHTML('afterend', video);
-  const videoElem = document.body.querySelector(`source[src=".${pathname}"]`)?.parentElement;
+  const videoElem = a.nextSibling;
+  videoObserver.observe(videoElem);
   applyHoverPlay(videoElem);
   a.remove();
 }
