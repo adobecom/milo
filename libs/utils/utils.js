628299828f1ab54456ecee1d926e40a78061404b
Ryan Clayton <rclayton@adobe.com>
2023-10-12 17:22:56 -0600

    Addressing PR feedback.

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -485,8 +485,7 @@ export function decorateImageLinks(el) {
     const [source, alt, icon] = img.alt.split('|');
     try {
       const url = new URL(source.trim());
-      const hash = url.hash || '';
-      const href = url.hostname.includes('.hlx.') ? `${url.pathname}${hash}` : url.href;
+      const href = url.hostname.includes('.hlx.') ? `${url.pathname}${url.hash}` : url.href;
       if (alt?.trim().length) img.alt = alt.trim();
       const pic = img.closest('picture');
       const picParent = pic.parentElement;
