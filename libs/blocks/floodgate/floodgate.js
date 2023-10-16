e3617e7fdae4bcb1ee4a6aa3664d5444ae5a16f5
Sunil Kamat <sukamat@adobe.com>
2023-10-16 13:54:19 -0700

    Deleting temp files

--- libs/blocks/floodgate/floodgate.js
+++
@@ -1,6 +0,0 @@
-import { html, render } from '../../deps/htm-preact.js';
-import Floodgate from './floodgate/view.js';
-
-export default function init(el) {
-  render(html`<${Floodgate} />`, el);
-}
