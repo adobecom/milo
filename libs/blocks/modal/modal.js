920d23f543f9b739bfab96ae8be646adba0ee1f5
saragajic11 <saragajic115@gmail.com>
2023-10-17 15:55:35 +0200

    Prevent background from scrolling when modal dialog with curtain is opened

--- libs/blocks/modal/modal.js
+++ libs/blocks/modal/modal.js
@@ -43,6 +43,7 @@ function closeModal(modal) {
 
   document.querySelectorAll(`#${id}`).forEach((mod) => {
     if (mod.nextElementSibling?.classList.contains('modal-curtain')) {
+      document.querySelector('body').style.removeProperty('overflow');
       mod.nextElementSibling.remove();
     }
     if (mod.classList.contains('dialog-modal')) {
@@ -166,6 +167,7 @@ export async function getModal(details, custom) {
 
   if (!dialog.classList.contains('curtain-off')) {
     const curtain = createTag('div', { class: 'modal-curtain is-open' });
+    document.querySelector('body').style.overflow = 'hidden';
     curtain.addEventListener('click', (e) => {
       if (e.target === curtain) closeModal(dialog);
     });
