842473a276f5e31c913c1a6548847182f1f9d551
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2023-10-17 14:52:13 -0400

    MWPW-137993 Setup global deferred promise
    For external code (martech) they want to know when the deferred event fires, but sometimes it fires before their code loads.
    
    This adds a global promise at `window.milo.deferredPromise` that will resolve at the same time as the milo deferred event fires.

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -861,9 +861,24 @@ export function scrollToHashedElement(hash) {
   });
 }
 
-export async function loadDeferred(area, blocks, config) {
+function setupDeferredPromise() {
+  let resolveFn;
+  window.milo.deferredPromise = new Promise((resolve) => {
+    resolveFn = resolve;
+  });
+  return resolveFn;
+}
+
+export async function loadDeferred(area, blocks, config, resolveDeferred) {
   const event = new Event(MILO_EVENTS.DEFERRED);
   area.dispatchEvent(event);
+
+  if (area !== document) {
+    return;
+  }
+
+  resolveDeferred(true);
+
   if (config.links === 'on') {
     const path = `${config.contentRoot || ''}${getMetadata('links-path') || '/seo/links.json'}`;
     import('../features/links.js').then((mod) => mod.default(path, area));
@@ -1006,10 +1021,13 @@ async function processSection(section, config, isDoc) {
 
 export async function loadArea(area = document) {
   const isDoc = area === document;
+  let resolveDeferredFn;
 
   if (isDoc) {
+    window.milo = {};
     await checkForPageMods();
     appendHtmlToCanonicalUrl();
+    resolveDeferredFn = setupDeferredPromise();
   }
 
   const config = getConfig();
@@ -1038,7 +1056,7 @@ export async function loadArea(area = document) {
 
   if (isDoc) await documentPostSectionLoading(config);
 
-  await loadDeferred(area, areaBlocks, config);
+  await loadDeferred(area, areaBlocks, config, resolveDeferredFn);
 }
 
 export function loadDelayed() {
