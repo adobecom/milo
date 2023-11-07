e5e3895ee43b23334d4925e665924028c45ee217
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2023-11-06 17:43:12 -0800

    MWPW-138337: initial commit for dynamic tutorials (#1499)
    * initial commit for dynamic tutorials
    
    * fix eslint
    
    ---------
    
    Co-authored-by: xiasun <xiasun@xiasuns-mbp-2.corp.adobe.com>

---
+++ libs/blocks/quiz-dynamic-tutorials/quiz-dynamic-tutorials-utils.js
@@ -0,0 +1,57 @@
+const getQuizTutorialsList = async (inputText, fiCode, numOfItems) => {
+  const apiUrl = 'https://cchome-stage.adobe.io/int/v1/models';
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'x-api-key': 'CCHomeWeb1',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      endpoint: 'community-recom-v1',
+      contentType: 'application/json',
+      payload: {
+        data: {
+          input: inputText,
+          metadata_importance: 0,
+          fi_code: fiCode,
+          cleaning: 'no',
+          num_items: numOfItems,
+        },
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log('Error:', error));
+
+  return res;
+};
+
+export const getQuizTutorialsDetails = async (
+  inputText,
+  fiCode,
+  numOfItems,
+) => {
+  const tutorialsList = await getQuizTutorialsList(
+    inputText,
+    fiCode,
+    numOfItems,
+  );
+
+  const contentIds = tutorialsList.data.map((item) => item.content_id);
+
+  const baseUrl = 'https://utut-service.adobe.com/api/ututs';
+  const queryParams = new URLSearchParams({
+    api_key: 'CCHomeMLRepo1',
+    ignore_missing_tutorials: 'true',
+    locale: 'en_US',
+  });
+
+  contentIds.forEach((contentId) => {
+    queryParams.append('aem_id', contentId);
+  });
+
+  const res = await fetch(`${baseUrl}?${queryParams.toString()}`)
+    .then((response) => response.json())
+    .catch((error) => console.log('Error:', error));
+  return res;
+};
---
+++ libs/blocks/quiz-dynamic-tutorials/quiz-dynamic-tutorials.js
@@ -0,0 +1,101 @@
+import { decorateButtons } from '../../utils/decorate.js';
+import { loadStyle, getConfig } from '../../utils/utils.js';
+import { addBackgroundImg, addWrapper, addFooter, addVideoBtn } from './quiz-dynamic-tutorials-utils.js';
+
+const HALF = 'OneHalfCard';
+const HALF_HEIGHT = 'HalfHeightCard';
+const PRODUCT = 'ProductCard';
+const DOUBLE_WIDE = 'DoubleWideCard';
+
+const getCardType = (styles) => {
+  const cardTypes = {
+    'half-card': HALF,
+    'half-height-card': HALF_HEIGHT,
+    'product-card': PRODUCT,
+    'double-width-card': DOUBLE_WIDE,
+  };
+  const authoredType = styles?.find((style) => style in cardTypes);
+  return cardTypes[authoredType] || HALF;
+};
+
+const addInner = (el, cardType, card) => {
+  const title = el.querySelector('h1, h2, h3, h4, h5, h6');
+  const text = Array.from(el.querySelectorAll('p'))?.find((p) => !p.querySelector('picture, a'));
+  let inner = el.querySelector(':scope > div:not([class])');
+
+  if (cardType === DOUBLE_WIDE) {
+    inner = document.createElement('a');
+    inner.href = el.querySelector('a')?.href || '';
+    inner.rel = 'noopener noreferrer';
+    inner.tabIndex = 0;
+    if (title) inner.append(title);
+    if (text) inner.append(text);
+    el.querySelector(':scope > div:not([class])')?.remove();
+  }
+
+  inner.classList.add(`consonant-${cardType}-inner`);
+  card.append(inner);
+
+  if (cardType === PRODUCT) {
+    inner.querySelector(':scope > div')?.classList.add('consonant-ProductCard-row');
+    if (text) inner.append(text);
+  }
+
+  if (cardType === HALF_HEIGHT) {
+    text?.remove();
+  }
+
+  title?.classList.add(`consonant-${cardType}-title`);
+  text?.classList.add(`consonant-${cardType}-text`);
+};
+
+const init = (el) => {
+  const { miloLibs, codeRoot } = getConfig();
+  const base = miloLibs || codeRoot;
+  loadStyle(`${base}/deps/caas.css`);
+
+  const section = el.closest('.section');
+  section.classList.add('milo-card-section');
+  const row = el.querySelector(':scope > div');
+  const picture = el.querySelector('picture');
+  const styles = Array.from(el.classList);
+  const cardType = getCardType(styles);
+  const merch = styles.includes('merch') && cardType === HALF;
+  const links = merch ? el.querySelector(':scope > div > div > p:last-of-type')
+    .querySelectorAll('a') : el.querySelectorAll('a:not(.consonant-play-btn)');
+  let card = el;
+
+  addWrapper(el, section, cardType);
+
+  if (cardType === HALF_HEIGHT) {
+    const [link] = links;
+
+    if (link) {
+      card = link;
+    } else {
+      card = document.createElement('a');
+      card.href = '';
+    }
+
+    el.prepend(card);
+  }
+
+  card.classList.add('consonant-Card', `consonant-${cardType}`);
+  if (!styles.includes('border')) card.classList.add('consonant-u-noBorders');
+
+  if (picture && cardType !== PRODUCT) {
+    addBackgroundImg(picture, cardType, card);
+    const playBtn = el.querySelector('a.consonant-play-btn');
+    if (playBtn) addVideoBtn(playBtn, cardType, card);
+  }
+
+  picture?.parentElement.remove();
+  addInner(el, cardType, card);
+  decorateButtons(el);
+
+  if (cardType === HALF || cardType === PRODUCT) {
+    addFooter(links, row, merch);
+  }
+};
+
+export default init;
