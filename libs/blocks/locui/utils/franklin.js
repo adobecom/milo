1286bbdc2322960e73e570a201e3a3d2860c5b67
nsivakum <nsivakum@adobe.com>
2023-11-09 11:48:11 +0530

    handle for cc folder to get fg info

--- libs/blocks/locui/utils/franklin.js
+++ libs/blocks/locui/utils/franklin.js
@@ -13,7 +13,7 @@ export async function preview(path) {
 }
 
 export async function getStatus(path = '', editUrl = 'auto', fgFlag = false, fgColor = null) {
-  let url = `${ADMIN}/status/${owner}/${fgFlag ? `milo-${fgColor}` : repo}/main${path}`;
+  let url = `${ADMIN}/status/${owner}/${fgFlag ? `${repo}-${fgColor}` : repo}/main${path}`;
   url = editUrl ? `${url}?editUrl=${editUrl}` : url;
   const resp = await fetch(url, { cache: 'reload' });
   const json = await resp.json();
