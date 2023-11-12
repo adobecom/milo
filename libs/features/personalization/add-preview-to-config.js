c7a5f311aed6a05b51cf038e962254c0c5e474d1
vivgoodrich <vivian.goodrich@gmail.com>
2023-11-11 22:06:34 -0700

    remove unneeded if

--- libs/features/personalization/add-preview-to-config.js
+++ libs/features/personalization/add-preview-to-config.js
@@ -35,9 +35,7 @@ export default async function addPreviewToConfig({
     });
   }
 
-  if (config.mep.preview && !targetEnabled && !persManifests.length) {
-    import('./preview.js')
-      .then(({ default: decoratePreviewMode }) => decoratePreviewMode([]));
-  }
+  import('./preview.js')
+    .then(({ default: decoratePreviewMode }) => decoratePreviewMode([]));
   return persManifests;
 }
