c171416e5700d6fa58a7882d04e8784796ded31c
Jason Slavin <slavin@adobe.com>
2023-11-13 14:32:24 -0800

    Re-adding category assignment

--- libs/blocks/article-feed/article-helpers.js
+++ libs/blocks/article-feed/article-helpers.js
@@ -90,7 +90,11 @@ function loadArticleTaxonomy(article) {
 
     const articleTax = computeTaxonomyFromTopics(topics, path);
 
+<<<<<<< HEAD
     clonedArticle.category ??= articleTax.category;
+=======
+    clonedArticle.category = articleTax.category;
+>>>>>>> 441f756e (Re-adding category assignment)
 
     // topics = tags as an array
     clonedArticle.topics = topics;
