3a1a12360d1c5e140e6ea1d2ebe3716ce0bc0d75
Marcel Boucher <mboucher@adobe.com>
2023-11-16 14:07:01 -0500

    initial form

---
+++ libs/blocks/sko-demo-signup/sko-demo-signup.js
@@ -0,0 +1,68 @@
+import { createTag } from '../../utils/utils.js';
+
+let _fileContents;
+
+export default async function init(blockEl) {
+  blockEl.classList.add('content');
+  const wrapper = createTag('div', { class: 'sko-form spectrum-form' });
+  const fieldNames = blockEl.querySelectorAll(':scope > div div');
+    fieldNames.forEach((item, i) => {
+      const fieldWapper = createTag('div');
+      const spectrumLabel = createTag('label', {class: 'sko-form-label'},item.innerText);
+      let formField;
+      const fieldID = generateFieldName(item.innerText);
+      if(item.innerText == 'Picture') {
+        formField = createTag('input', {id: fieldID, class: 'sko-form-input spectrum-Textfield-input',type:"file", accept:".png, .jpg",capture:"camera" });
+        formField.addEventListener("change", getBase64, false);
+      } else {
+        formField= createTag('input', {id: fieldID, class: 'sko-form-input spectrum-Textfield-input'});
+      }
+      const inputWrapper = createTag('div');
+      inputWrapper.append(formField);
+      fieldWapper.append(spectrumLabel)
+      fieldWapper.append(inputWrapper);
+      wrapper.append(fieldWapper);
+      item.remove();
+  });
+
+  const submitButton = createTag('button', {class:'con-button blue button-justified-mobile'},'Submit')
+  submitButton.addEventListener('click', onSubmit);
+  wrapper.append(submitButton);
+  blockEl.append(wrapper);
+}
+
+function onSubmit() {
+  const fieldCollection = document.querySelectorAll('.sko-form-input');
+  const payload = {};
+  fieldCollection.forEach(item => {
+    if(item.id === 'picture') {
+      payload[item.id] = _fileContents;
+    } else {
+      payload[item.id] = item.value;
+    }
+    
+  }); 
+  fetch("https://prod-148.westus.logic.azure.com:443/workflows/b99189fde390438f82ea53b71daed118/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aHHtKCMxZ0vLxQzQDoOlJdDE3l6su8a7uJRnxtAcbdE", {
+				  method: "POST",
+				  body: JSON.stringify(payload),
+				  headers: {
+				    "Content-type": "application/json; charset=UTF-8"
+				  }
+				});
+}
+
+function getBase64() {      
+  var reader = new FileReader();   
+  reader.readAsDataURL(this.files[0]);  
+  reader.onload = function () {  
+    _fileContents = reader.result;
+  };  
+  reader.onerror = function (error) {  
+      console.log('Error: ', error);  
+  };   
+}
+
+function generateFieldName(str) {
+  const concatString = str.replace(/\s/g, "");
+  return concatString[0].toLowerCase() + concatString.slice(1);
+}
