05b251eea225cffa067124ce88f6ea3b3bc4c554
Marcel Boucher <mboucher@adobe.com>
2023-11-17 15:19:20 -0500

    Error handling

--- libs/blocks/sko-demo-signup/sko-demo-signup.js
+++ libs/blocks/sko-demo-signup/sko-demo-signup.js
@@ -1,6 +1,6 @@
-import { createTag } from '../../utils/utils.js';
+import { createTag, getConfig } from '../../utils/utils.js';
 
-let _fileContents;
+let _fileContents = null;
 
 export default async function init(blockEl) {
   blockEl.classList.add('content');
@@ -26,19 +26,19 @@ export default async function init(blockEl) {
 
 function decorateFormField(fieldJson, el) {
   const fieldWapper = createTag('div');
-  const fieldLabel = createTag('label', {class: 'sko-form-label'},fieldJson.label);
+  const fieldLabel = createTag('label', {class: 'sko-form-label'},fieldJson.label + " *");
   const fieldID = fieldJson.id;
   let formField;
   switch (fieldJson.type) {
     case 'text':
-      formField = createTag('input', {id: fieldID, class: 'sko-form-input', type:'text'});
+      formField = createTag('input', {id: fieldID, class: 'sko-form-input', required:true, type:'text'});
       break;
     case 'email':
       formField = createTag('input', {id: fieldID, class: 'sko-form-input', type:'email'});
       break;
     case 'dropdown':
       if(fieldJson.options !== '') {
-        formField = createTag('select', {id: fieldID, class: 'sko-form-input', placeholder:'Please select one...'});
+        formField = createTag('select', {type:'select', id: fieldID, class: 'sko-form-input', placeholder:'Please select one...'});
         const options = fieldJson.options.split(',');
         const placeholder = createTag('option', {value:'', disabled:true, selected:true, required:true, hidden:true}, 'Select one...');
         formField.append(placeholder);
@@ -64,31 +64,88 @@ function decorateFormField(fieldJson, el) {
 
 async function onSubmit() {
   const fieldCollection = document.querySelectorAll('.sko-form-input');
+  let isValid = true;
   const payload = {};
   fieldCollection.forEach(item => {
-    if(item.id === 'picture') {
-      payload[item.id] = _fileContents;
+    if(item.type === 'file') {
+      if(_fileContents !== null) {
+        item.classList.remove('invalid')
+        payload[item.id] = _fileContents;
+      } else {
+        item.classList.add('invalid')
+        isValid = false;
+      }
     } else {
-      payload[item.id] = item.value;
+      if(item.value !== "" && item.value !== null) {
+        item.classList.remove('invalid')
+        payload[item.id] = item.value;
+      } else {
+        item.classList.add('invalid')
+        isValid = false;
+      }
+      
     }
     
   }); 
-  const response = await fetch("https://prod-149.westus.logic.azure.com/workflows/9617d5afd2e844159486024231f5dd75/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aH81nljMBnWzitSFGzYYkghXmPrwZl9j3reURjF30Wo", {
-				  method: "POST",
-				  body: JSON.stringify(payload),
-				  headers: {
-				    "Content-type": "application/json; charset=UTF-8"
-				  }
-				});
+    if(isValid) {
+      try {
+        const response = await fetch("https://prod-149.westus.logic.azure.com/workflows/9617d5afd2e844159486024231f5dd75/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aH81nljMBnWzitSFGzYYkghXmPrwZl9j3reURjF30Wo", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      });
 
-        if(response.ok) {
-          const wrapper = document.querySelector('.sko-demo-signup');
+      if(response.ok) {
+        const wrapper = document.querySelector('.sko-demo-signup');
+        const parent = wrapper.parentNode;
+        wrapper.remove();
+        const message = {
+          heading:'Thank you for your submission ' + payload.firstName,
+          subHeading: 'A bunch of stuff is happening which we will tell you all about at SKO.',
+          image: 'success.png'
+        }
+        displayMessage(message, parent)
+      }
+
+    } catch (e) {
+        const wrapper = document.querySelector('.sko-demo-signup');
           const parent = wrapper.parentNode;
           wrapper.remove();
-          const message = createTag('h1',{class:'content confirmation-message'},'Thank you for your submission ' + payload.firstName);
-          parent.append(message);
-        }
+
+          const message = {
+            heading:'Oops... This is embarrassing.',
+            subHeading: 'Something bad must have happened. Let us know so we can fix it.',
+            image: 'error.png'
+          }
+          displayMessage(message, parent);
+      }
+    }
+    
 }
+
+function displayMessage(message, parent) {
+  const { codeRoot } = getConfig();
+  const marqueeWrapper = createTag('div', {class:'marquee light'});
+  const container = createTag('div', {class:'foreground container'});
+  const text = createTag('div', {class:'text', 'data-valign':'middle'});
+  const heading = createTag('h2', {class: 'heading-xl'},message.heading);
+  const subHeading = createTag('p', {class:'body-m'},message.subHeading);
+
+  text.append(heading);
+  text.append(subHeading);
+
+  const image = createTag('div', {class:'media image', 'data-valign':'middle'});
+  const picture = createTag('img',{src:`${codeRoot}/blocks/sko-demo-signup/img/${message.image}`});
+
+  image.append(picture)
+  container.append(text);
+  container.append(image);
+  marqueeWrapper.append(container);
+  parent.append(marqueeWrapper);
+}
+
 
 const getCreds = () => {
   if(window.adobeIMS.isSignedInUser()) {
