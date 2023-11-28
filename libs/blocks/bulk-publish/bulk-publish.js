1fea2b6965b30e1e808b213e845850e955b667a0
Sartxi <sean.archibeque@gmail.com>
2023-11-27 19:23:25 -0700

    form elements and events

--- libs/blocks/bulk-publish/bulk-publish.js
+++ libs/blocks/bulk-publish/bulk-publish.js
@@ -1,37 +1,80 @@
 import { getSheet } from '../../../tools/utils/utils.js';
 import { LitElement, html } from '../../deps/lit-all.min.js';
 
-const styles = await getSheet('/blocks/bulk-publish/bulk-publish.css');
+const sheet = await getSheet('/libs/blocks/bulk-publish/bulk-publish-wc.css');
+const TYPES = [
+  'Preview',
+  'Publish',
+  'Preview & Publish',
+  'Unpublish',
+  'Delete',
+  'Index',
+];
 
-class BulkPublish extends LitElement {
-  static properties = {
-    urls: { state: true },
-    processType: { state: true },
-  };
+customElements.define(
+  'bulk-publish',
+  class extends LitElement {
+    static properties = {
+      urls: { state: true },
+      processType: { state: true },
+      results: { state: true },
+    };
 
-  constructor() {
-    super();
-    this.urls = [];
-    this.processType = 'preview';
-  }
+    constructor() {
+      super();
+      this.urls = [];
+      this.processType = 'Preview';
+      this.results = null;
+    }
 
-  connectedCallback() {
-    super.connectedCallback();
-    this.shadowRoot.adoptedStyleSheets = [styles];
-  }
+    connectedCallback() {
+      super.connectedCallback();
+      this.shadowRoot.adoptedStyleSheets = [sheet];
+    }
 
-  getHeader() {
-    return html`<div class="header">Header</div>`;
-  }
+    selectType(e) {
+      if (e.target.value !== this.processType) {
+        this.processType = e.target.value;
+      }
+    }
 
-  render() {
-    return html`
-      <div id="BulkPublishTool">${this.getHeader()}</div>
-    `;
-  }
-}
+    updateUrls(e) {
+      const urls = e.target.value.replace(/\n/g, ' ').split(' ');
+      if (urls !== this.urls) this.urls = urls;
+    }
 
-customElements.define('bulk-publish', BulkPublish);
+    runProcess() {
+      console.log(`Run the ${this.processType} process for ${this.urls.length}`);
+    }
+
+    render() {
+      return html`
+        <header>
+          <h1>Bulk Publish</h1>
+        </header>
+        <div class="form">
+          <div class="urls">
+            <label for="urls">URL(s):</label>
+            <textarea 
+              id="urls"
+              class="url-field"
+              placeholder="Ex: https://main--milo--adobecom.hlx.page/mypage"
+              @change=${this.updateUrls}></textarea>
+          </div>
+          <select name="process" @change=${this.selectType}>
+            ${TYPES.map((type) => (html`
+                <option class="type" value=${type}>
+                  ${type}
+                </option>
+            `))}
+          </select>
+          <button class="submit-button" @click=${this.runProcess}>Run</button>
+        </div>
+      `;
+    }
+  },
+);
+
 export default async function init(el) {
   el.append(document.createElement('bulk-publish'));
 }
