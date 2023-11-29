f9f8077fec25fd6afe96a39f6cd60433de67ae2c
nsivakum <nsivakum@adobe.com>
2023-11-29 17:46:32 +0530

    urls per page changed to 100

--- libs/blocks/floodgateui/urls/view.js
+++ libs/blocks/floodgateui/urls/view.js
@@ -3,7 +3,7 @@ import { urls } from '../utils/state.js';
 import Url from '../url/view.js';
 
 function Urls() {
-  const itemsPerPage = 10;
+  const itemsPerPage = 100;
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredUrls, setFilteredUrls] = useState([]);
