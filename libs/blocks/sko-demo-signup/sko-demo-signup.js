75785f9e0ae521149d16ecf6a4d27242b7b9cec4
Marcel Boucher <mboucher@adobe.com>
2023-11-30 09:47:09 -0800

    test

--- libs/blocks/sko-demo-signup/sko-demo-signup.js
+++ libs/blocks/sko-demo-signup/sko-demo-signup.js
@@ -95,6 +95,9 @@ async function onSubmit() {
     if(isValid) {
       const submitButton = document.querySelector('.submit-button > button');
       console.log(submitButton);
+      submitButton.label='Submitting...';
+      submitButton.disabled = true;
+      
       /*
       try {
         const response = await fetch("https://prod-149.westus.logic.azure.com/workflows/9617d5afd2e844159486024231f5dd75/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=aH81nljMBnWzitSFGzYYkghXmPrwZl9j3reURjF30Wo", {
