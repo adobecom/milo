82a8e5122811e1c8de0bfc8626802be370826535
Marcel Boucher <mboucher@adobe.com>
2023-11-30 16:01:44 -0800

    added redirect block

---
+++ libs/blocks/sko-redirect/sko-redirect.js
@@ -0,0 +1,56 @@
+import { createTag, getConfig } from '../../utils/utils.js';
+
+let destination, emailAddress = '';
+
+export default async function init(blockEl) {
+
+  const wrapper = createTag('div', {class:'redirect-wrapper'});
+
+  // Get the destination URL and user email from the request
+  if(window.location.search.length > 1) {
+    if(window.location.search.length > 1) {
+      destination = getQueryVariable('frameURL');
+      emailAddress = getQueryVariable('email');
+      const header = createTag('h1');
+      const timerMessage = createTag('text');
+
+      if(destination && emailAddress) {
+        header.innerHTML = "Sit back, we're taking you to the right place shortly";
+        wrapper.append(header);
+        wrapper.append(timerMessage);
+        blockEl.append(wrapper);
+        updateTimer(timerMessage);
+
+      } else {
+        header.innerHTML = "Oooops! Something bad happened";
+        timerMessage.innerHTML = "The url and email parameters were not provided."
+        wrapper.append(header);
+        wrapper.append(timerMessage);
+        blockEl.append(wrapper);
+      }
+		
+		 }
+  }
+
+}
+
+function updateTimer(element) {
+  if (count > 0) {
+    count--;
+    element.innerHTML = "This page will redirect in " + count + " seconds."; // Timer Message
+    setTimeout("countDown()", 1000);
+  } else {
+    window.location.href = destination;
+  }
+}
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+}
