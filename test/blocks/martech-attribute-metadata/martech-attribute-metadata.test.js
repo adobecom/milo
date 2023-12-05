3f075cda7b747930044c7f8df1804de3e4493dee
vivgoodrich <vivian.goodrich@gmail.com>
2023-12-04 21:20:21 -0700

    update UT

--- test/blocks/martech-attribute-metadata/martech-attribute-metadata.test.js
+++ test/blocks/martech-attribute-metadata/martech-attribute-metadata.test.js
@@ -3,7 +3,7 @@ import { expect } from '@esm-bundle/chai';
 
 document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 const { default: init } = await import('../../../libs/blocks/martech-attribute-metadata/martech-attribute-metadata.js');
-const { getConfig, setConfig } = await import('../../../libs/utils/utils.js');
+const { setConfig } = await import('../../../libs/utils/utils.js');
 
 describe('martech-attribute-metadata', () => {
   it('updated config', () => {
@@ -13,7 +13,6 @@ describe('martech-attribute-metadata', () => {
       locales: { es: { ietf: 'es-ES', tk: 'hah7vzn.css' } },
       locale: { ietf: 'es-ES' },
     };
-    /* TODO: need to figure out how to spoof non us-EN cuz this isn't working */
     setConfig(origConfig);
     const blocks = document.querySelectorAll('.martech-attribute-metadata');
     blocks.forEach((block) => {
