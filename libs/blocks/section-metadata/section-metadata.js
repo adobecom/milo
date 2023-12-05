157e30e3d0c0e1416a81ecca50ba71e1e282fdc1
mathuria <mathuria@adobe.com>
2023-12-05 15:29:59 +0530

    Fix

--- libs/blocks/section-metadata/section-metadata.js
+++ libs/blocks/section-metadata/section-metadata.js
@@ -28,7 +28,7 @@ export async function handleStyle(text, section) {
 }
 
 function handleMasonry(text, section) {
-  section.classList.add(...['masonry-layout', 'courses-up']);
+  section.classList.add(...['masonry-layout', 'course-up']);
   const divs = section.querySelectorAll(':scope > div');
   const spans = [];
   text.split('\n').forEach((line) => spans.push(...line.trim().split(',')));
