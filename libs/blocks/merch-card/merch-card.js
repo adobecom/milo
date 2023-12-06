2af32c1c974aaca8039803502831eb51fa7b144a
Vivian A Goodrich <101133187+vgoodric@users.noreply.github.com>
2023-12-06 13:44:06 -0700

    MWPW-140076 Update merch-card to match new analytics taxonomy (#1606)
    * MWPW-140076 Update merch-card to new analytics
    
    * interlinks
    
    * keep reference to attributes.js
    
    ---------
    
    Co-authored-by: vivgoodrich <vivian.goodrich@gmail.com>
    Co-authored-by: Honwai Wong <honstar.wong@gmail.com>

--- libs/blocks/merch-card/merch-card.js
+++ libs/blocks/merch-card/merch-card.js
@@ -1,7 +1,7 @@
 import { decorateButtons, decorateBlockHrs } from '../../utils/decorate.js';
 import { getConfig, createTag } from '../../utils/utils.js';
 import { getMetadata } from '../section-metadata/section-metadata.js';
-import { processTrackingLabels } from '../../martech/analytics.js';
+import { processTrackingLabels } from '../../martech/attributes.js';
 import { replaceKey } from '../../features/placeholders.js';
 import '../../deps/merch-card.js';
 
@@ -195,10 +195,11 @@ const addMerchCardGridIfMissing = (section, cardType) => {
 
 const decorateMerchCardLinkAnalytics = (el) => {
   [...el.querySelectorAll('a')].forEach((link, index) => {
+    const config = getConfig();
     const heading = el.querySelector('h3');
-    const linkText = `${processTrackingLabels(link.textContent)}-${index + 1}`;
-    const headingText = heading ? `${heading.textContent}` : '';
-    const analyticsString = heading ? `${linkText}|${headingText}` : linkText;
+    const linkText = `${processTrackingLabels(link.textContent, config)}-${index + 1}`;
+    const headingText = heading ? `${processTrackingLabels(heading.textContent, config)}` : '';
+    const analyticsString = heading ? `${linkText}--${headingText}` : linkText;
     link.setAttribute('daa-ll', analyticsString);
   });
 };
