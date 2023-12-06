8a3b5303dbe76f07db5d7ad3bff96622b2fc056f
vivgoodrich <vivian.goodrich@gmail.com>
2023-12-06 13:21:22 -0700

    restore processMartechAttributeMetadata to menu

--- libs/blocks/global-navigation/utilities/menu/menu.js
+++ libs/blocks/global-navigation/utilities/menu/menu.js
@@ -303,6 +303,7 @@ const decorateMenu = (config) => logErrorFor(async () => {
     if (res.status !== 200) return;
     const content = await res.text();
     const parsedContent = await replaceText(content, getFedsPlaceholderConfig(), undefined, 'feds');
+    processMartechAttributeMetadata(parsedContent);
     const menuContent = toFragment`<div class="feds-menu-content">${parsedContent}</div>`;
     menuTemplate = toFragment`<div class="feds-popup">
         <div class="feds-menu-container">
