f1dacdba7423b79ac40d31947a51a4f83bd6708f
Suhani Jain <suhjain@Suhanis-MacBook-Pro.local>
2023-12-11 08:56:31 +0530

    review changes

--- libs/blocks/youtube/youtube.js
+++ libs/blocks/youtube/youtube.js
@@ -1,11 +1,11 @@
 import { createIntersectionObserver, isInTextNode } from '../../utils/utils.js';
 
 export default function init(a) {
-  const searchParams = new URLSearchParams(a.search);
-  const id = searchParams.get('v') || a.pathname.split('/').pop();
   const embedVideo = () => {
     if (isInTextNode(a) || !a.origin?.includes('youtu')) return;
     const title = !a.textContent.includes('http') ? a.textContent : 'Youtube Video';
+    const searchParams = new URLSearchParams(a.search);
+    const id = searchParams.get('v') || a.pathname.split('/').pop();
     searchParams.delete('v');
     const src = `https://www.youtube.com/embed/${id}?${searchParams.toString()}`;
     const embedHTML = `
@@ -20,13 +20,12 @@ export default function init(a) {
     </div>`;
     a.insertAdjacentHTML('afterend', embedHTML);
     a.remove();
+    document.addEventListener('readystatechange', () => {
+      if (document.readyState === 'complete') {
+        /* eslint-disable-next-line no-underscore-dangle */
+        window._satellite?.track('trackYoutube');
+      }
+    })
   };
   createIntersectionObserver({ el: a, callback: embedVideo });
-  document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-      /* eslint-disable-next-line no-underscore-dangle */
-      window._satellite?.track('trackYoutube');
-      document.onreadystatechange = null;
-    }
-  };
 }
