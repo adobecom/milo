9d9d4d0b5a83122a13b4edf89de2d7f06f5266a8
nsivakum <nsivakum@adobe.com>
2023-12-12 12:04:33 +0530

    importing origin

--- libs/blocks/floodgateui/floodgate/promoteStatus.js
+++ libs/blocks/floodgateui/floodgate/promoteStatus.js
@@ -2,6 +2,7 @@ import { html, useEffect, useState, Component } from '../../../deps/htm-preact.j
 import { accessToken } from '../../../tools/sharepoint/state.js';
 import getServiceConfig from '../../../utils/service-config.js';
 import { getServiceConfigFg, getParamsFg, postData } from '../utils/miloc.js';
+import { origin } from '../../locui/utils/franklin.js';
 
 class PromoteStatusModal extends Component {
   constructor() {
