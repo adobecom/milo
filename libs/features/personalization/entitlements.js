d9cfe2dd22dbc5829cff5c1ccb656505075d4d95
ChrisChrisChris <chrischrischris@users.noreply.github.com>
2023-12-14 16:48:16 -0500

    Add cc-all-apps

--- libs/features/personalization/entitlements.js
+++ libs/features/personalization/entitlements.js
@@ -3,6 +3,7 @@ import { getConfig } from '../../utils/utils.js';
 export const ENTITLEMENT_MAP = {
   '9565ef55-faad-430b-b661-596ba7a036c4': 'all-apps',
   'c8d50cc2-491e-48df-a1b0-1509f0ca7323': 'photoshop',
+  'fd30e9c7-9ae9-44db-8e70-5c652a5bb1d2': 'cc-all-apps',
 };
 
 const getEntitlements = (data) => {
