97a35c4bc04e4024d0ddc3da9686babad9d148ca
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2023-12-20 16:02:08 +0100

    MWPW-139915: Spectrum Web Components delivery
    for commerce

---
+++ libs/features/spectrum-web-components/src/reactive-controllers.js
@@ -0,0 +1,2 @@
+export * from '@spectrum-web-components/reactive-controllers/src/index.js';
+export * from '@spectrum-web-components/reactive-controllers/src/ElementResolution.js';
---
+++ libs/features/spectrum-web-components/src/theme.js
@@ -0,0 +1,5 @@
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/theme-light.js';
+import '@spectrum-web-components/theme/scale-medium.js';
+
+export * from '@spectrum-web-components/theme';
