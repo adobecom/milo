479184bfada764c5f73414843a2ef285ba01eb19
Sean Choi <seanchoi@adobe.com>
2024-01-09 13:17:44 -0700

    remove colsole log

--- libs/blocks/faas/utils.js
+++ libs/blocks/faas/utils.js
@@ -33,7 +33,6 @@ const base = miloLibs || codeRoot;
 export const faasHostUrl = base.includes('localhost') ? `${base}/deps` : `https://${getFaasHostSubDomain()}apps.enterprise.adobe.com/faas/service`;
 const faasCurrentJS = `${faasHostUrl}/jquery.faas-current.js`;
 export const loadFaasFiles = () => {
-  console.log('base:', base);
   loadStyle(`${base}/blocks/faas/faas.css`);
   return Promise.all([
     loadScript(`${base}/deps/jquery-3.6.0.min.js`).then(() => loadScript(faasCurrentJS)),
