c9c95ef37a9a3decbc61556f1519a88f6d0f2377
vivgoodrich <vivian.goodrich@gmail.com>
2024-01-11 15:25:29 -0700

    MWPW-140268 Icon duplicates if nested in multiple sections

--- libs/features/icons/icons.js
+++ libs/features/icons/icons.js
@@ -57,6 +57,8 @@ export default async function loadIcons(icons, config) {
     if (classList.contains('icon-tooltip')) decorateToolTip(icon);
     const iconName = icon.classList[1].replace('icon-', '');
     if (!iconSVGs[iconName]) return;
+    const existingIcon = icon.querySelector('svg');
+    if (existingIcon) return;
     const parent = icon.parentElement;
     if (parent.childNodes.length > 1) {
       if (parent.lastChild === icon) {
