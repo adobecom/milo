0a7818a73efda5abbf0cfaebca72a7edad59394f
Volodymyr Kniaziev <vkniaziev@adobe.com>
2024-01-19 13:27:10 -0800

    Revert "MWPW-1351731 support linked text (#1749)"
    This reverts commit 104ff1a94b878e0b6a711e7d0d81caddeddf0b3e.

--- libs/blocks/merch/merch.js
+++ libs/blocks/merch/merch.js
@@ -120,19 +120,16 @@ export async function getPriceContext(el, params) {
 }
 
 export async function buildCta(el, params) {
+  const large = !!el.closest('.marquee');
+  const strong = el.firstElementChild?.tagName === 'STRONG' || el.parentElement?.tagName === 'STRONG';
   const context = await getCheckoutContext(el, params);
   if (!context) return null;
   const service = await initService();
-  const isCta = el.textContent?.startsWith('CTA');
   const text = el.textContent?.replace(/^CTA +/, '');
   const cta = service.createCheckoutLink(context, text);
-  if (isCta) {
-    const large = !!el.closest('.marquee');
-    const strong = el.firstElementChild?.tagName === 'STRONG' || el.parentElement?.tagName === 'STRONG';
-    cta.classList.add('con-button');
-    cta.classList.toggle('button-l', large);
-    cta.classList.toggle('blue', strong);
-  }
+  cta.classList.add('con-button');
+  cta.classList.toggle('button-l', large);
+  cta.classList.toggle('blue', strong);
   return cta;
 }
 
