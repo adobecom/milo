b5a196b056f5aa3bf10f31d6dd3225edf7c05843
Drashti Modasara <drashti@Drashtis-MacBook-Pro.local>
2024-01-25 03:38:49 +0530

    spacing

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -86,14 +86,12 @@ export default function init(el) {
     const count = foregroundDiv.querySelectorAll('h3').length;
     foregroundDiv.querySelectorAll('div').forEach((divElem, index) => {
       const h3 = divElem.querySelector('h3');
-      if(count){
+      if (count) {
         if (h3) {
           const sibling = index % 2 === 0 ? divElem.nextElementSibling : divElem.previousElementSibling;
           sibling?.classList.add('hspace');
-          if(index > 1) {
-            foregroundDiv.classList.add('gap-xl');
-            divElem.classList.add('has-heading');
-          }
+          if (index > 0) divElem.classList.add('has-heading');
+          if (index > 1) foregroundDiv.classList.add('gap-xl');
         } else {
           const headingElem = createTag('h3', { class: 'no-heading' });
           divElem.insertBefore(headingElem, divElem.firstChild);
