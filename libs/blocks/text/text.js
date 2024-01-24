fc8d8c2f340bedac8ba8c5f342552e5643411246
Drashti Modasara <drashti@Drashtis-MacBook-Pro.local>
2024-01-25 03:33:25 +0530

    spacing

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -86,20 +86,21 @@ export default function init(el) {
     const foregroundDiv = el.querySelectorAll('.foreground')[1];
     const count = foregroundDiv.querySelectorAll('h3').length;
     foregroundDiv.querySelectorAll('div').forEach((divElem, index) => {
-    const h3 = divElem.querySelector('h3');
-    if(count){
-      if (h3) {
-        const sibling = index % 2 === 0 ? divElem.nextElementSibling : divElem.previousElementSibling;
-        sibling?.classList.add('hspace');
-        if(index > 1) {
-          foregroundDiv.classList.add('gap-xl');
-          divElem.classList.add('has-heading');
+      const h3 = divElem.querySelector('h3');
+      if(count){
+        if (h3) {
+          const sibling = index % 2 === 0 ? divElem.nextElementSibling : divElem.previousElementSibling;
+          sibling?.classList.add('hspace');
+          if(index > 1) {
+            foregroundDiv.classList.add('gap-xl');
+            divElem.classList.add('has-heading');
+          }
+        } else {
+          const headingElem = createTag('h3', { class: 'no-heading' });
+          divElem.insertBefore(headingElem, divElem.firstChild);
         }
-      } else {
-        const headingElem = createTag('h3', { class: 'no-heading' });
-        divElem.insertBefore(headingElem, divElem.firstChild);
       }
-    }});
+    });
   }
   el.classList.add(...helperClasses);
   decorateTextOverrides(el);
