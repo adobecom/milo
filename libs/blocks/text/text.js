a69c7b2cc4a330071545f5660b2e48f917bf7c75
Drashti Modasara <drashti@Drashtis-MacBook-Pro.local>
2024-01-25 10:56:03 +0530

    lint fix

--- libs/blocks/text/text.js
+++ libs/blocks/text/text.js
@@ -88,7 +88,9 @@ export default function init(el) {
       const h3 = divElem.querySelector('h3');
       if (count) {
         if (h3) {
-          const sibling = index % 2 === 0 ? divElem.nextElementSibling : divElem.previousElementSibling;
+          const sibling = index % 2 === 0 
+          ? divElem.nextElementSibling 
+          : divElem.previousElementSibling;
           sibling?.classList.add('hspace');
           if (index > 0) divElem.classList.add('has-heading');
           if (index > 1) foregroundDiv.classList.add('gap-xl');
