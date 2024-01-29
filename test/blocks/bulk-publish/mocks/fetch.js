0b30865f4f14f1f2d0fec592613c364e28dae792
Sartxi <sean.archibeque@gmail.com>
2024-01-29 10:32:28 -0700

    final run

--- test/blocks/bulk-publish/mocks/fetch.js
+++ test/blocks/bulk-publish/mocks/fetch.js
@@ -1,30 +1,31 @@
 import { readFile } from '@web/test-runner-commands';
-import sinon from 'sinon';
+import { stub } from 'sinon';
+
+const requests = {
+  error: 'https://admin.hlx.page/preview/adobecom/milo/error/*',
+  preview: 'https://admin.hlx.page/preview/adobecom/milo/main/*',
+  prevstatus: 'https://admin.hlx.page/job/adobecom/milo/main/preview/job-2024-01-22t21-59-57-639z/details',
+  publish: 'https://admin.hlx.page/live/adobecom/milo/main/*',
+  pubstatus: 'https://admin.hlx.page/job/adobecom/milo/main/publish/job-2024-01-22t21-59-57-639z/details',
+  delete: 'https://admin.hlx.page/preview/adobecom/milo/main/*?paths[]=/drafts/sarchibeque/bulk-publish-test',
+  delstatus: 'https://admin.hlx.page/job/adobecom/milo/main/preview-remove/job-2024-01-24t23-16-20-377z/details',
+  retry: 'https://admin.hlx.page/live/adobecom/milo/main/drafts/sarchibeque/bulk-publish-test',
+  index: 'https://admin.hlx.page/index/adobecom/milo/main/drafts/sarchibeque/bulk-publish-test',
+};
 
 const getMocks = async () => {
-  const tests = {
-    error: 'https://admin.hlx.page/preview/adobecom/milo/error/*',
-    preview: 'https://admin.hlx.page/preview/adobecom/milo/main/*',
-    prevstatus: 'https://admin.hlx.page/job/adobecom/milo/main/preview/job-2024-01-22t21-59-57-639z/details',
-    publish: 'https://admin.hlx.page/live/adobecom/milo/main/*',
-    pubstatus: 'https://admin.hlx.page/job/adobecom/milo/main/publish/job-2024-01-22t21-59-57-639z/details',
-    delete: 'https://admin.hlx.page/preview/adobecom/milo/main/*?paths[]=/drafts/sarchibeque/bulk-publish-test',
-    delstatus: 'https://admin.hlx.page/job/adobecom/milo/main/preview-remove/job-2024-01-24t23-16-20-377z/details',
-    retry: 'https://admin.hlx.page/live/adobecom/milo/main/drafts/sarchibeque/bulk-publish-test',
-    index: 'https://admin.hlx.page/index/adobecom/milo/main/drafts/sarchibeque/bulk-publish-test',
-  };
   const mocks = [];
-  Object.keys(tests).forEach(async (test) => {
-    const data = await readFile({ path: `./mocks/response/${test}.json` });
-    const method = test === 'delete' ? 'DELETE' : 'POST';
-    mocks.push({ url: tests[test], method, data, status: test === 'error' ? 401 : 200 });
+  Object.keys(requests).forEach(async (request) => {
+    const data = await readFile({ path: `./mocks/response/${request}.json` });
+    const method = request === 'delete' ? 'DELETE' : 'POST';
+    mocks.push({ url: requests[request], method, data, status: request === 'error' ? 401 : 200 });
   });
   return mocks;
 };
 
 export async function mockFetch() {
   const mocks = await getMocks();
-  sinon.stub(window, 'fetch').callsFake((...args) => {
+  stub(window, 'fetch').callsFake((...args) => {
     const [resource] = args;
     const response = mocks.find((mock) => mock.url === resource);
     const { status, data } = response;
