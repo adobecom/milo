445e6d8d976448e29614b5b53b7e70a1ab6ab6d2
Elan Bartholomew <elan@thinkingbox.com>
2024-01-31 10:09:28 -0700

    update codeowners, update build flow

--- libs/blocks/martech-metadata-generator/martech-metadata-generator.js
+++ libs/blocks/martech-metadata-generator/martech-metadata-generator.js
@@ -1,15 +1,17 @@
 import parseMd from '../../../tools/loc/helix/parseMarkdown.bundle.js';
-import { createTag } from '../../utils/utils.js';
+import { createTag, getConfig } from '../../utils/utils.js';
+import { replaceText } from '../../features/placeholders.js';
 
+const config = getConfig();
 const params = new URLSearchParams(window.location.search);
 const referrer = params.get('referrer');
 const owner = params.get('owner');
 const repo = params.get('repo');
 const ref = params.get('ref');
-const formEl = createTag('form', { action: '#' });
-const statusEl = createTag('p', {}, 'Click Build to generate metadata table');
-const buildBtn = createTag('button', { class: 'con-button' }, 'Build');
-const copyBtn = createTag('button', { class: 'con-button hidden' }, 'Copy');
+const formEl = createTag('form', { action: '#', class: 'loading' });
+const statusEl = createTag('p', {}, 'Generating table, please wait...');
+const copyBtn = createTag('button', { class: 'con-button' }, 'Copy');
+const buildBtn = createTag('button', { class: 'con-button' }, 'Rebuild');
 let clipboardData = '';
 
 function getTable(strings) {
@@ -24,7 +26,6 @@ function getTable(strings) {
     tr.append(createTag('td', { colspan: 1 }, createTag('h3', { 'data-ccp-parastyle': 'DNT' }, str)));
     table.append(tr);
   });
-  console.log(table);
   return table.outerHTML;
 }
 
@@ -43,14 +44,12 @@ function handleError(e, msg = 'An unknown error occurred') {
 function handleSuccess() {
   formEl.classList.remove('loading');
   buildBtn.innerText = 'Rebuild';
-  copyBtn.classList.remove('hidden');
-  statusEl.innerText = 'Table built and copied, ready to paste into document';
+  statusEl.innerText = 'Table built, click Copy and paste into document';
 }
 
 async function handleBuild() {
   formEl.classList.add('loading');
-  statusEl.innerText = 'Please wait...';
-  buildBtn.innerText = 'Loading...';
+  statusEl.innerText = 'Generating table, please wait....';
   let json;
   let md;
   try {
@@ -58,14 +57,11 @@ async function handleBuild() {
     json = await res.json();
   } catch (e) { return handleError(e); }
   try {
-    const res = await fetch(
+    await fetch(
       `https://admin.hlx.page/preview/${owner}/${repo}/${ref}/${json.webPath}`,
       { method: 'POST' },
     );
-    const newJson = await res.json();
-    console.log(newJson);
-  } catch (e) { return handleError(e); }
-  if (json.preview.status === 404) return handleError(404, 'Failed to preview document');
+  } catch (e) { return handleError('Failed to preview document'); }
   try {
     const mdFetch = await fetch(`${json.resourcePath}`);
     md = await mdFetch.text();
@@ -73,20 +69,20 @@ async function handleBuild() {
   const doc = { content: { data: md }, log: '' };
   parseMd(doc);
   const items = [];
-  const searchItems = (arr) => {
+  const searchItems = async (arr) => {
     for (const child of arr) {
       if (child.type.match(/link|heading/) && !child.children[0].value?.startsWith('http')) {
-        items.push(child.children[0].value);
+        const str = await replaceText(child.children[0].value, config);
+        if (str) items.push(str);
         /* eslint-disable-next-line no-continue */
         continue;
       }
-      if (child.children) searchItems(child.children);
+      if (child.children) await searchItems(child.children);
     }
   };
-  searchItems(doc.content.mdast.children);
+  await searchItems(doc.content.mdast.children);
   /* global ClipboardItem */
-  clipboardData = [new ClipboardItem({ 'text/plain': new Blob([getTable(items)], { type: 'text/plain' }) })];
-  handleCopy();
+  clipboardData = [new ClipboardItem({ 'text/html': new Blob([getTable(items)], { type: 'text/html' }) })];
   handleSuccess();
   return true;
 }
@@ -94,8 +90,8 @@ async function handleBuild() {
 export default async function init(el) {
   el.classList.add('con-block', 'dark');
   formEl.append(statusEl);
-  formEl.append(buildBtn);
   formEl.append(copyBtn);
+  formEl.append(buildBtn);
   buildBtn.addEventListener('click', (e) => {
     e.preventDefault();
     handleBuild();
@@ -105,4 +101,5 @@ export default async function init(el) {
     handleCopy();
   });
   el.append(formEl);
+  handleBuild();
 }
