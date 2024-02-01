efe9d9302971c94b7ff5bcb0584d4aa9b68efa39
mathuria <mathuria@adobe.com>
2024-02-01 19:30:02 +0530

    Icon stack

--- libs/blocks/brick/brick.js
+++ libs/blocks/brick/brick.js
@@ -112,6 +112,7 @@ function decorateBrickIconStack(el) {
     if (icn) liEl.append(icn, txt);
     liEl.append(txt);
     txt.querySelector('picture')?.remove();
+    txt.querySelector('a:empty')?.remove();
   });
 }
 
