a432e854cb068a2b05a10162e0049336a6ee077d
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-02-02 12:28:12 +0100

    remove async from decorate function

--- libs/blocks/mnemonic-list/mnemonic-list.js
+++ libs/blocks/mnemonic-list/mnemonic-list.js
@@ -1,6 +1,6 @@
 import { createTag } from '../../utils/utils.js';
 
-export const decorateMnemonicList = async (container) => {
+export const decorateMnemonicList = (container) => {
   const mnemonicListElement = container.querySelector('.mnemonic-list');
   const targetElement = mnemonicListElement || container;
   const rows = targetElement.querySelectorAll(':scope p:not([class])');
