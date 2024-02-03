86416e5d0a5479860f3b3862b383099eee4d08f3
Carlos Miqueo <cmiqueo@adobe.com>
2024-02-02 16:30:38 -0800

    working progress

--- libs/blocks/caas-marquee-metadata/caas-marquee-metadata.js
+++ libs/blocks/caas-marquee-metadata/caas-marquee-metadata.js
@@ -1,5 +1,4 @@
 import { createTag } from '../../utils/utils.js';
-import { renderMarquee } from '../caas-marquee/caas-marquee.js';
 
 export function getMetadata(el) {
   const metadata = {};
@@ -11,10 +10,11 @@ export function getMetadata(el) {
     } else if (key.match(/^cta/)) {
       const ctaLink = child.querySelectorAll('a');
       ctaLink?.forEach((link, index) => {
-        metadata[`cta${index + 1}url`] = link.href;
-        metadata[`cta${index + 1}text`] = link.textContent.trim();
+        const count = index + 1;
+        metadata[`cta${count}url`] = link.href;
+        metadata[`cta${count}text`] = link.textContent.trim();
         /* eslint-disable no-nested-ternary */
-        metadata[`cta${index + 1}style`] = link.parentNode.tagName === 'STRONG' ? 'blue'
+        metadata[`cta${count}style`] = link.parentNode.tagName === 'STRONG' ? 'blue'
           : link.parentNode.tagName === 'EM' ? 'outline' : '';
         /* eslint-enable no-nested-ternary */
       });
@@ -34,7 +34,8 @@ export default function init(el) {
   const arbitraryValue = createTag('div', null, (`promoId: ${metadata.promoid},
     imageTablet: ${metadata.imagetablet || ''},
     imageDesktop: ${metadata.imagedesktop || ''},
-    variant: ${metadata.variant},
+    variant: ${metadata.variant || ''},
+    context: ${metadata.context || ''},
   `).replace(/\s+/g, ' '));
   arbitrary.append(arbitraryKey, arbitraryValue);
   el.append(arbitrary);
@@ -42,14 +43,8 @@ export default function init(el) {
   el.innerHTML += `<div><div>tags</div><div>caas:content-type/promotion</div></div>
     <div><div>cta1url</div><div>${metadata.cta1url}</div></div>
     <div><div>cta1text</div><div>${metadata.cta1text}</div></div>
-    <div><div>cta1style</div><div>${metadata.cta1style}</div></div>`;
-
-  const marquee = createTag('div', { class: `marquee ${metadata.variant.replaceAll(',', ' ')}` });
-  el.parentNode.prepend(marquee);
-
-  // Render marquee
-  renderMarquee(marquee, metadata);
-
-  // Degugging ((( Remove before release )))
-  console.log('metadata:', getMetadata(el)); // eslint-disable-line no-console
+    <div><div>cta1style</div><div>${metadata.cta1style}</div></div>
+    <div><div>cta2url</div><div>${metadata.cta2url}</div></div>
+    <div><div>cta2text</div><div>${metadata.cta2text}</div></div>
+    <div><div>cta2style</div><div>${metadata.cta2style}</div></div>`;
 }
