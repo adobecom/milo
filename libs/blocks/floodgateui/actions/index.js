cd117f291e32e36b0fb8e1d8efa8e96e33f8f91c
nsivakum <nsivakum@adobe.com>
2024-02-05 14:58:57 +0530

    handle video assets

--- libs/blocks/floodgateui/actions/index.js
+++ libs/blocks/floodgateui/actions/index.js
@@ -15,7 +15,10 @@ function getSanitizedUrl(link) {
 }
 
 function isReferencedAsset(link, baseUrlOrigin) {
-  return link && link.startsWith(baseUrlOrigin) && (link.endsWith('.svg') || link.endsWith('.pdf') || link.endsWith('.mp4'));
+  if (link && link.includes('#_autoplay')) {
+    link = link.replace('#_autoplay', '');
+  }
+  return link && link.startsWith(baseUrlOrigin) && (link.endsWith('.svg') || link.endsWith('.pdf') || (link.endsWith('.mp4') && link.includes('/assets/')));
 }
 
 function updateDomWithBaseUrl(dom, url) {
