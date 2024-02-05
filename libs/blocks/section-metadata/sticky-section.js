927e7c05b077806ccc7ac1381f7ad22604f9f028
Ruchika Sinha <tek10248@Ruchikas-MacBook-Pro.local>
2024-02-05 15:53:17 -0800

    variable name

--- libs/blocks/section-metadata/sticky-section.js
+++ libs/blocks/section-metadata/sticky-section.js
@@ -55,7 +55,7 @@ export default async function handleStickySection(sticky, section) {
       if (section.querySelector('.promobar')) {
         const metadata = getMetadata(section.querySelector('.section-metadata'));
         const delay = metadata.delay?.text;
-        if (delay) setTimeout(() => { handleStickyPromobar(section, delay); }, metadata.delay.text);
+        if (delay) setTimeout(() => { handleStickyPromobar(section, delay); }, delay);
         else handleStickyPromobar(section, delay);
       }
       main.append(section);
