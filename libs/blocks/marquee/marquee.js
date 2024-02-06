cedd15fde583b5c5beb0bd0918fcfbaf61a088ea
Anastasiya Kobrina <akobrina@adobe.com>
2024-02-06 13:32:54 -0800

    AK - new variant console log

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -81,6 +81,7 @@ export async function loadMnemonicList(foreground) {
 export default async function init(el) {
   const excDark = ['light', 'quiet'];
   if (!excDark.some((s) => el.classList.contains(s))) el.classList.add('dark');
+  if (el.classList.contains('new-variant')) console.log('Hello from new variant!');
   const children = el.querySelectorAll(':scope > div');
   const foreground = children[children.length - 1];
   if (children.length > 1) {
