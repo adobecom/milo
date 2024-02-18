df9f2d9c9b408e1636474a442fedfa0b8a796340
Sanjay Rai <sanrai@adobe.com>
2024-02-18 03:17:23 -0800

    Finalizing getting code Prod ready

--- libs/blocks/caas-marquee/caas-marquee.js
+++ libs/blocks/caas-marquee/caas-marquee.js
@@ -1,25 +1,29 @@
-// TODO: Fix tablet responsive class issue
-// TODO: Check line about logging 'default' segments and see if it's still relevant
-// TODO: Check LH scores after performance improvements.
-// TODO: Figure out workaround if segments API takes too long to load
-// TODO: Test network latency and if code handles that correctly
-// TODO: Go through all code paths to make sure no exceptions occur
-// TODO: Fix variants inconsistently supporting both ',' and '' (lines 95, 115, 198, 213)
-// TODO: Update SEGMENT_MAP with final from Martech team
-// TODO: Update Spectra AI endpoint to final one (instead of pointing to local Chimera IO instance)
-
 import { getMetadata } from '../caas-marquee-metadata/caas-marquee-metadata.js';
 import { createTag, getConfig } from '../../utils/utils.js';
 
-// TODO: Final list needs to come from Target List before release
-const SEGMENT_MAP = {
-  '5a5fd14e-f4ca-49d2-9f87-835df5477e3c': 'PHSP',
-  '09bc4ba3-ebed-4d05-812d-a1fb1a7e82ae': 'IDSN',
-  '25ede755-7181-4be2-801e-19f157c005ae': 'ILST',
-  '07609803-48a0-4762-be51-94051ccffb45': 'PPRO',
-  '73c3406b-32a2-4465-abf3-2d415b9b1f4f': 'AEFT',
-  'bf632803-4412-463d-83c5-757dda3224ee': 'CCSN',
-};
+const SEGMENTS_MAP = {
+  stage: {
+    '5a5fd14e-f4ca-49d2-9f87-835df5477e3c': 'PHSP',
+    '09bc4ba3-ebed-4d05-812d-a1fb1a7e82ae': 'IDSN',
+    '25ede755-7181-4be2-801e-19f157c005ae': 'ILST',
+    '07609803-48a0-4762-be51-94051ccffb45': 'PPRO',
+    '73c3406b-32a2-4465-abf3-2d415b9b1f4f': 'AEFT',
+    'bf632803-4412-463d-83c5-757dda3224ee': 'CCSN',
+  },
+  prod: {
+    '51b1f617-2e43-4e91-a98a-3b7716ecba8f': 'PHSP',
+    '8d3c8ac2-2937-486b-b6ff-37f02271b09b': 'ILST',
+    '8ba78b22-90fb-4b97-a1c4-f8c03a45cbc2': 'IDSN',
+    'fd30e9c7-9ae9-44db-8e70-5c652a5bb1d2': 'CCSN',
+    '4e2f2a6e-48c4-49eb-9dd5-c44070abb3f0': 'AEFT',
+    'e7650448-268b-4a0d-9795-05f604d7e42f': 'LPES',
+    '619130fc-c7b5-4b39-a687-b32061326869': 'PPRO',
+    'cec4d899-4b41-469e-9f2d-4658689abf29': 'PHLT',
+    '8da44606-9841-43d0-af72-86d5a9d3bba0': 'Any cc product with stock add-ons active users',
+    'ab713720-91a2-4e8e-b6d7-6f613e049566': 'Any CC product without stock add-ons active users',
+    '934fdc1d-7ba6-4644-908b-53e01e550086': 'DC Paid Active entitlements',
+  }
+}
 
 const WIDTHS = {
   split: 1199,
@@ -38,6 +42,17 @@ const HEIGHTS = {
 const LANA_OPTIONS = {
   tags: 'caasMarquee',
 };
+
+const API_CONFIGS = {
+  spectra: {
+    prod: 'https://cchome.adobe.io/int/v1/models',
+    stage: 'https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/models'
+  },
+  caas: {
+    prod: 'https://14257-chimera.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection',
+    stage: 'https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection'
+  }
+}
 
 const BUTTON_STYLES = ['blue', 'outline'];
 const prod = isProd();
@@ -61,19 +76,17 @@ function log(...args){
   }
 }
 
-// Our Chimera-SM BE has no caching on lower tiered environments (as of now) and requests will time out for authors
-// showing them fallback content.
 const REQUEST_TIMEOUT = isProd() ? 1500 : 10000;
 const SEGMENT_API_TIMEOUT = 2500;
 
-const TEXT_SIZE = {
+const TEXT = {
   small: 'm',
   medium: 'm',
   large: 'xl',
   xlarge: 'xl'
 }
 
-const HEADING_SIZE = {
+const HEADING = {
   small: 'xl',
   medium: 'xl',
   large: 'xxl',
@@ -86,18 +99,20 @@ const VALID_MODAL_RE = /fragments(.*)#[a-zA-Z0-9_-]+$/;
 
 let segments = [];
 let marquee;
-let cards = [];
+let marquees = [];
 let selectedId = '';
 let metadata;
 let loaded = false;
-let timeout;
 
-timeout = setTimeout(async function(){
+let timeout = setTimeout(async function(){
   clearTimeout(timeout);
   await loadFallback(marquee, metadata);
 }, SEGMENT_API_TIMEOUT);
 
-async function handler(e){
+// See https://experienceleague.adobe.com/docs/experience-platform/destinations/catalog/personalization/custom-personalization.html?lang=en
+// for more information on how to integrate with this API.
+async function segmentApiEventHandler(e){
+    const SEGMENT_MAP = isProd() ? SEGMENTS_MAP.prod : SEGMENTS_MAP.stage;
     if (e.detail.type === 'pageView') {
       let mappedUserSegments = [];
       let userSegments = e.detail?.result?.destinations?.[0]?.segments || [];
@@ -109,41 +124,41 @@ async function handler(e){
       if(mappedUserSegments.length){
         segments = mappedUserSegments;
         selectedId = await getMarqueeId();
-        renderMarquee(marquee, cards, selectedId, metadata);
+        renderMarquee(marquee, marquees, selectedId, metadata);
       }
     }
 }
+window.addEventListener('alloy_sendEvent', (e) => segmentApiEventHandler(e));
 
-// See https://experienceleague.adobe.com/docs/experience-platform/destinations/catalog/personalization/custom-personalization.html?lang=en
-// for more information on how to integrate with this API.
-window.addEventListener('alloy_sendEvent', (e) => handler(e));
+function fetchExceptionHandler(fnName, error){
+  log(`${fnName} fetch caught exception: ${error}`, LANA_OPTIONS);
+  return null;
+}
 
-async function getAllMarquees(promoId, origin) {
-  // TODO: Update this to https://14257-chimera.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection before release
-  const endPoint = 'https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection';
-  const payload = `originSelection=${origin}&promoId=${promoId}&language=en&country=US`;
-
-  // { signal: AbortSignal.timeout(TIMEOUT_TIME) } is way to cancel a request after T seconds using fetch
-  // See https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/timeout_static
-  let response = await fetch(`${endPoint}?${payload}`, {
-    signal: AbortSignal.timeout(REQUEST_TIMEOUT)
-  }).catch(error => {
-    log(`getAllMarquees failed: ${error}`, LANA_OPTIONS);
-    return null;
-  });
-
+async function responseHandler(response, fnName){
   try {
     if(!response || response?.status !== 200){
-      log(`getAllMarquees: Invalid response or status: response: ${response}, status: ${response?.status} `, LANA_OPTIONS);
+      log(`${fnName}: Invalid response or status: response: ${response}, status: ${response?.status} `, LANA_OPTIONS);
       return [];
     }
-    let json = await response?.json();
-    let marquees = json?.cards;
-    return Array.isArray(marquees) ? marquees  : [];
+    return await response?.json() || [];
   } catch(e){
-    log(`getAllMarquees exception: ${e} `, LANA_OPTIONS);
+    log(`${fnName}: response handler exception: ${e} `, LANA_OPTIONS);
     return [];
   }
+}
+
+async function getAllMarquees(promoId, origin) {
+  const endPoint = isProd() ? API_CONFIGS.caas.prod : API_CONFIGS.caas.stage;
+  const payload = `originSelection=${origin}&promoId=${promoId}&language=en&country=US`;
+
+  let response = await fetch(`${endPoint}?${payload}`, {
+    signal: AbortSignal.timeout(REQUEST_TIMEOUT)
+  }).catch(error => fetchExceptionHandler('getAllMarquees', error));
+
+  let json = await responseHandler(response, 'getAllMarquees');
+  let marquees = await json?.cards;
+  return Array.isArray(marquees) ? marquees  : [];
 }
 
 /**
@@ -153,12 +168,10 @@ async function getAllMarquees(promoId, origin) {
 async function getMarqueeId() {
   let visitedLinks = [document.referrer];
   log(`Segments: ${segments} sent to Spectra AI`, LANA_OPTIONS);
-
-  // { signal: AbortSignal.timeout(TIMEOUT_TIME) } is way to cancel a request after T seconds using fetch
-  // See https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal/timeout_static
+  const endPoint = isProd() ? API_CONFIGS.spectra.prod : API_CONFIGS.spectra.stage;
 
   // TODO: Update this to final Spectra AI model before release
-  let response = await fetch('https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/models', {
+  let response = await fetch(endPoint, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -166,21 +179,14 @@ async function getMarqueeId() {
     },
     body: `{"endpoint":"community-recom-v1","contentType":"application/json","payload":{"data":{"visitedLinks": ${visitedLinks}, "segments": ${segments}}}}`,
     signal: AbortSignal.timeout(REQUEST_TIMEOUT),
-  }).catch(error => {
-    log(`getMarqueeId promise caught: ${error}`, LANA_OPTIONS);
-    return null;
-  });
-  try {
-    if(!response || response?.status !== 200){
-      log(`getMarqueeId: Invalid response or status: response: ${response}, status: ${response?.status} `, LANA_OPTIONS);
-      return '';
-    }
-    let json = await response?.json();
-    return json?.data?.[0]?.content_id || '';
-  } catch(e){
-    log(`getMarqueeId exception: ${e} `, LANA_OPTIONS);
-    return '';
-  }
+  }).catch(error => fetchExceptionHandler('getMarqueeId', error));
+
+  let json = await responseHandler(response, 'getMarqueeId');
+  return json?.data?.[0]?.content_id || '';
+}
+
+function getArbitraryByKey(data, key) {
+  return data.arbitrary?.find(item => item.key === key)?.value || '';
 }
 
 /**
@@ -190,8 +196,8 @@ async function getMarqueeId() {
  */
 function normalizeData(data) {
   const images = {
-    tablet: data.arbitrary?.find((item) => item.key === 'imageTablet')?.value || '',
-    desktop: data.arbitrary?.find((item) => item.key === 'imageDesktop')?.value || '',
+    tablet: getArbitraryByKey(data, 'imageTablet'),
+    desktop: getArbitraryByKey(data, 'imageDesktop')
   };
 
   const metadata = {
@@ -213,9 +219,31 @@ function normalizeData(data) {
   const arbitrary = {};
   data.arbitrary?.forEach((item) => { arbitrary[item.key] = item.value; });
   metadata.variant = arbitrary.variant || 'dark, static-links';
-  metadata.backgroundcolor = arbitrary.backgroundColor;
+  metadata.backgroundcolor = arbitrary.backgroundColor || '';
 
   return metadata;
+}
+
+function getDefaultMetadata(){
+  return {
+    id: '',
+    title: '',
+    description: '',
+    details: '',
+    image: '',
+    imagetablet: '',
+    imagedesktop: '',
+    cta1url: '',
+    cta1text: '',
+    cta1style: '',
+    cta2url: '',
+    cta2text: '',
+    cta2style: '',
+    variant: '',
+    backgroundcolor: '',
+    promoid: '',
+    origin: ''
+  };
 }
 
 function getVideoHtml(src){
@@ -253,26 +281,15 @@ function getContent(src, screen){
   return `<div class=${screen}-only>${inner}</div>`
 }
 
-function getLoadingSpinnerHtml(){
-  const spinner = `<div class="lds-ring LOADING">
+function addLoadingSpinner(marquee){
+  marquee.innerHTML = `<div class="lds-ring LOADING">
                     <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
                   </div>`;
-  return spinner;
 }
 
-/*
-  Note: Modal must be written exactly in this format to be picked up by milo decorators.
-  Other formats/structures will not work.
-  <a
-    href="#abc"
-    data-modal-path="/fragment/path-to-fragment"
-    data-modal-hash="#abc">
-      Some Modal Text
-  </a>
- */
 function getModalHtml(ctaUrl, classes, ctaText){
   let [fragment, hash] = ctaUrl.split("#");
   return `<a href="#${hash}" data-modal-path="${fragment}" data-modal-hash="#${hash}" daa-ll="${ctaText}" class="modal link-block ${classes}">${ctaText}</a>`
@@ -288,88 +305,107 @@ function getCtaHtml(url, text, classes){
 const isValidModal = (u) => VALID_MODAL_RE.test(u);
 
 function getCtaClasses(ctaStyle, size){
-  return BUTTON_STYLES.includes(ctaStyle) ? `con-button ${ctaStyle} button-${TEXT_SIZE[size]} button-justified-mobile` : '';
+  return BUTTON_STYLES.includes(ctaStyle) ? `con-button ${ctaStyle} button-${TEXT[size]} button-justified-mobile` : '';
+}
+
+function getSize(classList){
+  const sizes = ['small', 'medium', 'large'];
+  for (const size of sizes) {
+    if (classList.includes(size)) {
+      return size;
+    }
+  }
+  return 'xlarge';
+}
+
+function removeLoader(marquee){
+  marquee.innerHTML = '';
+}
+
+function addBackground(marquee, metadata) {
+  if(metadata.backgroundcolor){
+    marquee.style.backgroundColor = metadata.backgroundcolor;
+  }
+}
+
+function applyVariants(marquee, metadata, classList){
+  if (metadata.variant) {
+    marquee.classList.add(...classList);
+  }
+}
+
+function getFgContent(metadata, size, cta, cta2){
+  return `<div class="text">
+    <p class="detail-l">${metadata.details}</p>
+    <h1 class="heading-${HEADING[size]}">${metadata.title}</h1>
+    <p class="body-${TEXT[size]}">${metadata.description}</p>
+    <p class="action-area">
+      ${cta} 
+      ${cta2}
+      </p>  
+  </div>`
+}
+
+function getReverseFiller(){
+  return `<div data-valign="middle" class="media image"></div>`;
+}
+
+function createBackground(splitContent){
+  let parser = new DOMParser();
+  let el = parser.parseFromString(splitContent, 'text/html');
+  return el?.body?.childNodes[0];
 }
 
 /**
  * function renderMarquee()
  * @param {HTMLElement} marquee - marquee container
- * @param {Array} cards - marquee data
+ * @param {Array} marquees - marquee data
  * @param {string} id - marquee id
  * @returns {void}
  */
-export function renderMarquee(marquee, cards, id, fallback) {
+export function renderMarquee(marquee, marquees, id, fallback) {
   if(loaded){
     return;
   }
   loaded = true;
-  let chosen = cards?.find(obj => obj.id === id);
-  let shouldRenderMarquee = cards?.length && chosen;
-  const metadata = shouldRenderMarquee ? normalizeData(chosen) : fallback;
+  let found = marquees?.find(obj => obj.id === id);
+  let shouldRenderMarquee = marquees?.length && found;
+  const metadata = shouldRenderMarquee ? normalizeData(found) : fallback;
 
-  // remove loader
-  marquee.innerHTML = '';
-  if(metadata.backgroundcolor){
-    marquee.style.backgroundColor = metadata.backgroundcolor;
-  }
+  removeLoader(marquee);
+  addBackground(marquee, metadata);
 
-  // configure block font sizes
-  const classList = metadata.variant.split(',').map((c) => c.trim());
-  const isSplit = metadata.variant.includes('split');
-  const isReversed = metadata.variant.includes('row-reversed');
-  // TODO: Update this to using a map to prevent nested ternaries
-  /* eslint-disable no-nested-ternary */
-  const size = classList.includes('small') ? 'small'
-    : classList.includes('medium') ? 'medium'
-      : classList.includes('large') ? 'large'
-        : 'xlarge';
-  /* eslint-enable no-nested-ternary */
+  const classList = getClasses(metadata.variant);
+  const isSplit = classList.includes('split');
+  const isReversed = classList.includes('row-reversed');
+  const size = getSize(classList);
+  const useReverseFiller = isReversed && !isSplit;
 
-  // background content
   const mobileBgContent = getContent(metadata.image, 'mobile');
   const tabletBgContent = getContent(metadata.imagetablet, 'tablet');
   const desktopBgContent = getContent(metadata.imagedesktop, 'desktop');
   const splitContent = getContent(metadata.imagedesktop, 'split');
 
   const bgContent = `${mobileBgContent}${tabletBgContent}${desktopBgContent}`;
-  let background = createTag('div', { class: 'background' });
+  let background = createTag('div', { class: 'background' }, '');
   if(isSplit) {
-    let parser = new DOMParser();
-    background = parser.parseFromString(splitContent, 'text/html').body.childNodes[0];
+    background = createBackground(splitContent);
   } else {
     background.innerHTML = bgContent;
   }
 
   let cta1Classes = getCtaClasses(metadata.cta1style, size);
   let cta2Classes = getCtaClasses(metadata.cta2style, size);
-  const reversedFiller = isReversed && !isSplit
-    ? '<div data-valign="middle" class="media image"></div>'
-    : '';
+  const reversedFiller = useReverseFiller ? getReverseFiller() : '';
 
-  // foreground content
   let cta = getCtaHtml(metadata.cta1url, metadata.cta1text, cta1Classes);
   let cta2 = getCtaHtml(metadata.cta2url, metadata.cta2text, cta2Classes);
 
-  const fgContent = `${reversedFiller}<div class="text">
-    <p class="detail-l">${metadata.details}</p>
-    <h1 class="heading-${HEADING_SIZE[size]}">${metadata.title}</h1>
-    <p class="body-${TEXT_SIZE[size]}">${metadata.description}</p>
-    <p class="action-area">
-      ${cta} 
-      ${cta2}
-      </p>  
-  </div>`;
-
-  const foreground = createTag('div', { class: 'foreground container' });
+  const fgContent = `${reversedFiller}${getFgContent(metadata, size, cta, cta2)}`;
+  const foreground = createTag('div', { class: 'foreground container' }, '');
   foreground.innerHTML = fgContent;
 
-  // apply marquee variant to viewer
-  if (metadata.variant) {
-    const classes = getClasses(metadata.variant);
-    marquee.classList.add(...classes);
-  }
-
-  // Note: Added data-block so marquee can be picked up by milo analytics decorators
+  applyVariants(marquee, metadata, classList);
   addAnalytics(marquee);
   marquee.append(background, foreground);
 }
@@ -379,11 +415,24 @@ function addAnalytics(marquee){
 }
 
 function getClasses(variant){
-  return variant.split(' ').map((c) => c.trim()).filter(i => i !== '');
+  return variant.split(/\s+|,/).map((c) => c.trim()).filter(i => i !== '');
 }
 
 function loadFallback(marquee, metadata){
   renderMarquee(marquee, [], '', metadata);
+}
+
+function shouldLoadFallback(){
+  return urlParams.get('previewFallback') || urlParams.get('martech');
+}
+
+function authorPreview(){
+  return urlParams.get('marqueeId');
+}
+
+function handleAuthoringMistakes(metadata){
+  let safeMetadata = {...getDefaultMetadata(), ...metadata};
+  return safeMetadata;
 }
 
 /**
@@ -392,38 +441,22 @@ function loadFallback(marquee, metadata){
  */
 export default async function init(el) {
   metadata = getMetadata(el);
+  metadata = handleAuthoringMistakes(metadata);
   const promoId = metadata.promoid;
   const origin = getConfig().chimeraOrigin || metadata.origin;
-
-  // We shouldn't be adding variant properties from the viewer table as the requirements are each marquee has
-  // all their viewing properties completely self-contained.
-  // const marquee = createTag('div', { class: `marquee split ${metadata.variant.replaceAll(',', ' ')}` });
-  marquee = createTag('div', { class: `marquee` });
-
-  // Only in the case of a fallback should we use the variant fields from the viewer table.
-  marquee.innerHTML = getLoadingSpinnerHtml();
+  marquee = createTag('div', { class: `marquee` }, '');
+  addLoadingSpinner(marquee);
   el.parentNode.prepend(marquee);
 
-  if (urlParams.get('previewFallback') || urlParams.get('martech')) {
-    // This query param ensures authors can verify the fallback looks good before publishing live.
-    // Requirement:
-    // As long as we add easy way for authors to preview their fallback content (via query param)
-    // Then we don't have to hardcode any fallbacks in the code.
+  if (shouldLoadFallback()) {
     loadFallback(marquee, metadata);
     return;
   }
 
-  /*
-    Note: We cannot do the following code to get the Marquees
-    due to performance issues.
-
-    const cards = await getAllMarquees();
-    This causes the code to run synchronously and block (has been empirically verified).
-  */
   getAllMarquees(promoId, origin).then(resp => {
-    cards = resp;
-    if (urlParams.get('marqueeId')) {
-      renderMarquee(marquee, cards, urlParams.get('marqueeId'), metadata);
+    marquees = resp;
+    if (authorPreview()) {
+      renderMarquee(marquee, marquees, urlParams.get('marqueeId'), metadata);
     }
   });
 }
