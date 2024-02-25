8a7948d6e056cc09a270fa037a992faf9e8fa7e4
Sanjay Rai <sanrai@adobe.com>
2024-02-24 22:13:45 -0800

    Changing back to test endpoints

--- libs/blocks/caas-marquee/caas-marquee.js
+++ libs/blocks/caas-marquee/caas-marquee.js
@@ -49,8 +49,8 @@ const API_CONFIGS = {
     stage: 'https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/models',
   },
   caas: {
-    prod: 'https://www.adobe.com/chimera-api/sm-collection',
-    stage: 'https://14257-chimera-stage.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection',
+    prod: 'https://14257-chimera.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection',
+    stage: 'https://14257-chimera-sanrai.adobeioruntime.net/api/v1/web/chimera-0.0.1/sm-collection',
   },
 };
 
