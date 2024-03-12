8c1987b4031c7cc1a99e85f93b69e60fed025506
Okan Sahin <osahin@adobe.com>
2024-03-12 19:13:09 +0100

    Fix formatting

--- .github/workflows/update-ims.js
+++ .github/workflows/update-ims.js
@@ -2,45 +2,49 @@ const https = require('https');
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-const fetch = () => new Promise((resolve, reject) => {
-  https.get('https://auth.services.adobe.com/imslib/imslib.min.js', (res) => {
-    if (res.statusCode < 200 || res.statusCode >= 300) {
-      return reject(new Error('statusCode=' + res.statusCode));
-    }
+const fetch = () =>
+  new Promise((resolve, reject) => {
+    https
+      .get('https://auth.services.adobe.com/imslib/imslib.min.js', (res) => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          return reject(new Error('statusCode=' + res.statusCode));
+        }
 
-    let data = '';
+        let data = '';
 
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
 
-    res.on('end', () => {
-      resolve({
-        data,
-        headers: res.headers
+        res.on('end', () => {
+          resolve({
+            data,
+            headers: res.headers,
+          });
+        });
+      })
+      .on('error', (err) => {
+        reject(err);
       });
-    });
-
-  }).on('error', (err) => {
-    reject(err);
   });
-});
 
-const main = async ({github, context, local = false}) => {
+const main = async ({ github, context, local = false }) => {
   const response = await fetch();
   const lastModified = new Date(response.headers['last-modified']);
   const now = new Date();
   const diffHours = Math.ceil((now - lastModified) / 1000 / 60 / 60);
-  console.log(`imslib.min.js last modified: ${lastModified}. Last modified ${diffHours} hours ago <= 24H, means new PR created.`);
+  console.log(
+    `imslib.min.js last modified: ${lastModified}. Last modified ${diffHours} hours ago <= 24H, means new PR created.`
+  );
 
   if (diffHours <= 2400 || local) {
-    console.log("Diff detected in the last 24h, creating PR.")
+    console.log('Diff detected in the last 24h, creating PR.');
     const header = `// Last modified ${lastModified.toISOString()}\n// Build at ${new Date().toISOString()}\n`;
 
-    if (local) return console.log("Local mode - skipped git commands.")
+    if (local) return console.log('Local mode - skipped git commands.');
     // If you run this on local, this might overwrite ur current git configs.
-    if(!local) execSync('git config --global user.name "GitHub Action"');
-    if(!local) execSync('git config --global user.email "action@github.com"');
+    if (!local) execSync('git config --global user.name "GitHub Action"');
+    if (!local) execSync('git config --global user.email "action@github.com"');
     execSync('git fetch');
     execSync('git checkout stage');
     try {
@@ -49,7 +53,10 @@ const main = async ({github, context, local = false}) => {
       console.log('update-imslib branch not found, skipping delete');
     }
     execSync('git checkout -b update-imslib');
-    fs.writeFileSync(`${local ? '../../' : ''}libs/deps/imslib.min.js`, header + response.data);
+    fs.writeFileSync(
+      `${local ? '../../' : ''}libs/deps/imslib.min.js`,
+      header + response.data
+    );
     execSync('git add libs/deps/imslib.min.js');
     execSync('git commit -m "Update imslib.min.js"');
     execSync('git push --force origin update-imslib');
@@ -95,12 +102,12 @@ Resolves: NO TICKET - AUTOMATED CREATED PR.
 
 **Milo:**
 - Before: https://main--milo--adobecom.hlx.page/ch_de/drafts/ramuntea/gnav-refactor?martech=off
-- After: https://update-imslib--milo--adobecom.hlx.page/ch_de/drafts/ramuntea/gnav-refactor?martech=off`
+- After: https://update-imslib--milo--adobecom.hlx.page/ch_de/drafts/ramuntea/gnav-refactor?martech=off`,
     });
   }
-}
+};
 
 // Enable and just run node update-ims.js to debug locally
 // main({local: true})
 
-module.exports = main
+module.exports = main;
