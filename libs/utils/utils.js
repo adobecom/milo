6cfd3260c488e244cfe8ad0f0efc5400867859d1
Okan Sahin <osahin@adobe.com>
2024-03-27 15:36:42 +0100

    Address PR feedback

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -136,7 +136,6 @@ export const MILO_EVENTS = { DEFERRED: 'milo:deferred' };
 
 const LANGSTORE = 'langstore';
 const PAGE_URL = new URL(window.location.href);
-
 function getEnv(conf) {
   const { host } = window.location;
   const query = PAGE_URL.searchParams.get('env');
@@ -670,8 +669,7 @@ function decorateHeader() {
     header.remove();
     return;
   }
-  const headerQuery = new URLSearchParams(window.location.search).get('headerqa');
-  header.className = headerQuery || headerMeta || 'gnav';
+  header.className = headerMeta || 'gnav';
   const metadataConfig = getMetadata('breadcrumbs')?.toLowerCase()
   || getConfig().breadcrumbs;
   if (metadataConfig === 'off') return;
@@ -711,8 +709,7 @@ async function loadFooter() {
     footer.remove();
     return;
   }
-  const footerQuery = new URLSearchParams(window.location.search).get('footerqa');
-  footer.className = footerQuery || footerMeta || 'footer';
+  footer.className = footerMeta || 'footer';
   await loadBlock(footer);
 }
 
@@ -793,13 +790,10 @@ export async function loadIms() {
       },
       onError: reject,
     };
-    const params = new URLSearchParams(window.location.search);
-    const useLHControlGroup = params.get('useAlternateImsDomain');
-    if (useLHControlGroup) {
-      loadScript('https://auth.services.adobe.com/imslib/imslib.min.js');
-    } else {
-      loadScript(`${base}/deps/imslib.min.js`);
-    }
+    const path = PAGE_URL.searchParams.get('useAlternateImsDomain')
+      ? 'https://auth.services.adobe.com/imslib/imslib.min.js'
+      : `${base}/deps/imslib.min.js`;
+    loadScript(path);
   }).then(() => {
     if (!window.adobeIMS?.isSignedInUser()) {
       getConfig().entitlements([]);
@@ -830,8 +824,7 @@ export async function loadMartech({ persEnabled = false, persManifests = [] } = 
 }
 
 async function checkForPageMods() {
-  const search = new URLSearchParams(window.location.search);
-  const offFlag = (val) => search.get(val) === 'off';
+  const offFlag = (val) => PAGE_URL.searchParams.get(val) === 'off';
   if (offFlag('mep')) return;
   const persMd = getMetadata('personalization');
   const promoMd = getMetadata('manifestnames');
