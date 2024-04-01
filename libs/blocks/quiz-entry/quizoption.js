0591efa5d6d8005355f9317b7c322122e2a43001
Jacky Sun <XIASUN@ADOBE.COM>
2024-04-01 11:39:33 -0700

    MWPW-144810: Quiz Entry - Add option cards and text to the block

---
+++ libs/blocks/quiz-entry/quizoption.js
@@ -0,0 +1,91 @@
+import { html } from '../../deps/htm-preact.js';
+
+export const OptionCard = ({
+  text, title, image, icon, iconTablet, iconDesktop, options, disabled, selected, background,
+}) => {
+  const getOptionClass = () => {
+    let className = '';
+    if (icon || iconTablet || iconDesktop) className += 'has-icon ';
+    if (image) className += 'has-image ';
+    if (background) className += 'has-background ';
+    if (disabled) className += disabled;
+    if (selected) className += selected;
+    return className;
+  };
+
+  const getIconClass = () => {
+    let className = '';
+    if (!icon) className += 'no-icon-default ';
+    if (!iconTablet && (icon || iconDesktop)) className += 'no-icon-tablet ';
+    return className;
+  };
+
+  const getIconHtml = () => html`<div class="quiz-option-icon ${getIconClass()}">
+    <picture>
+      ${iconDesktop && html`<source media="(min-width: 1024px)" srcset="${iconDesktop}" />`}
+      ${iconTablet && html`<source media="(min-width: 600px)" srcset="${iconTablet}" />`}
+      <img src="${icon}" alt="${`Icon - ${title || text}`}" loading="lazy" />
+    </picture>
+  </div>`;
+
+  const imageHtml = html`
+    <div class="quiz-option-image" 
+      style="background-image: url('${image}'); background-size: cover" loading="lazy">
+    </div>`;
+
+  const titleHtml = html`
+    <h2 class="quiz-option-title">${title}</h2>
+  `;
+
+  const textHtml = html`
+    <p class="quiz-option-text">${text}</p>
+  `;
+
+  return html`<button class="quiz-option ${getOptionClass()}" data-option-name="${options}" 
+        role="checkbox" aria-checked="${!!selected}" disabled="${disabled}">
+        ${(icon || iconTablet || iconDesktop) && getIconHtml()}
+        ${image && imageHtml}
+        <div class="quiz-option-text-container">  
+          ${title && titleHtml}
+          ${text && textHtml}
+        </div>
+    </button>`;
+};
+
+export const CreateOptions = ({
+  options, handleCardSelection, selectedCards, countSelectedCards = 0, maxSelections,
+  getOptionsIcons, background,
+}) => html`
+      ${options?.data.filter((option) => option.options !== 'fi_code').map((option, index) => (
+    html`<div key=${index} onClick=${handleCardSelection(option)}>
+          <${OptionCard} 
+            text=${option.text}
+            title=${option.title} 
+            icon=${getOptionsIcons(option.options, 'icon')}
+            iconTablet=${getOptionsIcons(option.options, 'icon-tablet')}
+            iconDesktop=${getOptionsIcons(option.options, 'icon-desktop')}
+            image=${getOptionsIcons(option.options, 'image')}
+            background=${background}
+            options=${option.options}
+            selected=${selectedCards[option.options] ? 'selected' : ''}
+            disabled=${(countSelectedCards > 0 && !selectedCards[option.options] && countSelectedCards >= maxSelections) ? 'disabled' : ''}/>
+        </div>`
+  ))}`;
+
+export const GetQuizOption = ({
+  options, maxSelections, selectedCards,
+  onOptionClick, countSelectedCards, getOptionsIcons,
+  background,
+}) => html`
+    <div class="quiz-question">
+        <div class="quiz-options-container" role="group" aria-labelledby="question">
+          <${CreateOptions} 
+            options=${options} 
+            selectedCards=${selectedCards}
+            countSelectedCards=${countSelectedCards} 
+            maxSelections=${maxSelections}
+            getOptionsIcons=${getOptionsIcons}
+            handleCardSelection=${onOptionClick}
+            background=${background} />
+        </div>
+    </div>`;
