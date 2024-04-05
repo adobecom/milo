0b4e397597f0cbc69027a9f2eb5bd67e68ed9c43
Mariia Lukianets <lukianet@adobe.com>
2024-04-05 10:19:59 +0200

    TWP block draft (#2105)
    * add draft for a merch-twp block
    
    ---------
    
    Co-authored-by: Axel Cureno Basurto <axel@adobe.com>

--- libs/deps/merch-offer-select.js
+++ libs/deps/merch-offer-select.js
@@ -1,15 +1,19 @@
-// Mon, 12 Feb 2024 17:51:13 GMT
-import{html as u,LitElement as p}from"/libs/deps/lit-all.min.js";import{css as d,html as a,LitElement as h}from"/libs/deps/lit-all.min.js";var o=class extends h{static styles=d`
+// branch: twp-panel commit: 1db48026128e58b82f612c98c350b6335065998e Wed, 03 Apr 2024 15:12:50 GMT
+import{html as f,LitElement as x}from"/libs/deps/lit-all.min.js";import{css as u,html as d,LitElement as b}from"/libs/deps/lit-all.min.js";var o=class extends b{static styles=u`
         :host .horizontal {
             display: flex;
             flex-direction: row;
         }
-    `;static properties={offers:{type:Array},selectedOffer:{type:Object},defaults:{type:Object},variant:{type:String}};constructor(){super(),this.defaults={}}saveContainerDefaultValues(){let e=this.closest(this.getAttribute("container")),t=e?.querySelector('[slot="description"]:not(merch-offer > *)')?.cloneNode(!0),i=e?.badgeText;return{description:t,badgeText:i}}getSlottedElement(e,t){return(t||this.closest(this.getAttribute("container"))).querySelector(`[slot="${e}"]:not(merch-offer > *)`)}updateSlot(e,t){let i=this.getSlottedElement(e,t);if(!i)return;let s=this.selectedOffer.getOptionValue(e)?this.selectedOffer.getOptionValue(e):this.defaults[e];s&&i.replaceWith(s.cloneNode(!0))}handleOfferSelection(e){let t=e.detail;this.selectOffer(t)}handleOfferSelectionByQuantity(e){let t=e.detail.option,i=Number.parseInt(t),s=this.findAppropriateOffer(i);this.selectOffer(s),this.getSlottedElement("cta").setAttribute("data-quantity",t)}selectOffer(e){if(!e)return;let t=this.selectedOffer;t&&(t.selected=!1),this.selectedOffer=e,this.updateContainer()}findAppropriateOffer(e){let t=null;return this.offers.find(s=>{let r=Number.parseInt(s.getAttribute("value"));if(r===e)return!0;if(r>e)return!1;t=s})||t}updateBadgeText(e){this.selectedOffer.badgeText===""?e.badgeText=null:this.selectedOffer.badgeText?e.badgeText=this.selectedOffer.badgeText:e.badgeText=this.defaults.badgeText}updateContainer(){let e=this.closest(this.getAttribute("container"));!e||!this.selectedOffer||(this.updateSlot("cta",e),this.updateSlot("secondary-cta",e),this.updateSlot("price",e),!this.manageableMode&&(this.updateSlot("description",e),this.updateBadgeText(e)))}connectedCallback(){super.connectedCallback();let e=this.closest("merch-quantity-select");this.manageableMode=e,this.offers=[...this.querySelectorAll("merch-offer")],this.manageableMode?e.addEventListener("change",t=>this.handleOfferSelectionByQuantity(t)):(this.defaults=this.saveContainerDefaultValues(),this.addEventListener("offer-selected",this.handleOfferSelection),this.offers[0].select()),this.selectedOffer=this.offers[0],this.updateContainer()}render(){return this.variant?a`<slot class="${this.variant}"></slot>`:a`<slot></slot>`}disconnectedCallback(){this.removeEventListener("offer-selected",this.handleOfferSelection),this.removeEventListener("change",this.handleOfferSelectionByQuantity)}};customElements.define("merch-offer-select",o);import{css as f}from"/libs/deps/lit-all.min.js";var l=f`
+    `;static properties={offers:{type:Array},selectedOffer:{type:Object},defaults:{type:Object},variant:{type:String}};constructor(){super(),this.defaults={}}saveContainerDefaultValues(){let e=this.closest(this.getAttribute("container")),t=e?.querySelector('[slot="description"]:not(merch-offer > *)')?.cloneNode(!0),i=e?.badgeText;return{description:t,badgeText:i}}getSlottedElement(e,t){return(t||this.closest(this.getAttribute("container"))).querySelector(`[slot="${e}"]:not(merch-offer > *)`)}updateSlot(e,t){let i=this.getSlottedElement(e,t);if(!i)return;let s=this.selectedOffer.getOptionValue(e)?this.selectedOffer.getOptionValue(e):this.defaults[e];s&&i.replaceWith(s.cloneNode(!0))}handleOfferSelection(e){let t=e.detail;this.selectOffer(t)}handleOfferSelectionByQuantity(e){let t=e.detail.option,i=Number.parseInt(t),s=this.findAppropriateOffer(i);this.selectOffer(s),this.getSlottedElement("cta").setAttribute("data-quantity",t)}selectOffer(e){if(!e)return;let t=this.selectedOffer;t&&(t.selected=!1),this.selectedOffer=e,this.updateContainer()}findAppropriateOffer(e){let t=null;return this.offers.find(s=>{let r=Number.parseInt(s.getAttribute("value"));if(r===e)return!0;if(r>e)return!1;t=s})||t}updateBadgeText(e){this.selectedOffer.badgeText===""?e.badgeText=null:this.selectedOffer.badgeText?e.badgeText=this.selectedOffer.badgeText:e.badgeText=this.defaults.badgeText}updateContainer(){let e=this.closest(this.getAttribute("container"));!e||!this.selectedOffer||(this.updateSlot("cta",e),this.updateSlot("secondary-cta",e),this.updateSlot("price",e),!this.manageableMode&&(this.updateSlot("description",e),this.updateBadgeText(e)))}connectedCallback(){super.connectedCallback();let e=this.closest("merch-quantity-select");this.manageableMode=e,this.offers=[...this.querySelectorAll("merch-offer")],this.manageableMode?e.addEventListener("change",t=>this.handleOfferSelectionByQuantity(t)):(this.defaults=this.saveContainerDefaultValues(),this.addEventListener("offer-selected",this.handleOfferSelection),this.offers[0]?.select()),this.selectedOffer=this.offers[0],this.updateContainer()}render(){return this.variant?d`<slot class="${this.variant}"></slot>`:d`<slot></slot>`}disconnectedCallback(){this.removeEventListener("offer-selected",this.handleOfferSelection),this.removeEventListener("change",this.handleOfferSelectionByQuantity)}};customElements.define("merch-offer-select",o);import{css as m}from"/libs/deps/lit-all.min.js";var h=m`
     :host {
         --merch-radio: rgba(82, 88, 228);
         --merch-radio-hover: rgba(64, 70, 202);
         --merch-radio-down: rgba(50, 54, 168);
         --merch-radio-selected: rgb(2, 101, 220);
+        --merch-hovered-shadow: 0 0 0 1px #aaa;
+        --merch-selected-shadow: 0 0 0 2px var(--merch-radio-selected);
+    }
+    .merch-Radio {
         align-items: flex-start;
         display: flex;
         max-inline-size: 100%;
@@ -94,10 +98,83 @@ import{html as u,LitElement as p}from"/libs/deps/lit-all.min.js";import{css as d
         text-align: start;
         transition: color 0.13s ease-in-out;
     }
-`;var n=class extends p{static properties={text:{type:String},selected:{type:Boolean,reflect:!0},badgeText:{type:String,attribute:"badge-text"}};static styles=[l];select(){this.selected||(this.selected=!0,this.parentElement.dispatchEvent(new CustomEvent("offer-selected",{detail:this})))}constructor(){super(),this.selected=!1,this.addEventListener("click",e=>{this.select()}),this.addEventListener("keyup",e=>{(e.key==="Enter"||e.key===" ")&&this.select()})}getOptionValue(e){return this[e]||(this[e]=this.querySelector(`[slot="${e}"]`)),this[e]}connectedCallback(){super.connectedCallback(),this.configuration=this.closest("quantity-selector"),!this.hasAttribute("tabindex")&&!this.configuration&&(this.tabIndex=0),!this.hasAttribute("role")&&!this.configuration&&(this.role="radio"),!this.hasAttribute("aria-checked")&&!this.configuration&&this.setAttribute("aria-checked",this.selected)}updated(e){e.has("selected")&&this.setAttribute("aria-checked",this.selected)}render(){return this.configuration?"":u` <input
-                      tabindex="-1"
-                      type="radio"
-                      class="merch-Radio-input"
-                  />
-                  <span class="merch-Radio-button"></span>
-                  <span class="merch-Radio-label">${this.text}</span>`}};customElements.define("merch-offer",n);
+
+    .merch-Div-in {
+        height: 0;
+        outline: none;
+        position: absolute;
+        width: 0;
+        z-index: -1;
+    }
+
+    .merch-Div-label {
+        background-color: white;
+        border: 1px solid transparent;
+        border-radius: var(--consonant-merch-spacing-xxxs);
+        cursor: pointer;
+        display: block;
+        margin: var(--consonant-merch-spacing-xs) 0;
+        padding: var(--consonant-merch-spacing-xs);
+        position: relative;
+    }
+
+    .merch-Div-label:hover {
+        box-shadow: var(--merch-hovered-shadow);
+    }
+
+    :host([selected]) .merch-Div-label {
+        box-shadow: var(--merch-selected-shadow);
+    }
+
+    .merch-Div .condition-icon {
+        background-position: center;
+        background-size: contain;
+        background: var(--info-icon) no-repeat;
+        content: '';
+        color: #6e6e6e;
+        display: inline-block;
+        height: 1.1em;
+        margin-bottom: -3px;
+        width: 1.1em;
+    }
+
+    ::slotted([slot='commitment']) {
+    }
+
+    ::slotted([slot='condition']) {
+        display: inline-block;
+        font-style: italic;
+    }
+
+    .merch-Div-label span[is='inline-price'] {
+        font-weight: 700;
+    }
+`;var n="commitment",a="condition",l="condition-tooltip";var c=class extends x{static properties={text:{type:String},selected:{type:Boolean,reflect:!0},badgeText:{type:String,attribute:"badge-text"},type:{type:String},offer:{type:String},planType:{type:String}};static styles=[h];#e;select(){this.selected||(this.selected=!0,this.parentElement.dispatchEvent(new CustomEvent("offer-selected",{detail:this})))}constructor(){super(),this.selected=!1,this.type="radio",this.offer=null,this.planType=null,this.addEventListener("click",e=>{this.select()}),this.addEventListener("keyup",e=>{(e.key==="Enter"||e.key===" ")&&this.select()})}getOptionValue(e){return this[e]||(this[e]=this.querySelector(`[slot="${e}"]`)),this[e]}connectedCallback(){super.connectedCallback(),this.configuration=this.closest("quantity-selector"),this.querySelector('span[is="inline-price"]')?.onceSettled().then(e=>{e.value.length&&(this.offer=e,this.planType=e.value[0].planType)}).catch(e=>{}),!this.hasAttribute("tabindex")&&!this.configuration&&(this.tabIndex=0),!this.hasAttribute("role")&&!this.configuration&&(this.role="radio"),!this.hasAttribute("aria-checked")&&!this.configuration&&this.setAttribute("aria-checked",this.selected)}updated(e){e.has("selected")&&this.setAttribute("aria-checked",this.selected)}renderRadio(){return f` <div class="merch-Radio">
+            <input tabindex="-1" type="radio" class="merch-Radio-input" />
+            <span class="merch-Radio-button"></span>
+            <span class="merch-Radio-label">${this.text}</span>
+        </div>`}renderDiv(){return this.#e||(this.#e=this.parentNode?.querySelector(`template[name="${this.planType}"]`)?.content?.cloneNode(!0),this.append(this.#e)),f`<div class="merch-Div">
+            <input
+                autocomplete="off"
+                ?checked="${this.selected}"
+                id="${this.planType}"
+                name="offer"
+                type="radio"
+                value="${this.planType}"
+                class="merch-Div-in"
+            />
+            <label class="merch-Div-label" for="${this.planType}">
+                <slot name="${n}"></slot>
+                ${this.offer}
+                <slot name="${a}"></slot>
+                <overlay-trigger placement="top" offset="4">
+                    <span class="condition-icon" slot="trigger"></span>
+                    <sp-tooltip slot="hover-content" delayed
+                        ><slot
+                            name="${this.planType}-${l}"
+                        ></slot
+                    ></sp-tooltip>
+                </overlay-trigger>
+            </label>
+        </div> `}render(){return this.configuration||!this.offer?"":this.type&&this.type=="div"?this.renderDiv():this.renderRadio()}};customElements.define("merch-offer",c);
+//# sourceMappingURL=merch-offer-select.js.map
---
+++ libs/deps/merch-subscription-tabs.js
@@ -0,0 +1,34 @@
+// branch: twp-panel commit: b84d063bcc7cfeff3355cf9950d3a24f0ba37040 Wed, 03 Apr 2024 13:20:13 GMT
+import{LitElement as p,html as m}from"/libs/deps/lit-all.min.js";import{css as h,unsafeCSS as c}from"/libs/deps/lit-all.min.js";var n="(min-width: 768px)",a="(min-width: 1200px)";var o=h`
+    :host {
+    }
+
+    sp-theme {
+        display: contents;
+    }
+
+    #cards {
+        align-items: center;
+        border-radius: var(--consonant-merch-spacing-xs);
+        display: flex;
+        flex-direction: column;
+        gap: var(--consonant-merch-spacing-xs);
+        grid-area: cards;
+        padding: var(--consonant-merch-spacing-xs) 0;
+    }
+
+    @media ${c(n)} {
+    }
+
+    @media ${c(a)} {
+        #cards {
+            align-items: flex-start;
+            background-color: #fbfbfb;
+            flex-direction: row;
+            height: min-content;
+            justify-content: center;
+            padding: 0 var(--consonant-merch-spacing-xs);
+        }
+    }
+`;var d='merch-offer-select [is="inline-price"][data-template="price"]',r=class extends p{static styles=[o];cards;panel;tabs=[];static properties={activeTab:{type:Object}};connectedCallback(){if(super.connectedCallback(),this.cards=this.querySelectorAll("merch-card"),this.panel=this.parentNode.querySelector("merch-subscription-panel"),this.panel&&this.cards.length){let t=[];[...this.cards].forEach(e=>{let s=e.querySelector(d);s&&(t=[...t,s.onceSettled()])}),Promise.all(t).then(()=>{this.cards.forEach(e=>{let s=e.querySelector(d);if(!s?.value?.length)return;let{marketSegments:i,customerSegment:l}=s.value[0];i?.length&&(i[0]==="COM"&&l==="INDIVIDUAL"?this.addCard("indiviual",e):i[0]==="COM"?this.addCard("business",e):i[0]==="EDU"&&this.addCard("students",e))}),this.tabs?.length&&(this.tabs[0].active=!0,this.activeTab=this.tabs.find(e=>e.active===!0),this.setActiveCard(this.activeTab.cards[0]))})}}setActiveCard(t){t.querySelector('merch-offer-select span[is="inline-price"]')?.onceSettled().then(()=>{this.panel.card=t})}addCard(t,e){let s=this.tabs.find(i=>i.name===t);s?s.cards?.push(e):this.tabs.push({name:t,cards:[e]})}render(){return m`<div id="cards">${this.activeTab?.cards}</div>`}};window.customElements.define("merch-subscription-tabs",r);export{r as MerchSubscriptionTabs};
+//# sourceMappingURL=merch-subscription-tabs.js.map
