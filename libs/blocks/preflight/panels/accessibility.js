54ac8d6cd9ba36c2bb2f3bed24bc14903ed801dd
Ryan Clayton <rclayton@adobe.com>
2024-04-10 17:23:54 -0600

    WIP refactor and clean up

--- libs/blocks/preflight/panels/accessibility.js
+++ libs/blocks/preflight/panels/accessibility.js
@@ -1,20 +1,41 @@
 import { html, signal, useEffect } from '../../../deps/htm-preact.js';
 
 const DEF_DESC = 'Checking...';
-const decorativeImage = signal([]);
-const altTextImage = signal([]);
+const decorativeImages = signal([]);
+const altTextImages = signal([]);
 const altResult = signal({ title: 'Image alt value', description: DEF_DESC });
+const groups = [
+  { title: 'Images with alt text', imgArray: altTextImages },
+  { title: 'Decorative images (empty alt text)', imgArray: decorativeImages, closed: true },
+];
+const filterOptions = [
+  { value: '', content: 'All' },
+  { value: 'show-main', content: 'Main content', selected: true },
+  { value: 'show-gnav', content: 'Gnav' },
+  { value: 'show-footer', content: 'Footer' },
+];
 
 function filterGrid(e) {
   const imgGrid = e.target.parentElement.parentElement;
-  imgGrid.classList.remove('show-main', 'show-gnav', 'show-footer');
-  imgGrid.classList.add(e.target.value);
+  filterOptions.forEach((option) => {
+    if (imgGrid.classList.contains(option.value)) {
+      imgGrid.classList.remove(option.value);
+    }
+  });
+  if (e.target.value) imgGrid.classList.add(e.target.value);
 }
 
 function toggleGrid(e) {
   e.preventDefault();
   const clickArea = e.target.nodeName === 'SPAN' ? e.target.parentElement.parentElement : e.target.parentElement;
   clickArea.classList.toggle('is-closed');
+}
+
+function dropdownOptions(props) {
+  const selected = props.option.selected === true;
+  return html`
+      <option value="${props.option.value}" selected="${selected}">${props.option.content}</option>
+  `;
 }
 
 async function checkAlt() {
@@ -33,7 +54,7 @@ async function checkAlt() {
     if (img.closest('footer')) parent = 'footer';
     if (alt === '') {
       img.dataset.altCheck = 'decorative';
-      decorativeImage.value = [...decorativeImage.value,
+      decorativeImages.value = [...decorativeImages.value,
         {
           src: img.getAttribute('src'),
           altCheck: img.dataset.altCheck,
@@ -41,7 +62,7 @@ async function checkAlt() {
         }];
     }
     if (alt) {
-      altTextImage.value = [...altTextImage.value,
+      altTextImages.value = [...altTextImages.value,
         {
           src: img.getAttribute('src'),
           alt,
@@ -64,60 +85,48 @@ function AccessibilityItem({ title, description }) {
     </div>`;
 }
 
+function ImageGroups({ group }) {
+  const setFilterView = filterOptions.find((option) => option.selected === true);
+  const { imgArray, closed } = group;
+  return html`
+    <div class='grid-heading ${closed === true ? 'is-closed' : ''}'>
+      <a href='#' onClick=${(e) => toggleGrid(e)} class='grid-toggle'>
+        <span class="preflight-group-expand"></span>
+        ${group.title}
+      </a>
+    </div>
+
+    ${imgArray.value.length > 0 && html`
+    <div class="access-image-grid ${setFilterView.value}">
+      <div class="access-image-grid-item filter">
+        Filter image by: 
+        <select onChange=${(e) => filterGrid(e)} class="image-filter">
+        ${filterOptions.map((option) => html`<${dropdownOptions} option=${option} />`)}
+        </select>
+      </div>
+
+      ${imgArray.value.map((img) => html`
+      <div class="access-image-grid-item in-${img.parent}">
+        <img src="${img.src}" />
+        <span>${!img.altCheck ? `Alt=${img.alt}` : `Marked as ${img.altCheck}`}</span>
+        <span>Located in ${img.parent}</span>
+      </div>`)}
+    </div>`}
+
+    ${!imgArray.value.length && html`
+      <div class="access-image-grid">
+        <div class="access-image-grid-item full-width">No images found</div>
+      </div>
+    `}
+  `;
+}
+
 export default function Accessibility() {
   useEffect(() => { checkAlt(); }, []);
 
   return html`
   <div class="access-columns">
     <${AccessibilityItem} title=${altResult.value.title} description=${altResult.value.description} />
-    <div class='grid-heading'>
-      <a href='#' onClick=${(e) => toggleGrid(e)} class='grid-toggle'>
-        <span class="preflight-group-expand"></span>
-        Images with alt text
-      </a>
-    </div>
-    ${altTextImage.value.length > 0 && html`
-    <div class="access-image-grid">
-      ${altTextImage.value.map((img) => html`
-      <div class="access-image-grid-item in-${img.parent}">
-        <img src="${img.src}" />
-        <span>Alt='${img.alt}'</span>
-        <span>Located in ${img.parent}</span>
-      </div>`)}
-    </div>`}
-    ${!altTextImage.value.length && html`
-      <div class="access-image-grid">
-        <div class="access-image-grid-item full-width">No images found</div>
-      </div>
-    `}
-    <div class='grid-heading is-closed'>
-      <a href='#' onClick=${(e) => toggleGrid(e)} class='grid-toggle'>
-        <span class="preflight-group-expand"></span>
-        Decorative images (empty alt text)
-      </a>
-    </div>
-    ${decorativeImage.value.length > 0 && html`
-    <div class="access-image-grid">
-      <div class="access-image-grid-item filter">
-        Filter: 
-        <select onChange=${(e) => filterGrid(e)} class="image-filter">
-          <option value="" selected="selected">Show all</option>
-          <option value="show-main">Show main</option>
-          <option value="show-gnav">Show gnav</option>
-          <option value="show-footer">Show footer</option>
-        </select>
-      </div>
-      ${decorativeImage.value.map((img) => html`
-      <div class="access-image-grid-item in-${img.parent}">
-        <img src="${img.src}" />
-        <span>Marked as ${img.altCheck}</span>
-        <span>Located in ${img.parent}</span>
-      </div>`)}
-    </div>`}
-    ${!decorativeImage.value.length && html`
-      <div class="access-image-grid">
-        <div class="access-image-grid-item full-width">No images found</div>
-      </div>
-    `}
+    ${groups.map((group) => html`<${ImageGroups} group=${group} />`)}
   </div>`;
 }
