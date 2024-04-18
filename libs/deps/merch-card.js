c4ba4b185fab1754ab482714d4dc81255a9bd929
Axel Cureno Basurto <axel@adobe.com>
2024-04-18 07:40:56 -0700

    Update merch-card.js

--- libs/deps/merch-card.js
+++ libs/deps/merch-card.js
@@ -1,4 +1,4 @@
-// branch: MWPW-142003-stable commit: fd49fb6f54e6b8ef08dc8e8b5b1ad19ac5370e3b Thu, 18 Apr 2024 14:33:39 GMT
+// branch: MWPW-142003-stable commit: fd49fb6f54e6b8ef08dc8e8b5b1ad19ac5370e3b Thu, 18 Apr 2024 14:40:41 GMT
 import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v,unsafeCSS as f}from"/libs/deps/lit-all.min.js";var m="(max-width: 767px)";var i="(min-width: 768px)",c="(min-width: 1200px)",s="(min-width: 1600px)";var k=v`
     :host {
         position: relative;
@@ -857,6 +857,10 @@ div[slot="footer"] {
 div[slot="footer"] a.con-button {
     margin-left: var(--consonant-merch-spacing-xs);
     width: max-content;
+}
+
+div[slot="footer"] .action-area > a.con-button:only-child {
+    margin-left: 0;
 }
 
 div[slot="footer"] a:not([class]) {
