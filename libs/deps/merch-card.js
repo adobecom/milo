c61e03f96581a87b6929b36ae2e6ba8aec1d9c5e
Axel Cureno Basurto <axel@adobe.com>
2024-04-18 13:05:32 -0700

    Update merch-card.js

--- libs/deps/merch-card.js
+++ libs/deps/merch-card.js
@@ -1,4 +1,4 @@
-// branch: MWPW-142003-stable commit: 3ba904f713082e0ba7323c2e2737a938b13643c8 Thu, 18 Apr 2024 19:51:30 GMT
+// branch: MWPW-142003-stable commit: 3ba904f713082e0ba7323c2e2737a938b13643c8 Thu, 18 Apr 2024 20:05:18 GMT
 import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v,unsafeCSS as x}from"/libs/deps/lit-all.min.js";var m="(max-width: 767px)";var i="(min-width: 768px)",c="(min-width: 1200px)",h="(min-width: 1600px)";var k=v`
     :host {
         position: relative;
@@ -282,6 +282,7 @@ import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v
         :host([variant='mini-compare-chart']) footer {
             justify-content: center;
             min-height: var(--consonant-merch-card-mini-compare-footer-height);
+            flex-flow: wrap;
         }
     }
 
