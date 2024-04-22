b23ebd4c24fa62527dd80badfc7d4eec7636608f
Sean Archibeque <sean.archibeque@gmail.com>
2024-04-22 11:03:52 -0600

    MWPW-146877 [LocUI] - Status Details & Sidekick (#2176)
    push status toast and connection down and fixed

--- libs/blocks/locui/sync/view.js
+++ libs/blocks/locui/sync/view.js
@@ -2,13 +2,19 @@ import { html } from '../../../deps/htm-preact.js';
 import { serviceStatus, heading } from '../utils/state.js';
 import { showContents, getPrettyDate, toggleContent } from './index.js';
 
+function isSidekickOpen() {
+  const sidekick = document.querySelector('helix-sidekick');
+  return !!sidekick;
+}
+
 export default function Sync() {
   const prettyDate = getPrettyDate();
   const prettySync = serviceStatus.value.replace(/[\W_]+/g, '-');
   const connectedTo = heading.value.env ? `${serviceStatus} (${heading.value.env})` : serviceStatus;
+  const sidekickOpen = isSidekickOpen() ? ' push-down' : '';
 
   return html`
-    <div class=locui-sync-badge-container>
+    <div class="locui-sync-badge-container${sidekickOpen}">
       <div class="locui-sync-badge locui-sync-badge-status-${prettySync}">
         <button class=locui-sync-badge-header onClick=${toggleContent}>${connectedTo}</button>
         ${showContents.value && html`
