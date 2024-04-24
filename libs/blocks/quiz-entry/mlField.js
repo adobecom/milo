07a349f0efdfcd143321a39732f059417fdc599e
Cody Lloyd <119891065+colloyd@users.noreply.github.com>
2024-04-24 10:45:23 -0600

    MWPW-146036 - Rig up quiz entry button (#2190)
    * Support for ml filtering
    * Debug support using ?debug=quiz-entry
    * Store quizState in local storage and redirect to the quiz
    
    Resolves: [MWPW-146036](https://jira.corp.adobe.com/browse/MWPW-146036)

--- libs/blocks/quiz-entry/mlField.js
+++ libs/blocks/quiz-entry/mlField.js
@@ -1,7 +1,7 @@
 import { html } from '../../deps/htm-preact.js';
 import { getConfig } from '../../utils/utils.js';
 
-export const getFiResults = async (endpoint, apiKey, input, numberOfItems, validFiCodes) => {
+export const getMLResults = async (endpoint, apiKey, threshold, input, count, validFiCodes) => {
   const { env } = getConfig();
   const subdomain = env === 'prod' ? 'cchome-dev' : 'cchome-dev';
   const apiUrl = `https://${subdomain}.adobe.io/int/v1/models`;
@@ -11,7 +11,7 @@ export const getFiResults = async (endpoint, apiKey, input, numberOfItems, valid
     payload: {
       data: {
         input,
-        num_items: numberOfItems || 10,
+        num_items: count || 10,
         given_prod_list: validFiCodes,
       },
     },
@@ -28,9 +28,20 @@ export const getFiResults = async (endpoint, apiKey, input, numberOfItems, valid
     .then((response) => response.json())
     .catch((error) => window.lana.log(`ERROR: Fetching fi codes ${error}`));
 
+  let highestProb = null;
+  result.filtered = result?.data?.filter((item) => {
+    let isValid = false;
+    if (!highestProb) {
+      highestProb = item.prob;
+      isValid = true;
+    } else if (item.prob / highestProb > threshold) {
+      isValid = true;
+    }
+    return isValid;
+  });
   return result;
 };
 
-export const mlField = ({ cardsUsed, onMLInput, placeholderText }) => html`<div class="ml-field-container">
-    <input id="ml-field-input" class="ml-input" type="textarea" placeholder="${placeholderText}" oninput="${onMLInput}" disabled="${cardsUsed}" />
+export const mlField = ({ cardsUsed, onMLInput, onMLEnter, placeholderText }) => html`<div class="ml-field-container">
+    <input id="ml-field-input" class="ml-input" type="textarea" placeholder="${placeholderText}" oninput="${onMLInput}" onkeypress="${onMLEnter}" disabled="${cardsUsed}" />
   </div>`;
