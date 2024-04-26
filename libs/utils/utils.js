ab15814a0d6203b5a506758bf3273f021c629714
Robert Bogos <robert.adobe.github@gmail.com>
2024-04-26 15:04:09 +0300

    hotfix

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1184,11 +1184,3 @@ export function loadLana(options = {}) {
 }
 
 export const reloadPage = () => window.location.reload();
-
-export const setAttributes = (el, attrs) => {
-  Object.keys(attrs).forEach((key) => el.setAttribute(key, attrs[key]));
-};
-
-export const removeAttributes = (el, attrsKeys) => {
-  attrsKeys.forEach((key) => el.removeAttribute(key));
-};
