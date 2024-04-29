7f196f54f5ad9a70e41a0186f537ea45f04c6afb
Ruchika Sinha <69535463+Ruchika4@users.noreply.github.com>
2024-04-29 08:04:54 -0700

    [Release] Stage to Main (#2208)
    * Update code owners for feds (#2194)
    
    Co-authored-by: Blaine Gunn <Blainegunn@gmail.com>
    Co-authored-by: Robert Bogos <146744221+robert-bogos@users.noreply.github.com>
    
    * Harden preflight link checking (#2169)
    
    * Filter out empty `hrefs` before sending to spidy.
    * Harden link check to be more robust against Spidy API.
    * Update link check language.
    * Better onboarding support.
      * Adds fallbacks if `.milo/config` has not been added.
    * Fix for missing Word doc reference, under general tab.
    
    Resolves: [MWPW-146695](https://jira.corp.adobe.com/browse/MWPW-146695)
    
    Co-authored-by: Ryan Clayton <rclayton@adobe.com>
    
    * MWPW-146755: RTL merch icon padding (#2162)
    
    * RTL padding merch icon
    
    * dep update
    
    * MWPW-146001 parallelize literals call (#2187)
    
    * MWPW-146001 parallelize literals call
    
    * MWPW-146001 move promise to the function
    
    * MWPW-146001 fix commerce library
    
    * MWPW-146001 update literals endpoint
    
    * MWPW-146001 fixing commerce ut
    
    ---------
    
    Co-authored-by: Narcis Radu <github@narcisradu.ro>
    Co-authored-by: Blaine Gunn <Blainegunn@gmail.com>
    Co-authored-by: Robert Bogos <146744221+robert-bogos@users.noreply.github.com>
    Co-authored-by: Ryan Clayton <rgclayton@gmail.com>
    Co-authored-by: Ryan Clayton <rclayton@adobe.com>
    Co-authored-by: Axel Cureno Basurto <axelcureno@gmail.com>
    Co-authored-by: Nicolas Peltier <1032754+npeltier@users.noreply.github.com>

--- libs/deps/merch-card.js
+++ libs/deps/merch-card.js
@@ -1,4 +1,4 @@
-// branch: MWPW-142003-stable commit: 14ec61525f24e7ad038184620e04dc096acf1eb3 Thu, 18 Apr 2024 21:37:21 GMT
+// branch: stable commit: d93a08fc2123843319c48865f2b71b9ee0b4f5ed Wed, 24 Apr 2024 15:49:31 GMT
 import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v,unsafeCSS as x}from"/libs/deps/lit-all.min.js";var m="(max-width: 767px)";var i="(min-width: 768px)",c="(min-width: 1200px)",h="(min-width: 1600px)";var k=v`
     :host {
         position: relative;
@@ -62,7 +62,7 @@ import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v
     }
 
     .top-section > .icons img:last-child {
-        margin-right: var(--consonant-merch-spacing-xs);
+        margin-inline-end: var(--consonant-merch-spacing-xs);
     }
 
     .icons {
@@ -74,7 +74,7 @@ import{html as n,LitElement as O}from"/libs/deps/lit-all.min.js";import{css as v
     .icons img {
         width: var(--consonant-merch-card-plans-icon-size);
         height: var(--consonant-merch-card-plans-icon-size);
-        margin-right: var(--consonant-merch-spacing-xxs);
+        margin-inline-end: var(--consonant-merch-spacing-xxs);
     }
 
     .body {
