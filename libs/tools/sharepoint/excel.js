6aa0099ff83586b2aea993ba45f0a31003fe71df
Sean Archibeque <sean.archibeque@gmail.com>
2024-05-02 16:24:01 -0600

    MWPW-147523 - [LocUI] Excel Service Error Handling (#2245)
    catch errors in update excel table function

--- libs/tools/sharepoint/excel.js
+++ libs/tools/sharepoint/excel.js
@@ -2,9 +2,13 @@ import { getSharePointDetails, getSiteOrigin } from './shared.js';
 import { getReqOptions } from './msal.js';
 
 export default async function updateExcelTable({ itemId, tablename, values }) {
-  const origin = getSiteOrigin();
-  const { site, driveId } = await getSharePointDetails(origin);
-  const options = getReqOptions({ body: { values }, method: 'POST' });
-  const url = `${site}/${driveId}/items/${itemId}/workbook/tables/${tablename}/rows/add`;
-  return fetch(url, options);
+  try {
+    const origin = getSiteOrigin();
+    const { site, driveId } = await getSharePointDetails(origin);
+    const options = getReqOptions({ body: { values }, method: 'POST' });
+    const url = `${site}/${driveId}/items/${itemId}/workbook/tables/${tablename}/rows/add`;
+    return fetch(url, options);
+  } catch (error) {
+    return error;
+  }
 }
