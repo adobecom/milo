533b3c724f7b10ceb1dcd4b1bdc87f340c5b6858
Okan Sahin <osahin@adobe.com>
2024-05-07 15:30:08 +0200

    Add the alert emoji

--- .github/workflows/merge-to-stage.js
+++ .github/workflows/merge-to-stage.js
@@ -143,7 +143,7 @@ const merge = async ({ prs }) => {
         html_url,
         number,
         title,
-        highImpact: isHighImpact(labels) ? ' :exclamation:' : '',
+        highImpact: isHighImpact(labels) ? ' :alert:' : '',
       })
     );
   }
