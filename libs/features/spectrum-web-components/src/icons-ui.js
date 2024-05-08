42073d03da2a9c7a3f50f8fbebdb6d80d7c4e771
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2024-05-08 16:56:24 +0200

    spectrum picker and chevron icon added;

--- libs/features/spectrum-web-components/src/icons-ui.js
+++ libs/features/spectrum-web-components/src/icons-ui.js
@@ -13,3 +13,4 @@ import '@spectrum-web-components/icons-ui/icons/sp-icon-cross200.js';
 import '@spectrum-web-components/icons-ui/icons/sp-icon-cross300.js';
 import '@spectrum-web-components/icons-ui/icons/sp-icon-cross400.js';
 import '@spectrum-web-components/icons-ui/icons/sp-icon-cross500.js';
+import '@spectrum-web-components/icons-ui/icons/sp-icon-chevron100.js';
