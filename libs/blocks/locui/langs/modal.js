206b0463d7363c95eaaef7bbf1e62f0b629a711d
Sartxi <sean.archibeque@gmail.com>
2024-05-09 09:04:06 -0600

    add button for existing urls in error modal

--- libs/blocks/locui/langs/modal.js
+++ libs/blocks/locui/langs/modal.js
@@ -1,7 +1,27 @@
 import { html, render } from '../../../deps/htm-preact.js';
 import { urls } from '../utils/state.js';
 import Url from '../url/view.js';
-import { origin } from '../utils/franklin.js';
+import { getStatus, origin } from '../utils/franklin.js';
+
+async function openFile(e, path) {
+  e.target.classList.add('locui-action-loading');
+  const details = await getStatus(path);
+  e.target.classList.remove('locui-action-loading');
+  if (details.edit.url) window.open(details.edit.url, '_blank');
+}
+
+function getErrorLink(error) {
+  const path = error.substring(error.indexOf('/'), error.lastIndexOf('.docx'));
+  const fileURL = urls.value.find((url) => path.includes(url.pathname));
+  if (fileURL) {
+    return html`
+      <button 
+        onClick="${(e) => { openFile(e, fileURL.pathname); }}" class="locui-urls-heading-action">
+        Open File
+      </button>`;
+  }
+  return '';
+}
 
 function Modal({ lang, prefix, error }) {
   const localeUrls = urls.value.map(
@@ -13,7 +33,7 @@ function Modal({ lang, prefix, error }) {
     return html`
       <h2><span class="error-icon" /> ${statusText}</h2>
       <p>Errors reported for <i><strong>${lang.Language}</strong>:</i></p>
-      <ol>${errors.map((err) => html`<li>${err}</li>`)}</ol>
+      <ol>${errors.map((err) => html`<li><span>${err}${getErrorLink(err)}</span></li>`)}</ol>
     `;
   }
 
