9b0102e37c8fb74e0bac176128c62c6a09070b41
Dragana Trajkovic <dragana.trajkovic@lilly021.com>
2024-05-10 10:52:49 +0200

    alphabetical order

--- libs/features/spectrum-web-components/dist/icons-ui.js
+++ libs/features/spectrum-web-components/dist/icons-ui.js
@@ -66,129 +66,7 @@ import{html as K}from"/libs/features/spectrum-web-components/dist/base.js";impor
     <path
       d="M3.667 9.07a.96.96 0 0 1-.737-.344L.753 6.114a.96.96 0 1 1 1.474-1.23l1.418 1.701 4.112-5.233a.96.96 0 0 1 1.51 1.186L4.422 8.704a.962.962 0 0 1-.741.367z"
     />
-  </svg>`;var p=class extends _{render(){return s(Z),B()}};import{defineElement as tt}from"/libs/features/spectrum-web-components/dist/base.js";tt("sp-icon-checkmark75",p);import{html as rt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as et}from"/libs/features/spectrum-web-components/dist/icon.js";var b=({width:t=24,height:r=24,title:e="Dash100"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 10 10"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path d="M8.5 6h-7a1 1 0 0 1 0-2h7a1 1 0 0 1 0 2" />
-  </svg>`;var n=class extends et{render(){return s(rt),b()}};import{defineElement as ot}from"/libs/features/spectrum-web-components/dist/base.js";ot("sp-icon-dash100",n);import{html as st}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as at}from"/libs/features/spectrum-web-components/dist/icon.js";var A=({width:t=24,height:r=24,title:e="Dash200"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 12 12"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path d="M10.021 7.042H1.98a1.042 1.042 0 1 1 0-2.083h8.043a1.042 1.042 0 0 1 0 2.083z" />
-  </svg>`;var h=class extends at{render(){return s(st),A()}};import{defineElement as it}from"/libs/features/spectrum-web-components/dist/base.js";it("sp-icon-dash200",h);import{html as mt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as lt}from"/libs/features/spectrum-web-components/dist/icon.js";var E=({width:t=24,height:r=24,title:e="Dash300"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 12 12"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path d="M10.61 7.085H1.39a1.085 1.085 0 0 1 0-2.17h9.22a1.085 1.085 0 0 1 0 2.17" />
-  </svg>`;var c=class extends lt{render(){return s(mt),E()}};import{defineElement as pt}from"/libs/features/spectrum-web-components/dist/base.js";pt("sp-icon-dash300",c);import{html as nt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as ht}from"/libs/features/spectrum-web-components/dist/icon.js";var z=({width:t=24,height:r=24,title:e="Dash75"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 8 8"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path d="M6.99 4.96H1.01a.96.96 0 0 1 0-1.92h5.98a.96.96 0 0 1 0 1.92" />
-  </svg>`;var g=class extends ht{render(){return s(nt),z()}};import{defineElement as ct}from"/libs/features/spectrum-web-components/dist/base.js";ct("sp-icon-dash75",g);import{html as gt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as ft}from"/libs/features/spectrum-web-components/dist/icon.js";var D=({width:t=24,height:r=24,title:e="Cross75"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 8 8"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m5.188 4 2.14-2.14A.84.84 0 1 0 6.141.672L4 2.812 1.86.672A.84.84 0 0 0 .672 1.86L2.812 4 .672 6.14A.84.84 0 1 0 1.86 7.328L4 5.188l2.14 2.14A.84.84 0 1 0 7.328 6.14z"
-    />
-  </svg>`;var f=class extends ft{render(){return s(gt),D()}};import{defineElement as dt}from"/libs/features/spectrum-web-components/dist/base.js";dt("sp-icon-cross75",f);import{html as ut}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as wt}from"/libs/features/spectrum-web-components/dist/icon.js";var M=({width:t=24,height:r=24,title:e="Cross100"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 8 8"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m5.238 4 2.456-2.457A.875.875 0 1 0 6.456.306L4 2.763 1.543.306A.875.875 0 0 0 .306 1.544L2.763 4 .306 6.457a.875.875 0 1 0 1.238 1.237L4 5.237l2.456 2.457a.875.875 0 1 0 1.238-1.237z"
-    />
-  </svg>`;var d=class extends wt{render(){return s(ut),M()}};import{defineElement as xt}from"/libs/features/spectrum-web-components/dist/base.js";xt("sp-icon-cross100",d);import{html as Ct}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as vt}from"/libs/features/spectrum-web-components/dist/icon.js";var H=({width:t=24,height:r=24,title:e="Cross200"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 10 10"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m6.29 5 2.922-2.922a.911.911 0 0 0-1.29-1.29L5 3.712 2.078.789a.911.911 0 0 0-1.29 1.289L3.712 5 .79 7.922a.911.911 0 1 0 1.289 1.29L5 6.288 7.923 9.21a.911.911 0 0 0 1.289-1.289z"
-    />
-  </svg>`;var u=class extends vt{render(){return s(Ct),H()}};import{defineElement as Tt}from"/libs/features/spectrum-web-components/dist/base.js";Tt("sp-icon-cross200",u);import{html as Lt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as It}from"/libs/features/spectrum-web-components/dist/icon.js";var j=({width:t=24,height:r=24,title:e="Cross300"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 12 12"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m7.344 6 3.395-3.396a.95.95 0 0 0-1.344-1.342L6 4.657 2.604 1.262a.95.95 0 0 0-1.342 1.342L4.657 6 1.262 9.396a.95.95 0 0 0 1.343 1.343L6 7.344l3.395 3.395a.95.95 0 0 0 1.344-1.344z"
-    />
-  </svg>`;var w=class extends It{render(){return s(Lt),j()}};import{defineElement as $t}from"/libs/features/spectrum-web-components/dist/base.js";$t("sp-icon-cross300",w);import{html as kt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Bt}from"/libs/features/spectrum-web-components/dist/icon.js";var q=({width:t=24,height:r=24,title:e="Cross400"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 12 12"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m7.398 6 3.932-3.932A.989.989 0 0 0 9.932.67L6 4.602 2.068.67A.989.989 0 0 0 .67 2.068L4.602 6 .67 9.932a.989.989 0 1 0 1.398 1.398L6 7.398l3.932 3.932a.989.989 0 0 0 1.398-1.398z"
-    />
-  </svg>`;var x=class extends Bt{render(){return s(kt),q()}};import{defineElement as bt}from"/libs/features/spectrum-web-components/dist/base.js";bt("sp-icon-cross400",x);import{html as At}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Et}from"/libs/features/spectrum-web-components/dist/icon.js";var y=({width:t=24,height:r=24,title:e="Cross500"}={})=>o`<svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 14 14"
-    aria-hidden="true"
-    role="img"
-    fill="currentColor"
-    aria-label=${e}
-    width=${t}
-    height=${r}
-  >
-    <path
-      d="m8.457 7 4.54-4.54a1.03 1.03 0 0 0-1.458-1.456L7 5.543l-4.54-4.54a1.03 1.03 0 0 0-1.457 1.458L5.543 7l-4.54 4.54a1.03 1.03 0 1 0 1.457 1.456L7 8.457l4.54 4.54a1.03 1.03 0 0 0 1.456-1.458z"
-    />
-  </svg>`;var C=class extends Et{render(){return s(At),y()}};import{defineElement as zt}from"/libs/features/spectrum-web-components/dist/base.js";zt("sp-icon-cross500",C);import{html as Dt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Mt}from"/libs/features/spectrum-web-components/dist/icon.js";var F=({width:t=24,height:r=24,title:e="Chevron100"}={})=>o`<svg
+  </svg>`;var p=class extends _{render(){return s(Z),B()}};import{defineElement as tt}from"/libs/features/spectrum-web-components/dist/base.js";tt("sp-icon-checkmark75",p);import{html as rt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as et}from"/libs/features/spectrum-web-components/dist/icon.js";var b=({width:t=24,height:r=24,title:e="Chevron100"}={})=>o`<svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 10 10"
     aria-hidden="true"
@@ -201,4 +79,126 @@ import{html as K}from"/libs/features/spectrum-web-components/dist/base.js";impor
     <path
       d="M3 9.95a.875.875 0 0 1-.615-1.498L5.88 5 2.385 1.547A.875.875 0 0 1 3.615.302L7.74 4.377a.876.876 0 0 1 0 1.246L3.615 9.698A.872.872 0 0 1 3 9.95"
     />
-  </svg>`;var v=class extends Mt{render(){return s(Dt),F()}};import{defineElement as Ht}from"/libs/features/spectrum-web-components/dist/base.js";Ht("sp-icon-chevron100",v);
+  </svg>`;var n=class extends et{render(){return s(rt),b()}};import{defineElement as ot}from"/libs/features/spectrum-web-components/dist/base.js";ot("sp-icon-chevron100",n);import{html as st}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as at}from"/libs/features/spectrum-web-components/dist/icon.js";var A=({width:t=24,height:r=24,title:e="Cross75"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 8 8"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m5.188 4 2.14-2.14A.84.84 0 1 0 6.141.672L4 2.812 1.86.672A.84.84 0 0 0 .672 1.86L2.812 4 .672 6.14A.84.84 0 1 0 1.86 7.328L4 5.188l2.14 2.14A.84.84 0 1 0 7.328 6.14z"
+    />
+  </svg>`;var h=class extends at{render(){return s(st),A()}};import{defineElement as it}from"/libs/features/spectrum-web-components/dist/base.js";it("sp-icon-cross75",h);import{html as mt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as lt}from"/libs/features/spectrum-web-components/dist/icon.js";var E=({width:t=24,height:r=24,title:e="Cross100"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 8 8"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m5.238 4 2.456-2.457A.875.875 0 1 0 6.456.306L4 2.763 1.543.306A.875.875 0 0 0 .306 1.544L2.763 4 .306 6.457a.875.875 0 1 0 1.238 1.237L4 5.237l2.456 2.457a.875.875 0 1 0 1.238-1.237z"
+    />
+  </svg>`;var c=class extends lt{render(){return s(mt),E()}};import{defineElement as pt}from"/libs/features/spectrum-web-components/dist/base.js";pt("sp-icon-cross100",c);import{html as nt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as ht}from"/libs/features/spectrum-web-components/dist/icon.js";var z=({width:t=24,height:r=24,title:e="Cross200"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 10 10"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m6.29 5 2.922-2.922a.911.911 0 0 0-1.29-1.29L5 3.712 2.078.789a.911.911 0 0 0-1.29 1.289L3.712 5 .79 7.922a.911.911 0 1 0 1.289 1.29L5 6.288 7.923 9.21a.911.911 0 0 0 1.289-1.289z"
+    />
+  </svg>`;var g=class extends ht{render(){return s(nt),z()}};import{defineElement as ct}from"/libs/features/spectrum-web-components/dist/base.js";ct("sp-icon-cross200",g);import{html as gt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as ft}from"/libs/features/spectrum-web-components/dist/icon.js";var D=({width:t=24,height:r=24,title:e="Cross300"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 12 12"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m7.344 6 3.395-3.396a.95.95 0 0 0-1.344-1.342L6 4.657 2.604 1.262a.95.95 0 0 0-1.342 1.342L4.657 6 1.262 9.396a.95.95 0 0 0 1.343 1.343L6 7.344l3.395 3.395a.95.95 0 0 0 1.344-1.344z"
+    />
+  </svg>`;var f=class extends ft{render(){return s(gt),D()}};import{defineElement as dt}from"/libs/features/spectrum-web-components/dist/base.js";dt("sp-icon-cross300",f);import{html as ut}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as wt}from"/libs/features/spectrum-web-components/dist/icon.js";var M=({width:t=24,height:r=24,title:e="Cross400"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 12 12"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m7.398 6 3.932-3.932A.989.989 0 0 0 9.932.67L6 4.602 2.068.67A.989.989 0 0 0 .67 2.068L4.602 6 .67 9.932a.989.989 0 1 0 1.398 1.398L6 7.398l3.932 3.932a.989.989 0 0 0 1.398-1.398z"
+    />
+  </svg>`;var d=class extends wt{render(){return s(ut),M()}};import{defineElement as xt}from"/libs/features/spectrum-web-components/dist/base.js";xt("sp-icon-cross400",d);import{html as Ct}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as vt}from"/libs/features/spectrum-web-components/dist/icon.js";var H=({width:t=24,height:r=24,title:e="Cross500"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 14 14"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path
+      d="m8.457 7 4.54-4.54a1.03 1.03 0 0 0-1.458-1.456L7 5.543l-4.54-4.54a1.03 1.03 0 0 0-1.457 1.458L5.543 7l-4.54 4.54a1.03 1.03 0 1 0 1.457 1.456L7 8.457l4.54 4.54a1.03 1.03 0 0 0 1.456-1.458z"
+    />
+  </svg>`;var u=class extends vt{render(){return s(Ct),H()}};import{defineElement as Tt}from"/libs/features/spectrum-web-components/dist/base.js";Tt("sp-icon-cross500",u);import{html as Lt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as It}from"/libs/features/spectrum-web-components/dist/icon.js";var j=({width:t=24,height:r=24,title:e="Dash100"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 10 10"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path d="M8.5 6h-7a1 1 0 0 1 0-2h7a1 1 0 0 1 0 2" />
+  </svg>`;var w=class extends It{render(){return s(Lt),j()}};import{defineElement as $t}from"/libs/features/spectrum-web-components/dist/base.js";$t("sp-icon-dash100",w);import{html as kt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Bt}from"/libs/features/spectrum-web-components/dist/icon.js";var q=({width:t=24,height:r=24,title:e="Dash200"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 12 12"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path d="M10.021 7.042H1.98a1.042 1.042 0 1 1 0-2.083h8.043a1.042 1.042 0 0 1 0 2.083z" />
+  </svg>`;var x=class extends Bt{render(){return s(kt),q()}};import{defineElement as bt}from"/libs/features/spectrum-web-components/dist/base.js";bt("sp-icon-dash200",x);import{html as At}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Et}from"/libs/features/spectrum-web-components/dist/icon.js";var y=({width:t=24,height:r=24,title:e="Dash300"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 12 12"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path d="M10.61 7.085H1.39a1.085 1.085 0 0 1 0-2.17h9.22a1.085 1.085 0 0 1 0 2.17" />
+  </svg>`;var C=class extends Et{render(){return s(At),y()}};import{defineElement as zt}from"/libs/features/spectrum-web-components/dist/base.js";zt("sp-icon-dash300",C);import{html as Dt}from"/libs/features/spectrum-web-components/dist/base.js";import{IconBase as Mt}from"/libs/features/spectrum-web-components/dist/icon.js";var F=({width:t=24,height:r=24,title:e="Dash75"}={})=>o`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 8 8"
+    aria-hidden="true"
+    role="img"
+    fill="currentColor"
+    aria-label=${e}
+    width=${t}
+    height=${r}
+  >
+    <path d="M6.99 4.96H1.01a.96.96 0 0 1 0-1.92h5.98a.96.96 0 0 1 0 1.92" />
+  </svg>`;var v=class extends Mt{render(){return s(Dt),F()}};import{defineElement as Ht}from"/libs/features/spectrum-web-components/dist/base.js";Ht("sp-icon-dash75",v);
