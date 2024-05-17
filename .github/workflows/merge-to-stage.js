63c104044017377684d94195a06996f1e87b8d5a
Okan Sahin <osahin@adobe.com>
2024-05-17 12:13:04 +0200

    Improved error handling

--- .github/workflows/merge-to-stage.js
+++ .github/workflows/merge-to-stage.js
@@ -125,41 +125,45 @@ const getPRs = async () => {
 const merge = async ({ prs }) => {
   console.log(`Merging ${prs.length || 0} PRs that are ready... `);
   for await (const { number, files, html_url, title, labels } of prs) {
-    if (files.some((file) => SEEN[file])) {
-      console.log(`Skipping ${number}: ${title} due to overlap in files.`);
-      continue;
-    }
-    files.forEach((file) => (SEEN[file] = true));
-    if (!process.env.LOCAL_RUN) {
-      await github.rest.pulls.merge({
-        owner,
-        repo,
-        pull_number: number,
-        merge_method: 'squash',
-      });
-    }
-    body = `- ${html_url}\n${body}`;
-    const isHighImpact = labels.includes(LABELS.highImpact);
-    if (isHighImpact && process.env.SLACK_HIGH_IMPACT_PR_WEBHOOK) {
+    try {
+      if (files.some((file) => SEEN[file])) {
+        console.log(`Skipping ${number}: ${title} due to overlap in files.`);
+        continue;
+      }
+      files.forEach((file) => (SEEN[file] = true));
+      if (!process.env.LOCAL_RUN) {
+        await github.rest.pulls.merge({
+          owner,
+          repo,
+          pull_number: number,
+          merge_method: 'squash',
+        });
+      }
+      body = `- ${html_url}\n${body}`;
+      const isHighImpact = labels.includes(LABELS.highImpact);
+      if (isHighImpact && process.env.SLACK_HIGH_IMPACT_PR_WEBHOOK) {
+        await slackNotification(
+          SLACK.merge({
+            html_url,
+            number,
+            title,
+            highImpact: ' :alert: High impact',
+          }),
+          process.env.SLACK_HIGH_IMPACT_PR_WEBHOOK
+        );
+      }
       await slackNotification(
         SLACK.merge({
           html_url,
           number,
           title,
-          highImpact: ' :alert: High impact',
-        }),
-        process.env.SLACK_HIGH_IMPACT_PR_WEBHOOK
+          highImpact: isHighImpact ? ' :alert: High impact' : '',
+        })
       );
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    } catch (error) {
+      console.log(`Error merging ${number}: ${title}`, error.message);
     }
-    await slackNotification(
-      SLACK.merge({
-        html_url,
-        number,
-        title,
-        highImpact: isHighImpact ? ' :alert: High impact' : '',
-      })
-    );
-    await new Promise((resolve) => setTimeout(resolve, 5000));
   }
 };
 
