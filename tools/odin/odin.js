b70a43e430398fd9e313f2f46b81405637289c43
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-05-17 14:24:27 +0200

    post msg

--- tools/odin/odin.js
+++ tools/odin/odin.js
@@ -115,20 +115,8 @@ class OdinSearch extends LitElement {
 
   onClick(e) {
     if (e.target?.id === 'copy') {
-      setTimeout(() => {
-        const { dataset: { miloBlock, odinPath, aueLabel } } = e.target.closest('li').querySelector('.block');
-        // create a link with the above params and copy it to clipboard both as text and html
-        const link = document.createElement('a');
-        link.href = `https://milo.adobe.com/tools/odin/index.html?fragment=${odinPath}`;
-        link.innerHTML = `<strong>${miloBlock}</strong>: ${aueLabel}`;
-        link.style.display = 'none';
-
-        const linkBlob = new Blob([link.outerHTML], { type: 'text/html' });
-        const textBlob = new Blob([link.href], { type: 'text/plain' });
-        // eslint-disable-next-line no-undef
-        const data = [new ClipboardItem({ [linkBlob.type]: linkBlob, [textBlob.type]: textBlob })];
-        navigator.clipboard.write(data, console.log, console.error);
-      }, 100);
+      const { dataset: { miloBlock, odinPath, aueLabel } } = e.target.closest('li').querySelector('.block');
+      window.top.postMessage({ type: 'odin:copy', data: { miloBlock, odinPath, aueLabel } }, '*');
     }
   }
 
