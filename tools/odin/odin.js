2b6a1fbaa275765172585736aaf8c0f067c20f7c
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-05-17 18:34:11 +0200

    update

--- tools/odin/odin.js
+++ tools/odin/odin.js
@@ -185,7 +185,7 @@ class OdinSearch extends LitElement {
   render() {
     return html`
       <sp-theme color="light" scale="medium">
-        <h1>Merch at Scale Fragments</h1>
+        <h1>Merch at Scale Studio</h1>
         <div>
         <sp-search placeholder="Search" value="Photoshop" size="m"></sp-search>
         <sp-picker label="Fragment model" size="m">
