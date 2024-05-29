475f4f4a7f8e27db215a12aeac2f0fc5c6ef47f8
Sean Archibeque <sean.archibeque@gmail.com>
2024-05-29 15:35:52 -0600

    MWPW-151416 - [LocUI] skip URL validation Language tiles (#2379)
    check validation is done on a url before showing an error

--- libs/blocks/locui/url/view.js
+++ libs/blocks/locui/url/view.js
@@ -4,11 +4,12 @@ import Tabs from './tabs.js';
 export default function Url({ suffix, item }) {
   const sourcePath = item.pathname;
   const langstorePath = item.langstore?.pathname;
+  const urlHasError = item.valid !== undefined && !item.valid;
 
   return html`
-    <li class="locui-url${!item.valid ? ' error' : ''}">
+    <li class="locui-url${urlHasError ? ' error' : ''}">
       <h3 class=locui-url-label>Path</h3>
-      <p class=locui-url-path>${sourcePath}${!item.valid ? html`<span>NOT FOUND</span>` : ''}</p>
+      <p class=locui-url-path>${sourcePath}${urlHasError ? html`<span>NOT FOUND</span>` : ''}</p>
       <div class="locui-url-tab-group locui-url-tab-group-cols-${suffix.length}">
         <${Tabs} suffix=${suffix[0]} path=${sourcePath} />
         ${langstorePath && html`
