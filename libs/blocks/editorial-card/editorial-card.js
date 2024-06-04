541876a78566d1a72b2b3fd23839cbc83f94b1f1
thi64146 <thi64146@adobe.com>
2024-06-04 16:01:08 -0600

    set some defaults on edy-card

--- libs/blocks/editorial-card/editorial-card.js
+++ libs/blocks/editorial-card/editorial-card.js
@@ -1,7 +1,27 @@
-import { decorateButtons } from '../../utils/decorate.js';
+import { decorateBlockBg, decorateBlockText, decorateBlockHrs } from '../../utils/decorate.js';
+
+function decorateLockupFromContent(el) {
+  const rows = el.querySelectorAll(':scope > div > p');
+  const firstRowImg = rows[0]?.querySelector('img');
+  if (firstRowImg) rows[0].classList.add('lockup-area');
+}
 
 const init = (el) => {
-  decorateButtons(el);
+  el.classList.add('con-block');
+  let rows = el.querySelectorAll(':scope > div');
+  if (rows.length > 1) {
+    if (rows[0].textContent !== '') el.classList.add('has-bg');
+    const [head, middle, ...tail] = rows;
+    decorateBlockBg(el, head);
+    middle?.classList.add('media-area');
+    rows = tail;
+    rows.forEach((row) => {
+      row.classList.add('foreground');
+      decorateBlockText(row, ['xxl', 'xs', 'xl']);
+    });
+  }
+  decorateBlockHrs(rows[0]);
+  decorateLockupFromContent(rows[0]);
 };
 
 export default init;
