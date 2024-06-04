789219e5bb8307b9bdb25564becd3693dac80aaa
thi64146 <thi64146@adobe.com>
2024-06-04 16:43:22 -0600

    styled lockup-area

--- libs/blocks/editorial-card/editorial-card.js
+++ libs/blocks/editorial-card/editorial-card.js
@@ -1,9 +1,19 @@
+import { createTag } from '../../utils/utils.js';
 import { decorateBlockBg, decorateBlockText, decorateBlockHrs } from '../../utils/decorate.js';
 
 function decorateLockupFromContent(el) {
   const rows = el.querySelectorAll(':scope > div > p');
   const firstRowImg = rows[0]?.querySelector('img');
-  if (firstRowImg) rows[0].classList.add('lockup-area');
+  if (firstRowImg) {
+    rows[0].classList.add('lockup-area');
+    rows[0].childNodes.forEach((node) => {
+      console.log(node.nodeType === 3, node.nodeValue !== ' ');
+      if (node.nodeType === 3 && node.nodeValue !== ' ') {
+        const newSpan = createTag('span', { class: 'lockup-label' }, node.nodeValue);
+        node.parentElement.replaceChild(newSpan, node);
+      }
+    });
+  }
 }
 
 const init = (el) => {
