3a9f700b494632209a6c6bb49d98212290e667ed
Aishwarya Mathuria <mathuria@adobe.com>
2024-06-05 15:26:39 +0530

    Update utils.js

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1145,8 +1145,20 @@ async function processSection(section, config, isDoc) {
 
   return section.blocks;
 }
-
-export async function loadArea(area = document) {
+export async function loadArea(area = document) {  const bannerData = getMetadata('app-banner-source');
+  console.log('drashti metadata', bannerData);
+  if (bannerData) {
+    // console.log('gnav', area.body.querySelector('header'));
+    const body = area.body;
+    // const header = area.body.querySelector('header');
+    // body.style.position = 'absolute';
+    // body.style.marginTop = '80px';
+    const x = document.createElement('div');
+    x.style.minHeight = '80px'; // class
+    x.style.display = 'flex';
+    x.classList.add('app-banner-con');
+    body.prepend(x);
+  }
   const isDoc = area === document;
 
   if (isDoc) {
