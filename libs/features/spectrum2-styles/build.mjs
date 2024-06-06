8e803ef1dd4296505d14c16ea584f313272123fa
SaaSha Pina <saasha@heysparkbox.com>
2024-06-05 22:56:14 -0400

    update: replace --spectrum prefix with --s2

--- libs/features/spectrum2-styles/build.mjs
+++ libs/features/spectrum2-styles/build.mjs
@@ -8,9 +8,9 @@ const miloCSSPath = path.resolve('../../deps/spectrum2-styles.css');
 const spectrumCSS = fs.readFileSync(spectrumCSSPath, 'utf8');
 const miloCSS = fs.readFileSync(miloCSSPath, 'utf8');
 
-// Formats and transforms Spectrum CSS Rgb and opacity properties into one value
+// Formats and transforms Spectrum CSS Rgb and opacity properties for performance
 const transformRgbProperties = (rule) => {
-  const propertiesToUpdate = {};
+  const properties = {};
 
   const replaceZeroPx = (value) => value.replace(/\b0px\b/g, '0');
 
@@ -21,28 +21,21 @@ const transformRgbProperties = (rule) => {
 
     if (prop.endsWith('-rgb')) {
       const baseName = prop.replace('-rgb', '');
-      propertiesToUpdate[baseName] = `rgb(${value.replace(/,\s*/g, ' ')})`;
+      properties[baseName] = `rgb(${value.replace(/,\s*/g, ' ')})`;
       decl.remove();
     }
 
     if (prop.endsWith('-opacity')) {
       const baseName = prop.replace('-opacity', '');
-      if (propertiesToUpdate[baseName]) {
-        const opacityPercent = parseFloat(value) * 100;
-        propertiesToUpdate[baseName] = propertiesToUpdate[baseName].replace(')', ` / ${opacityPercent}%)`);
-      } else {
-        propertiesToUpdate[baseName] = `rgb(0 0 0 / ${value})`; // Placeholder if no corresponding -rgb is found
-      }
+      properties[baseName] = properties[baseName] ? properties[baseName].replace(')', ` / ${parseFloat(value) * 100}%)`) : `rgb(0 0 0 / ${value})`;
       decl.remove();
     }
   });
 
-  Object.keys(propertiesToUpdate).forEach((prop) => {
-    rule.append({ prop, value: propertiesToUpdate[prop] });
-  });
+  Object.keys(properties).forEach((prop) => rule.append({ prop, value: properties[prop] }));
 };
 
-const extractAndTransformCustomProperties = (css) => {
+const extractAndTransformCustomProperties = (css, prefix) => {
   const customProperties = {};
   postcss.parse(css).walkRules((rule) => {
     const { selector } = rule;
@@ -53,41 +46,30 @@ const extractAndTransformCustomProperties = (css) => {
     transformRgbProperties(rule);
 
     rule.walkDecls((decl) => {
-      customProperties[selector][decl.prop] = decl.value;
+      let propName = decl.prop;
+      if (propName.startsWith(prefix)) propName = `--spectrum${propName.slice(prefix.length)}`;
+      customProperties[selector][propName] = decl.value;
     });
   });
   return customProperties;
 };
 
-const spectrumCustomProperties = extractAndTransformCustomProperties(spectrumCSS);
-const miloCustomProperties = extractAndTransformCustomProperties(miloCSS);
-
-const mergedCustomProperties = {};
-
-const allSelectors = Object.keys(miloCustomProperties);
-
-allSelectors.forEach((selector) => {
-  mergedCustomProperties[selector] = { ...miloCustomProperties[selector] };
-
-  if (spectrumCustomProperties[selector]) {
-    Object.keys(miloCustomProperties[selector]).forEach((prop) => {
-      if (spectrumCustomProperties[selector][prop] !== undefined) {
-        mergedCustomProperties[selector][prop] = spectrumCustomProperties[selector][prop];
-      }
-    });
-  }
-});
+const spectrumCustomProperties = extractAndTransformCustomProperties(spectrumCSS, '--spectrum');
+const miloCustomProperties = extractAndTransformCustomProperties(miloCSS, '--s2');
 
 const updateCustomPropertiesInMiloCSS = (css) => {
   const root = postcss.parse(css);
 
   root.walkRules((rule) => {
     const { selector } = rule;
-    if (mergedCustomProperties[selector]) {
+    if (miloCustomProperties[selector]) {
       rule.walkDecls((decl) => {
         const { prop } = decl;
-        if (mergedCustomProperties[selector][prop] !== undefined) {
-          decl.value = mergedCustomProperties[selector][prop];
+        if (prop.startsWith('--s2')) {
+          const spectrumProp = `--spectrum${prop.slice(4)}`;
+          if (spectrumCustomProperties[selector][spectrumProp] !== undefined) {
+            decl.value = spectrumCustomProperties[selector][spectrumProp];
+          }
         }
       });
     }
