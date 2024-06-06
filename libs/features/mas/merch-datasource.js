8a46cf1a84b12bef3cd392fa55b81764553bde11
Axel Cureno Basurto <axel@adobe.com>
2024-06-06 13:27:33 -0700

    mas integration

--- libs/features/mas/merch-datasource.js
+++ libs/features/mas/merch-datasource.js
@@ -1,6 +1,64 @@
 import { createTag } from '../../utils/utils.js';
 
 const ODIN = 'odin';
+const ODIN_AUTHOR = 'odin-author';
+
+let accessToken;
+
+const cardContent = {
+  catalog: {
+    name: 'catalog',
+    title: {
+      tag: 'h3',
+      slot: 'heading-xs',
+    },
+    prices: {
+      tag: 'h3',
+      slot: 'heading-xs',
+    },
+    description: {
+      tag: 'div',
+      slot: 'body-xs',
+    },
+  },
+  ah: {
+    name: 'ah',
+    title: {
+      tag: 'h3',
+      slot: 'heading-xs',
+    },
+    prices: {
+      tag: 'h3',
+      slot: 'heading-xs',
+    },
+    description: {
+      tag: 'div',
+      slot: 'body-xs',
+    },
+  },
+};
+
+let cb = `?cb=${Math.round(Math.random() * 1000000)}`;
+
+const fetchCache = new Map();
+
+const fetchSmart = async (url, options) => {
+  if (fetchCache.has(url)) {
+    const res = fetchCache.get(url);
+    await res;
+    return res;
+  }
+  const res = fetch(url, options).then((res) => res.json());
+  fetchCache.set(url, res);
+  await res;
+  return res;
+};
+
+window.addEventListener('message', (e) => {
+  if (e.data.type === 'mas:updateAccessToken') {
+    accessToken = e.data.accessToken;
+  }
+});
 
 async function parseMerchLinks(merchLinkHTML) {
   await parseMerchLinks.init;
@@ -15,26 +73,34 @@ async function parseMerchLinks(merchLinkHTML) {
 }
 
 async function parseMerchCard(cardJson, merchCard) {
-  const { type = 'ah' } = cardJson;
+  const { type = 'catalog' } = cardJson;
+  const cardType = cardContent[type] || cardContent.catalog;
+
   merchCard.setAttribute('variant', type);
-  if (cardJson.icon?.length > 0) {
-    const merchIcon = createTag('merch-icon', { slot: 'icons', src: cardJson.icon[0], alt: '', href: '', size: 's' });
+  cardJson.icon?.forEach((icon) => {
+    const merchIcon = createTag('merch-icon', { slot: 'icons', src: icon, alt: '', href: '', size: 'l' });
     merchCard.append(merchIcon);
-  }
+  });
 
   if (cardJson.title) {
-    merchCard.append(createTag('h4', { slot: 'heading-xxs' }, cardJson.title));
+    merchCard.append(createTag(cardType.title.tag, { slot: cardType.title.slot }, cardJson.title));
   }
 
   if (cardJson.prices?.html) {
     const prices = await parseMerchLinks(cardJson.prices.html);
-    const headingM = createTag('h3', { slot: 'heading-xs' }, prices);
+    const headingM = createTag(cardType.prices.tag, { slot: cardType.prices.slot }, prices);
     merchCard.append(headingM);
   }
 
+  merchCard.append(createTag('p', { slot: 'body-xxs', id: 'individuals1' }, 'Desktop'));
+
   if (cardJson.description?.html) {
-    const description = createTag('div', { slot: 'body-xxs' }, cardJson.description.html);
-    merchCard.append(description);
+    const bodyXS = createTag(
+      cardType.description.tag,
+      { slot: cardType.description.slot },
+      cardJson.description.html,
+    );
+    merchCard.append(bodyXS);
   }
 
   if (cardJson.ctas?.html) {
@@ -43,13 +109,30 @@ async function parseMerchCard(cardJson, merchCard) {
       cta.style.display = 'none';
       const variant = cta.classList.contains('blue') ? 'accent' : 'primary';
       const treatment = variant === 'primary' ? 'outline' : '';
-      const size = 's';
-      const spButton = createTag('sp-button', { variant, size, treatment });
+      const spButton = createTag('sp-button', { variant, treatment });
       spButton.innerHTML = cta.innerHTML;
-      spButton.addEventListener('click', () => cta.click());
+      spButton.addEventListener('click', (e) => {
+        e.stopImmediatePropagation();
+        const inApp = merchCard.getAttribute('in-app') === '';
+        if (inApp) {
+          const [{ productArrangementCode }] = cta.value;
+          const checkoutUrl = escape(cta.href);
+          const actionData = {
+            type: 'deep-link',
+            target: `inapp://ccd?workflow=routeToPath&routePath=%2FeditPlan%3Fpa%3D${productArrangementCode}%26cli%3Dcc_desktop%26co%3DUS%26landing_page%3D${checkoutUrl}`,
+          };
+          cta.dispatchEvent(new CustomEvent('deep-link', { detail: actionData, bubbles: true }));
+        } else {
+          cta.click();
+        }
+      });
+
       return [cta, spButton];
     });
-    const footer = createTag('div', { slot: 'footer' }, ctas);
+    const color = merchCard.getAttribute('color') ?? 'light';
+    const theme = createTag('sp-theme', { theme: 'spectrum', color, scale: 'medium' }, ctas);
+    theme.style.display = 'contents';
+    const footer = createTag('div', { slot: 'footer' }, theme);
     merchCard.append(footer);
   }
 }
@@ -72,17 +155,44 @@ class MerchDatasource extends HTMLElement {
     this.parentElement.style.opacity = 0;
   }
 
+  refresh() {
+    cb = `?cb=${Math.round(Math.random() * 1000000)}`;
+    setTimeout(() => {
+      this.parentElement.querySelectorAll('[slot]').forEach((el) => el.remove());
+      this.fetchData();
+    }, 500);
+  }
+
   async fetchData() {
     const source = this.getAttribute('source') ?? ODIN;
-    const path = this.getAttribute('path');
+    let path = this.getAttribute('path');
+    const originalPath = path;
     if (!path) return;
 
-    if (source === ODIN) {
-      const response = await fetch(`https://dev-odin.adobe.com${path}.cfm.gql.json`);
-      const { data: { merchCardByPath: { item } } } = await response.json();
-      this.data = item;
-      this.render();
+    if (![ODIN, ODIN_AUTHOR].includes(source)) return;
+
+    let baseUrl = 'https://dev-odin.adobe.com';
+    let headers = {};
+    const mepSource = this.getAttribute('mep');
+    let mepRules;
+    if (mepSource) {
+      mepRules = await (fetchSmart(mepSource).then(({ experiences: { data } }) => data));
     }
+    if (source === ODIN && mepRules) {
+      path = mepRules.filter(({ action }) => action === 'replaceFragment').find(({ selector }) => selector === path)?.all ?? path;
+    } else if (source === ODIN_AUTHOR) {
+      baseUrl = 'https://author-p22655-e59341.adobeaemcloud.com';
+      headers = { Authorization: `Bearer ${accessToken}` };
+    }
+    let response;
+    response = await fetchSmart(`${baseUrl}${path}.cfm.gql.json${cb}`, { headers });
+    if (response.status === 404) {
+      response = await fetchSmart(`${baseUrl}${originalPath}.cfm.gql.json${cb}`, { headers });
+    }
+
+    const { data: { merchCardByPath: { item } } } = await response;
+    this.data = item;
+    this.render();
   }
 
   render() {
