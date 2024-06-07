c0004a9bb58006cacdfa8e732ffbf6768ba8dd14
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-06-07 11:12:41 +0200

    update co link

--- libs/features/mas/merch-datasource.js
+++ libs/features/mas/merch-datasource.js
@@ -121,7 +121,8 @@ async function parseMerchCard(cardJson, merchCard) {
           const checkoutUrl = escape(cta.href);
           const actionData = {
             type: 'deep-link',
-            target: `inapp://ccd?workflow=routeToPath&routePath=%2FeditPlan%3Fpa%${productArrangementCode}%26cli%3Dcc_desktop%26co%3DUS%26landing_page%3D${checkoutUrl}`,
+            //target: `inapp://ccd?workflow=routeToPath&routePath=%2FeditPlan%3Fpa%3D${productArrangementCode}%26cli%3Dcc_desktop%26co%3DUS%26landing_page%3D${checkoutUrl}`,
+            target: `inapp://ccd?workflow=routeToPath&routePath=%2FeditPlan%3Fpa%3D${productArrangementCode}%26cli%3Dcc_desktop%26co%3DUS%26landing_page%3Dhttps%3A%2F%2Fcommerce-stg.adobe.com%2Fstore%2Fsegmentation%26ctx%3Da`,
           };
           cta.dispatchEvent(new CustomEvent('deep-link', { detail: actionData, bubbles: true }));
         } else {
