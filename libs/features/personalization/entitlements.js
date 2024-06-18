4a866a7f0114eca715412c5721ea40dd090b5a23
vgoodric <vgoodric@adobe.com>
2024-06-18 11:47:00 -0600

    use modified getFederatedContentRoot from gnav utilities

--- libs/features/personalization/entitlements.js
+++ libs/features/personalization/entitlements.js
@@ -1,12 +1,34 @@
 import { getConfig } from '../../utils/utils.js';
 import { fetchData, DATA_TYPE } from './personalization.js';
 
+export const getFederatedContentRoot = () => {
+  const allowedOrigins = [
+    'https://www.adobe.com',
+    'https://business.adobe.com',
+    'https://blog.adobe.com',
+    'https://milo.adobe.com',
+    'https://news.adobe.com',
+  ];
+  const { origin } = window.location;
+  let federatedContentRoot = allowedOrigins.some((o) => origin.replace('.stage', '') === o)
+    ? origin
+    : 'https://www.adobe.com';
+
+  if (origin.includes('localhost') || origin.includes('.hlx.')) {
+    // Akamai as proxy to avoid 401s, given AEM-EDS MS auth cross project limitations
+    federatedContentRoot = origin.includes('.hlx.live')
+      ? 'https://main--federal--adobecom.hlx.live'
+      : 'https://www.stage.adobe.com';
+  }
+
+  return federatedContentRoot;
+};
+
 export const getEntitlementUrl = () => {
   const { env } = getConfig();
   const sheet = env.name === 'prod' ? 'prod' : 'stage';
-  const origin = env.name === 'prod' ? '' : 'stage.';
   // eslint-disable-next-line max-len
-  return `https://www.${origin}adobe.com/federal/assets/data/mep-entitlement-tags.json?sheet=${sheet}`;
+  return `${getFederatedContentRoot()}/federal/assets/data/mep-entitlement-tags.json?sheet=${sheet}`;
 };
 
 export const getEntitlementMap = async () => {
