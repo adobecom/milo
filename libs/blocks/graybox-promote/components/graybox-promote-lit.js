4a11354b9f11b1513a3e673b22866776cc4b119c
Robert Bogos <robert.adobe.github@gmail.com>
2024-06-20 11:10:33 +0300

    hotfix

--- libs/blocks/graybox-promote/components/graybox-promote-lit.js
+++ libs/blocks/graybox-promote/components/graybox-promote-lit.js
@@ -133,7 +133,7 @@ class GrayboxPromote extends LitElement {
         throw new Error('sharepoint.site.enablePromote is not enabled in graybox config');
       }
       const driveId = await getSharepointDriveId(ref, repo, owner);
-      if (spToken) {
+      if (!this.spToken) {
         return html`<button @click="${() => this.getSpTokenTask.run()}">Login</button>`;
       } else {
         return html`<button @click="${() => this.promoteTask.run(experienceName, grayboxIoEnv)}">Promote</button>`;
