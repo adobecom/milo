ab3b1a0633bbec28459f4aaa0b59666824c8e315
mathuria <mathuria@adobe.com>
2024-06-27 15:56:13 +0530

    unity

--- libs/blocks/unity/unity.js
+++ libs/blocks/unity/unity.js
@@ -1,4 +1,4 @@
 export default async function init(el) {
-  const { default: init } = await import(`https://blockpoc--unity--adobecom.hlx.live/unitylibs/blocks/unity/unity.js`);
+  const { default: init } = await import(`https://blockpoc--unity--adobecom.hlx.page/unitylibs/blocks/unity/unity.js`);
   init(el);
 }
