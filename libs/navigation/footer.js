254f850e8075529290e89fa888dd48b801d6f931
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-11 12:28:02 +0530

    Adding feds block bootstraper for non milo

---
+++ libs/navigation/footer.js
@@ -0,0 +1,25 @@
+const blockConfig = {
+  name: 'global-footer',
+  targetEl: 'footer',
+  appendType: 'appendChild',
+};
+
+async function loadBlock() {
+  const { fedsGlobalConfig = {} } = window;
+  const miloLibs = fedsGlobalConfig.miloLibs || 'https://stage--milo--adobecom.hlx.page';
+
+  // Relative path can't be used, as the script will run on consumer's app
+  const { default: bootstrapBlock } = await import(`${miloLibs}/libs/navigation/bootstrapper.js`);
+  const { default: locales } = await import(`${miloLibs}/libs/utils/locales.js`);
+  const clientConfig = {
+    origin: miloLibs,
+    contentRoot: fedsGlobalConfig.contentRoot,
+    miloLibs: `${miloLibs}/libs`,
+    pathname: `/${fedsGlobalConfig.locale || ''}`,
+    locales: fedsGlobalConfig.locales || locales,
+  };
+
+  bootstrapBlock(clientConfig, blockConfig);
+}
+
+loadBlock();
