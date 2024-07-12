6cce270e201851671d59bb607dc6c9aa177d0d71
thi64146 <thi64146@adobe.com>
2024-07-12 16:55:51 -0600

    clear tests, were prone to build up shared test state

--- test/blocks/editorial-card/editorial-card.test.js
+++ test/blocks/editorial-card/editorial-card.test.js
@@ -2,9 +2,6 @@ import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 import { getLocale, setConfig } from '../../../libs/utils/utils.js';
 
-document.body.innerHTML = await readFile({ path: './mocks/body.html' });
-const { default: init } = await import('../../../libs/blocks/editorial-card/editorial-card.js');
-
 const locales = { '': { ietf: 'en-US', tk: 'hah7vzn.css' } };
 const config = {
   imsClientId: 'milo',
@@ -12,23 +9,35 @@ const config = {
   contentRoot: `${window.location.origin}${getLocale(locales).prefix}`,
   locales,
 };
+
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+const { default: init } = await import('../../../libs/blocks/editorial-card/editorial-card.js');
 setConfig(config);
 
 describe('editorial-card', () => {
-  const editorialCards = document.querySelectorAll('.editorial-card');
+  let editorialCards;
+
   beforeEach(async () => {
+    editorialCards = document.querySelectorAll('.editorial-card');
     editorialCards.forEach((card) => {
       init(card);
     });
   });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
   it('w/ 1 row has a foreground', () => {
     const foreground = editorialCards[3].querySelector('.foreground');
     expect(foreground).to.exist;
   });
+
   it('w/ 4 rows has a footer', () => {
     const footer = editorialCards[3].querySelector('.footer');
     expect(footer).to.exist;
   });
+
   it('w/ lockup gets decorated', () => {
     const label = editorialCards[4].querySelector('.lockup-label');
     expect(label).to.exist;
