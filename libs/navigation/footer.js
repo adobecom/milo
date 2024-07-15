948ce73d58c5a3dc0b075b0d3bed8d82edc0db67
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-15 16:31:46 +0530

    Taking configs and params and not making it iife

--- libs/navigation/footer.js
+++ libs/navigation/footer.js
@@ -4,22 +4,25 @@ const blockConfig = {
   appendType: 'appendChild',
 };
 
-async function loadBlock() {
-  const { fedsGlobalConfig = {} } = window;
-  const miloLibs = fedsGlobalConfig.miloLibs || 'https://www.adobe.com';
+export default async function loadBlock(configs = {}) {
+  let miloLibs = configs.miloLibs || 'https://www.adobe.com';
+  const branch = new URLSearchParams(window.location.search).get('branch');
+  if (branch) {
+    miloLibs = `https://${branch}--milo--adobecom.hlx.page`
+  }
+  const { locale,  contentRoot} = configs;
 
   // Relative path can't be used, as the script will run on consumer's app
   const { default: bootstrapBlock } = await import(`${miloLibs}/libs/navigation/bootstrapper.js`);
   const { default: locales } = await import(`${miloLibs}/libs/utils/locales.js`);
   const clientConfig = {
+    contentRoot,
     origin: miloLibs,
-    contentRoot: fedsGlobalConfig.contentRoot,
     miloLibs: `${miloLibs}/libs`,
-    pathname: `/${fedsGlobalConfig.locale || ''}`,
-    locales: fedsGlobalConfig.locales || locales,
+    pathname: `/${locale || ''}`,
+    locales: configs.locales || locales,
   };
 
   bootstrapBlock(clientConfig, blockConfig);
 }
 
-loadBlock();
