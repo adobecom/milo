9d9944f63503de61d4663dedcec29f42f9b017f5
Bandana Laishram <bandanalaishram@gmail.com>
2024-07-16 15:14:41 +0530

    Adding configurable env names

--- libs/navigation/footer.js
+++ libs/navigation/footer.js
@@ -4,13 +4,19 @@ const blockConfig = {
   appendType: 'appendChild',
 };
 
+const envMap = {
+  prod: 'https://www.adobe.com',
+  stage: 'https://www.stage.adobe.com/',
+  qa: 'https://feds--milo--adobecom.hlx.page'
+}
+
 export default async function loadBlock(configs = {}) {
-  let miloLibs = configs.miloLibs || 'https://www.adobe.com';
-  const branch = new URLSearchParams(window.location.search).get('branch');
+  const { locale, contentRoot, env = 'prod' } = configs;
+  let miloLibs = envMap[env];
+  const branch = new URLSearchParams(window.location.search).get('navbranch');
   if (branch) {
     miloLibs = `https://${branch}--milo--adobecom.hlx.page`;
   }
-  const { locale, contentRoot } = configs;
 
   // Relative path can't be used, as the script will run on consumer's app
   const { default: bootstrapBlock } = await import(`${miloLibs}/libs/navigation/bootstrapper.js`);
