89bd6de1018918f6ed06a4e7304595e79b399d2a
H Parra <h@hgpa.tv>
2024-07-16 20:39:22 -0700

    Add necessary helper functions

--- libs/features/seotech/seotech.js
+++ libs/features/seotech/seotech.js
@@ -1,5 +1,7 @@
 export const SEOTECH_API_URL_PROD = 'https://14257-seotech.adobeioruntime.net';
 export const SEOTECH_API_URL_STAGE = 'https://14257-seotech-stage.adobeioruntime.net';
+export const SEOTECH_CDN_URL_PROD = 'https://seotech.adobe.com'; // fixme
+export const SEOTECH_CDN_URL_STAGE = 'https://seotech.adobe.com'; // fixme
 
 export function logError(msg) {
   window.lana?.log(`SEOTECH: ${msg}`, {
@@ -21,6 +23,8 @@ export async function getVideoObject(url, seotechAPIUrl) {
   return body.videoObject;
 }
 
+// todo: getStructuredData(url, cloud)
+// Use url to determine hash of JSON file to fetch from seotech CDN
 export async function getStructuredData(url, sheetUrl, seotechAPIUrl) {
   const apiUrl = new URL(seotechAPIUrl);
   apiUrl.pathname = '/api/v1/web/seotech/getStructuredData';
@@ -60,6 +64,85 @@ export async function appendScriptTag({ locationUrl, getMetadata, createTag, get
       .catch((e) => logError(e.message)));
   }
   return Promise.all(promises);
+}
+
+export const HLX_MATCHER = /([\w-]+)--([\w-]+)--([\w-]+).hlx.(page|live)/;
+export const ADOBECOM_MATCHER = /([\w-]+)(.stage)?.adobe.com/;
+export const PATHNAME_MATCHER = /^(?:\/(?<geo>(?<country>[a-z]{2}|africa|mena)(?:_(?<lang>[a-z]{2,3}))?))?(?<geopath>(?:\/(?<cloudfolder>acrobat|creativecloud|express))?\/.*)$/;
+
+export function parseAdobeUrl(rawUrl) {
+  const url = new URL(rawUrl);
+
+  const path = url.pathname.match(PATHNAME_MATCHER).groups;
+
+  const hlx = url.hostname.match(HLX_MATCHER);
+  if (hlx) {
+    return {
+      ...path,
+      domain: url.hostname,
+      pathname: url.pathname,
+      cloud: hlx[2],
+      env: {
+        page: 'stage',
+        live: 'prod',
+      }[hlx[4]],
+    };
+  }
+
+  const adobe = url.hostname.match(ADOBECOM_MATCHER);
+  if (adobe) {
+    const cloud = {
+      business: 'bacom',
+      milo: 'milo',
+    }[adobe[1]] || {
+      acrobat: 'dc',
+      creativecloud: 'cc',
+      express: 'express',
+      default: 'homepage',
+    }[path.cloudfolder || 'default'];
+
+    return {
+      ...path,
+      domain: url.hostname,
+      pathname: url.pathname,
+      cloud,
+      env: adobe[2] ? 'stage' : 'prod',
+    };
+  }
+
+  return null;
+}
+
+export async function sha256(message) {
+  const msgUint8 = new TextEncoder().encode(message);
+  const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
+export async function calcAdobeUrlHash(url) {
+  const adobeUrl = parseAdobeUrl(url);
+  const cloud = adobeUrl.cloud || '';
+  const pathname = adobeUrl.pathname.replace('.html', '');
+  const key = `${cloud}${pathname}`;
+  const hash = await sha256(key);
+  return hash;
+}
+
+// todo: replace getStructuredData
+export async function fetchStructuredData(url) {
+  const hash = await calcAdobeUrlHash(url);
+  const jsonUrl = `${SEOTECH_CDN_URL_PROD}/structured-data/${hash}.json`; // fixme
+  try {
+    const resp = await fetch(jsonUrl);
+    if (!resp || !resp.ok) return null;
+    const body = await resp.json();
+    return body;
+  } catch (e) {
+    logError(e.message);
+  }
+  return null;
 }
 
 export default appendScriptTag;
--- test/features/seotech/seotech.test.js
+++ test/features/seotech/seotech.test.js
@@ -3,7 +3,232 @@ import { stub } from 'sinon';
 import { waitForElement } from '../../helpers/waitfor.js';
 
 import { getConfig, createTag } from '../../../libs/utils/utils.js';
-import { appendScriptTag } from '../../../libs/features/seotech/seotech.js';
+import { 
+  appendScriptTag,
+  ADOBECOM_MATCHER,
+  HLX_MATCHER,
+  PATHNAME_MATCHER,
+  parseAdobeUrl,
+  sha256,
+  calcAdobeUrlHash,
+} from '../../../libs/features/seotech/seotech.js';
+
+describe('HLX_MATCHER ', () => {
+  const tests = [
+    {
+      url: 'https://main--milo--adobecom.hlx.live/',
+      expected: [
+        'main--milo--adobecom.hlx.live',
+        'main',
+        'milo',
+        'adobecom',
+        'live',
+      ],
+    },
+    {
+      url: 'https://MWPW-123456--milo--hparra.hlx.page/',
+      expected: [
+        'MWPW-123456--milo--hparra.hlx.page',
+        'MWPW-123456',
+        'milo',
+        'hparra',
+        'page',
+      ],
+    },
+    {
+      url: 'https://www.adobe.com/',
+      expected: null,
+    },
+  ];
+  tests.forEach(({ url, expected }) => {
+    it(`should parse ${url}`, () => {
+      const parsed = url.match(HLX_MATCHER);
+      expect(parsed).deep.to.equal(expected);
+    });
+  });
+});
+
+describe('ADOBECOM_MATCHER ', () => {
+  const tests = [
+    {
+      url: 'https://main--milo--adobecom.hlx.live/',
+      expected: null,
+    },
+    {
+      url: 'https://www.adobe.com/',
+      expected: [
+        'www.adobe.com',
+        'www',
+        undefined,
+      ],
+    },
+    {
+      url: 'https://business.stage.adobe.com/',
+      expected: [
+        'business.stage.adobe.com',
+        'business',
+        '.stage',
+      ],
+    },
+  ];
+  tests.forEach(({ url, expected }) => {
+    it(`should parse ${url}`, () => {
+      const parsed = url.match(ADOBECOM_MATCHER);
+      expect(parsed).deep.to.equal(expected);
+    });
+  });
+});
+
+describe('PATHNAME_MATCHER ', () => {
+  const tests = [
+    {
+      pathname: '/africa/blah',
+      expected: {
+        geo: 'africa',
+        country: 'africa',
+        lang: undefined,
+        geopath: '/blah',
+        cloudfolder: undefined,
+      },
+    },
+    {
+      pathname: '/ab_cd/acrobat/blah.html?hello',
+      expected: {
+        geo: 'ab_cd',
+        country: 'ab',
+        lang: 'cd',
+        geopath: '/acrobat/blah.html?hello',
+        cloudfolder: 'acrobat',
+      },
+    },
+  ];
+  tests.forEach(({ pathname, expected }) => {
+    it(`should parse ${pathname}`, () => {
+      const parsed = pathname.match(PATHNAME_MATCHER);
+      expect(parsed.groups).deep.to.equal(expected);
+    });
+  });
+});
+
+describe('parseAdobeUrl', () => {
+  const tests = [
+    {
+      url: 'https://main--milo--adobecom.hlx.live/drafts',
+      expected: {
+        domain: 'main--milo--adobecom.hlx.live',
+        pathname: '/drafts',
+        cloud: 'milo',
+        cloudfolder: undefined,
+        env: 'prod',
+        country: undefined,
+        geo: undefined,
+        geopath: '/drafts',
+        lang: undefined,
+      },
+    },
+    {
+      url: 'https://www.adobe.com/',
+      expected: {
+        domain: 'www.adobe.com',
+        pathname: '/',
+        cloud: 'homepage',
+        cloudfolder: undefined,
+        env: 'prod',
+        country: undefined,
+        geo: undefined,
+        geopath: '/',
+        lang: undefined,
+      },
+    },
+    {
+      url: 'https://www.adobe.com/in/express/templates/logo/youtube',
+      expected: {
+        domain: 'www.adobe.com',
+        pathname: '/in/express/templates/logo/youtube',
+        cloud: 'express',
+        cloudfolder: 'express',
+        env: 'prod',
+        country: 'in',
+        geo: 'in',
+        geopath: '/express/templates/logo/youtube',
+        lang: undefined,
+      },
+    },
+    {
+      url: 'https://business.stage.adobe.com/',
+      expected: {
+        domain: 'business.stage.adobe.com',
+        pathname: '/',
+        cloud: 'bacom',
+        cloudfolder: undefined,
+        env: 'stage',
+        country: undefined,
+        geo: undefined,
+        geopath: '/',
+        lang: undefined,
+      },
+    },
+  ];
+  tests.forEach(({ url, expected }) => {
+    it(`should parse ${url}`, () => {
+      const parsed = parseAdobeUrl(url);
+      expect(parsed).deep.to.equal(expected);
+    });
+  });
+});
+
+describe('sha256', () => {
+  it('should return a hash', async () => {
+    const message = 'hello';
+    const hash = await sha256(message);
+    expect(hash).to.equal('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824');
+  });
+});
+
+describe('calcAdobeUrlHash', () => {
+  const tests = [
+    // set 1
+    {
+      url: 'https://MWPW-123456--milo--hparra.hlx.page/drafts/example?foo=bar',
+      expected: '17d0d0712b603227422e78ab8113271c6ace1b7bf034804fa773b370e99de691',
+    },
+    {
+      url: 'https://stage--milo--adobecom.hlx.live/drafts/example?filter=foo',
+      expected: '17d0d0712b603227422e78ab8113271c6ace1b7bf034804fa773b370e99de691',
+    },
+    {
+      url: 'https://milo.stage.adobe.com/drafts/example?nothing#something',
+      expected: '17d0d0712b603227422e78ab8113271c6ace1b7bf034804fa773b370e99de691',
+    },
+    {
+      url: 'https://milo.adobe.com/drafts/example',
+      expected: '17d0d0712b603227422e78ab8113271c6ace1b7bf034804fa773b370e99de691',
+    },
+    // set 2
+    {
+      url: 'https://main--cc--adobecom.hlx.page/in/creativecloud/example?foo=bar',
+      expected: '2c17ad64467b6c0b6a960279a0dbe7d8b2792d8d066907c990e2f62933895584',
+    },
+    {
+      url: 'https://main--cc--adobecom.hlx.live/in/creativecloud/example?filter=foo',
+      expected: '2c17ad64467b6c0b6a960279a0dbe7d8b2792d8d066907c990e2f62933895584',
+    },
+    {
+      url: 'https://www.stage.adobe.com/in/creativecloud/example?nothing#something',
+      expected: '2c17ad64467b6c0b6a960279a0dbe7d8b2792d8d066907c990e2f62933895584',
+    },
+    {
+      url: 'https://www.adobe.com/in/creativecloud/example.html',
+      expected: '2c17ad64467b6c0b6a960279a0dbe7d8b2792d8d066907c990e2f62933895584',
+    },
+  ];
+  tests.forEach(({ url, expected }) => {
+    it(`should hash ${url}`, async () => {
+      const hash = await calcAdobeUrlHash(url);
+      expect(hash).to.equal(expected);
+    });
+  });
+});
 
 describe('seotech', () => {
   describe('appendScriptTag + seotech-structured-data', () => {
