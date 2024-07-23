06b0aabe6047510af9ee989d26ceb7cf9dd3f1be
vgoodric <vgoodric@adobe.com>
2024-07-23 02:26:50 -0600

    add preload

--- libs/features/personalization/personalization.js
+++ libs/features/personalization/personalization.js
@@ -950,6 +950,13 @@ export async function init(enablements = {}) {
     };
     manifests = manifests.concat(await combineMepSources(pzn, promo, mepParam));
   }
+  if (target === true && manifests.length) {
+    manifests.forEach((manifest) => {
+      if (manifest.disabled) return;
+      const localizedURL = localizeLink(manifest.manifestPath);
+      loadLink(localizedURL, { as: 'fetch', crossorigin: 'anonymous', rel: 'preload' });
+    });
+  }
   if (target === true || (target === 'gnav' && postLCP)) {
     const { getTargetPersonalization } = await import('../../martech/martech.js');
     const { targetManifests, targetPropositions } = await getTargetPersonalization();
