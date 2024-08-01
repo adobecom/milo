6ae09a3911616b6eb2a46e59f625987a5f3ae555
theankit-sinha <ankitsinha@adobe.com>
2024-08-01 21:47:50 +0530

    Query Parms update & sendTargetResponse function update (#2664)
    * Query Parms update & sendTargetResponse function update
    - sendTargetResponse: Added Check for window._satellite and changed direct alloy call to _satellite call
    - Added additional parameter as marketingtech to disable analytics calls
    
    * Query Parms update & sendTargetResponse function update
    - Updated conditional chaining
    
    ---------
    
    Co-authored-by: milo-pr-merge[bot] <169241390+milo-pr-merge[bot]@users.noreply.github.com>

--- libs/martech/martech.js
+++ libs/martech/martech.js
@@ -89,20 +89,20 @@ function sendTargetResponseAnalytics(failure, responseStart, timeout, message) {
   const timeoutTime = roundToQuarter(timeout);
   let val = `target response time ${responseTime}:timed out ${failure}:timeout ${timeoutTime}`;
   if (message) val += `:${message}`;
-  window.alloy('sendEvent', {
-    documentUnloading: true,
-    xdm: {
-      eventType: 'web.webinteraction.linkClicks',
-      web: {
-        webInteraction: {
-          linkClicks: { value: 1 },
-          type: 'other',
-          name: val,
+    window._satellite?.track?.('event', {
+      documentUnloading: true,
+      xdm: {
+        eventType: 'web.webinteraction.linkClicks',
+        web: {
+          webInteraction: {
+            linkClicks: { value: 1 },
+            type: 'other',
+            name: val,
+          },
         },
       },
-    },
-    data: { _adobe_corpnew: { digitalData: { primaryEvent: { eventInfo: { eventName: val } } } } },
-  });
+      data: { _adobe_corpnew: { digitalData: { primaryEvent: { eventInfo: { eventName: val } } } } },
+    });
 }
 
 export const getTargetPersonalization = async () => {
