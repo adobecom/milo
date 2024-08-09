4a8f41ee3eb34bd28c80c68a46574e563b0acbe7
Mira <myrosvas@gmail.com>
2024-08-09 18:26:10 +0200

    debugging

--- libs/features/spectrum-web-components/src/overlay.js
+++ libs/features/spectrum-web-components/src/overlay.js
@@ -1,2 +1,4 @@
 import '@spectrum-web-components/overlay/sp-overlay.js';
 import '@spectrum-web-components/overlay/overlay-trigger.js';
+
+export { SlottableRequestEvent } from '@spectrum-web-components/overlay/src/slottable-request-event.js';
