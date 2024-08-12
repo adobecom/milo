37b6538bd34f33bdb3f87caf80c17a7b753ac44b
Jason Slavin <slavin@adobe.com>
2024-08-12 15:23:42 -0700

    Working, needs refactoring and tests

--- libs/features/dynamic-navigation.js
+++ libs/features/dynamic-navigation.js
@@ -1,6 +1,29 @@
 import { getMetadata } from '../utils/utils.js';
 
+function checkForIgnoreValues() {
+  const dynamicNavDisableValues = getMetadata('dynamic-nav-disable');
+
+  if (!dynamicNavDisableValues) {
+    window.lana.log('Dynamic-nav-disables not present to deactivate dynamic nav');
+    return false;
+  }
+
+  let match = false;
+
+  const metadataPairsMap = dynamicNavDisableValues.split(',').map((pair) => pair.split(';'));
+  metadataPairsMap.forEach((pair) => {
+    const [metadataKey, metadataContent] = pair;
+    const metaTagContent = getMetadata(metadataKey.toLowerCase());
+    if (!metaTagContent) return false;
+    if (metaTagContent.toLowerCase() !== metadataContent.toLowerCase()) return false;
+    match = true;
+  });
+
+  return match;
+}
+
 export default function dynamicNav(url, key) {
+  if (checkForIgnoreValues()) return url;
   const metadataContent = getMetadata('dynamic-nav');
 
   if (metadataContent === 'entry') {
