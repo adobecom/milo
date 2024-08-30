6f1f6cc9ecf3b4e8821d02966396a6735c08de73
Robert Bogos <robert.adobe.github@gmail.com>
2024-08-30 09:54:08 +0300

    added facades for youtube & vimeo

--- libs/blocks/vimeo/vimeo.js
+++ libs/blocks/vimeo/vimeo.js
@@ -1,25 +1,81 @@
 import { createIntersectionObserver, createTag, isInTextNode } from '../../utils/utils.js';
 
-export default function init(a) {
+function addPrefetch(kind, url, as) {
+  const linkElem = createTag('link', { rel: kind, href: url });
+  if (as) linkElem.as = as;
+  linkElem.crossorigin = true;
+  document.head.append(linkElem);
+}
+
+class LiteVimeo extends HTMLElement {
+  static preconnected = false;
+
+  connectedCallback() {
+    this.videoId = this.getAttribute('videoid');
+    this.language = this.getAttribute('language') || '';
+    this.setupThumbnail();
+    this.setupPlayButton();
+    this.addEventListener('pointerover', LiteVimeo.warmConnections, { once: true });
+    this.addEventListener('click', this.addIframe);
+  }
+
+  static warmConnections() {
+    if (LiteVimeo.preconnected) return;
+    LiteVimeo.preconnected = true;
+    addPrefetch('preconnect', 'https://player.vimeo.com');
+    addPrefetch('preconnect', 'https://i.vimeocdn.com');
+    addPrefetch('preconnect', 'https://f.vimeocdn.com');
+    addPrefetch('preconnect', 'https://fresnel.vimeocdn.com');
+  }
+
+  setupThumbnail() {
+    const { width, height } = this.getBoundingClientRect();
+    const roundedWidth = Math.min(Math.ceil(width / 100) * 100, 1920);
+    const roundedHeight = Math.round((roundedWidth / width) * height);
+
+    fetch(`https://vimeo.com/api/v2/video/${this.videoId}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const thumbnailUrl = data[0].thumbnail_large?.replace(/-d_[\dx]+$/i, `-d_${roundedWidth}x${roundedHeight}`);
+        this.style.backgroundImage = `url("${thumbnailUrl}")`;
+      })
+      .catch((error) => {
+        console.error('Error fetching Vimeo thumbnail:', error);
+      });
+  }
+
+  setupPlayButton() {
+    const playBtnEl = createTag('button', { type: 'button', 'aria-label': 'Play video', class: 'ltv-playbtn' });
+    this.append(playBtnEl);
+  }
+
+  addIframe() {
+    if (this.classList.contains('ltv-activated')) return;
+    this.classList.add('ltv-activated');
+    const iframeEl = createTag('iframe', {
+      style: 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute; background-color: #000;',
+      frameborder: '0',
+      title: 'Content from Vimeo',
+      allow: 'accelerometer; fullscreen; autoplay; encrypted-media; gyroscope; picture-in-picture',
+      allowFullscreen: true,
+      src: `https://player.vimeo.com/video/${encodeURIComponent(this.videoId)}?autoplay=1`,
+    });
+    this.insertAdjacentElement('afterend', iframeEl);
+    this.remove();
+    iframeEl.addEventListener('load', () => iframeEl.focus(), { once: true });
+  }
+}
+
+if (!customElements.get('lite-vimeo')) customElements.define('lite-vimeo', LiteVimeo);
+
+export default async function init(a) {
   if (isInTextNode(a)) return;
+
   const embedVimeo = () => {
     const url = new URL(a.href);
-    let src = url.href;
-    if (url.hostname !== 'player.vimeo.com') {
-      const video = url.pathname.split('/')[1];
-      src = `https://player.vimeo.com/video/${video}?app_id=122963`;
-    }
-    const iframe = createTag('iframe', {
-      src,
-      style: 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;',
-      frameborder: '0',
-      allow: 'autoplay; fullscreen; picture-in-picture',
-      allowfullscreen: 'true',
-      title: 'Content from Vimeo',
-      loading: 'lazy',
-    });
-    const wrapper = createTag('div', { class: 'embed-vimeo' }, iframe);
-
+    const videoid = url.pathname.split('/')[url.hostname === 'player.vimeo.com' ? 2 : 1];
+    const liteVimeo = createTag('lite-vimeo', { videoid });
+    const wrapper = createTag('div', { class: 'embed-vimeo' }, liteVimeo);
     a.parentElement.replaceChild(wrapper, a);
   };
 
--- libs/blocks/youtube/youtube.js
+++ libs/blocks/youtube/youtube.js
@@ -1,36 +1,111 @@
-import { createIntersectionObserver, isInTextNode } from '../../utils/utils.js';
+import { createIntersectionObserver, createTag, isInTextNode } from '../../utils/utils.js';
 
-export default function init(a) {
+class LiteYTEmbed extends HTMLElement {
+  connectedCallback() {
+    this.videoId = this.getAttribute('videoid');
+    const playBtnEl = createTag('button', { type: 'button', class: 'lty-playbtn' });
+    this.append(playBtnEl);
+    this.playLabel = this.getAttribute('playlabel') || 'Play';
+    this.style.backgroundImage = `url("https://i.ytimg.com/vi/${this.videoId}/hqdefault.jpg")`;
+    this.style.backgroundSize = 'cover';
+    this.style.backgroundPosition = 'center';
+    const playBtnLabelEl = createTag('span', { class: 'lyt-visually-hidden' });
+    playBtnLabelEl.textContent = this.playLabel;
+    playBtnEl.append(playBtnLabelEl);
+    this.addEventListener('pointerover', LiteYTEmbed.warmConnections, { once: true });
+    this.addEventListener('click', this.addIframe);
+    this.needsYTApiForAutoplay = navigator.vendor.includes('Apple') || navigator.userAgent.includes('Mobi');
+  }
+
+  static addPrefetch(kind, url, as) {
+    const linkEl = createTag('link', { rel: kind, href: url });
+    if (as) linkEl.as = as;
+    document.head.append(linkEl);
+  }
+
+  static warmConnections() {
+    if (LiteYTEmbed.preconnected) return;
+    LiteYTEmbed.addPrefetch('preconnect', 'https://www.youtube-nocookie.com');
+    LiteYTEmbed.addPrefetch('preconnect', 'https://www.google.com');
+    LiteYTEmbed.addPrefetch('preconnect', 'https://googleads.g.doubleclick.net');
+    LiteYTEmbed.addPrefetch('preconnect', 'https://static.doubleclick.net');
+    LiteYTEmbed.preconnected = true;
+  }
+
+  static loadYouTubeAPI() {
+    return new Promise((resolve) => {
+      if (window.YT && window.YT.Player) {
+        resolve();
+        return;
+      }
+      const tag = createTag('script', { src: 'https://www.youtube.com/iframe_api' });
+      window.onYouTubeIframeAPIReady = resolve;
+      document.head.appendChild(tag);
+    });
+  }
+
+  async addIframe() {
+    if (this.classList.contains('lyt-activated')) return;
+
+    this.classList.add('lyt-activated');
+    const params = new URLSearchParams(this.getAttribute('params') || []);
+    params.append('autoplay', '1');
+    params.append('playsinline', '1');
+
+    if (this.needsYTApiForAutoplay) {
+      await LiteYTEmbed.loadYouTubeAPI();
+      await new Promise((resolve) => { window.YT.ready(resolve); });
+      // eslint-disable-next-line
+      new window.YT.Player(this, {
+        videoId: this.videoId,
+        playerVars: Object.fromEntries(params),
+        allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
+        allowfullscreen: true,
+        title: this.playLabel,
+      });
+    } else {
+      const iframeEl = createTag('iframe', {
+        src: `https://www.youtube-nocookie.com/embed/${encodeURIComponent(this.videoId)}?${params.toString()}`,
+        allowFullscreen: true,
+        allow: 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture',
+        title: this.playLabel,
+      });
+      this.insertAdjacentElement('afterend', iframeEl);
+      iframeEl.focus();
+      this.remove();
+    }
+  }
+}
+
+if (!customElements.get('lite-youtube')) customElements.define('lite-youtube', LiteYTEmbed);
+
+export default async function init(a) {
   const embedVideo = () => {
     if (isInTextNode(a) || !a.origin?.includes('youtu')) return;
     const title = !a.textContent.includes('http') ? a.textContent : 'Youtube Video';
     const searchParams = new URLSearchParams(a.search);
     const id = searchParams.get('v') || a.pathname.split('/').pop();
     searchParams.delete('v');
-    const src = `https://www.youtube.com/embed/${id}?${searchParams.toString()}`;
-    const embedHTML = `
-    <div class="milo-video">
-      <iframe src="${src}" class="youtube"
-        webkitallowfullscreen mozallowfullscreen allowfullscreen
-        allow="encrypted-media; accelerometer; gyroscope; picture-in-picture"
-        scrolling="no"
-        id="player-${id}"
-        title="${title}">
-      </iframe>
-    </div>`;
-    a.insertAdjacentHTML('afterend', embedHTML);
+    const liteYTElement = createTag('lite-youtube', { videoid: id, playlabel: title });
+
+    if (searchParams.toString()) liteYTElement.setAttribute('params', searchParams.toString());
+
+    const ytContainer = createTag('div', { class: 'milo-video dark-background' }, liteYTElement);
+    a.insertAdjacentElement('afterend', ytContainer);
     a.remove();
+
     if (document.readyState === 'complete') {
-      /* eslint-disable-next-line no-underscore-dangle */
+      // eslint-disable-next-line no-underscore-dangle
       window._satellite?.track('trackYoutube');
     } else {
       document.addEventListener('readystatechange', () => {
         if (document.readyState === 'complete') {
-          /* eslint-disable-next-line no-underscore-dangle */
+          // eslint-disable-next-line no-underscore-dangle
           window._satellite?.track('trackYoutube');
         }
       });
     }
   };
+
   createIntersectionObserver({ el: a, callback: embedVideo });
 }
