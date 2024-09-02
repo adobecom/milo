07f89a7454ba4269e62e4b0632427e813b01109c
Rahul Gupta <rahulgupta999@gmail.com>
2024-09-02 15:11:40 +0530

    Block UTC for CDT

---
+++ test/blocks/countdown-timer/countdown-timer.test.js
@@ -0,0 +1,26 @@
+import { readFile } from '@web/test-runner-commands';
+import { expect } from '@esm-bundle/chai';
+import { getLocale, setConfig } from '../../../libs/utils/utils.js';
+import { waitForElement } from '../../helpers/waitfor.js';
+
+const locales = { '': { ietf: 'en-US', tk: 'hah7vzn.css' } };
+const config = {
+  imsClientId: 'milo',
+  codeRoot: '/libs',
+  contentRoot: `${window.location.origin}${getLocale(locales).prefix}`,
+  locales,
+};
+
+setConfig(config);
+
+document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+const { default: init } = await import('../../../libs/blocks/countdown-timer/countdown-timer.js');
+
+describe('countdown-timer', () => {
+  it('decorates cdt', async () => {
+    const block = document.querySelector('.countdown-timer');
+    init(block);
+    const cdt = await waitForElement('.countdown-timer');
+    expect(cdt).to.exist;
+  });
+});
