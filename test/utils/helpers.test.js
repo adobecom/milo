cca7028e18d9ee61ce530278a8c97a5c9e9ba178
Bozo Jovicic <bozo@hitthecode.com>
2024-09-05 12:47:30 +0200

    MWPW-157210 STE Promo card not appearing

--- test/utils/helpers.test.js
+++ test/utils/helpers.test.js
@@ -2,7 +2,6 @@ import { expect } from '@esm-bundle/chai';
 import { overrideUrlOrigin } from '../../libs/utils/helpers.js';
 
 describe('overrideUrlOrigin', () => {
-
   it('Change origin to http://localhost:2000', async () => {
     const link = overrideUrlOrigin('http://www.qa.adobe.com/some/page.html?a=b#hash');
     expect(link).to.equal('http://localhost:2000/some/page.html?a=b#hash');
@@ -12,5 +11,4 @@ describe('overrideUrlOrigin', () => {
     const link = overrideUrlOrigin('/some/page.html?a=b#hash');
     expect(link).to.equal('/some/page.html?a=b#hash');
   });
-
 });
