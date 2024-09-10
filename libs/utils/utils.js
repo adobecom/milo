62426d401272f7637049c982b9fe242d57fbb1d8
Sartxi <sean.archibeque@gmail.com>
2024-09-10 16:50:49 -0600

    dial in user can publish funtion

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1304,26 +1304,24 @@ export const userCanPublishPage = async (detail, isBulk = true) => {
   let canPublish = isBulk ? live?.permissions?.includes('write') : true;
   let message = 'Publishing is currently disabled for this page';
   const config = await getCustomConfig('/.milo/publish-permissions-config.json');
-  if (config) {
-    const item = config.urls?.data?.find(({ url }) => (
-      url.endsWith('**') ? webPath.includes(url.slice(0, -2)) : url === webPath
-    ));
-    if (item) {
-      canPublish = false;
-      if (item.message) message = item.message;
-      const group = config[item.group];
-      if (group && profile?.email) {
-        let isDeny;
-        const user = group.data?.find(({ allow, deny }) => {
-          if (deny) {
-            /* c8 ignore next 3 */
-            isDeny = true;
-            return deny === profile.email;
-          }
-          return allow === profile.email;
-        });
-        canPublish = isDeny ? !user : !!user;
-      }
+  const item = config?.urls?.data?.find(({ url }) => (
+    url.endsWith('**') ? webPath.includes(url.slice(0, -2)) : url === webPath
+  ));
+  if (item) {
+    canPublish = false;
+    if (item.message) message = item.message;
+    const group = config[item.group];
+    if (group && profile?.email) {
+      let isDeny;
+      const user = group.data?.find(({ allow, deny }) => {
+        if (deny) {
+          /* c8 ignore next 3 */
+          isDeny = true;
+          return deny === profile.email;
+        }
+        return allow === profile.email;
+      });
+      canPublish = isDeny ? !user : !!user;
     }
   }
   return { canPublish, message };
