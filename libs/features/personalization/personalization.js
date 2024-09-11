3679fd07498889cb974f46602dace93b3c577445
Vivian A Goodrich <101133187+vgoodric@users.noreply.github.com>
2024-09-11 13:39:40 -0600

    REVERT MWPW-157686 [MEP] Cannot spoof an experience that exists in manifest but not in Target #2844  (#2870)
    revert

--- libs/features/personalization/personalization.js
+++ libs/features/personalization/personalization.js
@@ -894,6 +894,8 @@ export function cleanAndSortManifestList(manifests) {
           freshManifest = manifestObj[manifest.manifestPath];
         }
         freshManifest.name = fullManifest.name;
+        freshManifest.selectedVariantName = fullManifest.selectedVariantName;
+        freshManifest.selectedVariant = freshManifest.variants[freshManifest.selectedVariantName];
         manifestObj[manifest.manifestPath] = freshManifest;
       } else {
         manifestObj[manifest.manifestPath] = manifest;
