d4134c85979cfbb88a01d331f8d946eb67251de5
Okan Sahin <39759830+mokimo@users.noreply.github.com>
2024-09-17 11:15:35 +0200

    MWPW-156749: Fix video CLS  (#2849)
    * MWPW-155412: Fix video CLS netting +10 lighthouse points (#2724)
    
    * fix cls by adding the videoEl without a source
    
    * adapt the video hover and in view port play
    
    * Remove no-lazy and return earlier
    
    * Fix linting issue
    
    * Consolidate duplicated logic
    
    * Move functions into the init function
    
    * Move root margin and use optional chaining
    
    * Only query for the videoEl once
    
    * Add default hash at in the right spot
    
    * Fix the figure/video interaction
    
    * Remove unused export statement
    
    * Keep the previous defaults
    
    * Re-add adding autoplay
    
    * Retain decorateBlockBg logic
    
    * Move autoplay logic to the right location
    
    * Fix linting issues
    
    * Adapt figure tests
    
    * PR Feedback
    
    * Safeguard decorateAnchorVideo
    
    * Remove anchor if neccessary
    
    * Add video test

--- test/blocks/video/video.test.js
+++ test/blocks/video/video.test.js
@@ -1,25 +1,26 @@
 import { readFile } from '@web/test-runner-commands';
 import { expect, assert } from '@esm-bundle/chai';
-
 import sinon from 'sinon';
+import { waitFor, waitForElement } from '../../helpers/waitfor.js';
 
-import { waitForElement } from '../../helpers/waitfor.js';
-import { setConfig } from '../../../libs/utils/utils.js';
+import { setConfig, createTag } from '../../../libs/utils/utils.js';
 
 setConfig({});
 const { default: init } = await import('../../../libs/blocks/video/video.js');
-document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 
 describe('video uploaded using franklin bot', () => {
-  it('decorates no-lazy video', async () => {
-    const block = document.querySelector('.video.no-lazy');
-    const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
-    block.append(a);
+  beforeEach(async () => {
+    document.body.innerHTML = await readFile({ path: './mocks/body.html' });
+  });
 
-    init(a);
-    const video = await waitForElement('.video.no-lazy video');
-    expect(video).to.exist;
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('decorates video', (done) => {
+    const anchor = createTag('a');
+    anchor.remove = () => done();
+    init(anchor);
   });
 
   it('decorates video', async () => {
@@ -74,7 +75,6 @@ describe('video uploaded using franklin bot', () => {
   it('no hoverplay attribute added when with autoplay on loop', async () => {
     const block = document.querySelector('.video.autoplay.playonhover');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -86,7 +86,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with hoverplay when only hoverplay is added to url', async () => {
     const block = document.querySelector('.video.hoveronly');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -97,7 +96,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with viewportplay only with autoplay', async () => {
     const block = document.querySelector('.video.autoplay.viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -108,7 +106,6 @@ describe('video uploaded using franklin bot', () => {
   it('play video when element reached 80% viewport', async () => {
     const block = document.querySelector('.video.autoplay.viewportplay.scrolled-80');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
     const nextFrame = () => new Promise((resolve) => {
       requestAnimationFrame(resolve);
@@ -116,13 +113,10 @@ describe('video uploaded using franklin bot', () => {
 
     init(a);
     const video = block.querySelector('video');
-    const source = video.querySelector('source');
-    source.setAttribute('src', 'https://www.adobe.com/creativecloud/media_1167374e3354ef57f126fa78a55cbc1708ac4babd.mp4');
-    source.setAttribute('type', 'video/mp4');
-
     const playSpy = sinon.spy(video, 'play');
     const pauseSpy = sinon.spy(video, 'pause');
-
+    const intersectionObserverAddsSource = () => video.querySelector('source');
+    await waitFor(intersectionObserverAddsSource);
     video.scrollIntoView();
     await nextFrame();
     await new Promise((resolve) => {
@@ -130,20 +124,22 @@ describe('video uploaded using franklin bot', () => {
     });
     assert.isTrue(playSpy.calledOnce);
 
-    document.body.scrollIntoView();
+    // push the video out of the viewport
+    const div = document.createElement('div');
+    div.style.height = '2000px';
+    video.parentNode.insertBefore(div, video);
+
     await nextFrame();
     await new Promise((resolve) => {
       setTimeout(resolve, 100);
     });
     assert.isTrue(pauseSpy.calledOnce);
-
     expect(video.hasAttribute('data-play-viewport')).to.be.true;
   });
 
   it('Don\'t play the video once it end when autoplay1 enabled', async () => {
     const block = document.querySelector('.video.autoplay1.viewportplay.ended');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
     const nextFrame = () => new Promise((resolve) => {
       requestAnimationFrame(resolve);
@@ -151,15 +147,13 @@ describe('video uploaded using franklin bot', () => {
 
     init(a);
     const video = block.querySelector('video');
-    const source = video.querySelector('source');
-    source.setAttribute('src', 'https://www.adobe.com/creativecloud/media_1167374e3354ef57f126fa78a55cbc1708ac4babd.mp4');
-    source.setAttribute('type', 'video/mp4');
-
     const playSpy = sinon.spy(video, 'play');
     const pauseSpy = sinon.spy(video, 'pause');
     const endedSpy = sinon.spy();
-    video.addEventListener('ended', endedSpy);
+    const intersectionObserverAddsSource = () => video.querySelector('source');
+    await waitFor(intersectionObserverAddsSource);
 
+    video.addEventListener('ended', endedSpy);
     video.scrollIntoView();
     await nextFrame();
     await new Promise((resolve) => {
@@ -167,13 +161,16 @@ describe('video uploaded using franklin bot', () => {
     });
     assert.isTrue(playSpy.calledOnce);
 
-    document.body.scrollIntoView();
+    // push the video out of the viewport
+    const div = document.createElement('div');
+    div.style.height = '2000px';
+    video.parentNode.insertBefore(div, video);
+
     await nextFrame();
     await new Promise((resolve) => {
       setTimeout(resolve, 100);
     });
     assert.isTrue(pauseSpy.calledOnce);
-
     video.dispatchEvent(new Event('ended'));
     await nextFrame();
     await new Promise((resolve) => {
@@ -193,7 +190,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with viewportplay only with autoplay1', async () => {
     const block = document.querySelector('.video.autoplay1.viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -204,7 +200,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with no viewportplay with autoplay1 hoverplay', async () => {
     const block = document.querySelector('.video.autoplay1.hoverplay.no-viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -215,7 +210,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with no viewportplay with hoverplay', async () => {
     const block = document.querySelector('.video.hoverplay.no-viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -226,7 +220,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with no viewportplay no autoplay', async () => {
     const block = document.querySelector('.video.no-autoplay.no-viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
@@ -237,7 +230,6 @@ describe('video uploaded using franklin bot', () => {
   it('decorate video with no viewportplay no autoplay1', async () => {
     const block = document.querySelector('.video.no-autoplay1.no-viewportplay');
     const a = block.querySelector('a');
-    a.textContent = 'no-lazy';
     block.append(a);
 
     init(a);
