417908984df14e4165d9e847087e6757dccf1d15
Okan Sahin <39759830+mokimo@users.noreply.github.com>
2024-09-18 12:25:21 +0200

    Revert "MWPW-156749: Fix video CLS " (#2899)
    Revert "MWPW-156749: Fix video CLS  (#2849)"
    
    This reverts commit d4134c85979cfbb88a01d331f8d946eb67251de5.

--- libs/utils/decorate.js
+++ libs/utils/decorate.js
@@ -1,4 +1,4 @@
-import { createTag, createIntersectionObserver } from './utils.js';
+import { createTag } from './utils.js';
 
 export function decorateButtons(el, size) {
   const buttons = el.querySelectorAll('em a, strong a, p > a strong');
@@ -113,6 +113,8 @@ export async function decorateBlockBg(block, node, { useHandleFocalpoint = false
     const allVP = [['mobile-only'], ['tablet-only'], ['desktop-only']];
     const viewports = childCount === 2 ? binaryVP : allVP;
     [...node.children].forEach((child, i) => {
+      const videoLink = child.querySelector('a[href*=".mp4"]');
+      if (videoLink && !videoLink.hash) videoLink.hash = 'autoplay';
       if (childCount > 1) child.classList.add(...viewports[i]);
       const pic = child.querySelector('picture');
       if (useHandleFocalpoint && pic
@@ -198,7 +200,7 @@ export function getImgSrc(pic) {
   return source?.srcset ? `poster='${source.srcset}'` : '';
 }
 
-function getVideoAttrs(hash, dataset) {
+export function getVideoAttrs(hash, dataset) {
   const isAutoplay = hash?.includes('autoplay');
   const isAutoplayOnce = hash?.includes('autoplay1');
   const playOnHover = hash?.includes('hoverplay');
@@ -256,7 +258,7 @@ export function handleObjectFit(bgRow) {
   });
 }
 
-function getVideoIntersectionObserver() {
+export function getVideoIntersectionObserver() {
   if (!window?.videoIntersectionObs) {
     window.videoIntersectionObs = new window.IntersectionObserver((entries) => {
       entries.forEach((entry) => {
@@ -277,7 +279,7 @@ function getVideoIntersectionObserver() {
   return window.videoIntersectionObs;
 }
 
-function applyInViewPortPlay(video) {
+export function applyInViewPortPlay(video) {
   if (!video) return;
   if (video.hasAttribute('data-play-viewport')) {
     const observer = getVideoIntersectionObserver();
@@ -306,23 +308,4 @@ export function decorateMultiViewport(el) {
     });
   }
   return foreground;
-}
-
-export function decorateAnchorVideo({ src, anchorTag }) {
-  if (!src.length || !(anchorTag instanceof HTMLElement)) return;
-  if (anchorTag.closest('.marquee, .aside, .hero-marquee') && !anchorTag.hash) anchorTag.hash = '#autoplay';
-  const { dataset, parentElement } = anchorTag;
-  const video = `<video ${getVideoAttrs(anchorTag.hash, dataset)} data-video-source=${src}></video>`;
-  anchorTag.insertAdjacentHTML('afterend', video);
-  const videoEl = parentElement.querySelector('video');
-  createIntersectionObserver({
-    el: parentElement,
-    options: { rootMargin: '1000px' },
-    callback: () => {
-      videoEl?.appendChild(createTag('source', { src, type: 'video/mp4' }));
-    },
-  });
-  applyHoverPlay(videoEl);
-  applyInViewPortPlay(videoEl);
-  anchorTag.remove();
 }
