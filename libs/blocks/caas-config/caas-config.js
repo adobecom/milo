a18abed694556eb8fe5c1d1ab7987e79c142d637
Megan Thomas <methomas@adobe.com>
2024-09-18 14:30:21 -0700

    update version numb

--- libs/blocks/caas-config/caas-config.js
+++ libs/blocks/caas-config/caas-config.js
@@ -867,7 +867,7 @@ const CopyBtn = () => {
       hour12: false,
     });
     const collectionName = state.collectionName ? `- ${state.collectionName} ` : '';
-    link.textContent = `Content as a Service v2 ${collectionName}- ${dateStr}${state.doNotLazyLoad ? ' (no-lazy)' : ''}`;
+    link.textContent = `Content as a Service v3 ${collectionName}- ${dateStr}${state.doNotLazyLoad ? ' (no-lazy)' : ''}`;
 
     const blob = new Blob([link.outerHTML], { type: 'text/html' });
     const data = [new ClipboardItem({ [blob.type]: blob })];
