549c71f6e650496b20d14fa5748ee4d1f7307272
Ryan Clayton <rgclayton@gmail.com>
2024-09-19 02:14:22 -0600

    MWPW-154059 - Milo templates library (#2855)
    * MWPW-154059 - Milo templates library
    
    * Allows template-metadata block to output metadata block in template
    * Fixes inital listing order
    * Adds search ability to templates
    
    Resolves: MWPW-154059
    
    * Updating unit tests
    
    * Addressing eslint issue
    
    * Address PR feedback
    
    ---------
    
    Co-authored-by: Ryan Clayton <rclayton@adobe.com>

--- libs/blocks/library-config/library-utils.js
+++ libs/blocks/library-config/library-utils.js
@@ -1,4 +1,24 @@
-/* global ClipboardItem */
+import { getSearchTags } from './lists/blocks.js';
+import { getTemplateSearchTags } from './lists/templates.js';
+
+/* search utility */
+export function isMatching(container, query, type, titleText) {
+  let tagsString;
+
+  switch (type) {
+    case 'blocks':
+      tagsString = getSearchTags(container);
+      break;
+    case 'templates':
+      tagsString = getTemplateSearchTags(container, titleText);
+      break;
+    default:
+  }
+  if (!query || !tagsString) return false;
+  const searchTokens = query.split(' ');
+  return searchTokens.every((token) => tagsString.toLowerCase().includes(token.toLowerCase()));
+}
+
 export default function createCopy(blob) {
   const data = [new ClipboardItem({ [blob.type]: blob })];
   navigator.clipboard.write(data);
