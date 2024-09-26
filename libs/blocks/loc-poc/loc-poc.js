41ad135186ecc81a9e9c750413cd11422ef0137e
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2024-09-26 17:55:31 +0530

    Added custom preact component

---
+++ libs/blocks/loc-poc/loc-poc.js
@@ -0,0 +1,23 @@
+import { html, render } from '../../deps/htm-preact.js';
+
+// Cutsom Preact component
+const App = ({ name, count, onClick } = {}) => {
+  return html`<div class="preact-container">
+    <h1>Preact Container</h1>
+    <h2>Hello ${name}</h2>
+    <h2>Count: ${count}</h2>
+    <button onClick=${onClick}>Increase count</button>
+  </div> `;
+};
+
+export default async function init(el, props) {
+  const app = html`
+    <${App}
+      name="${props.name}"
+      count="${props.count}"
+      onClick="${props.onClick}"
+      dispatch=${props.dispatch}
+    />
+  `;
+  render(app, el);
+}
