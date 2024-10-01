d1ba82368f58c55b65400403d150eb97269ea242
Axel Cureno Basurto <axel@adobe.com>
2024-10-01 14:02:44 -0700

    dark theme ut

--- libs/features/mas/web-components/test/merch-card.ccd-suggested.test.html.js
+++ libs/features/mas/web-components/test/merch-card.ccd-suggested.test.html.js
@@ -34,7 +34,13 @@ runTests(async () => {
       it('should have strip with size small for the ccd-suggested wide card', async () => {
         const ccdSliceWideCard = document.querySelector('merch-card[variant="ccd-suggested"][strip-size="small"]');
         expect(ccdSliceWideCard.getAttribute('variant')).to.equal('ccd-suggested');
-    });
+     });
+
+     it('should have dark theme', async () => {
+      const ccdSliceDarkCard = document.querySelector('merch-card[variant="ccd-suggested"][strip-size="small"].dark-theme');
+      const theme = ccdSliceDarkCard.theme.color;
+      expect(theme.to.equal('dark'));
+   });
     });
 
 });
