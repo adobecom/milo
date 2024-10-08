52acc9bf028a593be277e99ec03ba55707090462
Raghav Sharma <raghavs@adobe.com>
2024-10-08 19:46:22 +0530

    Correctly parse all the information we need out of the metadata

--- libs/blocks/cai/cai.js
+++ libs/blocks/cai/cai.js
@@ -1,5 +1,5 @@
 import { createTag, getConfig, loadStyle } from '../../utils/utils.js';
-import { createC2pa, selectFormattedGenerator, selectEditsAndActivity } from '../../deps/cai-tools.min.js';
+import { createC2pa, selectFormattedGenerator, selectEditsAndActivity, createL2ManifestStore, selectGenerativeInfo, generateVerifyUrl } from '../../deps/cai-tools.min.js';
 
 const miloLibs = getConfig().miloLibs ?? '/libs';
 loadStyle(`${miloLibs}/blocks/cai/cai.css`);
@@ -29,6 +29,36 @@ const extractMetadata = async (img) => {
       `${miloLibs}/deps/cai-worker.min.js`,
   });
 
+  const generativeInfoTypes = {
+    compositeWithTrainedAlgorithmicMedia: 'This image has been augmented, corrected or enhanced using a Generative AI model, such as with inpainting or outpainting operations',
+    trainedAlgorithmicMedia: 'This image has been created algorithmically using an Artificial Intellgence model trained on captured content',
+    default: 'An AI tool was not used in creating this image',
+  };
+
+  const parseGenerativeInfo = (xs) => {
+    // the only two options at this point are compositeWithTrainedAlgorithmicMedia
+    // and trainedAlgorithmic media, and the former overrides the latter
+    const aiTool = [...new Set(xs.map(({ softwareAgent }) => softwareAgent))].join(', ') ?? 'None';
+    const composite = xs.find(({ type }) => type === 'compositeWithTrainedAlgorithmicMedia');
+    if (composite) {
+      return {
+        info: generativeInfoTypes.compositeWithTrainedAlgorithmicMedia,
+        aiTool,
+      };
+    }
+    const trained = xs.find(({ type }) => type === 'trainedAlgorithmicMedia');
+    if (trained) {
+      return {
+        info: generativeInfoTypes.trainedAlgorithmicMedia,
+        aiTool,
+      };
+    }
+    return {
+      info: generativeInfoTypes.default,
+      aiTool,
+    };
+  };
+
   try {
     // Read in our sample image and get a manifest store
     const [src] = img.src.split('?');
@@ -40,7 +70,9 @@ const extractMetadata = async (img) => {
     const date = new Date(time).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
     const app = selectFormattedGenerator(activeManifest);
     console.log(await selectEditsAndActivity(activeManifest));
-    return { issuer, date, info: 'soup', app };
+    const generativeInfo = await selectGenerativeInfo(activeManifest);
+    const { info, aiApp } = parseGenerativeInfo(generativeInfo);
+    return { issuer, date, info, app, aiApp };
   } catch (err) {
     console.error('Error reading image:', err);
   }
