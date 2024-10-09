523ec508c10e65d087bab0a8094620538e2648ed
Dev Ashish Sardana <devashish.3093@gmail.com>
2024-10-09 09:26:57 +0530

    fix reveiw comment

--- libs/navigation/navigation.js
+++ libs/navigation/navigation.js
@@ -4,7 +4,7 @@ const blockConfig = [
     name: 'global-navigation',
     targetEl: 'header',
     appendType: 'prepend',
-    params: ['imsClientId', 'searchEnabled', 'unav', 'customLinks', 'jarvis', 'layout'],
+    params: ['imsClientId', 'searchEnabled', 'unav', 'customLinks', 'jarvis'],
   },
   {
     key: 'footer',
