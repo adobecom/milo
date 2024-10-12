d623719bdb979254f6277f03f1f699224eff77cc
Ilyas Türkben <ilyas@adobe.com>
2024-10-12 10:56:46 +0200

    remove literals

--- libs/features/mas/commerce/src/literals.js
+++ libs/features/mas/commerce/src/literals.js
@@ -10,15 +10,5 @@ import { equalsCaseInsensitive } from './external.js';
  * @returns {Promise<Record<string, string>>}
  */
 export async function getPriceLiterals(settings, priceLiterals) {
-    //we are expecting an array of objects with lang and literals
-    const { data } = priceLiterals ? priceLiterals : await import('../price-literals.json');
-    if (Array.isArray(data)) {
-        const find = (language) =>
-            data.find((candidate) =>
-                equalsCaseInsensitive(candidate.lang, language),
-            );
-        const literals = find(settings.language) ?? find(Defaults.language);
-        if (literals) return Object.freeze(literals);
-    }
     return {};
 }
