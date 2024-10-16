92b263381d56307f94f7bed55565e2e92c40942c
John Pratt <jpratt@adobe.com>
2024-10-16 14:19:23 -0600

    add nala test for timeline block

---
+++ nala/blocks/timeline/timeline.spec.js
@@ -0,0 +1,19 @@
+module.exports = {
+  BlockName: 'Timeline Block',
+  features: [
+    {
+      tcid: '0',
+      name: '@Verify text in timeline block',
+      path: '/drafts/nala/blocks/timeline/timeline',
+      data: {},
+      tags: '@timeline @timeline1 @smoke @regression @milo',
+    },
+    {
+      tcid: '1',
+      name: '@Verify CSS in timeline block',
+      path: '/drafts/nala/blocks/timeline/timeline',
+      data: {},
+      tags: '@timeline @timeline2 @smoke @regression @milo',
+    },
+  ],
+};
