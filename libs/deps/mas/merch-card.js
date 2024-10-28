3826088c2a78c7c832640270ec5088ae4375d991
Axel Cureno Basurto <axel@adobe.com>
2024-10-28 09:05:31 -0700

    gray 800 link slice card

--- libs/deps/mas/merch-card.js
+++ libs/deps/mas/merch-card.js
@@ -1475,7 +1475,7 @@ merch-card[variant="ccd-slice"] [slot='body-s'] a:not(.con-button) {
     font-weight: 400;
     line-height: var(--consonant-merch-card-body-xxs-line-height);
     text-decoration-line: underline;
-    color: var(--spectrum-gray-600, var(--merch-color-grey-60));
+    color: var(--spectrum-gray-800, var(--merch-color-grey-80));
   }
 
   merch-card[variant="ccd-slice"] [slot='image'] img {
