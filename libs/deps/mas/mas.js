b403714a915a3704de6630784390c7fe6a701b19
rohitsahu <sahurohit9@gmail.com>
2024-10-28 14:48:03 +0530

    [MWPW-159140] Overlapping and alignment issues in product merch cards (#3018)
    * Fixed max width of heading and badge if badge is present for product cards
    
    * review comment
    
    * review comment
    
    * run build
    
    ---------
    
    Co-authored-by: Rohit Sahu <rosahu@adobe.com>

--- libs/deps/mas/mas.js
+++ libs/deps/mas/mas.js
@@ -996,7 +996,7 @@ merch-card[variant="plans"] [slot="quantity-select"] {
           <slot name="callout-content"></slot>
           <slot name="body-lower"></slot>
       </div>
-      ${this.secureLabelFooter}`}connectedCallbackHook(){super.connectedCallbackHook(),window.addEventListener("resize",this.postCardUpdateHook.bind(this))}postCardUpdateHook(){Nr()||this.adjustProductBodySlots()}};p(Me,"variantStyle",w`
+      ${this.secureLabelFooter}`}connectedCallbackHook(){super.connectedCallbackHook(),window.addEventListener("resize",this.postCardUpdateHook.bind(this))}postCardUpdateHook(){Nr()||this.adjustProductBodySlots(),this.adjustTitleWidth()}};p(Me,"variantStyle",w`
     :host([variant='product']) > slot:not([name='icons']) {
         display: block;
     }
@@ -1019,6 +1019,10 @@ merch-card[variant="plans"] [slot="quantity-select"] {
     :host([variant='product']) slot[name='callout-content'] {
         min-height: var(--consonant-merch-card-product-callout-content-height);
         display: block;
+    }
+      
+    :host([variant='product']) ::slotted([slot='heading-xs']) {
+      max-width: var(--consonant-merch-card-heading-xs-max-width, 100%);
     }
   `);var Ba=`
 :root {
