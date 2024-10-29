de1eaf53719a49740570f8b360b52c079d616a08
Axel Cureno Basurto <axel@adobe.com>
2024-10-29 14:05:15 -0700

    null safe check

--- libs/blocks/merch/merch.js
+++ libs/blocks/merch/merch.js
@@ -571,7 +571,7 @@ export async function initService(force = false, attributes = {}) {
   extraAttrs.forEach((attr) => {
     const camelCaseAttr = attr.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
     // eslint-disable-next-line no-prototype-builtins
-    if (commerce.hasOwnProperty(camelCaseAttr)) {
+    if (commerce?.hasOwnProperty(camelCaseAttr)) {
       const value = commerce[camelCaseAttr];
       delete commerce[camelCaseAttr];
       commerce[attr] = value;
