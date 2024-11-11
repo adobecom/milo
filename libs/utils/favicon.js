b8657254648cbea076a508a624d539961ea36911
sharath kannan <shkannan@adobe.com>
2024-11-11 15:42:15 +0530

    test

--- libs/utils/favicon.js
+++ libs/utils/favicon.js
@@ -4,7 +4,7 @@ export default function loadFavicon(createTag, config, getMetadata) {
   const favBase = `${codeRoot}/img/favicons/${name}`;
 
   const favicon = document.head.querySelector('link[rel="icon"]');
-  const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png" sizes="any>
+  const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png" sizes="any">
                 <link rel="manifest" href="${favBase}.webmanifest">`;
   favicon.insertAdjacentHTML('afterend', tags);
   favicon.href = `${favBase}.ico`;
