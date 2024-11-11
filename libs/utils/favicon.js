0eb2b6528b1b561aef143700c42663b46de9cbde
sharath kannan <shkannan@adobe.com>
2024-11-11 15:06:07 +0530

    favicon changes pinned tab

--- libs/utils/favicon.js
+++ libs/utils/favicon.js
@@ -6,6 +6,7 @@ export default function loadFavicon(createTag, config, getMetadata) {
   const favicon = document.head.querySelector('link[rel="icon"]');
   const tags = `<link rel="apple-touch-icon" href="${favBase}-180.png">
                 <link rel="icon" href="${favBase}-180.png" sizes="any">
+                <link rel="icon" href="${codeRoot}/img/favicons/safari-pinned-tab.svg" sizes="16x16">
                 <link rel="manifest" href="${favBase}.webmanifest">`;
   favicon.insertAdjacentHTML('afterend', tags);
   favicon.href = `${favBase}.ico`;
