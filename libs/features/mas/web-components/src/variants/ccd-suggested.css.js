252d81007b3eab8c92ed34a5d9736a4756cc7081
Axel Cureno Basurto <axel@adobe.com>
2024-11-12 20:20:50 -0800

    Updated styles

--- libs/features/mas/web-components/src/variants/ccd-suggested.css.js
+++ libs/features/mas/web-components/src/variants/ccd-suggested.css.js
@@ -1,6 +1,6 @@
 export const CSS = `
 :root {
-  --merch-card-ccd-suggested-width: 304px;
+  --merch-card-ccd-suggested-width: 302px;
   --merch-card-ccd-suggested-height: 205px;
   --merch-card-ccd-suggested-background-img-size: 119px;
 }
@@ -10,7 +10,15 @@ sp-theme[color='light'] merch-card[variant="ccd-suggested"] {
   color: var(--ccd-gray-700-dark, #464646);
   border: 1px solid var(--ccd-gray-200-light, #E6E6E6);
 }
-  
+
+sp-theme[color='light'] merch-card[variant="ccd-suggested"] [slot="body-xs"] {
+  color: var(--ccd-gray-700-dark, #464646);
+}
+
+sp-theme[color='light'] merch-card[variant="ccd-suggested"] [slot="body-xs"] {
+  color: var(--ccd-gray-100-light, #F8F8F8);
+}
+
 sp-theme[color='dark'] merch-card[variant="ccd-suggested"] {
   background-color: var(--ccd-gray-800-dark, #222);
   color: var(--ccd-gray-100-light, #F8F8F8);
