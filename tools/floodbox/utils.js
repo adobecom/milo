0f12fa41c4e0957e521029156351d7c036059ef2
sukamat <sukamat@adobe.com>
2024-11-18 15:49:12 -0800

    Fixed lint issues

--- tools/floodbox/utils.js
+++ tools/floodbox/utils.js
@@ -2,11 +2,11 @@ function isEditableFile(fileExt) {
   return ['html', 'json'].includes(fileExt);
 }
 
-function getFileName(path) {  
-  const fileNameWithExt = path.split('/').pop();  
+function getFileName(path) {
+  const fileNameWithExt = path.split('/').pop();
   if (fileNameWithExt.includes('.')) {
-      return fileNameWithExt.split('.').slice(0, -1).join('.');
-  }  
+    return fileNameWithExt.split('.').slice(0, -1).join('.');
+  }
   return fileNameWithExt;
 }
 
