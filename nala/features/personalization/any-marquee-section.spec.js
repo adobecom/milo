ba8eabfa514e08fdb8d275ee1c323046f4323d81
John Pratt <jpratt@adobe.com>
2024-11-20 11:16:30 -0700

    update spec file for description

--- nala/features/personalization/any-marquee-section.spec.js
+++ nala/features/personalization/any-marquee-section.spec.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       tcid: '0',
       name: '@test the default page',
-      desc: 'there should be marquees present, not any "replacement text"',
+      desc: 'there should be marquees present in the default page, "replacement text" should be present in the PZN page',
       path: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section',
       data: { defaultURL: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Fany-marquee-section%2Fany-marquee-section.json--default' },
       tags: '@anymarquee0 @smoke @regression @milo ',
