35d79cfbf51d52b7bc846beb2c9491c9ab642fe0
John Pratt <jpratt@adobe.com>
2024-11-20 11:33:42 -0700

    fix the name in the spec file

--- nala/features/personalization/any-marquee-section.spec.js
+++ nala/features/personalization/any-marquee-section.spec.js
@@ -3,7 +3,7 @@ module.exports = {
   features: [
     {
       tcid: '0',
-      name: '@test the default page',
+      name: '@test the default and personalized pages',
       desc: 'there should be marquees present in the default page, "replacement text" should be present in the PZN page',
       path: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section',
       data: { defaultURL: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Fany-marquee-section%2Fany-marquee-section.json--default' },
