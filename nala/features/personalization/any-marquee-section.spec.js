044990ff24ecfe1c80468a9edf916e4bb2b56c97
John Pratt <jpratt@adobe.com>
2024-11-20 10:14:41 -0700

    add nala test for any-marquee-section

---
+++ nala/features/personalization/any-marquee-section.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'test the any-marquee-section selector',
+  features: [
+    {
+      tcid: '0',
+      name: '@test the default page',
+      desc: 'there should be marquees present, not any "replacement text"',
+      path: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section',
+      data: { defaultURL: '/drafts/nala/features/personalization/any-marquee-section/any-marquee-section?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Fany-marquee-section%2Fany-marquee-section.json--default' },
+      tags: '@anymarquee0 @smoke @regression @milo ',
+    },
+  ],
+};
