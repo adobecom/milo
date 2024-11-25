518c55a1517a7eb378bb2d811c87609270e03bef
Raghav Sharma <raghavs@adobe.com>
2024-11-25 18:58:51 +0530

    Added a workflow to release standalone feds and removed dist from the PR

--- libs/navigation/dist/chunk-NE6SFPCS.js
+++
@@ -1,8 +0,0 @@
-var __freeze = Object.freeze;
-var __defProp = Object.defineProperty;
-var __template = (cooked, raw) => __freeze(__defProp(cooked, "raw", { value: __freeze(raw || cooked.slice()) }));
-
-export {
-  __template
-};
-//# sourceMappingURL=chunk-NE6SFPCS.js.map
--- libs/navigation/dist/sidekick-decorate-DJSQ7GPH.js
+++
@@ -1,9 +0,0 @@
-import {
-  stylePublish
-} from "./chunk-CVDLXPU4.js";
-import "./chunk-MWL56EAN.js";
-import "./chunk-NE6SFPCS.js";
-export {
-  stylePublish as default
-};
-//# sourceMappingURL=sidekick-decorate-DJSQ7GPH.js.map
