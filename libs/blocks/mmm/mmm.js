c5473c04b19eafc505ef6abdc8bbb3236735f1e1
viloria <viloria@adobe.com>
2024-11-26 17:52:15 -0500

    cleaned up

--- libs/blocks/mmm/mmm.js
+++ libs/blocks/mmm/mmm.js
@@ -1,10 +1,5 @@
 import { createTag } from '../../utils/utils.js';
-// import { decorateButtons } from '../../utils/decorate.js';
-// import { processTrackingLabels } from '../../martech/attributes.js';
 import { createPanelContents } from '../../features/personalization/preview.js';
-
-// const faq = { '@context': 'https://schema.org', '@type': 'FAQPage', mainEntity: [] };
-// const mediaCollection = {};
 
 const testData = [
   {
@@ -803,59 +798,13 @@ const testData = [
   },
 ];
 
-// function setSEO(questions) {
-//   faq.mainEntity.push(questions.map(({ name, text }) => (
-//     { '@type': 'Question', name, acceptedAnswer: { text, '@type': 'Answer' } })));
-//   const script = createTag('script', { type: 'application/ld+json' }, JSON.stringify(faq));
-//   document.head.append(script);
-// }
-
-// function toggleMedia(con, trig, status) {
-//   if (status === 'open') {
-//     trig.setAttribute('hidden', '');
-//     trig.setAttribute('aria-expanded', 'false');
-//     con.setAttribute('hidden', '');
-//     con.setAttribute('aria-expanded', 'false');
-//   } else {
-//     trig.setAttribute('aria-expanded', 'true');
-//     trig.removeAttribute('hidden');
-//     con.setAttribute('aria-expanded', 'true');
-//     con.removeAttribute('hidden');
-//   }
-// }
-
-// function displayMedia(displayArea, el, dd, i, expanded) {
-//   const id = el.getAttribute('aria-controls').split('-')[1];
-//   [...mediaCollection[id]].forEach(
-//     (mediaCollectionItem, idx, total) => {
-//       mediaCollectionItem.classList.remove('expanded');
-
-//       total.forEach((element, index) => {
-//         const trigger = document.querySelector(`#mmm-${id}-trigger-${index + 1}`);
-//         const content = document.querySelector(`#mmm-${id}-content-${index + 1}`);
-//         toggleMedia(content, trigger, 'open');
-//       });
-//       toggleMedia(dd, el);
-//       displayArea.childNodes[i - 1].classList.add('expanded');
-
-//       if (expanded) {
-//         toggleMedia(dd, el, 'open');
-//         displayArea.childNodes[i - 1]?.classList.remove('expanded');
-//       }
-//     },
-//   );
-// }
-
-function handleClick(el, dd) { // removed num from args
+function handleClick(el, dd) {
   const expanded = el.getAttribute('aria-expanded') === 'true';
-  // const analyticsValue = el.getAttribute('daa-ll');
   if (expanded) {
     el.setAttribute('aria-expanded', 'false');
-    // el.setAttribute('daa-ll', analyticsValue.replace(/close-/, 'open-'));
     dd.setAttribute('hidden', '');
   } else {
     el.setAttribute('aria-expanded', 'true');
-    // el.setAttribute('daa-ll', analyticsValue.replace(/open-/, 'close-'));
     if (!dd.classList.contains('placeholder-resolved')) {
       const { page } = dd.dataset;
       dd.innerHTML = createPanelContents(`from ${page}`);
@@ -863,96 +812,46 @@ function handleClick(el, dd) { // removed num from args
     }
     dd.removeAttribute('hidden');
   }
-
-  // const closestEditorial = el.closest('.editorial');
-  // eslint-disable-next-line max-len
-  // if (closestEditorial) displayMedia(closestEditorial.querySelector('.mmm-media'), el, dd, num, expanded);
 }
-
-// function defalutOpen(mmm) {
-//   handleClick(mmm.querySelector('.mmm-trigger'), mmm.querySelector('dd'), 1, 0);
-// }
 
 function createItem(mmm, id, heading, num) {
   const triggerId = `mmm-${id}-trigger-${num}`;
   const panelId = `mmm-${id}-content-${num}`;
   const icon = createTag('span', { class: 'mmm-icon' });
-  // const hTag = heading.querySelector('h1, h2, h3, h4, h5, h6');
   const hTag = createTag('h3', false, heading);
-  // const analyticsString = `open-${num}--${processTrackingLabels(heading.textContent)}`;
   const button = createTag('button', {
     type: 'button',
     id: triggerId,
     class: 'mmm-trigger tracking-header',
     'aria-expanded': 'false',
     'aria-controls': panelId,
-    // 'daa-ll': analyticsString,
-    // 'data-page': heading,
   }, hTag);
   button.append(icon);
 
   const panel = heading.nextElementSibling?.firstElementChild;
-
   const para = panel?.querySelector('p');
   const text = para ? para.textContent : panel?.textContent;
   const dtAttrs = { 'data-page': heading };
   const dtHtml = hTag ? createTag(hTag.tagName, { class: 'mmm-heading' }, button) : button;
   const dt = createTag('dt', dtAttrs, dtHtml);
   const dd = createTag('dd', { id: panelId, hidden: true, 'data-page': heading }, panel);
-  // const dm = createTag('div', { class: 'media-p' });
-
-  // if (edit) {
-  //   const ogMedia = mediaCollection[id][num - 1];
-  //   const mediaCopy = ogMedia.cloneNode(true);
-  //   dm.append(mediaCopy);
-  //   dd.prepend(dm);
-  // }
-
   button.addEventListener('click', (e) => { handleClick(e.target, dd, num, id); });
   mmm.append(dt, dd);
-
   return { name: heading.textContent, text };
 }
-
-// function getUniqueId(el) {
-//   const mmms = document.querySelectorAll('.mmm');
-//   return [...mmms].indexOf(el) + 1;
-// }
-
-// function populateMedia(mmm, id, num, collection) {
-//   mediaCollection[id] = collection;
-//   mmm.append(mediaCollection[id][num]);
-// }
 
 export default function init(el) {
   const id = 'mmm';
   const mmm = createTag('dl', { class: 'mmm', id: `mmm-${id}`, role: 'presentation' });
-  // eslint-disable-next-line max-len
-  // const mmmMedia = createTag('div', { class: 'mmm-media', id: `mmm-media-${id}` });
-  // const isSeo = el.classList.contains('seo');
-  // const isEditorial = el.classList.contains('editorial');
-  // decorateButtons(el);
-
-  // if (isEditorial) {
-  //   const editorialMedia = el.querySelectorAll(':scope > div:nth-child(3n)');
-  //   [...editorialMedia].map(
-  //     (media, idx, collection) => populateMedia(mmmMedia, id, idx, collection),
-  //   );
-  // }
-
-  // const headings = Object.keys(testData[0]);
   Object.keys(testData[0]).map(
     (heading, idx) => createItem(
       mmm,
       id,
       heading,
       idx + 1,
-      // isEditorial,
-      // mmmMedia,
     ),
   );
 
-  // if (isSeo) { setSEO(items); }
   el.innerHTML = '';
   el.className = `mmm-container ${el.className}`;
   el.classList.remove('mmm');
@@ -960,10 +859,6 @@ export default function init(el) {
   el.classList.add('con-block', maxWidthClass || 'max-width-10-desktop');
   mmm.classList.add('foreground');
   el.append(mmm);
-  // if (isEditorial) {
-  //   el.append(mmmMedia);
-  //   defalutOpen(el);
-  // }
 }
 /*
 todo:
