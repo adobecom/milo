d4a0c6f1f6b94e0193529e94df5c58b92aa2ae95
Dev Ashish Sardana <devashish.3093@gmail.com>
2024-12-02 12:57:31 +0530

    lint-fix

--- libs/blocks/global-navigation/utilities/utilities.js
+++ libs/blocks/global-navigation/utilities/utilities.js
@@ -435,7 +435,7 @@ export const transformTemplateToMobile = async (popup, item, localnav = false) =
   const CTA = popup.querySelector('.feds-cta')?.outerHTML ?? '';
   const mainMenu = `
       <span class="main-menu">
-        <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12" fill="none"><path d="M5.55579 1L1.09618 5.45961C1.05728 5.4985 1.0571 5.56151 1.09577 5.60062L5.51027 10.0661" stroke=${isDarkMode() ? "#f2f2f2": "black"} stroke-width="2" stroke-linecap="round"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="7" height="12" viewBox="0 0 7 12" fill="none"><path d="M5.55579 1L1.09618 5.45961C1.05728 5.4985 1.0571 5.56151 1.09577 5.60062L5.51027 10.0661" stroke=${isDarkMode() ? '#f2f2f2' : 'black'} stroke-width="2" stroke-linecap="round"/></svg>
         {{main-menu}}
       </span>
   `;
@@ -446,8 +446,8 @@ export const transformTemplateToMobile = async (popup, item, localnav = false) =
       ${localnav ? brand : await replaceText(mainMenu, getFedsPlaceholderConfig())}
       <span class="close-icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-          <path d="M1.5 1L13 12.5" stroke=${isDarkMode() ? "#f2f2f2": "black"} stroke-width="1.7037" stroke-linecap="round"/>
-          <path d="M13 1L1.5 12.5" stroke=${isDarkMode() ? "#f2f2f2": "black"} stroke-width="1.7037" stroke-linecap="round"/>
+          <path d="M1.5 1L13 12.5" stroke=${isDarkMode() ? '#f2f2f2' : 'black'} stroke-width="1.7037" stroke-linecap="round"/>
+          <path d="M13 1L1.5 12.5" stroke=${isDarkMode() ? '#f2f2f2' : 'black'} stroke-width="1.7037" stroke-linecap="round"/>
         </svg>
       </span>
     </div>
