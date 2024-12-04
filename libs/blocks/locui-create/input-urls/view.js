e603ad60ef30f99973a757deb48c2ff3a533ea5d
zweihand3r <arnabbordoloi49@gmail.com>
2024-12-04 15:49:49 +0530

    MWPW-163463: Added project type switcher and bifurcated 1st step

--- libs/blocks/locui-create/input-urls/view.js
+++ libs/blocks/locui-create/input-urls/view.js
@@ -1,4 +1,9 @@
-import { html, useCallback, useEffect, useState } from '../../../deps/htm-preact.js';
+import {
+  html,
+  useCallback,
+  useEffect,
+  useState,
+} from '../../../deps/htm-preact.js';
 import FragmentsSection from '../fragments/view.js';
 import { nextStep, project, setProject } from '../store.js';
 import StepControls from '../components/stepControls.js';
@@ -15,6 +20,7 @@ import { getUrls } from '../../locui/loc/index.js';
 import { URL_SEPARATOR_PATTERN } from '../utils/constant.js';
 
 export default function InputUrls() {
+  const [type, setType] = useState('translation');
   const [name, setName] = useState('');
   const [htmlFlow, setHtmlFlow] = useState(false);
   const [editBehavior, setEditBehavior] = useState('');
@@ -35,19 +41,28 @@ export default function InputUrls() {
 
   const fetchFragments = async (urls, selectAll = false) => {
     if (urls && !errors.urlsStr) {
-      const inputUrls = urls.split(URL_SEPARATOR_PATTERN)
-        .filter((url) => url).map((url) => new URL(url));
+      const inputUrls = urls
+        .split(URL_SEPARATOR_PATTERN)
+        .filter((url) => url)
+        .map((url) => new URL(url));
       setFragmentsLoading(true);
       const found = await findFragments(getUrls(inputUrls));
       setAllFragments(found || []);
       const validFrags = found?.filter((frag) => !frag?.valid);
-      setNoOfValidFragments((found?.filter((frag) => !frag?.valid) ?? []).length);
+      setNoOfValidFragments(
+        (found?.filter((frag) => !frag?.valid) ?? []).length
+      );
       if (selectAll) {
         setFragments(validFrags.map(({ pathname }) => pathname));
       }
       setFragmentsLoading(false);
     }
   };
+
+  function handleTypeChange(type) {
+    setType(type);
+    setErrors({ ...errors, editBehavior: '' });
+  }
 
   function handleNameChange(ev) {
     const text = ev.target.value;
@@ -82,17 +97,25 @@ export default function InputUrls() {
     }
   }
 
-  const handleFragmentsChange = useCallback((_fragments) => {
-    setFragments(_fragments);
-    const error = validateFragments(fragmentsEnabled, noOfValidFrag, _fragments);
-    setErrors((prev) => ({
-      ...prev,
-      fragments: error,
-    }));
-  }, [fragmentsEnabled, noOfValidFrag]);
+  const handleFragmentsChange = useCallback(
+    (_fragments) => {
+      setFragments(_fragments);
+      const error = validateFragments(
+        fragmentsEnabled,
+        noOfValidFrag,
+        _fragments
+      );
+      setErrors((prev) => ({
+        ...prev,
+        fragments: error,
+      }));
+    },
+    [fragmentsEnabled, noOfValidFrag]
+  );
 
   function handleNext() {
     const formErrors = validateForm({
+      type,
       name,
       editBehavior,
       urlsStr,
@@ -106,9 +129,10 @@ export default function InputUrls() {
     }
 
     setProject({
+      type,
       name,
-      htmlFlow,
-      editBehavior,
+      htmlFlow: type === 'translation' ? htmlFlow : false,
+      editBehavior: type === 'rollout' ? editBehavior : '',
       urls: urlsStr.split(/,|\n/),
       fragments,
     });
@@ -116,21 +140,27 @@ export default function InputUrls() {
   }
 
   useEffect(() => {
+    setType(project.value?.type || 'translation');
     setName(project.value?.name || '');
     setHtmlFlow(project.value?.htmlFlow || false);
     setEditBehavior(project.value?.editBehavior || '');
     setUrlsStr(project.value?.urls?.join('\n') || '');
     setFragmentsEnabled(project.value?.fragments?.length > 0);
     setFragments(project.value?.fragments || []);
-    if (project.value?.fragments?.length > 0 && project.value?.urls.length > 0) {
+    if (
+      project.value?.fragments?.length > 0 &&
+      project.value?.urls.length > 0
+    ) {
       fetchFragments(project.value?.urls?.join('\n'));
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleUrlsBlur = () => {
     if (urlsStr && !errors.urlsStr) {
-      const splittedUrls = urlsStr.split(URL_SEPARATOR_PATTERN).filter((url) => url);
+      const splittedUrls = urlsStr
+        .split(URL_SEPARATOR_PATTERN)
+        .filter((url) => url);
       const uniqueUrls = Array.from(new Set(splittedUrls)).join('\n');
       setUrlsStr(uniqueUrls);
       if (fragmentsEnabled) {
@@ -148,95 +178,123 @@ export default function InputUrls() {
   return html`
     <div class="locui-form-container">
       <div class="locui-input-form-area">
-        <div class="locui-title-bar">Localization</div>
-        <div class="locui-form-body">   
-         <div class="form-field">
-           <div class="form-field-label">* Enter Project Name</div>
+        <div class="locui-title-bar">
+          Localization${' '}
+          <span>- ${type.substring(0, 1).toUpperCase() + type.substring(1)}</span>
+        </div>
+        <div class="locui-form-body">
+          <div class="segment-ctrl pb-12">
+            <div
+              class=${`${type === 'translation' && 'active'}`}
+              onclick=${() => handleTypeChange('translation')}
+            >
+              Translation
+            </div>
+            <div
+              class=${`${type === 'rollout' && 'active'}`}
+              onclick=${() => handleTypeChange('rollout')}
+            >
+              Rollout
+            </div>
+          </div>
+          <div class="form-field">
+            <div class="form-field-label">* Enter Project Name</div>
             <div>
               <input
-               class=${`form-field-input ${errors.name && 'error'}`}
-               value=${name}
-               onInput=${handleNameChange}
-               placeholder='Enter letters, alphabet and hyphens only'
-             />
-             ${errors.name
-             && html`<div class="form-field-error">${errors.name}</div>`}
-           </div>
-         </div>
-
-        <div class="form-field">
-          <div class="form-field-label">HTML Localization Flow</div>
-          <input
-            class="form-field-checkbox"
-            type="checkbox"
-            checked=${htmlFlow}
-            onclick=${() => setHtmlFlow(!htmlFlow)}
-          />
-        </div>
-
-        <div class="form-field">
-          <div class="form-field-label">* Regional Edit Behavior</div>
-          <div>
-            <select
-              value=${editBehavior}
-              class=${`form-field-select ${errors.editBehavior && 'error'}`}
-              onChange=${handleeditBehaviorChange}
-            >
-              <option value="" disabled selected hidden>Select</option>
-              <option value="skip">Skip</option>
-              <option value="merge">Merge</option>
-              <option value="override">Override</option>
-            </select>
-            ${errors.editBehavior
-            && html`<div class="form-field-error">${errors.editBehavior}</div>`}
+                class=${`form-field-input ${errors.name && 'error'}`}
+                value=${name}
+                onInput=${handleNameChange}
+                placeholder="Enter letters, alphabet and hyphens only"
+              />
+              ${errors.name &&
+              html`<div class="form-field-error">${errors.name}</div>`}
+            </div>
           </div>
-        </div>
 
-        <div class="form-field">
-          <div class="form-field-label">* Enter the URLs</div>
-          <div class="form-field-desc">
-            (for multiple URLs, enter each on a new line.)
-          </div>
-        </div>
-        <textarea
-          class=${`form-field-textarea ${errors.urlsStr && 'error'}`}
-          rows="10"
-          value=${urlsStr}
-          onInput=${handleUrlsChange}
-          onBlur=${handleUrlsBlur}
-          placeholder=${`Enter the full URL. E.g, ${origin}/drafts/localization/projects/raga/image-test-one`}
-        />
-        ${errors.urlsStr
-        && html`<div class="form-field-error">${errors.urlsStr}</div>`}
-
-        <div class="form-field flex-items-center">
-          <input
-            class="form-field-switch"
-            type="checkbox"
-            id="includeFragments"
-            name="includeFragments"
-            checked=${fragmentsEnabled}
-            disabled=${urlsStr.length === 0 || errors?.urlsStr?.length > 0}
-            onClick=${handleFragmentsToggle}
-          />
-          <label class="form-field-switch-label" for="includeFragments"></label>
-          <span class="form-field-label">Include Fragments</span>
-        </div>
-
-        <div class="field-col">
-          ${fragmentsEnabled
-          && html`
-            <${FragmentsSection}
-              allFragments=${allFragments}
-              selectedFragments=${fragments}
-              setSelectedFragments=${handleFragmentsChange}
-              isLoading=${isFragmentsLoading}
-              formErrors=${errors.fragments}
-            />
+          ${type === 'translation' &&
+          html`
+            <div class="form-field">
+              <div class="form-field-label">HTML Localization Flow</div>
+              <input
+                class="form-field-checkbox"
+                type="checkbox"
+                checked=${htmlFlow}
+                onclick=${() => setHtmlFlow(!htmlFlow)}
+              />
+            </div>
           `}
-          ${errors.fragments && html`<div class='form-field-error'>${errors.fragments}</div>`}
+          ${type === 'rollout' &&
+          html`
+            <div class="form-field">
+              <div class="form-field-label">* Regional Edit Behavior</div>
+              <div>
+                <select
+                  value=${editBehavior}
+                  class=${`form-field-select ${errors.editBehavior && 'error'}`}
+                  onChange=${handleeditBehaviorChange}
+                >
+                  <option value="" disabled selected hidden>Select</option>
+                  <option value="skip">Skip</option>
+                  <option value="merge">Merge</option>
+                  <option value="override">Override</option>
+                </select>
+                ${errors.editBehavior &&
+                html`<div class="form-field-error">
+                  ${errors.editBehavior}
+                </div>`}
+              </div>
+            </div>
+          `}
+
+          <div class="form-field pb-4">
+            <div class="form-field-label">* Enter the URLs</div>
+            <div class="form-field-desc">
+              (for multiple URLs, enter each on a new line.)
+            </div>
+          </div>
+          <textarea
+            class=${`form-field-textarea ${errors.urlsStr && 'error'}`}
+            rows="10"
+            value=${urlsStr}
+            onInput=${handleUrlsChange}
+            onBlur=${handleUrlsBlur}
+            placeholder=${`Enter the full URL. E.g, ${origin}/drafts/localization/projects/raga/image-test-one`}
+          />
+          ${errors.urlsStr &&
+          html`<div class="form-field-error">${errors.urlsStr}</div>`}
+
+          <div class="form-field flex-items-center">
+            <input
+              class="form-field-switch"
+              type="checkbox"
+              id="includeFragments"
+              name="includeFragments"
+              checked=${fragmentsEnabled}
+              disabled=${urlsStr.length === 0 || errors?.urlsStr?.length > 0}
+              onClick=${handleFragmentsToggle}
+            />
+            <label
+              class="form-field-switch-label"
+              for="includeFragments"
+            ></label>
+            <span class="form-field-label">Include Fragments</span>
+          </div>
+
+          <div class="field-col">
+            ${fragmentsEnabled &&
+            html`
+              <${FragmentsSection}
+                allFragments=${allFragments}
+                selectedFragments=${fragments}
+                setSelectedFragments=${handleFragmentsChange}
+                isLoading=${isFragmentsLoading}
+                formErrors=${errors.fragments}
+              />
+            `}
+            ${errors.fragments &&
+            html`<div class="form-field-error">${errors.fragments}</div>`}
+          </div>
         </div>
-       </div>
       </div>
 
       <div>
