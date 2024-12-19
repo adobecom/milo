c8261271b3ffd1faf256eb44ef4e90837066f9a0
Saurabh Sircar <saurabhsircar11@gmail.com>
2024-12-19 11:46:03 +0530

    feat: changed text for goto list

--- libs/blocks/locui-create/locui-create.js
+++ libs/blocks/locui-create/locui-create.js
@@ -1,11 +1,8 @@
 import { createTag } from '../../utils/utils.js';
 
 function makeGotoSteps(link, linkText) {
-  const goTo = createTag('p', null, 'Access the ');
   const goToAnchor = createTag('a', { href: link, target: '_blank', rel: 'noopener noreferrer' }, linkText);
-  goTo.append(goToAnchor);
-  goTo.append('.');
-  return goTo;
+  return goToAnchor;
 }
 
 export default function init(el) {
@@ -13,13 +10,13 @@ export default function init(el) {
   const heading = createTag('h2', null, 'Missing project details');
   const paragraph = createTag('p', null, 'The project details were removed after you logged in. To resolve this:');
   const steps = createTag('ol', null);
-  const gotoSteps = createTag('ol', null);
-  const gotoURls = [{ link: 'https://milostudio.adobe.com', linkText: 'MiloStudio Production environment' }, { link: 'https://milostudio.stage.adobe.com', linkText: 'MiloStudio Staging environment' }, { link: 'https://milostudio.dev.adobe.com', linkText: 'MiloStudio Development environment' }];
+  const gotoSteps = createTag('ul', null);
+  const gotoURls = [{ link: 'https://milostudio.adobe.com', linkText: 'Production' }, { link: 'https://milostudio.stage.adobe.com', linkText: 'Stage' }, { link: 'https://milostudio.dev.adobe.com', linkText: 'Development' }];
   const goToContainer = createTag('div', { class: 'goto-step' });
   gotoURls.forEach((gotoUrl) => gotoSteps.append(createTag('li', null, makeGotoSteps(gotoUrl.link, gotoUrl.linkText))));
-  goToContainer.append(createTag('p', null, 'Please navigate to the respective MiloStudio environments:'));
+  goToContainer.append(createTag('p', null, 'Please navigate to the respective MiloStudio environment:'));
   goToContainer.append(gotoSteps);
-  const stepsList = ['Close this window or tab.', goToContainer, 'Select the specific tenant.', 'Click on Localization.', 'Click on "Add New Project".'];
+  const stepsList = ['Close this window or tab.', goToContainer, 'Select the tenant.', 'Click on Localization.', 'Click on "Add New Project".'];
   stepsList.forEach((step) => steps.append(createTag('li', null, step)));
   el.append(heading, paragraph, steps);
 }
