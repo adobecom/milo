3e74c5f1134fe25b5ff32dd14290d63681151b9b
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2024-12-24 11:56:44 +0100

    [MWPW-161923] tab nested focus

--- libs/blocks/tabs/tabs.js
+++ libs/blocks/tabs/tabs.js
@@ -37,7 +37,8 @@ const removeAttributes = (el, attrsKeys) => {
 function changeTabs(e) {
   const { target } = e;
   const parent = target.parentNode;
-  const content = parent.parentNode.parentNode.lastElementChild;
+  const content = parent.parentNode.parentNode.querySelector('.tab-content');
+  
   const blockId = target.closest('.tabs').id;
   parent
     .querySelectorAll(`[aria-selected="true"][data-block-id="${blockId}"]`)
@@ -79,9 +80,10 @@ function configTabs(config, rootElem) {
 function initTabs(elm, config, rootElem) {
   const tabs = elm.querySelectorAll('[role="tab"]');
   const tabLists = elm.querySelectorAll('[role="tablist"]');
-  tabLists.forEach((tabList) => {
-    let tabFocus = 0;
-    tabList.addEventListener('keydown', (e) => {
+  let tabFocus = 0;
+
+  tabLists.forEach((tabList) => {    
+    tabList.addEventListener('keydown', (e) => {      
       if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
         if (e.key === 'ArrowRight') {
           tabFocus += 1;
@@ -93,7 +95,7 @@ function initTabs(elm, config, rootElem) {
           if (tabFocus < 0) tabFocus = tabs.length - 1;
         }
         tabs[tabFocus].setAttribute('tabindex', 0);
-        tabs[tabFocus].focus();
+        tabs[tabFocus].focus();        
       }
     });
   });
@@ -115,15 +117,13 @@ function nextTab(current, i, arr) {
   return (previous && isTabInTabListView(previous) && !isTabInTabListView(current));
 }
 
-function initPaddles(tabList, tabPaddles) {
-  const left = tabPaddles.firstElementChild;
-  const right = tabPaddles.lastElementChild;
+function initPaddles(tabList, left, right) {
   const tabListItems = tabList.querySelectorAll('[role="tab"]');
   const tabListItemsArray = [...tabListItems];
   const firstTab = tabListItemsArray[0];
   const lastTab = tabListItemsArray[tabListItemsArray.length - 1];
 
-  left.addEventListener('click', () => {
+  left.addEventListener('click', () => {    
     const previous = tabListItemsArray.find(previousTab);
     if (previous) {
       scrollTabIntoView(previous, 'end');
@@ -133,7 +133,7 @@ function initPaddles(tabList, tabPaddles) {
       tabList.scrollBy({ left: -(width / 2), behavior: 'smooth' });
     }
   });
-  right.addEventListener('click', () => {
+  right.addEventListener('click', () => {    
     const next = tabListItemsArray.find(nextTab);
     if (next) {
       scrollTabIntoView(next, 'start');
@@ -145,7 +145,7 @@ function initPaddles(tabList, tabPaddles) {
   });
 
   tabList.addEventListener('scroll', debounce(() => {
-    tabPaddles.setAttribute(
+    tabList.setAttribute(
       'aria-valuenow',
       ((tabList.scrollLeft / (tabList.scrollWidth - tabList.clientWidth)) * 100).toFixed(0),
     );
@@ -268,12 +268,11 @@ const init = (block) => {
   }
 
   // Tab Paddles
-  const tabPaddles = createTag('div', { class: 'tab-paddles', role: 'scrollbar', 'aria-valuenow': 0 });
   const paddleLeft = createTag('button', { class: 'paddle paddle-left', disabled: '', 'aria-hidden': true, 'aria-label': 'Scroll tabs to left' }, PADDLE);
   const paddleRight = createTag('button', { class: 'paddle paddle-right', disabled: '', 'aria-hidden': true, 'aria-label': 'Scroll tabs to right' }, PADDLE);
-  tabPaddles.append(paddleLeft, paddleRight);
-  tabList.after(tabPaddles);
-  initPaddles(tabList, tabPaddles);
+  tabList.insertAdjacentElement('afterend', paddleRight);
+  block.prepend(paddleLeft);
+  initPaddles(tabList, paddleLeft, paddleRight);
 
   // Tab Sections
   const allSections = Array.from(rootElem.querySelectorAll('div.section'));
