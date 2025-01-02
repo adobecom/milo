4f32805d0ee2754ed4d517c2d70f9d61bb95dfff
Dev Ashish Sardana <devashish.3093@gmail.com>
2025-01-02 19:26:00 +0530

    [MWPW-164088] - UT fixes

--- test/blocks/global-navigation/gnav-main-nav.test.js
+++ test/blocks/global-navigation/gnav-main-nav.test.js
@@ -8,6 +8,7 @@ import {
   isElementVisible,
   viewports,
   unavVersion,
+  addMetaDataV2
 } from './test-utilities.js';
 import { isDesktop, setActiveLink, toFragment } from '../../../libs/blocks/global-navigation/utilities/utilities.js';
 import globalNavigationActiveMock from './mocks/global-navigation-active.plain.js';
@@ -150,6 +151,7 @@ describe('main nav', () => {
     });
 
     it('marks simple link as active', async () => {
+      document.head.appendChild(addMetaDataV2('off'));
       const targetSelector = '#simple-link';
       const template = toFragment`<div></div>`;
       template.innerHTML = globalNavigationActiveMock;
@@ -164,6 +166,7 @@ describe('main nav', () => {
     });
 
     it('marks item with sync dropdown containing active link', async () => {
+      document.head.appendChild(addMetaDataV2('off'));
       const targetSelector = '#link-in-dropdown';
       const template = toFragment`<div></div>`;
       template.innerHTML = globalNavigationActiveMock;
@@ -175,6 +178,7 @@ describe('main nav', () => {
     });
 
     it('marks item from a nav with a single async dropdown containing active link', async () => {
+      document.head.appendChild(addMetaDataV2('off'));
       await createFullGlobalNavigation({ globalNavigation: globalNavigationActiveMock });
       const sections = document.querySelectorAll('section.feds-navItem--section');
       expect(sections.length).to.equal(1);
@@ -207,6 +211,7 @@ describe('main nav', () => {
     });
 
     it('marks a single item as active if multiple links match URL', async () => {
+      document.head.appendChild(addMetaDataV2('off'));
       const targetSelector1 = '#simple-link';
       const targetSelector2 = '#link-in-dropdown';
       const template = toFragment`<div></div>`;
