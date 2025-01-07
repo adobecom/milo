c2aabc7cf14e8dd9e1a36f782340dfd976db6f4c
Bandana Laishram <bandanalaishram@gmail.com>
2025-01-07 16:01:51 +0530

    Decorating localnav

--- libs/blocks/global-navigation/global-navigation.js
+++ libs/blocks/global-navigation/global-navigation.js
@@ -980,7 +980,7 @@ class Gnav {
         setActiveLink(true);
       }
     }
-    if (this.newMobileNav && !isDesktop.matches) {
+    if (this.newMobileNav) {
       await this.decorateLocalNav();
     }
     return this.elements.mainNav;
