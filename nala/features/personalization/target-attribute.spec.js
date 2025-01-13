bcdc3068c64930f19ff45ac8c4b4eb4f78588b64
John Pratt <jpratt@adobe.com>
2025-01-13 14:56:54 -0700

    start work on target-attribute test

---
+++ nala/features/personalization/target-attribute.spec.js
@@ -0,0 +1,38 @@
+module.exports = {
+  name: '',
+  features: [
+    {
+      tcid: '0',
+      name: '@test for the presence of the attribute "data-adobe-target-testid"',
+      desc: 'test for the presence of "data-adobe-target-testid" in Target tests',
+      path: '',
+      data: {
+        defaultURL: '/drafts/nala/features/personalization/target-attribute/addanalyticsattribute?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Ftarget-attribute%2Faddanalyticsattribute.json--default',
+        targetReplacePageURL: '/drafts/nala/features/personalization/target-attribute/addanalyticsattribute?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Ftarget-attribute%2Faddanalyticsattribute.json--target-replacepage',
+        replacePageURL: '/drafts/nala/features/personalization/target-attribute/addanalyticsattribute?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Ftarget-attribute%2Faddanalyticsattribute.json--replacepage',
+        targetOtherActionsURL: '/drafts/nala/features/personalization/target-attribute/addanalyticsattribute?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Ftarget-attribute%2Faddanalyticsattribute.json--target-otheractions',
+        otherActionsURL: '/drafts/nala/features/personalization/target-attribute/addanalyticsattribute?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Ftarget-attribute%2Faddanalyticsattribute.json--otheractions',
+      },
+      tags: '@targetattr0 @smoke @regression @milo ',
+    },
+    {
+      tcid: '1',
+      name: '@test for the presence of the attribute "data-adobe-target-testid" using an inblock gnav version',
+      desc: 'test for the presence of "data-adobe-target-testid" in Target tests in an inblock gnav version of the page',
+      path: '',
+      data: {
+        defaultURL: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion',
+        targetBlockURL: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--target-block',
+        targetGnavFragmentURL: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--target-gnavfragment',
+        targetGnavChainFragmentsURL: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--target-gnavchainfragments',
+        targetGnavWithInsetURL: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--target-gnavwithinset',
+        block: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--block',
+        gnavFragment: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--gnavfragment',
+        gnavChainFragments: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--gnavchainfragments',
+        gnavWithInset: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--gnavwithinset',
+        updateInGnavFragment: '/drafts/mepdev/fragments/2024/q3/addanalyticsattribute/inblockgnavversion?mep=%2Fdrafts%2Fmepdev%2Ffragments%2F2024%2Fq3%2Faddanalyticsattribute%2Finblockgnavversion.json--update+in+gnav+fragment',
+      },
+      tags: '@targetattr1 @smoke @regression @milo ',
+    },
+  ],
+};
---
+++ nala/features/personalization/target-attribute.test.js
@@ -0,0 +1,77 @@
+// to run the test: npm run nala stage target-attribute.test.js
+
+import { expect, test } from '@playwright/test';
+import { features } from './target-attribute.spec.js';
+
+const miloLibs = process.env.MILO_LIBS || '';
+
+// Test 0: verify presence of "data-adobe-target-testid" in Target tests
+// target-replacepage should have 1 target attribute, target-otheractions should have 8 target attributes; the others should have 0
+test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+  const defaultURL = `${baseURL}${features[0].data.defaultURL}${miloLibs}`;
+  const targetReplacePageURL = `${baseURL}${features[0].data.targetReplacePageURL}${miloLibs}`;
+  const replacePageURL = `${baseURL}${features[0].data.replacePageURL}${miloLibs}`;
+  const targetOtherActionsURL = `${baseURL}${features[0].data.targetOtherActionsURL}${miloLibs}`;
+  const otherActionsURL = `${baseURL}${features[0].data.otherActionsURL}${miloLibs}`;
+  const targetAttribute = '[data-adobe-target-testid]';
+
+  await test.step('step-1: verify default test case', async () => {
+    console.info(`[Test Page]: ${defaultURL}`);
+    await page.goto(defaultURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+
+  await test.step('step-2: verify the target-replacepage case', async () => {
+    console.info(`[Test Page]: ${targetReplacePageURL}`);
+    await page.goto(targetReplacePageURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(1);
+  });
+
+  await test.step('step-3: verify the replacepage case', async () => {
+    console.info(`[Test Page]: ${replacePageURL}`);
+    await page.goto(replacePageURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+
+  await test.step('step-4: verify target-otheractions case', async () => {
+    console.info(`[Test Page]: ${targetOtherActionsURL}`);
+    await page.goto(targetOtherActionsURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(8);
+  });
+
+  await test.step('step-5: verify otheractions case', async () => {
+    console.info(`[Test Page]: ${otherActionsURL}`);
+    await page.goto(otherActionsURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+
+  await test.step('step-1: verify default test case', async () => {
+    console.info(`[Test Page]: ${defaultURL}`);
+    await page.goto(defaultURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+
+  await test.step('step-2: verify the target-replacepage case', async () => {
+    console.info(`[Test Page]: ${targetReplacePageURL}`);
+    await page.goto(targetReplacePageURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(1);
+  });
+
+  await test.step('step-3: verify the replacepage case', async () => {
+    console.info(`[Test Page]: ${replacePageURL}`);
+    await page.goto(replacePageURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+
+  await test.step('step-4: verify target-otheractions case', async () => {
+    console.info(`[Test Page]: ${targetOtherActionsURL}`);
+    await page.goto(targetOtherActionsURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(8);
+  });
+
+  await test.step('step-5: verify otheractions case', async () => {
+    console.info(`[Test Page]: ${otherActionsURL}`);
+    await page.goto(otherActionsURL);
+    await expect(page.locator(targetAttribute)).toHaveCount(0);
+  });
+});
