bdf1c703c3705c46ca78772a6af74476e2b2f418
Drashti Modasara <dmodasara@drashtis-mbp.corp.adobe.com>
2025-01-13 15:59:21 +0530

    Handled loader delay

--- libs/features/branch-quick-links/branch-quick-links.js
+++ libs/features/branch-quick-links/branch-quick-links.js
@@ -3,13 +3,18 @@ import { getMetadata, loadStyle, getConfig, createTag } from '../../utils/utils.
 function addLoader(elem, text) {
   const { base } = getConfig();
   loadStyle(`${base}/features/branch-quick-links/branch-quick-links.css`);
-  const container = createTag('div', { class: 'pbar-container' });
   const lbl = createTag('div', { class: 'pbar-label' });
   lbl.textContent = text;
   const tr = createTag('div', { class: 'pbar-value' });
   tr.style.display = 'block';
-  const pBar = createTag('div', { class: 'pbar' }, tr);
-  container.append(lbl, pBar);
+  const pb = createTag('div', { class: 'pbar' }, tr);
+  pb.style.width = '100%';
+  pb.style.height = '10px';
+  pb.style.borderRadius = '5px';
+  pb.style.backgroundColor = '#D5D5D5';
+  pb.style.marginTop = '10px';
+  pb.style.overflow = 'hidden';
+  const container = createTag('div', { class: 'pbar-container' }, [lbl, pb]);
   elem.replaceWith(container);
   return container;
 }
@@ -43,28 +48,30 @@ export default function processQL(a) {
     return cookieGrp?.includes('C0002') && cookieGrp?.includes('C0004');
   };
 
-  const waitForConsent = new Promise((resolve) => {
-    const fallbackTimeout = setTimeout(() => resolve(false), 10000);
-    if (window.adobePrivacy) resolve(getConsentStatus());
-    window.addEventListener('adobePrivacy:PrivacyConsent', () => {
-      clearTimeout(fallbackTimeout);
-      resolve(true);
+  function waitForConsent() {
+    return new Promise((resolve) => {
+      const fallbackTimeout = setTimeout(() => resolve(false), 30000);
+      if (window.adobePrivacy) resolve(getConsentStatus());
+      window.addEventListener('adobePrivacy:PrivacyConsent', () => {
+        clearTimeout(fallbackTimeout);
+        resolve(true);
+      });
+      window.addEventListener('adobePrivacy:PrivacyReject', () => {
+        clearTimeout(fallbackTimeout);
+        resolve(false);
+      });
+      window.addEventListener('adobePrivacy:PrivacyCustom', () => {
+        clearTimeout(fallbackTimeout);
+        resolve(getConsentStatus());
+      });
     });
-    window.addEventListener('adobePrivacy:PrivacyReject', () => {
-      clearTimeout(fallbackTimeout);
-      resolve(false);
-    });
-    window.addEventListener('adobePrivacy:PrivacyCustom', () => {
-      clearTimeout(fallbackTimeout);
-      resolve(getConsentStatus());
-    });
-  });
+  }
 
   a.addEventListener('click', async (e) => {
     e.preventDefault();
     let pb;
     if (isLoader === 'on') pb = addLoader(a, loaderText);
-    const hasConsent = await waitForConsent;
+    const hasConsent = await waitForConsent();
     if (pb) removeLoader(pb, a);
     decorateQuickLink(a, hasConsent);
   });
