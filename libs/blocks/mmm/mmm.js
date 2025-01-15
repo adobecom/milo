185fff0160f1486610784533f9bc249fe4607ff6
Denys Fedotov <dfedotov@Denyss-MacBook-Pro.local>
2025-01-15 13:04:23 -0700

    Fixed potential repetative API calls

--- libs/blocks/mmm/mmm.js
+++ libs/blocks/mmm/mmm.js
@@ -3,6 +3,7 @@ import { fetchData, DATA_TYPE } from '../../features/personalization/personaliza
 import { getMepPopup, API_URLS } from '../../features/personalization/preview.js';
 
 const SEARCH_CRITERIA_CHANGE_EVENT = 'mmm-search-change';
+let cashedSearchCriteria = '';
 export const DEBOUNCE_TIME = 800;
 
 async function toggleDrawer(target, dd, pageId) {
@@ -319,7 +320,11 @@ async function createPageList(el, search) {
 
 function subscribeToSearchCriteriaChanges() {
   document.addEventListener(SEARCH_CRITERIA_CHANGE_EVENT, (el) => {
-    createPageList(document.querySelector('.mmm').parentNode, el.detail);
+    const searchCriteria = JSON.stringify(el?.detail || {});
+    if (cashedSearchCriteria !== searchCriteria) {
+      createPageList(document.querySelector('.mmm').parentNode, el.detail);
+    }
+    cashedSearchCriteria = searchCriteria;
   });
 }
 
