d6620d03b40095764392e83ca7f15baaf194a598
John Pratt <jpratt@adobe.com>
2025-01-17 10:23:58 -0700

    separate into 3 separate tests

--- nala/features/personalization/target-on.test.js
+++ nala/features/personalization/target-on.test.js
@@ -1,5 +1,4 @@
 // to run the test: npm run nala stage target-on.test.js
-
 // Note: this test deliberately skips checking for the global metadata spreadsheet because one does not exist in the milo repository
 
 import { expect, test } from '@playwright/test';
@@ -8,28 +7,29 @@ import MarqueeBlock from '../../blocks/marquee/marquee.page.js';
 
 const miloLibs = process.env.MILO_LIBS || '';
 
-// Test 0: check for running Target tests
+// Test 0: check the default page
 test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
-  const defaultURL = `${baseURL}${features[0].data.defaultURL}${miloLibs}`;
-  const metadataURL = `${baseURL}${features[0].data.metadataURL}${miloLibs}`;
-  const parameterURL = `${baseURL}${features[0].data.parameterURL}${miloLibs}`;
+  const defaultURL = `${baseURL}${features[0].path}${miloLibs}`;
   const marquee = new MarqueeBlock(page);
+  console.info(`[Test Page]: ${defaultURL}`);
+  await page.goto(defaultURL);
+  await expect(marquee.headingXL).toHaveText('Heading XL Marquee standard medium left');
+});
 
-  await test.step('step-1: verify default test page', async () => {
-    console.info(`[Test Page]: ${defaultURL}`);
-    await page.goto(defaultURL);
-    await expect(marquee.headingXL).toHaveText('Heading XL Marquee standard medium left');
-  });
+// Test 1: check for Target enablement via page metadata
+test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
+  const metadataURL = `${baseURL}${features[1].path}${miloLibs}`;
+  const marquee = new MarqueeBlock(page);
+  console.info(`[Test Page]: ${metadataURL}`);
+  await page.goto(metadataURL);
+  await expect(marquee.headingXL).toHaveText('Target is running');
+});
 
-  await test.step('step-2: verify metadata test page', async () => {
-    console.info(`[Test Page]: ${metadataURL}`);
-    await page.goto(metadataURL);
-    await expect(marquee.headingXL).toHaveText('Target is running');
-  });
-
-  await test.step('step-3: verify parameter test page', async () => {
-    console.info(`[Test Page]: ${parameterURL}`);
-    await page.goto(parameterURL);
-    await expect(marquee.headingXL).toHaveText('Target is running');
-  });
+// Test 2: check for Target enablement via URL parameter
+test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
+  const parameterURL = `${baseURL}${features[2].path}${miloLibs}`;
+  const marquee = new MarqueeBlock(page);
+  console.info(`[Test Page]: ${parameterURL}`);
+  await page.goto(parameterURL);
+  await expect(marquee.headingXL).toHaveText('Target is running');
 });
