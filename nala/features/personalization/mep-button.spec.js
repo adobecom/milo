bd8bc80053e230d4117aed38c99f83cbed6c7003
John Pratt <jpratt@adobe.com>
2025-01-22 11:27:54 -0700

    add mep-button test files to branch

---
+++ nala/features/personalization/mep-button.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'test the mep-button functionality',
+  features: [
+    {
+      tcid: '0',
+      name: '@0 - the pencil icon of the MEP Button',
+      desc: 'the pencil icon of the MEP Button should end in .json (it should point to the MEP manifest)',
+      path: '/drafts/nala/features/personalization/mep-button/mep-button',
+      data: { defaultURL: '/drafts/nala/features/personalization/mep-button/mep-button?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Fmep-button%2Fmep-button.json--default' },
+      tags: '@mep @mepbutton0 @smoke @regression @milo',
+    },
+  ],
+};
