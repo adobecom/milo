413866e6a3beef46623774bf16b748e3486aa053
John Pratt <jpratt@adobe.com>
2025-01-23 16:35:27 -0700

    more updates for mep-button test

--- nala/features/personalization/mep-button.spec.js
+++ nala/features/personalization/mep-button.spec.js
@@ -6,8 +6,21 @@ module.exports = {
       name: '@0 - the pencil icon of the MEP Button',
       desc: 'the pencil icon of the MEP Button should end in .json (it should point to the MEP manifest)',
       path: '/drafts/nala/features/personalization/mep-button/mep-button',
-      data: { defaultURL: '/drafts/nala/features/personalization/mep-button/mep-button?mep=%2Fdrafts%2Fnala%2Ffeatures%2Fpersonalization%2Fmep-button%2Fmep-button.json--default' },
       tags: '@mep @mepbutton0 @smoke @regression @milo',
+    },
+    {
+      tcid: '1',
+      name: '@1 - the mep button should show with the mep parameter',
+      desc: 'the mep button should show with the mep URL parameter present',
+      path: 'https://milo.adobe.com/',
+      tags: '@mep @mepbutton1 @smoke @regression @milo',
+    },
+    {
+      tcid: '2',
+      name: '@2 - the highlight function should work',
+      desc: 'the hightlight option should work, including on merch cards',
+      path: '/drafts/nala/features/personalization/mep-button/mep-button',
+      tags: '@mep @mepbutton2 @smoke @regression @milo',
     },
   ],
 };
--- nala/features/personalization/mep-button.test.js
+++ nala/features/personalization/mep-button.test.js
@@ -2,16 +2,45 @@
 
 import { expect, test } from '@playwright/test';
 import { features } from './mep-button.spec.js';
+import TextBlock from '../../blocks/text/text.page.js';
+import MerchCard from '../../blocks/merchcard/merchcard.pages.js';
+
 
 const miloLibs = process.env.MILO_LIBS || '';
 const mepButtonLoc = '.mep-badge';
 
-// Test 0: the href of the pencil icon in the MEP Button should have a link which ends in .json (a mep manifest)"
+// Test 0: the href of the pencil icon in the MEP Button should have a link which ends in .json (linking to a mep manifest)"
 test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
   const pencilIconHrefLoc = '.mep-edit-manifest[href$=".json"]'; // pencil icon with href that ends in .json
-  const defaultURL = `${baseURL}${features[0].data.defaultURL}${miloLibs}`;
-  console.info(`[Test Page]: ${defaultURL}`);
-  await page.goto(defaultURL);
+  const URL = `${baseURL}${features[0].path}${miloLibs}`;
+  console.info(`[Test Page]: ${URL}`);
+  await page.goto(URL);
   await page.locator(mepButtonLoc).click();
   await expect(page.locator(pencilIconHrefLoc)).toHaveCount(1);
+});
+
+// Test 1: the mep parameter enables the mep button
+test(`${features[1].name},${features[1].tags}`, async ({ page }) => {
+  const URL = features[1].path;
+  console.info(`[Test Page]: ${URL}`);
+  await page.goto(URL);
+  await expect(page.locator(mepButtonLoc)).toHaveCount(0);
+
+  await page.goto(`${URL}?mep`); // with mep parameter
+  await expect(page.locator(mepButtonLoc)).toHaveCount(1);
+});
+
+// Test 2: the highlight option works, including on merch cards
+test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
+  const URL = `${baseURL}${features[2].path}${miloLibs}`;
+  const highlightLoc = '#mepHighlightCheckbox'; // hightlight checkbox
+
+  console.info(`[Test Page]: ${URL}`);
+  await page.goto(URL);
+  await page.locator(mepButtonLoc).click();
+  await page.locator(highlightLoc).check();
+  const highlightCSSContentTextBox = await page.evaluate("window.getComputedStyle(document.getElementById('text-intro'), '::before').getPropertyValue('content')");
+  const highlightCSSContentMerchCard = await page.evaluate("window.getComputedStyle(document.getElementById('merch-card'), '::before').getPropertyValue('content')");
+  await expect(highlightCSSContentTextBox).toContain('content updated by: mep-button.json');
+  await expect(highlightCSSContentMerchCard).toContain('content updated by: mep-button.json');
 });
