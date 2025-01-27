e4f24721774614baa8602447c1a8d0b50947d8d5
skholkhojaev <skholkhojaev@adobe.com>
2025-01-27 11:43:11 +0100

    removed dependency on gist.github.com by mocking script request

--- test/blocks/gist/gist.test.js
+++ test/blocks/gist/gist.test.js
@@ -1,3 +1,4 @@
+import sinon from 'sinon';
 import { readFile } from '@web/test-runner-commands';
 import { expect } from '@esm-bundle/chai';
 import { waitForElement } from '../../helpers/waitfor.js';
@@ -6,10 +7,43 @@ document.body.innerHTML = await readFile({ path: './mocks/body.html' });
 const { default: init } = await import('../../../libs/blocks/gist/gist.js');
 
 describe('adobetv autoblock', () => {
-  it('creates iframe video block', async () => {
+  let createElementStub;
+  let originalCreateElement;
+
+  beforeEach(() => {
+    originalCreateElement = document.createElement;
+
+    createElementStub = sinon.stub(document, 'createElement').callsFake((tagName) => {
+      if (tagName === 'script') {
+        const scriptMock = originalCreateElement.call(document, 'script');
+        Object.defineProperty(scriptMock, 'src', {
+          set(url) {
+            const callbackMatch = url.match(/callback=([^&]+)/);
+            if (callbackMatch) {
+              const callbackName = callbackMatch[1];
+              setTimeout(() => {
+                if (window[callbackName]) {
+                  window[callbackName]({ div: '<div class="gist-data">Mock Gist Content</div>' });
+                }
+              }, 0);
+            }
+          },
+        });
+        return scriptMock;
+      }
+      return originalCreateElement.call(document, tagName);
+    });
+  });
+
+  afterEach(() => {
+    createElementStub.restore();
+  });
+
+  it('creates gist block with mocked data', async () => {
     const gistLink = document.body.querySelector('a');
     init(gistLink);
     const div = await waitForElement('div.gist-data');
     expect(div).to.exist;
+    expect(div.innerHTML).to.contain('Mock Gist Content');
   });
 });
