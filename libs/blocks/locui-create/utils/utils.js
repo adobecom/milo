115a86c57048519c76783a49822204a3b95c5e82
Nishant Kumar Thakur <nishant.thakur.ece@gmail.com>
2025-01-27 18:50:34 +0530

    MWPW-166465 Eslint fix

--- libs/blocks/locui-create/utils/utils.js
+++ libs/blocks/locui-create/utils/utils.js
@@ -114,7 +114,8 @@ export function setSelectedLocalesAndRegions() {
 
 export function getLanguageDetails(lang) {
   const langDetails = stLocales.value?.find(
-    ({ languagecode }) => languagecode.toLowerCase() === lang.toLowerCase()) ?? {};
+    ({ languagecode }) => languagecode.toLowerCase() === lang.toLowerCase()
+  ) ?? {};
   return [
     {
       action: 'Rollout',
