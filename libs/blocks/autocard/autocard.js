52a8198f117f8817757016f6aebaa7c7c407d3d2
Bozo Jovicic <bozo@hitthecode.com>
2025-01-28 10:11:21 +0100

    MWPW-165482 milo autoblock feature

---
+++ libs/blocks/autocard/autocard.js
@@ -0,0 +1,15 @@
+import {createTag} from '../../utils/utils.js';
+
+export default function init(el) {
+  const { hash } = new URL(el.href);
+  const hashValue = hash.startsWith('#') ? hash.substring(1) : hash;
+  const searchParams = new URLSearchParams(hashValue);
+  const fragment = searchParams.get('fragment');
+
+  if (!fragment) return;
+
+  import('../../deps/mas/mas.js');
+  const aemFragment = createTag('aem-fragment', { fragment });
+  const merchCard = createTag('merch-card', { consonant: '' }, aemFragment);
+  el.replaceWith(merchCard);
+}
