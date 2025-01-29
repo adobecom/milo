35b3d1335378391e1c5c197123ab290159bce0fb
Bozo Jovicic <bozo@hitthecode.com>
2025-01-29 11:15:00 +0100

    MWPW-165482 milo autoblock feature

--- libs/blocks/mas-autoblock/mas-autoblock.js
+++ libs/blocks/mas-autoblock/mas-autoblock.js
@@ -1,12 +1,29 @@
 import { createTag, getConfig } from '../../utils/utils.js';
 import { initService } from '../merch/merch.js';
 
-export default async function init(el) {
+export function getFragmentId(el) {
   const { hash } = new URL(el.href);
   const hashValue = hash.startsWith('#') ? hash.substring(1) : hash;
   const searchParams = new URLSearchParams(hashValue);
-  const fragment = searchParams.get('fragment');
+  return searchParams.get('fragment');
+}
 
+export function getTagName(el) {
+  const defaultTagName = 'merch-card';
+  const tokens = el.textContent.trim().split(' ');
+  return tokens[0].replace(':', '').trim() || defaultTagName;
+}
+
+export async function createCard(el, fragment) {
+  const aemFragment = createTag('aem-fragment', { fragment });
+  const merchCard = createTag(getTagName(el), { consonant: '' }, aemFragment);
+  el.replaceWith(merchCard);
+  await aemFragment.updateComplete;
+  await merchCard.checkReady();
+}
+
+export default async function init(el) {
+  const fragment = getFragmentId(el);
   if (!fragment) return;
 
   const { base } = getConfig();
@@ -14,9 +31,5 @@ export default async function init(el) {
     import(`${base}/deps/mas/mas.js`),
     initService(),
   ]);
-  const aemFragment = createTag('aem-fragment', { fragment });
-  const merchCard = createTag('merch-card', { consonant: '' }, aemFragment);
-  el.replaceWith(merchCard);
-  await aemFragment.updateComplete;
-  await merchCard.checkReady();
+  await createCard(el, fragment);
 }
