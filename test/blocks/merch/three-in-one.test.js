fe6f00318958bca4ac69e8b69f6cc4865e34b544
Mira <myrosvas@gmail.com>
2025-01-30 14:03:46 +0100

    more unit tests

--- test/blocks/merch/three-in-one.test.js
+++ test/blocks/merch/three-in-one.test.js
@@ -10,16 +10,14 @@ const config = {
   codeRoot: '/libs',
   env: { name: 'prod' },
 };
+setConfig(config);
 
 describe('Three-in-one modal', () => {
   const originalOpen = window.open;
   const twpLink = document.querySelector('#twp-link');
-
   beforeEach(async () => {
-    setConfig(config);
     window.open = sinon.stub(window, 'open');
   });
-
   afterEach(() => {
     window.open = originalOpen;
   });
@@ -57,37 +55,53 @@ describe('Three-in-one modal', () => {
     const modal = await openThreeInOneModal(twpLink);
     const iframe = modal.querySelector('iframe');
     const spTheme = modal.querySelector('sp-theme');
+    const spProgressCircle = spTheme.querySelector('sp-progress-circle');
     expect(iframe).to.exist;
     expect(spTheme).to.exist;
+    expect(spProgressCircle).to.exist;
     handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.AppLoaded}"}` });
     expect(iframe.classList.contains('loading')).to.be.false;
-    expect(spTheme).to.be.null;
+    modal.remove();
   });
 
   it('should open external link in a new tab', async () => {
-    const modal = await openThreeInOneModal(twpLink);
     handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.EXTERNAL}", "data":{"externalUrl":"https://www.google.com/maps","target":"_blank"}}` });
     expect(window.open.calledOnceWith('https://www.google.com/maps', '_blank')).to.be.true;
-    modal.remove();
   });
 
   it('should open link in a new tab when message subtype is "switch"', async () => {
-    const modal = await openThreeInOneModal(twpLink);
     handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.SWITCH}", "data":{"externalUrl":"https://www.google.com/maps","target":"_blank"}}` });
     expect(window.open.calledOnceWith('https://www.google.com/maps', '_blank')).to.be.true;
-    modal.remove();
   });
 
   it('should open link in a new tab when message subtype is "return_back"', async () => {
-    const modal = await openThreeInOneModal(twpLink);
     handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.RETURN_BACK}", "data":{"externalUrl":"https://www.google.com/maps","target":"_blank"}}` });
     expect(window.open.calledOnceWith('https://www.google.com/maps', '_blank')).to.be.true;
-    modal.remove();
   });
 
   it('should close modal when message subtype is "close"', async () => {
     const modal = await openThreeInOneModal(twpLink);
+    const spy = sinon.spy(modal, 'dispatchEvent');
     handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.Close}"}` });
-    expect(modal).to.be.null;
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWithMatch(sinon.match.instanceOf(Event))).to.be.true;
+    expect(spy.calledWithMatch(sinon.match.has('type', 'closeModal'))).to.be.true;
+    modal.dispatchEvent.restore();
+    modal.remove();
+  });
+
+  it('should do nothing if message subtype is not recognized or message data is invalid', async () => {
+    const modal = await openThreeInOneModal(twpLink);
+    const iframe = modal.querySelector('iframe');
+    handle3in1IFrameEvents('unrelevant message');
+    handle3in1IFrameEvents({ data: '{"subType": "unknown"}' });
+    handle3in1IFrameEvents({ data: '{"app":"ucv3","subType": "unknown"}' });
+    handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.EXTERNAL}"}` });
+    handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.SWITCH}"}` });
+    handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.RETURN_BACK}"}` });
+    handle3in1IFrameEvents({ data: `{"app":"ucv3","subType": "${MSG_SUBTYPE.EXTERNAL}", "data":{}}` });
+    expect(iframe.classList.contains('loading')).to.be.true;
+    expect(window.open.notCalled).to.be.true;
+    modal.remove();
   });
 });
