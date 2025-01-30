30c93caf507967f909d07490b3676bf408691493
Bandana Laishram <bandanalaishram@gmail.com>
2025-01-30 16:32:53 +0530

    Code fix

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -822,8 +822,7 @@ async function decorateHeader() {
   const dynamicNavActive = getMetadata('dynamic-nav') === 'on'
     && window.sessionStorage.getItem('gnavSource') !== null;
   if (!dynamicNavActive && (baseBreadcrumbs || breadcrumbs || autoBreadcrumbs)) header.classList.add('has-breadcrumbs');
-  const hasLocalNav = await isLocalNav();
-  if (hasLocalNav) {
+  if (isLocalNav()) {
     // Preserving space to avoid CLS issue
     const localNavWrapper = createTag('div', { class: 'feds-localnav' });
     header.after(localNavWrapper);
