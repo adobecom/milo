1daaa7f60e2861e82f8f9d7db48819221c3f8191
Mariia Lukianets <mariia.lukianets@gmail.com>
2025-02-07 16:34:23 +0100

    remove sitemap

--- libs/deps/mas/mas.js
+++ libs/deps/mas/mas.js
@@ -2167,4 +2167,3 @@ lit-html/is-server.js:
    * SPDX-License-Identifier: BSD-3-Clause
    *)
 */
-//# sourceMappingURL=mas.js.map
--- libs/features/mas/docs/src/build-docs.mjs
+++ libs/features/mas/docs/src/build-docs.mjs
@@ -79,9 +79,11 @@ const htmlTemplate = `
     <a href="/libs/features/mas/docs/mas.html">Home</a>
     <a href="/libs/features/mas/docs/mas.js.html">mas.js</a>
     <a href="/libs/features/mas/docs/checkout-link.html">Checkout Link</a>
+    <a href="/libs/features/mas/docs/checkout-button.html">Checkout Button</a>
     <a href="/libs/features/mas/docs/inline-price.html">Inline Price</a>
     <a href="/libs/features/mas/docs/merch-card.html">Merch Card</a>
     <a href="/libs/features/mas/docs/ccd.html">CCD Gallery</a>
+    <a href="/libs/features/mas/docs/plans.html">Plans Gallery</a>
     <a href="/libs/features/mas/docs/benchmarks.html">Benchmarks</a>
 </aside>
 <main>
