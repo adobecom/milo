73ee3d4b1eb48cd675f8f62be2e921921747c0ea
astatescu <angelostatescu.adobe@gmail.com>
2025-02-07 14:07:55 +0200

    init upt-link

---
+++ libs/features/mas/src/upt-link.js
@@ -0,0 +1,41 @@
+import { useService } from "./utilities.js";
+
+/**
+ * Universal Promo Terms Link
+ */
+export class UptLink extends HTMLAnchorElement {
+  static is = 'upt-link';
+  static tag = 'a';
+
+  // static createUptLink(options = {}, innerHTML = '') {
+  //     return createCheckoutElement(UptLink, options, innerHTML);
+  // }
+
+  masElement = new MasElement(this);
+
+  connectedCallback() {
+      this.masElement.connectedCallback();
+      this.addEventListener('click', this.clickHandler);
+  }
+
+  disconnectedCallback() {
+      this.masElement.disconnectedCallback();
+      this.removeEventListener('click', this.clickHandler);
+  }
+
+  get isUptLink() {
+      return true;
+  }
+
+  clickHandler(e) {
+      const service = useService();
+      console.log('Service', e, service);
+  }
+}
+
+// Define custom DOM element
+if (!window.customElements.get(UptLink.is)) {
+  window.customElements.define(UptLink.is, UptLink, {
+      extends: UptLink.tag,
+  });
+}
