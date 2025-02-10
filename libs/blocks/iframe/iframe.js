4f4025912e27ab968ea41f5ef205b0c012290d8c
Vivian A Goodrich <vgoodric@adobe.com>
2025-02-10 12:46:22 -0700

    iframe lazy

--- libs/blocks/iframe/iframe.js
+++ libs/blocks/iframe/iframe.js
@@ -1,5 +1,15 @@
 import { createTag } from '../../utils/utils.js';
 
+// eslint-disable-next-line func-names
+window.resizeIframe = function (obj) {
+  const checkHeight = setInterval(() => {
+    if (obj.contentWindow.document.body.scrollHeight !== 0) {
+      clearInterval(checkHeight);
+      obj.style.height = `${obj.contentWindow.document.body.scrollHeight}px`;
+    }
+  }, 100);
+};
+
 export default function init(el) {
   const url = el.href ?? el.querySelector('a')?.href;
   el.classList.remove('iframe');
@@ -7,8 +17,25 @@ export default function init(el) {
 
   if (!url) return;
 
-  const iframe = createTag('iframe', { src: url, allowfullscreen: true });
+  const iframeProperties = { src: url, allowfullscreen: true };
+  if (el.classList.contains('lazy-load')) {
+    iframeProperties.loading = 'lazy';
+    // iframeProperties.style = 'height: 100vh';
+    // iframeProperties.onload = 'resizeIframe(this)';
+  }
+  if (el.classList.contains('auto-height')) {
+    iframeProperties.scrolling = 'no';
+    iframeProperties.style = 'height: 100%';
+    iframeProperties.onload = 'resizeIframe(this)';
+  }
+  const iframe = createTag('iframe', iframeProperties);
   const embed = createTag('div', { class: `milo-iframe ${classes}` }, iframe);
+
+  if (el.classList.contains('auto-height')) {
+    window.addEventListener('scroll', () => {
+      window.resizeIframe(iframe);
+    });
+  }
 
   el.insertAdjacentElement('afterend', embed);
   el.remove();
