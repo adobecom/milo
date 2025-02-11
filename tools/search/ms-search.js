3bbbcf6dccf30c59c998c7b627edcc4a30de4671
Manasvi Agrawal <maagrawal@manasvis-mbp.corp.adobe.com>
2025-02-11 11:17:09 +0530

    fixed lint issues, removed dist for linting

--- tools/search/ms-search.js
+++ tools/search/ms-search.js
@@ -27,5 +27,4 @@ const ORG_MAP = {
   const fullURL = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}${window.location.pathname}#/search`;
   window.history.replaceState({}, null, fullURL);
   document.body.style.visibility = 'visible';
-  } 
-)();
+}());
