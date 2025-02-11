49373cab60b49bd87553e68cfc5ec469c66e625d
markpadbe <markp@adobe.com>
2025-02-11 12:47:26 -0800

    Fix updateheight on load

--- libs/blocks/iframe/iframe.js
+++ libs/blocks/iframe/iframe.js
@@ -1,41 +1,43 @@
 import { createTag } from '../../utils/utils.js';
 
-window.resizeIframe = function resizeIframe(obj) {
-  const checkHeight = setInterval(() => {
-    const { scrollHeight } = obj.contentWindow.document.body;
-    if (scrollHeight !== 0) {
-      clearInterval(checkHeight);
-      const newHeight = `${scrollHeight}px`;
-      obj.style.height = newHeight;
-      if (obj.parentElement) obj.parentElement.style.height = newHeight;
-    }
-  }, 100);
+const updateHeight = (obj) => {
+  const { scrollHeight } = obj.contentWindow.document.body;
+  if (scrollHeight !== 0) {
+    const newHeight = `${scrollHeight}px`;
+    obj.style.height = newHeight;
+    if (obj.parentElement) obj.parentElement.style.height = newHeight;
+  }
+};
+const onloadHandler = function onloadHandler() {
+  updateHeight(this);
+  const observer = new MutationObserver(() => updateHeight(this));
+  observer.observe(this.contentWindow.document.body, { childList: true, subtree: true });
 };
 
 export default async function init(el) {
   const url = el.href ?? el.querySelector('a')?.href;
-  el.classList.remove('iframe');
-
   if (!url) return;
+
+  el.classList.remove('iframe');
   const classes = [...el.classList].join(' ');
 
   const iframeProperties = {
     src: url,
     allowfullscreen: true,
     ...(el.classList.contains('lazy-load') && { loading: 'lazy' }),
-    ...(el.classList.contains('auto-height') && {
-      scrolling: 'no',
-      style: 'height: 100%',
-      onload() { window.resizeIframe(this); },
-    }),
+    scrolling: el.classList.contains('auto-height') ? 'no' : 'auto',
+    style: el.classList.contains('auto-height') ? 'height: 100%' : '',
   };
 
   const iframe = createTag('iframe', iframeProperties);
+
   const embed = createTag('div', { class: `milo-iframe ${classes}` }, iframe);
 
   if (el.classList.contains('auto-height')) {
     const { debounce } = await import('../../utils/action.js');
-    window.addEventListener('resize', debounce(() => window.resizeIframe(iframe), 100));
+    iframe.addEventListener('load', onloadHandler);
+    const debouncedResize = debounce(() => updateHeight(iframe), 100);
+    window.addEventListener('resize', debouncedResize);
   }
 
   el.insertAdjacentElement('afterend', embed);
