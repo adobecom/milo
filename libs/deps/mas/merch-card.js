6d6d337a72932ea2c442637bc84d0a0fbba7e365
Bozo Jovicic <bozo@hitthecode.com>
2025-02-19 15:25:52 +0100

    Fix regression

--- libs/deps/mas/merch-card.js
+++ libs/deps/mas/merch-card.js
@@ -1944,12 +1944,10 @@ merch-card [slot='heading-xl'] {
 }
 
 merch-card [slot='callout-content'] {
-    margin: var(--consonant-merch-spacing-xxs) 0;
-    background: rgba(203 203 203 / 50%);
-    border-radius: var(--consonant-merch-spacing-xxxs);
-    padding: var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxs);
-    width: fit-content;
-    color: var(--consonant-merch-card-callout-font-color);
+    display: flex;
+    flex-direction: column;
+    margin: var(--consonant-merch-spacing-xxxs) 0px;
+    gap: var(--consonant-merch-card-callout-spacing-xxs);
 }
 
 merch-card [slot='callout-content'] .icon-button {
@@ -1961,6 +1959,37 @@ merch-card [slot='callout-content'] .icon-button {
 
 merch-card [slot='callout-content'] .icon-button:hover {
     background-color: transparent;
+}
+
+merch-card [slot='callout-content'] > div {
+    display: flex;
+    flex-direction: column;
+    margin: var(--consonant-merch-spacing-xxxs) 0px;
+    gap: var(--consonant-merch-card-callout-spacing-xxs);
+    align-items: flex-start;
+}
+
+merch-card [slot='callout-content'] > div > div {
+    display: flex;
+    background: rgba(203 203 203 / 50%);
+    border-radius: var(--consonant-merch-spacing-xxxs);
+    padding: var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxxs) var(--consonant-merch-spacing-xxs);
+}
+
+merch-card [slot='callout-content'] > div > div > div {
+    display: inline-block;
+    text-align: start;
+    font: normal normal normal var(--consonant-merch-card-callout-font-size)/var(--consonant-merch-card-callout-line-height) var(--body-font-family, 'Adobe Clean');
+    letter-spacing: var(--consonant-merch-card-callout-letter-spacing);
+    color: var(--consonant-merch-card-callout-font-color);
+}
+
+merch-card [slot='callout-content'] img {
+    width: var(--consonant-merch-card-callout-icon-size);
+    height: var(--consonant-merch-card-callout-icon-size);
+    margin-inline-end: 2.5px;
+    margin-inline-start: 9px;
+    margin-block-start: 2.5px;
 }
 
 merch-card [slot='detail-s'] {
