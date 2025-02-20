3d2be924807180a059f6192f8d8fc3811e1148f4
Mariia Lukianets <mariia.lukianets@gmail.com>
2025-02-20 16:41:11 +0100

    add locale/country support to ccd gallery

--- libs/features/mas/docs/common.js
+++ libs/features/mas/docs/common.js
@@ -1,10 +1,25 @@
 import '../../../deps/custom-elements.js';
-
+let masCommerceService;
 const toggleTheme = (themeParam) => {
   const theme = themeParam ?? 'light';
   document.body.className = 'spectrum spectrum--medium';
   document.body.classList.add(`spectrum--${theme}`);
   params.set('theme', theme);
+  history.replaceState(null, '', `${location.pathname}?${params}`);
+}
+
+const toggleLocale = (locale) => {
+  if (locale.includes(',')) {
+    const [country, language] = locale.split(',');
+    masCommerceService.setAttribute('country', country);
+    masCommerceService.setAttribute('language', language);
+    params.set('country', country);
+    params.set('language', language);
+  } else {
+    masCommerceService.setAttribute('locale', locale);
+    params.set('locale', locale);
+  }
+  masCommerceService.refreshFragments();
   history.replaceState(null, '', `${location.pathname}?${params}`);
 }
 
@@ -19,9 +34,15 @@ const init = () => {
       toggleTheme(el.getAttribute('value'));
     });
   });
+  document.querySelectorAll('.locale-toggle').forEach((el) => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault();
+      toggleLocale(el.getAttribute('value'));
+    });
+  });
   // mas-commerce-service
-  const masCommerceService = document.createElement('mas-commerce-service');
-  ['locale','language','env'].forEach((attribute) => {
+  masCommerceService = document.createElement('mas-commerce-service');
+  ['locale','language','env','country','language'].forEach((attribute) => {
     const value = params.get(attribute);
     if (value) masCommerceService.setAttribute(attribute, value);
   });
