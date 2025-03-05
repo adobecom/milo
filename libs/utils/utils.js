6a2f4a2a0e2be488c5ec1b4ba08419181d5b0767
markpadbe <markp@adobe.com>
2025-03-05 14:15:55 -0800

    Add maxTouchPoints

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1523,7 +1523,8 @@ export async function loadArea(area = document) {
       uaFullVersion: ${data.uaFullVersion}<br>
       platform: ${data.platform}</br>
       maintest: ${data.platform === 'Linux' && navigator.maxTouchPoints > 1 && window.screen.width >= 800 && window.screen.width <= 2560 && window.screen.height >= 1280 && window.screen.height <= 1600 && !navigator.userAgent.includes('X11')}</br>
-      end`;
+      maxTouchPoints: ${navigator.maxTouchPoints}</br>
+      end</br>`;
   }
   const { PERSONALIZATION_TAGS } = await import('../features/personalization/personalization.js');
   textString += `navigator.userAgent: ${navigator.userAgent}</br>
