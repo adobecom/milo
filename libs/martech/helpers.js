c8aa93e2d8b99e55604d55ca17251364d9f99f05
Swati Mukherjee <swati020494@gmail.com>
2025-03-06 11:41:51 +0530

    Add values for event95

--- libs/martech/helpers.js
+++ libs/martech/helpers.js
@@ -149,6 +149,22 @@ function getOrGenerateUserId() {
   };
 }
 
+function handleConsentAndSecondVisit() {
+  const { hostname } = window.location;
+  const secondVisitAttempt = Number(localStorage.getItem('secondHit')) || 0;
+
+  const isAdobeDomain = hostname === 'www.adobe.com' || hostname === 'www.stage.adobe.com';
+  const consentCookieValue = getCookie('OptanonConsent');
+
+  if (consentCookieValue.includes('C0002:1') && isAdobeDomain) {
+    const updatedVisitAttempt = secondVisitAttempt === 0 ? 1 : secondVisitAttempt + 1;
+    localStorage.setItem('secondHit', updatedVisitAttempt);
+    return updatedVisitAttempt;
+  }
+
+  return secondVisitAttempt;
+}
+
 function getPageNameForAnalytics({ locale }) {
   const { host, pathname } = new URL(window.location.href);
   const [modifiedPath] = pathname.split('/').filter((x) => x !== locale.prefix).join(':').split('.');
@@ -269,6 +285,12 @@ function createRequestPayload({ updatedContext, pageName, locale, env, hitType }
     data.web = { webPageDetails };
     data.eventType = hitTypeEventTypeMap[hitType];
 
+    if (handleConsentAndSecondVisit() === 2) {
+      digitalData.adobe = {
+        libraryVersions: 'alloy-api',
+        experienceCloud: { secondVisits: 'setEvent' },
+      };
+    }
     xdm.implementationDetails = {
       name: 'https://ns.adobe.com/experience/alloy/reactor',
       version: '1.0',
