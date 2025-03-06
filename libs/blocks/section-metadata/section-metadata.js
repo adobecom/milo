a182127081815ad02f7b3ab3576232fa4883724b
Ratko Zagorac <ratkozagorac@gmail.com>
2025-03-06 16:08:33 +0100

    MWPW-164462: Replace whitespace

--- libs/blocks/section-metadata/section-metadata.js
+++ libs/blocks/section-metadata/section-metadata.js
@@ -57,7 +57,7 @@ function handleDelay(time, section) {
 
 function handleAnchor(anchor, section) {
   if (!anchor || !section) return;
-  section.id = anchor;
+  section.id = anchor.toLowerCase().trim().replaceAll(' ', '-');
   section.classList.add('section-anchor');
 }
 
