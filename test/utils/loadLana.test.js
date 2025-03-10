cc7927dbb08090bd30ca031f5f2454f5647efcd9
casalino <casalino@adobe.com>
2025-03-10 18:54:35 -0400

    more linting fails 🤡

--- test/utils/loadLana.test.js
+++ test/utils/loadLana.test.js
@@ -18,12 +18,12 @@ describe('Utils loadLana', () => {
     if (window.location.href !== originalLocation) {
       window.history.pushState({}, '', originalLocation);
     }
-    
+
     // Always restore console.log if it was spied on
     if (console.log.restore && typeof console.log.restore === 'function') {
       console.log.restore();
     }
-    
+
     // Reset lana for the next test
     delete window.lana;
   });
@@ -35,19 +35,19 @@ describe('Utils loadLana', () => {
 
     // Store the initial placeholder log function
     const initialLana = window.lana.log;
-    
+
     // Spy on console.log before making the first call
     sinon.spy(console, 'log');
-    
+
     // Call lana.log which should trigger dynamic import
     // This returns a Promise that resolves after the real lana.js is loaded
     await window.lana.log('test', { clientId: 'myclient', sampleRate: 0 });
-    
+
     // After the import, window.lana.log should be replaced with the real implementation
     expect(window.lana.log).not.to.equal(initialLana);
     expect(window.lana.options).to.exist;
     expect(console.log.called).to.be.true;
-    
+
     // Check the logging arguments
     const firstCallArgs = console.log.getCall(0).args;
     expect(firstCallArgs[0]).to.equal('LANA Msg: ');
