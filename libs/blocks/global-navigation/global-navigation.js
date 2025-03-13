0ee08e783e2fde6dde41567e532ca4889b12cd8b
sonawanesnehal3 <152426902+sonawanesnehal3@users.noreply.github.com>
2025-03-13 23:07:53 +0530

    MWPW-169371 : Adding countryCode config to UNav (#3793)
    * Adding countryCode config to UNav
    
    * Updating countryCode for uk
    
    * Updating for upperCase
    
    * Passing countryCode according to MAS mapping for UCv3 cart
    
    ---------
    
    Co-authored-by: Snehal Sonawane <sonawane@snehals-mbp.corp.adobe.com>
    Co-authored-by: Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>

--- libs/blocks/global-navigation/global-navigation.js
+++ libs/blocks/global-navigation/global-navigation.js
@@ -49,6 +49,8 @@ import {
 
 import { replaceKey, replaceKeyArray } from '../../features/placeholders.js';
 
+import { getMiloLocaleSettings } from '../merch/merch.js';
+
 const SIGNIN_CONTEXT = getConfig()?.signInContext;
 
 function getHelpChildren() {
@@ -776,6 +778,7 @@ class Gnav {
       target: this.blocks.universalNav,
       env: environment,
       locale,
+      countryCode: getMiloLocaleSettings(getConfig().locale)?.country || 'US',
       imsClientId: window.adobeid?.client_id,
       theme: isDarkMode() ? 'dark' : 'light',
       analyticsContext: {
