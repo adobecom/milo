a4d8bba3856821affbb35398fb5ee1581e52d08f
Bozo Jovicic <bozo@hitthecode.com>
2025-03-17 10:19:12 +0100

    MWPW-166076 Update a.com Business plans page CTAs to point to CME-1 M7 Business plans page

--- libs/blocks/m7/m7.js
+++ libs/blocks/m7/m7.js
@@ -1,12 +1,22 @@
 import { getConfig, getMetadata } from '../../utils/utils.js';
 import { getMiloLocaleSettings } from '../merch/merch.js';
 
-export function generateM7Link(href) {
+async function getImsCountry() {
+  if (window.adobeIMS?.isSignedInUser()) {
+    const profile = await window.adobeIMS.getProfile();
+    return profile.countryCode;
+  }
+
+  return null;
+}
+
+export async function generateM7Link(href) {
   const paCode = getMetadata('m7-pa-code');
   if (!paCode) return href;
 
+  const imsCountry = await getImsCountry();
   const { locale } = getConfig();
-  const country = getMiloLocaleSettings(locale).country || 'US';
+  const country = imsCountry || getMiloLocaleSettings(locale).country || 'US';
 
   const m7link = new URL('https://commerce.adobe.com/store/segmentation?cli=creative&cs=t');
   m7link.searchParams.append('co', country);
@@ -17,9 +27,9 @@ export function generateM7Link(href) {
   return m7link.toString();
 }
 
-export default function init(el) {
+export default async function init(el) {
   try {
-    el.href = generateM7Link(el.href);
+    el.href = await generateM7Link(el.href);
   } catch (e) {
     window.lana.log(`Cannot generate M7 URL. ${e}`, { tags: 'm7', errorType: 'i' });
   }
--- test/blocks/m7/m7.test.js
+++ test/blocks/m7/m7.test.js
@@ -1,6 +1,6 @@
 import { expect } from '@esm-bundle/chai';
 import sinon from 'sinon';
-import init from '../../../libs/blocks/m7/m7.js';
+import init, { generateM7Link } from '../../../libs/blocks/m7/m7.js';
 
 describe('m7business autoblock', () => {
   before(async () => {
@@ -11,36 +11,46 @@ describe('m7business autoblock', () => {
     document.head.innerHTML = '';
   });
 
-  it('Converts business plans link to M7 link no meta m7-pa-code', () => {
+  it('Converts business plans link to M7 link no meta m7-pa-code', async () => {
     const a = document.createElement('a');
     a.setAttribute('href', 'https://www.adobe.com/creativecloud/business-plans.html');
-    init(a);
+    await init(a);
     expect(a.href).to.equal('https://www.adobe.com/creativecloud/business-plans.html');
   });
 
-  it('Converts business plans link to M7 link', () => {
+  it('Converts business plans link to M7 link', async () => {
     document.head.innerHTML = '<meta name="m7-pa-code" content="ccsn_direct_individual">';
     const a = document.createElement('a');
     a.setAttribute('href', 'https://www.adobe.com/creativecloud/business-plans.html');
-    init(a);
+    await init(a);
     expect(a.href).to.equal('https://commerce.adobe.com/store/segmentation?cli=creative&cs=t&co=US&pa=ccsn_direct_individual');
   });
 
-  it('Converts education plans link to M7 link', () => {
+  it('Converts education plans link to M7 link', async () => {
     document.head.innerHTML = '<meta name="m7-pa-code" content="ccsn_direct_individual">';
     const a = document.createElement('a');
     a.setAttribute('href', 'https://www.adobe.com/creativecloud/education-plans.html');
-    init(a);
+    await init(a);
     expect(a.href).to.equal('https://commerce.adobe.com/store/segmentation?cli=creative&cs=t&co=US&pa=ccsn_direct_individual&ms=EDU');
   });
 
-  it('Handles the errors gracefully', () => {
+  it('Handles the errors gracefully', async () => {
     document.head.innerHTML = '<meta name="m7-pa-code" content="ccsn_direct_individual">';
     const element = { href: null };
-    init(element);
+    await init(element);
     expect(element).to.exist;
     expect(element.href).to.be.null;
     expect(window.lana.log.calledOnce).to.be.true;
     expect(window.lana.log.calledWith('Cannot generate M7 URL. TypeError: Cannot read properties of null (reading \'includes\')')).to.be.true;
+  });
+
+  it('Converts business plans link to M7 link for signed in user', async () => {
+    document.head.innerHTML = '<meta name="m7-pa-code" content="ccsn_direct_individual">';
+    const buIms = window.adobeIMS;
+    const profile = { countryCode: 'CH' };
+    window.adobeIMS = { getProfile: () => profile, isSignedInUser: () => true };
+    const m7Link = await generateM7Link([]);
+    expect(m7Link).to.equal('https://commerce.adobe.com/store/segmentation?cli=creative&cs=t&co=CH&pa=ccsn_direct_individual');
+    window.adobeIMS = buIms;
   });
 });
