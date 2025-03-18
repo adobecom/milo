45d146eab20adfa667dae9fba1aa3707cc1a453d
Dev Ashish Sardana <glo77801@adobe.com>
2025-03-18 11:51:34 +0530

    lint fix

--- libs/scripts/delayed.js
+++ libs/scripts/delayed.js
@@ -25,7 +25,7 @@ export const loadJarvisChat = async (getConfig, getMetadata, loadScript, loadSty
 };
 
 export const loadPrivacy = async (getConfig, loadScript) => {
-  const { privacyId, env } = getConfig();
+  const { privacyId } = getConfig();
   const acom = 'b02782c1-a5e1-4d3b-99bb-537f2bf36700';
   const ids = {
     'hlx.page': 'f5b9e81a-54b5-40cb-afc3-84ca26e7dbaf-test',
