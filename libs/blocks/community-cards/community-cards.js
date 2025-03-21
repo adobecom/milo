90c202fd9b8275be2a53242f23ea58775662cbd0
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-21 17:17:10 +0530

    Update community-cards.js

--- libs/blocks/community-cards/community-cards.js
+++ libs/blocks/community-cards/community-cards.js
@@ -1,12 +1,5 @@
 import { loadStyle, loadScript } from '../../utils/utils.js';
 
-async function loadReact() {
-    await Promise.all([
-        loadScript('https://unpkg.com/react@18/umd/react.development.js'),
-        loadScript('https://unpkg.com/react-dom@18/umd/react-dom.development.js')
-    ]);
-}
-
 async function loadComponent() {
     await Promise.all([
         loadScript(`${window.location.origin}/libs/blocks/community-cards/featured.bundle.js`),
@@ -15,6 +8,5 @@ async function loadComponent() {
 }
 
 export default async function init(el) {
-  await loadReact();
   await loadComponent();
 }
