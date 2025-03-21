39c3dd63acd21e745af1b1ede3325685418b4469
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-21 15:59:58 +0530

    Update community-cards.js

--- libs/blocks/community-cards/community-cards.js
+++ libs/blocks/community-cards/community-cards.js
@@ -2,8 +2,8 @@ import { loadStyle, loadScript } from '../../utils/utils.js';
 
 async function loadReact() {
     await Promise.all([
-        loadScript('https://unpkg.com/react@16.14.0/umd/react.development.js'),
-        loadScript('https://unpkg.com/react-dom@16.14.0/umd/react-dom.development.js'),
+        // loadScript('https://unpkg.com/react@16.14.0/umd/react.development.js'),
+        // loadScript('https://unpkg.com/react-dom@16.14.0/umd/react-dom.development.js'),
         loadScript(`${window.location.origin}/libs/blocks/community-cards/featured.bundle.js`),
         loadStyle(`${window.location.origin}/libs/blocks/community-cards/featured.css`)
     ]);
@@ -11,10 +11,10 @@ async function loadReact() {
 
 export default async function init(el) {
   await loadReact();
-  ReactDOM.render(
-    React.createElement('FeaturedPostWrapper', {
-      'locale': 'en-US',
-      'environment': 'stage',
-      'categoryId': 'ct-adobe-firefly'
-      }), el);
+  // ReactDOM.render(
+  //   React.createElement('FeaturedPostWrapper', {
+  //     'locale': 'en-US',
+  //     'environment': 'stage',
+  //     'categoryId': 'ct-adobe-firefly'
+  //     }), el);
 }
