5e2e9f1e1e1f22848e75f01bcdd8fadb30140d60
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-21 16:30:18 +0530

    Update community-cards.js

--- libs/blocks/community-cards/community-cards.js
+++ libs/blocks/community-cards/community-cards.js
@@ -3,7 +3,12 @@ import { loadStyle, loadScript } from '../../utils/utils.js';
 async function loadReact() {
     await Promise.all([
         loadScript('https://unpkg.com/react@18/umd/react.development.js'),
-        loadScript('https://unpkg.com/react-dom@18/umd/react-dom.development.js'),
+        loadScript('https://unpkg.com/react-dom@18/umd/react-dom.development.js')
+    ]);
+}
+
+async function loadComponent() {
+    await Promise.all([
         loadScript(`${window.location.origin}/libs/blocks/community-cards/featured.bundle.js`),
         loadStyle(`${window.location.origin}/libs/blocks/community-cards/featured.css`)
     ]);
@@ -11,10 +16,5 @@ async function loadReact() {
 
 export default async function init(el) {
   await loadReact();
-  // ReactDOM.render(
-  //   React.createElement('FeaturedPostWrapper', {
-  //     'locale': 'en-US',
-  //     'environment': 'stage',
-  //     'categoryId': 'ct-adobe-firefly'
-  //     }), el);
+  await loadComponent();
 }
