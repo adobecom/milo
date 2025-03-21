328e569b7683ff312e25bc8ddcba9c6f50372050
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-21 21:47:57 +0530

    Update community-gallery.js

--- libs/blocks/community-gallery/community-gallery.js
+++ libs/blocks/community-gallery/community-gallery.js
@@ -12,10 +12,10 @@ export default async function init(el) {
   await Promise.all([
       loadScript('https://unpkg.com/react@18/umd/react.development.js'),
       loadScript('https://unpkg.com/react-dom@18/umd/react-dom.development.js'),
-      loadScript(`${window.location.origin}/libs/features/spectrum-web-components/dist/theme.js`, 'module'),
-      loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-base.js`, 'module'),
-      loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-lit-all.min.js`, 'module'),
-      loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-theme.js`, 'module')
+      // loadScript(`${window.location.origin}/libs/features/spectrum-web-components/dist/theme.js`, 'module'),
+      // loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-base.js`, 'module'),
+      // loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-lit-all.min.js`, 'module'),
+      // loadScript(`${window.location.origin}/libs/blocks/community-gallery/spectrum-theme.js`, 'module')
   ]);
   await loadComponent();
   const c = createTag('sp-theme', { dir: 'ltr', scale: 'medium', color: 'light'});
