03c882ac9ee562c0b0a80b3de45eba3fbba3bebb
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-21 11:50:33 +0530

    Update community-cards.js

--- libs/blocks/community-cards/community-cards.js
+++ libs/blocks/community-cards/community-cards.js
@@ -1,3 +1,27 @@
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
+
+async function loadReact() {
+    await Promise.all([
+        loadScript('https://unpkg.com/react@18/umd/react.development.js'),
+        loadScript('https://unpkg.com/react-dom@18/umd/react-dom.development.js'),
+        loadScript(`${window.location.origin}/libs/blocks/community-cards/featured.bundle.js`)
+    ]);
+    renderReactApp();
+}
+
 export function init(el) {
-  const module = await import(`${window.location.origin}/libs/blocks/community-cards/community-cards.js`);
+  await loadReact();
+  ReactDOM.render(<FeaturedPostWrapper
+    locale=‘en-US’
+    environment=‘stage’
+              categoryId=‘ct-adobe-firefly’
+    />, el);
 }
