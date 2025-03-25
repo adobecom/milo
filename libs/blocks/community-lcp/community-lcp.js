81b248caa8da12bd24c04c311c1796b01349476e
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-25 16:43:30 +0530

    Update community-lcp.js

--- libs/blocks/community-lcp/community-lcp.js
+++ libs/blocks/community-lcp/community-lcp.js
@@ -2,7 +2,8 @@ import { createTag } from '../../utils/utils.js';
 
 function createImageCard(el) {
   const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
-  const textContent = el.querySelector('p');
+  const textContent = el.querySelector('a').innerText.trim();
+  const redirectLink = el.querySelector('a').href;
   const img = el.querySelector('picture');
   const background = el.querySelector('div:first-child').innerText.trim();
   const dcCardContent = createTag('div', {class: 'directory-card-content', style: `background: ${background}`});
@@ -11,6 +12,9 @@ function createImageCard(el) {
   const dcimg = createTag('div', {class: 'ufd-image-card', style: "width: 240px;"}, img);
   const dcContainer = createTag('div', {}, dcimg);
   dcContainer.append(dcCardContent);
+  dcContainer.addEventListener('click' => {
+    window.location.href = redirectLink;
+  });
   return dcContainer;
 }
 
