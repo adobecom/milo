34d2c55090b8f57a5ccc15a23f78233221c357cc
Aishwarya Mathuria <mathuria@adobe.com>
2025-03-25 11:14:57 +0530

    Update community-lcp.js

--- libs/blocks/community-lcp/community-lcp.js
+++ libs/blocks/community-lcp/community-lcp.js
@@ -1,2 +1,28 @@
+import { createTag } from '../../utils/utils.js';
+
+function createImageCard(el) {
+  const heading = el.querySelector('h1, h2, h3, h4, h5, h6');
+  const textContent = el.querySelector('p');
+  const img = el.querySelector('picture');
+  const background = el.querySelector('div:first-child').innerText.trim();
+  const dcCardContent = createTag('div', {class: 'directory-card-content'});
+  dcCardContent.append(createTag('div', {class: 'content-card-header'}, heading.innerText.trim()));
+  dcCardContent.append(createTag('div', {class: 'content-card-subheader'}, textContent.innerText.trim()));
+  const dcimg = createTag('div', {class: 'ufd-image-card', style: "width: 240px;"});
+  const dcContainer = createTag('div', {}, dcimg);
+  const dcContainer.append(dcCardContent);
+  return dcContainer;
+}
+
 export async function init(el) {
+  const cardsec = el.closest('.section').querySelector('ufd-directory-card');
+  if (cardsec) {
+    const imgCard = createImageCard(el);
+    cardsec.append(imgCard);
+    return;
+  }
+  const imgCard = createImageCard(el);
+  const dc = createTag('div', {class: 'ufd-directory-card'}, imgCard);
+  el.closest('.section').append(dc)
+  el.remove()
 }
