152e6fa8e5de5da74bed5d9967c03cc5d0187297
Sheridan Sunier <sunier@Sheridans-MacBook-Pro.local>
2025-04-04 12:37:48 -0400

    MWPW-170567: send to caas language first localization

--- tools/send-to-caas/send-utils.js
+++ tools/send-to-caas/send-utils.js
@@ -1,6 +1,6 @@
 import getUuid from '../../libs/utils/getUuid.js';
 import { getMetadata } from '../../libs/utils/utils.js';
-import { LOCALES } from '../../libs/blocks/caas/utils.js';
+import { LANGS, LOCALES } from '../../libs/blocks/caas/utils.js';
 
 const CAAS_TAG_URL = 'https://www.adobe.com/chimera-api/tags';
 const HLX_ADMIN_STATUS = 'https://admin.hlx.page/status';
@@ -306,6 +306,16 @@ const getBulkPublishLangAttr = async (options) => {
 };
 
 const getCountryAndLang = async (options) => {
+  const langFirst = getMetadata('langfirst');
+  if (langFirst) {
+    const localeArr = window.location.pathname.split('/');
+    const langStr = (localeArr.length > 1) ? LANGS[localeArr[1]] || LANGS[''] : 'en';
+    const countryStr = (localeArr.length > 2) ? LOCALES[localeArr[2]].ietf.split('-')[1] || 'xx' : 'xx';
+    return {
+      country: countryStr,
+      lang: langStr,
+    }
+  }
   /* c8 ignore next */
   const langStr = window.location.pathname.includes('/tools/send-to-caas/bulkpublisher')
     ? await getBulkPublishLangAttr(options)
