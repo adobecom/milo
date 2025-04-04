302fc0ecb0fb208405e334c25ef478ec414cf68e
Ilyas Türkben <ilyas@adobe.com>
2025-04-04 23:35:28 +0200

    still regression in variants

--- libs/features/mas/docs/common.js
+++ libs/features/mas/docs/common.js
@@ -68,7 +68,6 @@ const createMasCommerceService = (params) => {
 
 const init = async () => {
   await polyfills();
-  await import('../dist/mas.js');
   const params = new URLSearchParams(document.location.search);
 
   // theme
@@ -76,6 +75,7 @@ const init = async () => {
 
   // mas-commerce-service
   createMasCommerceService(params);
+  await import('../dist/mas.js');
 
   document.querySelectorAll('a.theme-toggle').forEach((link) => 
     link.addEventListener('click', (event) =>
