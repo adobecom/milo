b52da48e4005d6821a0f498f661a9fcf4f21c86a
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-04-07 16:29:44 +0200

    [MWPW-169532] - nala update

--- nala/blocks/video/video.test.js
+++ nala/blocks/video/video.test.js
@@ -163,6 +163,8 @@ test.describe('Milo Video Block test suite', () => {
       await expect(await video.youtubePlayButton).toBeVisible();
       await expect(await video.youtubePlayButton).toHaveAttribute('type', 'button');
       await expect(await video.liteYoutube).toHaveAttribute('videoid', data.videoId);
+      await video.youtubePlayButton.click();
+      await page.waitForTimeout(1000);
       const iframeTitle = await video.iframe.getAttribute('title');
       expect(iframeTitle).not.toBeNull();
       expect(iframeTitle).not.toBe('');
