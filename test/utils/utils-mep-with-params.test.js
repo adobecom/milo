373e88dd5f12c80e2887ee65688761e2bbf99961
markpadbe <markp@adobe.com>
2025-04-08 15:01:43 -0700

    Add tests

--- test/utils/utils-mep-with-params.test.js
+++ test/utils/utils-mep-with-params.test.js
@@ -7,16 +7,18 @@ describe('MEP Utils', () => {
   describe('getMepEnablement', async () => {
     it('checks param overwrites', async () => {
       document.head.innerHTML = await readFile({ path: './mocks/mep/head-promo.html' });
-      spoofParams({ target: 'postlcp', promo: 'off', personalization: 'off' });
+      spoofParams({ target: 'postlcp', promo: 'off', personalization: 'off', ajo: 'off', geoLocation: 'off' });
       setTimeout(() => {
         const persEnabled = getMepEnablement('personalization');
         const promoEnabled = getMepEnablement('manifestnames', 'promo');
         const ajoEnabled = getMepEnablement('ajo');
         const targetEnabled = getMepEnablement('target');
+        const geoLocation = getMepEnablement('geoLocation');
         expect(promoEnabled).to.equal(false);
         expect(persEnabled).to.equal(false);
         expect(ajoEnabled).to.equal(false);
         expect(targetEnabled).to.equal('postlcp');
+        expect(geoLocation).to.equal(false);
       }, 1000);
     });
   });
