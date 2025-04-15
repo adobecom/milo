89c4c20fb003612536b195cb111710bd90243568
Manasvi Agrawal <maagrawal@adobe.com>
2025-04-15 15:47:39 +0530

    fix: bacom blog mapping and added ref in session storage

--- tools/da-apps/auth.js
+++ tools/da-apps/auth.js
@@ -1,6 +1,6 @@
 const ORG_MAP = {
   'da-bacom': 'DA BACOM',
-  'da-bacom-blog': 'DA BACOM_BLOG',
+  'da-bacom-blog': 'DA BACOM BLOG',
   'da-cc-sandbox': 'DA BACOM',
 };
 
@@ -8,13 +8,17 @@ const ORG_MAP = {
   document.body.style.visibility = 'hidden';
 
   const searchParams = new URLSearchParams(window.location.search);
+
   const repo = searchParams.get('tenant');
   const token = searchParams.get('token');
+  const ref = searchParams.get('ref');
   const redirectPath = searchParams.get('path');
+
   const tenant = ORG_MAP[repo];
 
   window.sessionStorage.setItem('da-repo', tenant);
   window.sessionStorage.setItem('da-token', token);
+  window.sessionStorage.setItem('da-ref', ref);
 
   if (token === 'undefined') {
     document.body.innerHTML = `
@@ -26,7 +30,9 @@ const ORG_MAP = {
     return;
   }
 
-  const fullURL = `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}${window.location.pathname}#/${redirectPath}`;
+  const fullURL = `${window.location.protocol}//${window.location.hostname}${
+    window.location.port ? `:${window.location.port}` : ''
+  }${window.location.pathname}#/${redirectPath}`;
   window.history.replaceState({}, null, fullURL);
   document.body.style.visibility = 'visible';
 }());
