5c9a4246567e07dfc4ed6821d308026a59ec6be4
Victor Hargrave <vhargrave@adobe.com>
2025-04-15 15:45:18 +0200

    MWPW-170805 initial refactoring of seo checks.

---
+++ libs/blocks/preflight/checks/constants.js
@@ -0,0 +1,16 @@
+export const STATUS = {
+  PASS: 'pass',
+  FAIL: 'fail',
+  LIMBO: 'limbo',
+  EMPTY: 'empty',
+};
+
+export const SEO_TITLES = {
+  H1Count: 'SEO - H1 count',
+  TitleSize: 'SEO -Title size',
+  Canonical: 'SEO - Canonical',
+  MetaDescription: 'SEO -Meta description',
+  BodySize: 'SEO -Body size',
+  Lorem: 'SEO - Lorem Ipsum',
+  Links: 'SEO -Links',
+};
