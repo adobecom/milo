354bdb903bd06f88eb481089ff37121a9abd91e6
skholkhojaev <skholkhojaev@adobe.com>
2025-04-16 14:34:35 +0200

    changed

--- .github/workflows/merge-to-main.js
+++ .github/workflows/merge-to-main.js
@@ -9,7 +9,7 @@ const {
 const PR_TITLE = '[Release] Stage to Main';
 const STAGE = 'stage';
 const PROD = 'main';
-const MIN_SOT_APPROVALS = process.env.MIN_SOT_APPROVALS ? Number(process.env.MIN_SOT_APPROVALS) : 0;
+const MIN_SOT_APPROVALS = process.env.MIN_SOT_APPROVALS ? Number(process.env.MIN_SOT_APPROVALS) : 1;
 
 let github, owner, repo;
 
