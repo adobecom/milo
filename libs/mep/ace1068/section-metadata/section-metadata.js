6d05a945d2a11406c69e6c4fb7eae302ea21cd2a
AdobeLinhart <dlinhart@adobe.com>
2025-04-17 16:00:26 -0600

    File fixes.

---
+++ libs/mep/ace1068/section-metadata/section-metadata.js
@@ -0,0 +1,83 @@
+import { handleFocalpoint } from '../../utils/decorate.js';
+
+export function handleBackground(div, section) {
+  const pic = div.background.content?.querySelector('picture');
+  if (pic) {
+    section.classList.add('has-background');
+    pic.classList.add('section-background');
+    handleFocalpoint(pic, div.background.content);
+    section.insertAdjacentElement('afterbegin', pic);
+  } else {
+    const color = div.background.content?.textContent;
+    if (color) {
+      section.style.background = color;
+    }
+  }
+}
+
+export async function handleStyle(text, section) {
+  if (!text || !section) return;
+  const styles = text.split(', ').map((style) => style.replaceAll(' ', '-'));
+  const sticky = styles.find((style) => style === 'sticky-top' || style === 'sticky-bottom');
+  if (sticky) {
+    const { default: handleStickySection } = await import('./sticky-section.js');
+    await handleStickySection(sticky, section);
+  }
+  if (styles.includes('masonry')) styles.push('masonry-up');
+  section.classList.add(...styles);
+}
+
+function handleMasonry(text, section) {
+  section.classList.add(...['masonry-layout', 'masonry-up']);
+  const divs = section.querySelectorAll(":scope > div:not([class*='metadata'])");
+  const spans = [];
+  text.split('\n').forEach((line) => spans.push(...line.trim().split(',')));
+  [...divs].forEach((div, i) => {
+    const spanWidth = spans[i] ? spans[i] : 'span 4';
+    div.classList.add(`grid-${spanWidth.trim().replace(' ', '-')}`);
+  });
+}
+
+function handleLayout(text, section) {
+  if (!(text || section)) return;
+  const layoutClass = `grid-template-columns-${text.replaceAll(' | ', '-')}`;
+  section.classList.add(layoutClass);
+}
+
+export function getDelayTime(time) {
+  if (time > 99) return time;
+  return (time * 1000);
+}
+
+function handleDelay(time, section) {
+  if (!(time || section)) return;
+  section.classList.add('hide-sticky-section');
+  setTimeout(() => { section.classList.remove('hide-sticky-section'); }, getDelayTime(time));
+}
+
+function handleAnchor(anchor, section) {
+  if (!anchor || !section) return;
+  section.id = anchor.toLowerCase().trim().replaceAll(/\s+/g, '-');
+  section.classList.add('section-anchor');
+}
+
+export const getMetadata = (el) => [...el.childNodes].reduce((rdx, row) => {
+  if (row.children) {
+    const key = row.children[0].textContent.trim().toLowerCase();
+    const content = row.children[1];
+    const text = content.textContent.trim().toLowerCase();
+    if (key && content) rdx[key] = { content, text };
+  }
+  return rdx;
+}, {});
+
+export default async function init(el) {
+  const section = el.closest('.section');
+  const metadata = getMetadata(el);
+  if (metadata.style) await handleStyle(metadata.style.text, section);
+  if (metadata.background) handleBackground(metadata, section);
+  if (metadata.layout) handleLayout(metadata.layout.text, section);
+  if (metadata.masonry) handleMasonry(metadata.masonry.text, section);
+  if (metadata.delay) handleDelay(metadata.delay.text, section);
+  if (metadata.anchor) handleAnchor(metadata.anchor.text, section);
+}
