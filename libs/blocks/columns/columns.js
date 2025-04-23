a72dc9126ea60f941206471f84c0616685ac62f2
Ratko Zagorac <ratkozagorac@gmail.com>
2025-04-23 15:12:21 +0200

    MWPW-170339: Move row title

--- libs/blocks/columns/columns.js
+++ libs/blocks/columns/columns.js
@@ -1,13 +1,30 @@
+function isTableRowTitle(el, index) {
+  return !index
+  && el.childNodes.length === 1
+  && el.childNodes[0].tagName === 'STRONG';
+}
+
+function isTableHeadingAndEmpty(el, rdx, cdx) {
+  return !rdx && !cdx && !el.innerText;
+}
+
+function isTable(el) {
+  return el.classList.contains('table');
+}
+
 export default function init(el) {
   const rows = el.querySelectorAll(':scope > div');
   rows.forEach((row, rdx) => {
-    row.className = `row row-${rdx + 1}`;
+    row.classList.add('row', `row-${rdx + 1}`);
     const cols = row.querySelectorAll(':scope > div');
     cols.forEach((col, cdx) => {
-      col.className = `col col-${cdx + 1}`;
+      col.classList.add('col', `col-${cdx + 1}`);
+      if (!isTable(el)) return;
+      if (isTableHeadingAndEmpty(col, rdx, cdx)) col.classList.add('empty-table-heading');
+      if (isTableRowTitle(col, cdx)) col.classList.add('row-title');
     });
   });
-  if (el.classList.contains('table')) {
+  if (isTable(el)) {
     el.classList.add('columns-table');
     el.classList.remove('table');
   }
