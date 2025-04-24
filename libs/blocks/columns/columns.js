27026857ef98b41ced5b8f216a7cf03f23850f75
Ratko Zagorac <ratkozagorac@gmail.com>
2025-04-24 10:22:53 +0200

    MWPW-170339: Remove unnecessary functions

--- libs/blocks/columns/columns.js
+++ libs/blocks/columns/columns.js
@@ -1,30 +1,18 @@
-function isTableRowTitle(el, index) {
-  return !index
-  && el.childNodes.length === 1
-  && el.childNodes[0].tagName === 'STRONG';
-}
-
-function isTableHeadingAndEmpty(el, rdx, cdx) {
-  return !rdx && !cdx && !el.innerText;
-}
-
-function isTable(el) {
-  return el.classList.contains('table');
-}
-
 export default function init(el) {
   const rows = el.querySelectorAll(':scope > div');
+  const isTable = el.classList.contains('table');
   rows.forEach((row, rdx) => {
     row.classList.add('row', `row-${rdx + 1}`);
     const cols = row.querySelectorAll(':scope > div');
     cols.forEach((col, cdx) => {
       col.classList.add('col', `col-${cdx + 1}`);
-      if (!isTable(el)) return;
-      if (isTableHeadingAndEmpty(col, rdx, cdx)) col.classList.add('empty-table-heading');
-      if (isTableRowTitle(col, cdx)) col.classList.add('row-title');
+      if (!isTable) return;
+      if (!rdx && !cdx && !col.innerText) col.classList.add('empty-table-heading');
+      const isTableRowTitle = !cdx && col.childNodes.length === 1 && col.childNodes[0].tagName === 'STRONG';
+      if (isTableRowTitle) col.classList.add('row-title');
     });
   });
-  if (isTable(el)) {
+  if (isTable) {
     el.classList.add('columns-table');
     el.classList.remove('table');
   }
