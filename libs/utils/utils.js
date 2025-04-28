44f5382ebdc681be07b200aa8f4388c71bef0c33
Ratko Zagorac <90400759+zagi25@users.noreply.github.com>
2025-04-28 18:10:40 +0200

    Mwpw 169999 header obscures content (#3898)
    * MWPW-169999: Fix header hiding content when tabbing
    
    * MWPW-169999: Sticky and fixed content not hiding focused content
    
    * MWPW-169999: Add scroll padding using JS
    
    * MWPW-169999: Fix lint
    
    * MWPW-169999: use elementFromPoint
    
    * MWPW-169999: Pr update
    
    * MWPW-169999: Handle iframe stealing focus
    
    * MWPW-169999: Set scroll-padding to 25vh
    
    * MWPW-169999: Use scroll-padding-block

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1543,6 +1543,9 @@ function decorateDocumentExtras() {
 
 async function documentPostSectionLoading(config) {
   decorateFooterPromo();
+  import('../scripts/accessibility.js').then((accessibility) => {
+    accessibility.default();
+  });
   if (getMetadata('seotech-structured-data') === 'on' || getMetadata('seotech-video-url')) {
     import('../features/seotech/seotech.js').then((module) => module.default(
       { locationUrl: window.location.href, getMetadata, createTag, getConfig },
