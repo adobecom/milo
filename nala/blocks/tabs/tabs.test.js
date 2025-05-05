41053f5a3a5bbe7b96beba3dd02ca19b70a8bf11
Mira <myrosvas@gmail.com>
2025-05-05 14:10:36 +0200

    decreased timeout (testing)

--- nala/blocks/tabs/tabs.test.js
+++ nala/blocks/tabs/tabs.test.js
@@ -179,7 +179,7 @@ test.describe('Milo Tab block feature test suite', () => {
       const newUrl = await new URL(await page.url());
       await newUrl.searchParams.set('tab', 'demo-3');
       await page.goto(newUrl.toString());
-      await page.waitForTimeout(5000);
+      await page.waitForTimeout(2000);
       await expect(await page.url()).toContain('tabs-page-3?tab=demo-3');
       await tab.tab2.click();
       await expect(await page.url()).toContain('tabs-page-2?tab=demo-2');
