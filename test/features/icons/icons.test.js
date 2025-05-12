617d13636522e40f4f777216e4d5adcbac99b618
Sheridan Sunier <sunier@Sheridans-MacBook-Pro.local>
2025-05-12 12:16:56 -0700

    weird merge issue

--- test/features/icons/icons.test.js
+++ test/features/icons/icons.test.js
@@ -50,7 +50,7 @@ describe('Icon Support', () => {
     expect(otherIcons[0].querySelector('svg')).to.exist;
   });
 
-  it('Renders an SVG after loading the icons',, async () => {
+  it('Renders an SVG after loading the icons', () => {
     const selector = icons[0].querySelector(':scope svg');
     expect(selector).to.exist;
   });
