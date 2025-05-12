f203835815546739d6aee7a96d910e089f4644cf
Bandana Laishram <bandanalaishram@gmail.com>
2025-05-12 15:12:10 +0530

    Adding item wrapper back

--- libs/blocks/global-navigation/global-navigation.js
+++ libs/blocks/global-navigation/global-navigation.js
@@ -512,7 +512,8 @@ class Gnav {
     localNav.setAttribute('daa-lh', `${title}_localNav`);
     const localNavBtn = toFragment`<button class="feds-navLink--hoverCaret feds-localnav-title" aria-haspopup="true" aria-expanded="false" daa-ll="${title}_localNav|open"></button>`;
     const localNavCurtain = toFragment` <div class="feds-localnav-curtain"></div>`;
-    localNav.append(localNavBtn, localNavCurtain, toFragment`<a href="#" class="feds-sr-only feds-localnav-exit">.</a>`);
+    // Skip keyboard navigation on localnav items if it is closed
+    localNav.append(localNavBtn, localNavCurtain, toFragment` <div class="feds-localnav-items"></div>`, toFragment`<a href="#" class="feds-sr-only feds-localnav-exit">.</a>`);
 
     const itemWrapper = localNav.querySelector('.feds-localnav-items');
     const localNavTitle = document.querySelector('.feds-localnav-title');
