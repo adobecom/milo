38450117612e4fb26cf8e0c62ebfb4cd0413ac22
Bandana Laishram <bandanalaishram@gmail.com>
2025-05-19 19:25:27 +0530

    Adding arrow Up/Down navigation for localnav (#4092)
    * Adding arrow navigation for localnav
    
    * lint fix
    
    * Adding support for nested dropdown
    
    * Applying inert attribute if localnav is closed
    
    * lint fix
    
    * Removing inert as not supported in old browsers and firefox
    
    * lint fix
    
    * lint fix
    
    * lint fix
    
    * Adding item wrapper back

--- libs/blocks/global-navigation/utilities/keyboard/utils.js
+++ libs/blocks/global-navigation/utilities/keyboard/utils.js
@@ -48,6 +48,8 @@ const selectors = {
   localNavActive: '.feds-localnav--active',
   localNavTitle: '.feds-localnav-title',
   localNavExit: '.feds-localnav-exit',
+  localNavItem: '.feds-localnav-items .feds-navItem',
+  localNavItems: '.feds-navItem .feds-menu-column > a, .feds-navItem > a, .feds-navItem > button, .feds-navItem > .feds-cta-wrapper > .feds-cta',
 };
 
 selectors.profileDropdown = `
