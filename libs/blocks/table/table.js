6ee7ce55e2b529c89e10ebaf1adf5b0b233fb043
Dušan Kosanović <dusan.kosanovic@hitthecode.com>
2025-05-19 15:55:05 +0200

    [MWPW-171327] Table sticky heading (#4054)
    * [MWPW-171327] - talbe sticky percentage
    
    * [MWPW-171327] - update percentage
    
    * [MWPW-171327] - update
    
    * [MWPW-171327] - shorten code
    
    * [MWPW-171327] - variable rename
    
    * [MWPW-171327] - logic and test update
    
    * [MWPW-171327] - code shorten
    
    * [MWPW-171327] - update
    
    * [MWPW-171327] - update
    
    * [MWPW-171327] - boundary moved, remove cancel sticky added
    
    * [MWPW-171327] - optimize code

--- libs/blocks/table/table.js
+++ libs/blocks/table/table.js
@@ -598,6 +598,11 @@ function applyStylesBasedOnScreenSize(table, originTable) {
   setRowStyle();
 }
 
+function handleStickyHeader(el) {
+  if (!el.classList.value.includes('sticky')) return;
+  setTimeout(() => el.classList.toggle('cancel-sticky', !(el.querySelector('.row-heading').offsetHeight / window.innerHeight < 0.45)));
+}
+
 export default function init(el) {
   el.setAttribute('role', 'table');
   if (el.parentElement.classList.contains('section')) {
@@ -657,10 +662,12 @@ export default function init(el) {
       setTooltipPosition(el);
     };
     handleResize();
+    handleStickyHeader(el);
 
     let deviceBySize = defineDeviceByScreenSize();
     window.addEventListener('resize', () => {
       debounce(handleEqualHeight(el, '.row-heading'), 300);
+      handleStickyHeader(el);
       if (deviceBySize === defineDeviceByScreenSize()) return;
       deviceBySize = defineDeviceByScreenSize();
       handleResize();
