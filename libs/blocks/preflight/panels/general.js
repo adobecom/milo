d82fc4da3fbf60c68952708d8d3183b4d0d23840
skholkhojaev <skholkhojaev@adobe.com>
2025-05-23 14:10:37 +0200

    made css more specific & code improvment

--- libs/blocks/preflight/panels/general.js
+++ libs/blocks/preflight/panels/general.js
@@ -9,6 +9,7 @@ const NOT_FOUND = {
 };
 const DA_DOMAIN = 'da.live';
 const nonEDSContent = 'Non AEM EDS Content';
+const EXCLUDED_PATHS = ['/tools/caas'];
 
 const content = signal({});
 
@@ -68,16 +69,14 @@ function getUrl(el) {
 }
 
 function findLinks(selector) {
-  const hrefs = [];
+  const hrefs = new Set();
   return [...document.body.querySelectorAll(selector)]
     .reduce((links, el) => {
       const url = getUrl(el);
-      const baseUrl = url.origin + url.pathname;
-      if (url.pathname.includes('/tools/caas')) {
-        return links;
-      }
-      if (!hrefs.includes(baseUrl)) {
-        hrefs.push(baseUrl);
+      const baseUrl = `${url.origin}${url.pathname}`;
+      if (EXCLUDED_PATHS.some((path) => url.pathname.includes(path))) return links;
+      if (!hrefs.has(baseUrl)) {
+        hrefs.add(baseUrl);
         links.push({ url, edit: null, preview: 'Fetching', live: 'Fetching' });
       }
       return links;
