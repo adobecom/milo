4c70b62462429fd9150f3809da8ecc06df912f6b
Prince Patel <prpatel0949@gmail.com>
2025-05-27 11:32:54 +0530

    add some review changes

--- libs/blocks/global-navigation/utilities/keyboard/mobileGnav.js
+++ libs/blocks/global-navigation/utilities/keyboard/mobileGnav.js
@@ -1,6 +1,8 @@
+import { isDesktop } from '../utilities.js';
+
 const MobileGnav = {
   init() {
-    this.isMobile = window.matchMedia('(max-width: 899px)').matches;
+    this.isMobile = !isDesktop.matches;
     this.toggleButton = document.querySelector('.feds-toggle');
     this.menuItemLinks = document.querySelectorAll('.feds-nav-wrapper section.feds-navItem > .feds-navLink');
     this.eventInitialized = false;
@@ -8,10 +10,12 @@ const MobileGnav = {
   },
   addEventListeners() {
     this.toggleButton.addEventListener('keydown', (e) => {
-      const gnavWrapper = document.querySelector('.feds-nav-wrapper');
-      if (e.code === 'ArrowDown' && gnavWrapper && gnavWrapper.classList.contains('feds-nav-wrapper--expanded')) {
-        const firstNavLink = gnavWrapper.querySelector('.feds-nav .feds-navLink');
-        firstNavLink.focus();
+      if (this.isMobile) {
+        const gnavWrapper = document.querySelector('.feds-nav-wrapper');
+        if (e.code === 'ArrowDown' && gnavWrapper && gnavWrapper.classList.contains('feds-nav-wrapper--expanded')) {
+          const firstNavLink = gnavWrapper.querySelector('.feds-nav .feds-navLink');
+          firstNavLink.focus();
+        }
       }
     });
     this.toggleButton.addEventListener('click', () => {
@@ -27,14 +31,10 @@ const MobileGnav = {
               tab.addEventListener('keydown', (e) => {
                 if (e.code === 'ArrowUp') {
                   const prevTab = tab.previousElementSibling;
-                  if (prevTab) {
-                    prevTab.focus();
-                  }
+                  if (prevTab) prevTab.focus();
                 } else if (e.code === 'ArrowDown') {
                   const nextTab = tab.nextElementSibling;
-                  if (nextTab) {
-                    nextTab.focus();
-                  }
+                  if (nextTab) nextTab.focus();
                 } else if (e.code === 'ArrowRight') {
                   const currentTabs = [...popupTabs].filter((currentTab) => currentTab.closest('.feds-dropdown--active'));
                   const activeTab = currentTabs.find((currentTab) => currentTab.getAttribute('aria-selected') === 'true');
@@ -76,9 +76,7 @@ const MobileGnav = {
                 } else if (e.code === 'ArrowLeft') {
                   const currentTabs = [...popupTabs].filter((currentTab) => currentTab.closest('.feds-dropdown--active'));
                   const activeTab = currentTabs.find((currentTab) => currentTab.getAttribute('aria-selected') === 'true');
-                  if (activeTab) {
-                    activeTab.focus();
-                  }
+                  if (activeTab) activeTab.focus();
                 }
               });
             });
@@ -92,16 +90,11 @@ const MobileGnav = {
         if (this.isMobile) {
           if (e.code === 'ArrowUp') {
             const prevLink = this.menuItemLinks[index - 1];
-            if (prevLink) {
-              prevLink.focus();
-            } else {
-              this.toggleButton.focus();
-            }
+            if (prevLink) prevLink.focus();
+            else this.toggleButton.focus();
           } else if (e.code === 'ArrowDown') {
             const nextLink = this.menuItemLinks[index + 1];
-            if (nextLink) {
-              nextLink.focus();
-            }
+            if (nextLink) nextLink.focus();
           }
         }
       });
