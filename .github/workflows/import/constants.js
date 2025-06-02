3fa0d756861c8e9202d3bc65312dcc5b31c10421
Okan Sahin <39759830+mokimo@users.noreply.github.com>
2025-06-02 13:00:56 +0200

    Centralized DA importer hosted within milo (#4255)
    * WP
    
    * WP
    
    * wp
    
    * Fix a few small issues
    
    * Set max length for finishing logs
    
    * Remove unused env var
    
    * Fix linting issues
    
    * add org
    
    * Add PR feedback
    
    * Add yaml to trigger bacom imports
    
    * Always log the first 500 successfully imported live paths
    
    * Add EOF (thx Rares)
    
    * Dont error if there are no logs
    
    * Remove double slack notification
    
    * Add Rares feedback

---
+++ .github/workflows/import/constants.js
@@ -0,0 +1,15 @@
+export const AEM_ORIGIN = 'https://admin.hlx.page';
+
+export const SUPPORTED_FILES = {
+  html: 'text/html',
+  jpeg: 'image/jpeg',
+  json: 'application/json',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  gif: 'image/gif',
+  mp4: 'video/mp4',
+  pdf: 'application/pdf',
+  svg: 'image/svg+xml',
+};
+
+export const DA_ORIGIN = 'https://admin.da.live';
