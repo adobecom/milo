79a3ee038b00af092e6f2a3c71f9102ef44145e9
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2025-06-03 10:17:18 +0530

    adjusting the border radius of the see more button

--- libs/mep/ace1052/carousel/carousel.js
+++ libs/mep/ace1052/carousel/carousel.js
@@ -573,11 +573,4 @@ export default function init(el) {
   }
 
   parentArea.addEventListener(MILO_EVENTS.DEFERRED, handleLateLoadingNavigation, true);
-
-  // mweb-dev changes for equal height of cards
-  function handleEqualHeight() {
-    setEqualHeight(slides);
-    parentArea.removeEventListener(MILO_EVENTS.DEFERRED, handleEqualHeight, true);
-  }
-  parentArea.addEventListener(MILO_EVENTS.DEFERRED, handleEqualHeight, true);
 }
