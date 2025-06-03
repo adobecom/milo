3c8876db73d88ab5c67b0120f67b3aaa91c3d4ad
Aishwarya Mathuria <mathuria@adobe.com>
2025-06-03 13:36:11 +0530

    MWPW-174220 | Accessibility | Changing table heading roles to not have them treated as text. (#4276)
    * Update table.js
    
    * Update table.js
    
    * Update table.js
    
    * Update table.js

--- libs/blocks/table/table.js
+++ libs/blocks/table/table.js
@@ -315,6 +315,11 @@ function handleSection(sectionParams) {
       handleTitleText(sectionHeadTitle);
       sectionHeadTitle.classList.add('section-head-title');
       sectionHeadTitle.setAttribute('role', 'rowheader');
+      const sectionHeadText = sectionHeadTitle.querySelector('.table-title-text');
+      if (sectionHeadText) {
+        sectionHeadText.setAttribute('role', 'heading');
+        sectionHeadText.setAttribute('aria-level', '4');
+      }
     }
 
     if (isCollapseTable) {
