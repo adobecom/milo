6efb467faf166461033af099dfe496bec36183d3
Vivian A Goodrich <vgoodric@adobe.com>
2025-06-03 17:41:40 -0600

    Move replacecell handling to utils from fragments

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1167,7 +1167,9 @@ function decorateSection(section, idx) {
     const blockName = block.classList[0];
     links.filter((link) => block.contains(link))
       .forEach((link) => {
-        if (link.classList.contains('fragment')
+        if (link.classList.contains('fragment') && link.href.includes('#_replacecell')) {
+          link.href = link.href.replace('#_replacecell', '');
+        } else if (link.classList.contains('fragment')
           && MILO_BLOCKS.includes(blockName) // do not inline consumer blocks (for now)
           && !doNotInline.includes(blockName)) {
           if (!link.href.includes('#_inline')) {
