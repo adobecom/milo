52a07a8995187882effe3c385a117519da6edb6e
sharathkannan <138484653+sharath-kannan@users.noreply.github.com>
2025-06-03 13:37:19 +0530

    Fix(Mwpw-173452): Console error for invalid url parse is fixed. (#4243)
    url parse error fix

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -226,7 +226,7 @@ export function getLanguage(languages, locales, pathname = window.location.pathn
   }
 
   const isLegacyLocaleRoutingMode = !language
-      || (language.languageBased === false && !language.region);
+    || (language.languageBased === false && !language.region);
   if (isLegacyLocaleRoutingMode) {
     const locale = getLocale(locales, pathname);
     const englishLang = languages.en;
@@ -762,6 +762,7 @@ export function decorateImageLinks(el) {
   [...images].forEach((img) => {
     const [source, alt, icon] = img.alt.split('|');
     try {
+      if (!URL.canParse(source.trim())) return;
       const url = new URL(source.trim());
       const href = (url.hostname.includes('.aem.') || url.hostname.includes('.hlx.')) ? `${url.pathname}${url.search}${url.hash}` : url.href;
       img.alt = alt?.trim() || '';
