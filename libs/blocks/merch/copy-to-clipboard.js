af7f3659634a97a3f442d5c37156d5e0b8592207
Mira <myrosvas@gmail.com>
2025-06-09 16:12:33 +0200

    replaced the icon

--- libs/blocks/merch/copy-to-clipboard.js
+++ libs/blocks/merch/copy-to-clipboard.js
@@ -11,17 +11,37 @@ function addCopyToClipboard(ostLink, cta) {
   const button = createTag('button', {
     style: 'background: none; border: none; padding: 0; cursor: pointer;',
     title: 'Copy',
-  }, `<svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" id="S_AddTo_22_N" viewBox="0 0 22 22">
-      <defs>
-        <style>
-          .fill {
-            fill: #464646;
-          }
-        </style>
-      </defs>
-      <rect id="Canvas" fill="#ff13dc" opacity="0" width="22" height="22" />
-      <path class="fill" d="M19,8H14V3a.5.5,0,0,0-.5-.5H3a.5.5,0,0,0-.5.5V13.5A.5.5,0,0,0,3,14H8v5a.5.5,0,0,0,.5.5H19a.5.5,0,0,0,.5-.5V8.5A.5.5,0,0,0,19,8Z" />
-    </svg>`);
+  }, `<svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 18 18" width="18">
+        <defs>
+          <style>
+            .fill {
+              fill: #464646;
+            }
+          </style>
+        </defs>
+        <title>Copy</title>
+        <rect id="Canvas" fill="#ff13dc" opacity="0" width="18" height="18"/><rect class="fill" height="1" rx="0.25" width="1" x="16" y="11"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="16" y="9"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="16" y="7"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="16" y="5"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="16" y="3"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="16" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="14" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="12" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="10" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="8" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="1"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="3"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="5"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="7"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="9"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="6" y="11"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="8" y="11"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="10" y="11"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="12" y="11"/>
+        <rect class="fill" height="1" rx="0.25" width="1" x="14" y="11"/>
+        <path class="fill" d="M5,6H1.5a.5.5,0,0,0-.5.5v10a.5.5,0,0,0,.5.5h10a.5.5,0,0,0,.5-.5V13H5.5a.5.5,0,0,1-.5-.5Z"/>
+      </svg>`);
 
   button.addEventListener('click', () => {
     const url = new URL(ostLink);
