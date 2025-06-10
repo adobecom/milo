6ce715015abd80cdb478a12126dd0b2817782a23
Predrag Markovic <cod14230@adobe.com>
2025-06-10 13:45:28 +0200

    remove modal close butto elem from three-in-one

--- libs/blocks/merch/three-in-one.js
+++ libs/blocks/merch/three-in-one.js
@@ -82,8 +82,7 @@ export const handle3in1IFrameEvents = ({ data: msgData }) => {
       iframe?.classList.remove('loading');
       threeInOne.querySelector('sp-theme')?.remove();
       if (closeBtn) {
-        closeBtn.setAttribute('aria-hidden', 'true');
-        closeBtn.style.opacity = '0';
+        closeBtn.remove();
       }
       break;
     case MSG_SUBTYPE.EXTERNAL:
