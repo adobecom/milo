fa8fcfe4f0d86c194cceb72050ccc2d89f375913
markpadbe <markp@adobe.com>
2025-06-12 16:42:01 -0700

    Move loc placeholders to promise

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1422,6 +1422,8 @@ async function checkForPageMods() {
   } = Object.fromEntries(PAGE_URL.searchParams);
   let targetInteractionPromise = null;
   let countryIPPromise = null;
+  let mphPromise = null;
+
   let calculatedTimeout = null;
 
   if (mepParam === 'off') return;
@@ -1445,6 +1447,11 @@ async function checkForPageMods() {
       const { getAkamaiCode } = await import('../features/georoutingv2/georoutingv2.js');
       countryIPPromise = getAkamaiCode(true);
     }
+  }
+  if (mph) {
+    const { getMepPlaceHolders } = await import('../features/personalization/personalization.js');
+    mphPromise = getMepPlaceHolders(mph);
+    console.log('mph', mph);
   }
   const enablePersV2 = enablePersonalizationV2();
   if ((target || xlg) && enablePersV2) {
@@ -1484,6 +1491,7 @@ async function checkForPageMods() {
     mepButton,
     pzn,
     mph,
+    mphPromise,
     pznroc,
     promo,
     target,
