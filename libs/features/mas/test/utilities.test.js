d36e164747111cd52087ce20946acf7a1c3addcf
Mariia Lukianets <mariia.lukianets@gmail.com>
2025-06-13 12:07:06 +0200

    add more descriptive unit test

--- libs/features/mas/test/utilities.test.js
+++ libs/features/mas/test/utilities.test.js
@@ -55,4 +55,27 @@ describe('function "selectWcsOffers"', () => {
         expect(offers[0].language).to.equal('MULT');
         expect(offers[0].term).to.equal('12');
     });
+
+    it('selects offers without term first, then for GB selects EN offers if present', () => {
+      const offers = selectOffers(
+          [
+            { offerId: '1', language: 'MULT', term: '12' }, 
+            { offerId: '2', language: 'EN', term: '12' },
+            { offerId: '3', language: 'MULT' },
+            { offerId: '4', language: 'EN'}],
+          { country: 'GB' }
+      );
+      expect(offers[0].offerId).to.equal('4'); // best match - EN, without term
+  });
+
+  it('selects offers without term first, then for GB selects MULT offers if EN offer is not present', () => {
+    const offers = selectOffers(
+        [
+          { offerId: '1', language: 'MULT', term: '12' }, 
+          { offerId: '2', language: 'EN', term: '12' },
+          { offerId: '3', language: 'MULT' }],
+        { country: 'GB' }
+    );
+    expect(offers[0].offerId).to.equal('3'); // second best match - MULT, without term
+});
 });
