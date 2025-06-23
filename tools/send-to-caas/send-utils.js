e3c82dbd2ed066aef351f43e15e7522c21de83e7
Sheridan Sunier <sunier@Sheridans-MacBook-Pro.local>
2025-06-23 11:22:27 -0400

    MWPW-74582: carlos revsion

--- tools/send-to-caas/send-utils.js
+++ tools/send-to-caas/send-utils.js
@@ -362,8 +362,9 @@ const parseCardMetadata = () => {
 };
 
 function checkCtaUrl(s, options, i) {
-  if ((s?.trim() === '' || s === undefined) && i > 1) return '';
-  const url = (s?.trim() !== '' && s !== undefined) ? s : (options.prodUrl || window.location.origin + window.location.pathname);
+  if (s?.trim() === '') return '';
+  const url = s || options.prodUrl || window.location.origin + window.location.pathname;
+  if (url.includes('/tools/send-to-caas/bulkpublisher.html')) return '';
   return checkUrl(url, `Invalid Cta${i}Url: ${url}`);
 }
 
