ecc04668979c0c13a809f9cdc831ab9134c83299
Narcis Radu <github@narcisradu.ro>
2025-06-26 15:11:40 +0300

    Revert "MWPW-174987: Change DOM order of elements based on viewport (#4438)"
    This reverts commit 9d02d5391a7aa2c26c46724840c9e47a4b7c73b1.

--- test/blocks/hero-marquee/hero-marquee.test.js
+++ test/blocks/hero-marquee/hero-marquee.test.js
@@ -3,7 +3,6 @@ import { expect } from '@esm-bundle/chai';
 import { stub } from 'sinon';
 import { waitForElement } from '../../helpers/waitfor.js';
 import { setConfig } from '../../../libs/utils/utils.js';
-import { getViewportOrder } from '../../../libs/blocks/hero-marquee/hero-marquee.js';
 
 window.lana = { log: stub() };
 
@@ -35,28 +34,5 @@ describe('Hero Marquee', () => {
   it('supports authorable horizontal rules', async () => {
     const hr = await waitForElement('.has-divider');
     expect(hr).to.exist;
-  });
-  it('sorts con-block elements based on order class and viewport', async () => {
-    const orderMarquee = document.querySelector('#hero-order');
-    const orderCopy = orderMarquee.querySelector('.copy');
-    const mobileOrder = [...orderCopy.children];
-    const tabletOrder = getViewportOrder('tablet', orderCopy, mobileOrder);
-    const desktopOrder = getViewportOrder('desktop', orderCopy, tabletOrder);
-    expect(tabletOrder[0].classList.contains('main-copy')).to.be.true;
-    expect(desktopOrder[0].classList.contains('main-copy')).to.be.true;
-    tabletOrder.splice(0, 1);
-    desktopOrder.splice(0, 1);
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < tabletOrder.length; i++) {
-      expect(tabletOrder[i].classList.contains(`order-${i}-tablet`)).to.be.true;
-      expect(desktopOrder[i].classList.contains(`order-${i}-desktop`)).to.be.true;
-    }
-  });
-  it('order of con-blocks is the same as mobile if there is no order class', async () => {
-    const nonOrderMarquee = document.querySelector('#hero-all');
-    const nonOrderCopy = nonOrderMarquee.querySelector('.copy');
-    const mobileOrder = [...nonOrderCopy.children];
-    const tabletOrder = getViewportOrder('tablet', nonOrderCopy, mobileOrder);
-    for (const [index, el] of tabletOrder.entries()) expect(el === mobileOrder[index]).to.be.true;
   });
 });
