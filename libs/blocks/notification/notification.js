bd7e80601248b603625f2a5abb8739261528838b
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-06-30 12:33:14 +0200

    [MWPW-171875] - notification code optimization

--- libs/blocks/notification/notification.js
+++ libs/blocks/notification/notification.js
@@ -167,13 +167,6 @@ function curtainCallback(el) {
   el.insertAdjacentElement('afterend', curtain);
   el.setAttribute('role', 'dialog');
 
-  document.body.appendChild(createTag('div', {
-    class: 'notification-visibility-hidden',
-    'aria-live': 'polite',
-    role: 'status',
-    tabindex: '-1',
-  }, ''));
-
   const focusableElements = [...el.querySelectorAll(
     'button, a[href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
   )];
@@ -344,4 +337,11 @@ export default async function init(el) {
     if (el.matches(`.${pill}`)) addTooltip(el);
     decorateMultiViewport(el);
   }
+
+  document.body.appendChild(createTag('div', {
+    class: 'notification-visibility-hidden',
+    'aria-live': 'polite',
+    role: 'status',
+    tabindex: '-1',
+  }, ''));
 }
