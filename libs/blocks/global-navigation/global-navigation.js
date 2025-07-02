ebec07d4879d6a2b44d86d8cb0f7b1c30dd05dea
Prince Patel <prpatel0949@gmail.com>
2025-07-02 12:19:21 +0530

    switched between portrait and landscape without refresh the gap is seen

--- libs/blocks/global-navigation/global-navigation.js
+++ libs/blocks/global-navigation/global-navigation.js
@@ -1190,6 +1190,7 @@ class Gnav {
         header.style.top = 0;
         localNav.style.top = promoHeight;
       }
+      if (!isDesktop.matches) this.updatePopupPosition();
     };
 
     if (this.elements.aside.clientHeight > fedsPromoWrapper.clientHeight) {
