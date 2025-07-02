f10a59a966745addddd17b7a13b566c7c2c0baaa
cod23684 <cod23684@adobe.com>
2025-07-02 10:45:08 +0200

    Nala: add await for card locator

--- nala/features/mas/ccd/mini/mini.test.js
+++ nala/features/mas/ccd/mini/mini.test.js
@@ -25,7 +25,7 @@ test.describe('CCD Mini Cards Feature', () => {
 
       await test.step('2. Verify CCD Mini Card content', async () => {
         const cardLocator = miniCard.getCard(data.fragment);
-        await expect(cardLocator).toBeVisible();
+        await expect(await cardLocator).toBeVisible();
         await cardLocator.evaluate((card) => card.checkReady());
 
         const title = await cardLocator.evaluate((card) => card.title);
