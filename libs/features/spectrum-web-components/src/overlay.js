d34b88d55df9d4bd73a5f8707e680861fb011856
astatescu <angelostatescu.adobe@gmail.com>
2025-07-08 11:55:29 +0300

    fix redundant export

--- libs/features/spectrum-web-components/src/overlay.js
+++ libs/features/spectrum-web-components/src/overlay.js
@@ -2,4 +2,4 @@ import '@spectrum-web-components/overlay/sp-overlay.js';
 import '@spectrum-web-components/overlay/overlay-trigger.js';
 import { SlottableRequestEvent } from '@spectrum-web-components/overlay/src/slottable-request-event.js';
 
-export { Overlay, SlottableRequestEvent };
+export { SlottableRequestEvent };
