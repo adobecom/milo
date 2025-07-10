78b033349b2144327e9959ed3ff4354c94cd5b25
Okan Sahin <osahin@adobe.com>
2025-07-10 09:38:56 +0200

    Pull in https://github.com/adobe/da-nx/pull/14

--- .github/workflows/import/index.js
+++ .github/workflows/import/index.js
@@ -200,7 +200,7 @@ async function importUrl(aemPath, importedMedia) {
     return;
   }
 
-  const isExt = EXTS.some((ext) => href.endsWith(`.${ext}`));
+  const isExt = EXTS.some((ext) => pathname.endsWith(`.${ext}`));
   const path = href.endsWith('/') ? `${pathname}index` : pathname;
   const srcPath = isExt ? path : `${path}.md`;
   url.destPath = isExt ? path : `${path}.html`;
