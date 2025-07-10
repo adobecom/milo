2b65af0873cef9f6990425cfa6cd32a2a0470009
Nadiia Sokolova <cod41013@adobe.com>
2025-07-10 11:05:36 +0200

    added block-group test

---
+++ nala/blocks/block-group/block-group.page.js
@@ -0,0 +1,9 @@
+export default class BlockGroup {
+  constructor (page) {
+    this.page = page;
+    
+    // block group types locators
+    this.start = this.page.locator('.block-group-start');
+    this.end = this.page.locator('.block-group-end');
+  }
+}
