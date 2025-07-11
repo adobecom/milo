6cf82014aac86e13d91be335cea715ee1ee6be11
Nadiia Sokolova <cod41013@adobe.com>
2025-07-11 13:09:36 +0200

    removed a comment

--- nala/blocks/block-group/block-group.page.js
+++ nala/blocks/block-group/block-group.page.js
@@ -2,7 +2,6 @@ export default class BlockGroup {
   constructor(page) {
     this.page = page;
 
-    // block group types locators
     this.start = this.page.locator('.block-group-start');
     this.end = this.page.locator('.block-group-end');
   }
