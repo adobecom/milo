7c2e8ad462bd952a110d9d8e6dc13e95f3955eb0
Mira <myrosvas@gmail.com>
2025-07-17 15:02:35 +0200

    added merch-modal.md, enableModalOpeningOnPageLoad for autoblock, fixes for prevHash

--- libs/blocks/merch-card-collection-autoblock/merch-card-collection-autoblock.js
+++ libs/blocks/merch-card-collection-autoblock/merch-card-collection-autoblock.js
@@ -4,6 +4,7 @@ import {
   getOptions,
   MEP_SELECTOR,
   overrideOptions,
+  updateModalState,
   loadMasComponent,
   MAS_MERCH_CARD,
   MAS_MERCH_QUANTITY_SELECT,
@@ -162,6 +163,14 @@ function enableAnalytics(el) {
   });
 }
 
+export const enableModalOpeningOnPageLoad = () => {
+  window.addEventListener('mas:ready', ({ target }) => {
+    target.querySelectorAll('[is="checkout-link"][data-modal-id]').forEach((cta) => {
+      updateModalState({ cta });
+    });
+  });
+};
+
 export async function createCollection(el, options) {
   const aemFragment = createTag('aem-fragment', { fragment: options.fragment });
   // Get MEP overrides if available
@@ -198,6 +207,7 @@ export async function createCollection(el, options) {
 export default async function init(el) {
   let options = { ...DEFAULT_OPTIONS, ...getOptions(el) };
   if (!options.fragment) return;
+  enableModalOpeningOnPageLoad();
   options = overrideOptions(options.fragment, options);
   await loadDependencies(options);
   await createCollection(el, options);
