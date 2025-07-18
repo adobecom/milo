81b2e1a8ea53e281b27ecc15e58ea4a94cd8d96b
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-07-18 17:19:32 +0200

    [MWPW-171041] - notification rename function

--- libs/utils/notification.js
+++ libs/utils/notification.js
@@ -1,4 +1,4 @@
-export default function returnNotificationFocusableString(className) {
+export default function returnFocusableElementsString(className) {
   return className ? `${className} button,${className} a[href],${className} [tabindex]:not([tabindex="-1"])`
     : 'button, a[href], [tabindex]:not([tabindex="-1"])';
 }
