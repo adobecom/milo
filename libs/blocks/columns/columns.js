6c55f8cca1e8897d3bd977c6c73b8892ea20cf30
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-07-25 13:14:19 +0200

    [MWPW-174763] - column table a11y attributes added

--- libs/blocks/columns/columns.js
+++ libs/blocks/columns/columns.js
@@ -3,17 +3,35 @@ export default function init(el) {
   const isTable = el.classList.contains('table');
   rows.forEach((row, rdx) => {
     row.classList.add('row', `row-${rdx + 1}`);
+    if (isTable) row.setAttribute('role', 'row');
     const cols = row.querySelectorAll(':scope > div');
     cols.forEach((col, cdx) => {
       col.classList.add('col', `col-${cdx + 1}`);
       if (!isTable) return;
-      if (!rdx && !cdx && !col.innerText) col.classList.add('empty-table-heading');
+
+      if (!rdx && !cdx && !col.innerText) {
+        col.classList.add('empty-table-heading');
+        col.setAttribute('role', 'columnheader');
+        return;
+      }
+
+      if (!rdx) {
+        col.setAttribute('role', 'columnheader');
+        return;
+      }
+
       const isTableRowTitle = !cdx && col.childNodes.length === 1 && col.childNodes[0].tagName === 'STRONG';
-      if (isTableRowTitle) col.classList.add('row-title');
+      if (isTableRowTitle) {
+        col.classList.add('row-title');
+        col.setAttribute('role', 'rowheader');
+      } else {
+        col.setAttribute('role', 'cell');
+      }
     });
   });
   if (isTable) {
     el.classList.add('columns-table');
     el.classList.remove('table');
+    el.setAttribute('role', 'table');
   }
 }
