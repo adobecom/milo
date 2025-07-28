2e779b9fa5116b6a4fdb8b72f2044538ed48c508
Mariia Lukianets <mariia.lukianets@gmail.com>
2025-07-28 17:06:57 +0200

    analytics fixes v1

--- libs/blocks/merch/autoblock.js
+++ libs/blocks/merch/autoblock.js
@@ -35,9 +35,54 @@ export async function decorateCardCtasWithA11y(element, self) {
   });
 }
 
+export function handleCustomAnalyticsEvent(eventName, element) {
+  let daaLhValue = '';
+  let daaLhElement = element.closest('[daa-lh]');
+  while (daaLhElement) {
+    if (daaLhValue) {
+      daaLhValue = `|${daaLhValue}`;
+    }
+    const daaLhAttrValue = daaLhElement.getAttribute('daa-lh');
+    daaLhValue = `${daaLhAttrValue}${daaLhValue}`;
+    daaLhElement = daaLhElement.parentElement.closest('[daa-lh]');
+  }
+  if (daaLhValue) {
+    console.log('daaLhValue: ', `${eventName}|${daaLhValue}`);
+    // eslint-disable-next-line no-underscore-dangle
+    window._satellite?.track('event', {
+      xdm: {},
+      data: { web: { webInteraction: { name: `${eventName}|${daaLhValue}` } } },
+    });
+  }
+}
+
+function enableCardAnalytics(el, self) {
+  const cards = self ? [el] : el.querySelectorAll('merch-card');
+  cards.forEach(async (card) => {
+    await card.checkReady();
+    card.setAttribute('name', card.getAttribute('daa-lh'));
+    card.removeAttribute('daa-lh');
+    card.querySelectorAll('a[daa-ll]').forEach((anchor) => {
+      const ll = anchor.getAttribute('daa-ll');
+      anchor.setAttribute('daa-ll', `${ll.slice(0, ll.indexOf('|'))}|${card.name}--card`);
+    });
+    card.querySelectorAll('merch-addon').forEach((ao) => {
+      ao.addEventListener('change', (aoe) => {
+        handleCustomAnalyticsEvent(`addon-${aoe.detail.checked ? 'checked' : 'unchecked'}|${card.name}--card`, aoe.target);
+      });
+    });
+    card.querySelectorAll('merch-quantity-select').forEach((qs) => {
+      qs.addEventListener('merch-quantity-selector:change', (qse) => {
+        handleCustomAnalyticsEvent(`quantity-${qse.detail.option}|${card.name}--card`, qse.target);
+      });
+    });
+  });
+}
+
 export function postProcessAutoblock(autoblockEl, self = false) {
   decorateLinks(autoblockEl);
   localizePreviewLinks(autoblockEl);
   autoblockEl.querySelectorAll('.modal.link-block').forEach((blockEl) => loadBlock(blockEl));
   decorateCardCtasWithA11y(autoblockEl, self);
+  enableCardAnalytics(autoblockEl, self);
 }
