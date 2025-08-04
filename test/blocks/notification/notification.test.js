b0699d275b8a59a8595aa744cc048a051254f993
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-08-04 14:02:54 +0200

    [MWPW-176948] - notification tests optimization

--- test/blocks/notification/notification.test.js
+++ test/blocks/notification/notification.test.js
@@ -156,33 +156,22 @@ describe('notification', async () => {
   });
 
   describe('sticky notifications accessibility attributes', () => {
-    let stickyTopNotification;
-    let stickyBottomNotification;
-    let stickyNoHeadingNotification;
-
-    beforeEach(() => {
-      const stickyTopSection = document.querySelector('.section.sticky-top');
-      const stickyBottomSection = document.querySelector('.section.sticky-bottom');
-      const stickyNoHeadingSections = document.querySelectorAll('.section.sticky-top');
-
-      stickyTopNotification = stickyTopSection?.querySelector('.notification');
-      stickyBottomNotification = stickyBottomSection?.querySelector('.notification');
-      stickyNoHeadingNotification = stickyNoHeadingSections[1]?.querySelector('.notification');
-    });
-
     it('adds role="region" and proper aria-label to sticky-top notifications with heading', async () => {
+      const stickyTopNotification = document.querySelector('.section.sticky-top')?.querySelector('.notification');
       await delay(350);
       expect(stickyTopNotification.getAttribute('role')).to.equal('region');
       expect(stickyTopNotification.getAttribute('aria-label')).to.equal('Test Sticky Top Notification');
     });
 
     it('adds role="region" and proper aria-label to sticky-bottom notifications with heading', async () => {
+      const stickyBottomNotification = document.querySelector('.section.sticky-bottom')?.querySelector('.notification');
       await delay(350);
       expect(stickyBottomNotification.getAttribute('role')).to.equal('region');
       expect(stickyBottomNotification.getAttribute('aria-label')).to.equal('Test Sticky Bottom Notification');
     });
 
     it('adds role="region" and default aria-label to sticky notifications without heading', async () => {
+      const stickyNoHeadingNotification = document.querySelectorAll('.section.sticky-top')[1]?.querySelector('.notification');
       await delay(350);
       expect(stickyNoHeadingNotification.getAttribute('role')).to.equal('region');
       expect(stickyNoHeadingNotification.getAttribute('aria-label')).to.equal('Promotional Banner Top');
