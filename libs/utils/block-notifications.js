8f86692a9e6bd83bb267099d44c27381fc7c6801
Ryan Clayton <rclayton@adobe.com>
2025-08-04 17:54:32 -0600

    Update xlsx path and add serviceConfig

--- libs/utils/block-notifications.js
+++ libs/utils/block-notifications.js
@@ -1,6 +1,9 @@
+import getServiceConfig from './service-config.js';
+
 const INFO_ICON = `<svg id="info" viewBox="0 0 18 18" class="icon-milo icon-milo-info">
     <path fill="currentcolor" d="M10.075,6A1.075,1.075,0,1,1,9,4.925H9A1.075,1.075,0,0,1,10.075,6Zm.09173,6H10V8.2A.20005.20005,0,0,0,9.8,8H7.83324S7.25,8.01612,7.25,8.5c0,.48365.58325.5.58325.5H8v3H7.83325s-.58325.01612-.58325.5c0,.48365.58325.5.58325.5h2.3335s.58325-.01635.58325-.5C10.75,12.01612,10.16673,12,10.16673,12ZM9,.5A8.5,8.5,0,1,0,17.5,9,8.5,8.5,0,0,0,9,.5ZM9,15.6748A6.67481,6.67481,0,1,1,15.67484,9,6.67481,6.67481,0,0,1,9,15.6748Z"></path>
   </svg>`;
+const CONSONANT_SHEET_FALLBACK = 'consonant.json';
 
 async function getJson(url) {
   try {
@@ -54,8 +57,9 @@ function decorateAlertControls(element) {
 }
 
 export default async function blockNotifications(base) {
-  const url = `${base}/consonant-test.json`;
-  const json = await getJson(url);
+  const { blocknotifications } = await getServiceConfig(window.location.origin);
+  const url = blocknotifications.url || CONSONANT_SHEET_FALLBACK;
+  const json = await getJson(`${base}/${url}`);
   if (!json) return;
 
   const { blocks: rawBlocks } = json;
@@ -67,14 +71,14 @@ export default async function blockNotifications(base) {
 
   const blockQueries = blocksKeyToLowercase.data.reduce((acc, row) => {
     const notConsonant = row.consonant === 'false';
-    const decision = row.decision !== '' ? row.decision.toLowerCase() === 'obsolete' || 'delist' || 'delisted' : undefined;
+    const decision = row.decision !== '' ? row.decision?.toLowerCase() === 'obsolete' || 'delist' || 'delisted' : undefined;
     if (row.block && (notConsonant || decision)) {
       const variation = row.variant && row.variant.replaceAll(',', '.').replaceAll(' ', '-').toLowerCase();
       const blockName = row.block.toLowerCase();
       const documentation = row.documentation !== '' ? row.documentation : undefined;
       const name = variation ? `${blockName} ${variation}` : `${blockName}`;
       const queryProps = { selector: variation ? `.${blockName}.${variation}` : `.${blockName}` };
-      if (decision) queryProps.decision = row.decision.toLowerCase();
+      if (decision) queryProps.decision = row.decision?.toLowerCase();
       if (notConsonant) queryProps.notConsonant = 'Not Consonant';
       if (documentation) queryProps.documentation = row.documentation;
       if (name) queryProps.name = name;
@@ -124,7 +128,7 @@ export default async function blockNotifications(base) {
         conLabel.classList.add('con-label');
         conLabel.textContent = 'Not consonant';
         conLabel.title = `${query.name} is not a consonant block`;
-        blockLabelContainer.appendChild(conLabel);
+        blockLabelContainer?.appendChild(conLabel);
       }
     });
   });
