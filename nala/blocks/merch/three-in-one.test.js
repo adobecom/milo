471dcc0e37e1df0ad95e5bafcccda80df11b8178
cod23684 <cod23684@adobe.com>
2025-08-04 16:07:24 +0200

    update 3in1 modal opening

--- nala/blocks/merch/three-in-one.test.js
+++ nala/blocks/merch/three-in-one.test.js
@@ -4,6 +4,10 @@ import ThreeInOne from './three-in-one.page.js';
 
 const miloLibs = process.env.MILO_LIBS || '';
 
+async function openModal(cta) {
+  await expect(cta).not.toHaveClass(/loading-entitlements|placeholder-pending/);
+  await cta.click();
+}
 test.describe('ThreeInOne Block test suite', () => {
   test.beforeEach(async ({ page, browserName }) => {
     if (browserName === 'chromium') {
@@ -30,7 +34,7 @@ test.describe('ThreeInOne Block test suite', () => {
 
     await test.step('Validate if modal reopens on back navigation', async () => {
       const cta = await page.locator('[data-wcs-osi="ByqyQ6QmyXhzAOnjIcfHcoF1l6nfkeLgbzWz-aeM8GQ"][data-checkout-workflow-step="segmentation"]');
-      await cta.click();
+      await openModal(cta);
       await page.waitForSelector('.dialog-modal');
       const modal = threeInOne.getModal();
       expect(modal).toBeVisible();
@@ -55,7 +59,7 @@ test.describe('ThreeInOne Block test suite', () => {
         for (const [key, value] of Object.entries(attributes)) {
           await expect(cta).toHaveAttribute(key, value);
         }
-        await cta.click();
+        await openModal(cta);
         const modal = threeInOne.getModal();
         const iframe = await modal.locator('iframe');
         await expect(iframe).toHaveAttribute('src', iframeSrc);
@@ -76,7 +80,7 @@ test.describe('ThreeInOne Block test suite', () => {
       for (const [key, value] of Object.entries(attributes)) {
         await expect(cta).toHaveAttribute(key, value);
       }
-      await cta.click();
+      await openModal(cta);
       const modal = threeInOne.getModal();
       const iframe = await modal.locator('iframe');
       await expect(iframe).toHaveAttribute('src', iframeSrc);
@@ -95,7 +99,7 @@ test.describe('ThreeInOne Block test suite', () => {
       for (const [key, value] of Object.entries(attributes)) {
         await expect(cta).toHaveAttribute(key, value);
       }
-      await cta.click();
+      await openModal(cta);
       const modal = threeInOne.getModal();
       const iframe = await modal.locator('iframe');
       await expect(iframe).toHaveAttribute('src', iframeSrc);
@@ -119,11 +123,7 @@ test.describe('ThreeInOne Block test suite', () => {
         await expect(cta).toHaveAttribute(key, value);
       }
       await cta.waitFor({ state: 'visible' });
-
-      // Wait for entitlements to load
-      await expect(cta).not.toHaveClass(/loading-entitlements|placeholder-pending/);
-
-      await cta.click();
+      await openModal(cta);
       const modal = threeInOne.getModal();
       await page.waitForSelector('.dialog-modal');
       expect(modal).toBeVisible();
@@ -144,11 +144,7 @@ test.describe('ThreeInOne Block test suite', () => {
         await expect(cta).toHaveAttribute(key, value);
       }
       await cta.waitFor({ state: 'visible' });
-
-      // Wait for entitlements to load
-      await expect(cta).not.toHaveClass(/loading-entitlements|placeholder-pending/);
-
-      await cta.click();
+      await openModal(cta);
       const modal = threeInOne.getModal();
       await page.waitForSelector('.dialog-modal');
       expect(modal).toBeVisible();
