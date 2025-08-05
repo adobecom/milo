33a672bc573933ca57582675051cea61793854e5
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-08-05 15:33:49 +0200

    [MWPW-174763] - columns table code optimization

--- libs/blocks/columns/columns.js
+++ libs/blocks/columns/columns.js
@@ -7,8 +7,7 @@ const isRowHeader = (cols, col, cdx, rows) => {
     .filter((rowCol) => getMeaningfulChildren(rowCol)
       .some((child) => child.tagName?.match(/^(H[1-6]|STRONG)$/)));
 
-  const firstRow = rows[0];
-  const firstRowCols = firstRow.querySelectorAll(':scope > div');
+  const firstRowCols = rows[0].querySelectorAll(':scope > div');
   const correspondingFirstRowCol = firstRowCols[cdx];
   const hasColumnHeaderInFirstRow = correspondingFirstRowCol?.getAttribute('role') === 'columnheader';
 
@@ -22,46 +21,52 @@ const isColumnHeader = (rdx, containsHTag, meaningfulChildren) => (
     && !meaningfulChildren.some((child) => child.classList?.contains('image-link'))) || containsHTag
 );
 
-const applyTableRow = (row) => { if (!row.getAttribute('role')) row.setAttribute('role', 'row'); };
-
-const applyAccessibilityAttributes = ({
-  col, rdx, cols, cdx, rows, el, row,
-}) => {
-  const meaningfulChildren = getMeaningfulChildren(col);
-  const containsHTag = meaningfulChildren[0]?.tagName?.match(/^H[1-6]$/);
-  if (containsHTag) meaningfulChildren[0].setAttribute('role', 'paragraph');
-
-  if (rdx > 0 && !cdx && isRowHeader(cols, col, cdx, rows)) {
-    col.classList.add('row-title');
-    col.setAttribute('role', 'rowheader');
-    applyTableRow(row);
-    return;
-  }
-
-  if (isColumnHeader(rdx, containsHTag, meaningfulChildren)) {
-    col.setAttribute('role', 'columnheader');
-    applyTableRow(row);
-    return;
-  }
-
-  if (!col.innerText.trim()) return;
-  if (!el.getAttribute('role')) el.setAttribute('role', 'table');
-  col.setAttribute('role', 'cell');
-  applyTableRow(row);
+const setElsRole = (els) => {
+  els.forEach(({ el, role }) => {
+    if (el.getAttribute('role')) return;
+    el.setAttribute('role', role);
+  });
 };
 
 const applyCellIfRowColHasText = (cols, col) => {
   if (Array.from(cols).some((otherCol) => otherCol.innerText.trim())) col.setAttribute('role', 'cell');
 };
 
+const applyAccessibilityAttributes = ({
+  col, rdx, cols, cdx, rows, row, el,
+}) => {
+  if (!rdx && !cdx && !col.innerText) {
+    col.classList.add('empty-table-heading');
+    applyCellIfRowColHasText(cols, col);
+    return;
+  }
+
+  if (!col.innerText.trim()) applyCellIfRowColHasText(cols, col);
+
+  const meaningfulChildren = getMeaningfulChildren(col);
+  const containsHTag = meaningfulChildren[0]?.tagName?.match(/^H[1-6]$/);
+  if (containsHTag) meaningfulChildren[0].setAttribute('role', 'paragraph');
+
+  if (rdx > 0 && !cdx && isRowHeader(cols, col, cdx, rows)) {
+    col.classList.add('row-title');
+    setElsRole([{ el: col, role: 'rowheader' }, { el: row, role: 'row' }]);
+    return;
+  }
+
+  if (isColumnHeader(rdx, containsHTag, meaningfulChildren)) {
+    setElsRole([{ el: col, role: 'columnheader' }, { el: row, role: 'row' }]);
+    return;
+  }
+
+  if (!col.innerText.trim()) return;
+  setElsRole([{ el, role: 'table' }, { el: col, role: 'cell' }, { el: row, role: 'row' }]);
+};
+
 export default function init(el) {
   const rows = el.querySelectorAll(':scope > div');
   const isTable = el.classList.contains('table');
 
-  if (isTable) {
-    el.classList.add('columns-table');
-    el.classList.remove('table');
-  }
+  if (isTable) el.classList.replace('table', 'columns-table');
 
   rows.forEach((row, rdx) => {
     row.classList.add('row', `row-${rdx + 1}`);
@@ -70,13 +75,6 @@ export default function init(el) {
     cols.forEach((col, cdx) => {
       col.classList.add('col', `col-${cdx + 1}`);
       if (!isTable) return;
-      if (!rdx && !cdx && !col.innerText) {
-        col.classList.add('empty-table-heading');
-        applyCellIfRowColHasText(cols, col);
-        return;
-      }
-
-      if (!col.innerText.trim()) applyCellIfRowColHasText(cols, col);
 
       applyAccessibilityAttributes({
         col, rdx, cols, cdx, rows, el, row,
