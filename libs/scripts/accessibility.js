3c8063eb1cbfbca08b723abf7d870b7989ebe76a
Ratko Zagorac <ratkozagorac@gmail.com>
2025-08-12 11:38:13 +0200

    Add tests and remove code

--- libs/scripts/accessibility.js
+++ libs/scripts/accessibility.js
@@ -97,17 +97,6 @@ export const setDialogAndElementAttributes = ({ element, title }) => {
   element.closest('.dialog-modal')?.setAttribute('aria-label', title);
 };
 
-export function setListRoles() {
-  const selector = ':is(.section[class*="-up"]:has(> :is(.action-item, .icon-block)), .scroller):not(:has(h1, h2, h3, h4, h5, h6))';
-  document.querySelectorAll(selector).forEach((parent) => {
-    parent.setAttribute('role', 'list');
-    [...parent.children].forEach((child) => {
-      if (child.classList.contains('section-metadata')) return;
-      child.setAttribute('role', 'listitem');
-    });
-  });
-}
-
 export default function init() {
   scrollTabFocusedElIntoView();
 }
