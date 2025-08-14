64c09f1783b8b60db7034c10aa3f901354c01af0
Axel Cureno Basurto <axel@adobe.com>
2025-08-14 07:00:52 -0700

    Update collection container width from 90vw to fit-content in merch-card styles

--- libs/features/mas/src/variants/plans.css.js
+++ libs/features/mas/src/variants/plans.css.js
@@ -281,7 +281,7 @@ merch-card-collection:has([slot="subtitle"]) merch-card {
     }
 
     .collection-container.plans:has(merch-sidenav) {
-        width: 90vw;
+        width: fit-content;
         position: relative;
         left: 50%;
         transform: translateX(-50vw);
