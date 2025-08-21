3fb91f4f0253cffe7d3785a019c73ea1ed48ef3c
Vivian A Goodrich <vgoodric@adobe.com>
2025-08-21 12:54:46 -0600

    Add MEP event handling and associated tests
    - Introduced a new event.js file for managing event-related functionality, including user authentication and data fetching from Rainfocus.
    - Updated personalization logic to include event registration checks.
    - Modified loadMepAddons to support the new event-id addon.
    - Added comprehensive tests for the new event functionality, covering various scenarios including consent checks and user login status.

---
+++ libs/features/mep/addons/event.js
@@ -0,0 +1,52 @@
+import { isSignedOut, getConfig, getMepEnablement } from '../../../utils/utils.js';
+
+export function imsReady({ interval = 200, maxAttempts = 25 } = {}) {
+  return new Promise((resolve) => {
+    let count = 0;
+    function checkIms() {
+      if (window.adobeIMS?.initialized || count > maxAttempts) resolve();
+      setTimeout(checkIms, interval);
+      count += 1;
+    }
+    checkIms();
+  });
+}
+function getCookie(key) {
+  const cookie = document.cookie.split(';')
+    .map((x) => decodeURIComponent(x.trim()).split(/=(.*)/s))
+    .find(([k]) => k === key);
+  return cookie ? cookie[1] : null;
+}
+async function getUserId() {
+  if (isSignedOut() && getMepEnablement('signedin') !== true) return false;
+  const isImsReady = await imsReady({ interval: 100, maxAttempts: 25 });
+  if (!isImsReady) return false;
+  const { userId } = await window.adobeIMS.getProfile();
+  return userId;
+}
+async function fetchFromRainfocus(eventId) {
+  const userId = await getUserId();
+  if (!userId) return {};
+
+  const accessToken = window.adobeIMS.getAccessToken()?.token;
+  if (!accessToken) return {};
+
+  const domainSuffix = getConfig()?.env?.name === 'prod' ? '' : '.stage';
+  const url = `https://www${domainSuffix}.adobe.com/events/api/rf-auth-seq-generic/${eventId}?user_id=${encodeURIComponent(userId)}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${accessToken}` },
+    credentials: 'same-origin',
+  });
+  if (!response.ok) return {};
+  return response.json();
+}
+export default async function init(eventId) {
+  if (eventId === true) return eventId;
+
+  const consentCookieValue = getCookie('OptanonConsent');
+  if (consentCookieValue?.includes('C0002:0')) return false;
+
+  const eventDetails = await fetchFromRainfocus(eventId);
+  return eventDetails?.isRegistered === true;
+}
