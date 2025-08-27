1c8fcb5195eba1a672e64accbad7013790e456a2
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2025-08-26 17:02:06 -0700

    update webkit worker count

--- playwright.config.js
+++ playwright.config.js
@@ -92,7 +92,7 @@ const config = {
       use: {
         ...devices['Desktop Safari'],
         userAgent: USER_AGENT_DESKTOP,
-        workers: 6,
+        workers: 5,
       },
     },
     /* MAS test */
