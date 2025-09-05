403dc0b0f4df9d6e27c1915a259f056f630217e4
Megan Thomas <methomas@adobe.com>
2025-09-05 15:50:29 -0700

    remove prompt suggestion border

--- libs/blocks/brand-concierge/chat-ui-config.js
+++ libs/blocks/brand-concierge/chat-ui-config.js
@@ -162,6 +162,7 @@ export default {
     '--product-card-title-font-weight': '700',
     '--prompt-suggestion-button-background': '#F8F8F8',
     '--prompt-suggestion-button-border-radius': '10px',
+    '--prompt-suggestion-button-border-width': '0',
     '--prompt-suggestion-button-gap': '8px',
     '--prompt-suggestion-button-padding': '8px 16px 8px 8px',
     '--prompt-suggestion-button-text-color': '#292929',
