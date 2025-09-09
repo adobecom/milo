1357b91f4ec1dbcfac244f15d4ddb0331fc4357c
Sean Choi <seanchoi@adobe.com>
2025-09-09 16:19:25 -0600

    Update test/features/placeholders/placeholders.test.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/features/placeholders/placeholders.test.js
+++ test/features/placeholders/placeholders.test.js
@@ -108,7 +108,7 @@ describe('Placeholders', () => {
 
     it('Returns config.locale.contentRoot when no placeholders-content-root metadata', () => {
       const testConfig = {
-        locale: {contentRoot: '/test/content/root'
+        locale: { contentRoot: '/test/content/root'
         },
       };
       const result = getPlaceholderRoot(testConfig);
