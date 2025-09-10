4c575a168231509530fd3f8974d58c2f21845f43
Sabya <sabyasachi.exe@gmail.com>
2025-09-10 13:42:41 -0700

    Added adobe branding

--- libs/blocks/graybox-promote/components/graybox-promote-lit.js
+++ libs/blocks/graybox-promote/components/graybox-promote-lit.js
@@ -483,7 +483,21 @@ class GrayboxPromote extends LitElement {
     return html`
       <div class="wrapper">
         <div class="page-header">
-          <h1>Graybox Promote Tool</h1>
+          <h1>
+            <svg width="32" height="28" viewBox="0 0 91 80" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 12px; vertical-align: middle;">
+              <g clip-path="url(#clip0_906_1839)">
+                <path d="M56.9686 0H90.4318V80L56.9686 0Z" fill="#EB1000"/>
+                <path d="M33.4632 0H0V80L33.4632 0Z" fill="#EB1000"/>
+                <path d="M45.1821 29.4668L66.5199 80.0002H52.5657L46.1982 63.9461H30.6182L45.1821 29.4668Z" fill="#EB1000"/>
+              </g>
+              <defs>
+                <clipPath id="clip0_906_1839">
+                  <rect width="90.4318" height="80" fill="white"/>
+                </clipPath>
+              </defs>
+            </svg>
+            Graybox Promote & Bulk Copy Tool
+          </h1>
         </div>
         <div class="tabs">
           <div class="tab-buttons">
