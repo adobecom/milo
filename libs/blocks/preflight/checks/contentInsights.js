bcf3919f96df4420439698d94678ff2a7e73da27
Raghu A <125877471+raga-adbe-gh@users.noreply.github.com>
2025-09-16 19:11:29 +0530

    Update contentInsights.js

--- libs/blocks/preflight/checks/contentInsights.js
+++ libs/blocks/preflight/checks/contentInsights.js
@@ -1,7 +1,6 @@
-console.log('---------------------');
-import { runGeneralChecks } from 'https://studio-preflight-import--milo--adobecom.aem.live/libs/blocks/preflight/panels/general.js';
-import { openAllModals, checkImageDimensions } from 'https://studio-preflight-import--milo--adobecom.aem.live/libs/blocks/preflight/checks/assets.js';
-import { checkAlt } from 'https://studio-preflight-import--milo--adobecom.aem.live/libs/blocks/preflight/accessibility/audit-image-alt-text.js';
+import { runGeneralChecks } from '../panels/general.js';
+import { openAllModals, checkImageDimensions } from './assets.js';
+import { checkAlt } from '../accessibility/audit-image-alt-text.js';
 import { 
     checkLcpEl, 
     checkSingleBlock, 
@@ -11,57 +10,67 @@ import {
     checkForPersonalization, 
     checkPlaceholders, 
     checkIcons 
-} from 'https://studio-preflight-import--milo--adobecom.aem.live/libs/blocks/preflight/checks/performance.js';
-import { checkH1s, checkTitle, checkCanon, checkDescription, checkBody, checkLorem } from 'https://studio-preflight-import--milo--adobecom.aem.live/libs/blocks/preflight/checks/seo.js';
+} from './performance.js';
+import { checkH1s, checkTitle, checkCanon, checkDescription, checkBody, checkLorem } from './seo.js';
 
-export default async function executeCheck() {
+const [ PERFORMANANCE_CHK, GENERAL_CHK, SEO_CHK, ACCESSIBILITY_CHK, ASSETS_CHK ] = ['performanance', 'general', 'seo', 'accessibility', 'assets'];
+
+export default async function executeCheck(options = {}) {
+  const { excludes = [] } = options;
   window.contentInsights = {};
-  console.log('---------2------------');
-  const perfChecks = [
-      checkLcpEl(window.location.href, document),
-      checkSingleBlock(document),
-      checkImageSize(window.location.href, document),
-      checkVideoPoster(window.location.href, document),
-      checkFragments(window.location.href, document),
-      checkForPersonalization(document),
-      checkPlaceholders(window.location.href, document),
-      checkIcons(window.location.href, document),
-  ];
-  console.log('---------3------------');
-  const perfKeys = ['a','b','c','d','e','f','g'];
-  const perfResults = await Promise.all(perfChecks);
-  window.contentInsights.performance = perfKeys.reduce((acc, key, idx) => {
-      acc[key] = perfResults[idx];
-      return acc;
-  }, {});
+  if (skipChecks.includes(PERFORMANANCE_CHK)) {
+    const perfChecks = [
+        checkLcpEl(window.location.href, document),
+        checkSingleBlock(document),
+        checkImageSize(window.location.href, document),
+        checkVideoPoster(window.location.href, document),
+        checkFragments(window.location.href, document),
+        checkForPersonalization(document),
+        checkPlaceholders(window.location.href, document),
+        checkIcons(window.location.href, document),
+    ];
+    const perfKeys = ['lcpEl','singleBlock','imageSize','videoPoster','fragments','placeholders','personalization'];
+    const perfResults = await Promise.all(perfChecks);
+    window.contentInsights.performance = perfKeys.reduce((acc, key, idx) => {
+        acc[key] = perfResults[idx];
+        return acc;
+    }, {});
+  }
   
-  window.contentInsights.general = runGeneralChecks();
-  const seoChecks = [
-      checkH1s(document),
-      checkTitle(document),
-      checkCanon(document),
-      checkDescription(document),
-      checkBody(document),
-      checkLorem(document),
-  ];
-  const keys = ['h1', 'title', 'canon', 'description', 'body', 'lorem'];
-  const results = await Promise.all(seoChecks);
-  window.contentInsights.seo = keys.reduce((acc, key, idx) => {
-      acc[key] = results[idx];
-      return acc;
-  }, {});
-  window.contentInsights.accessibility = await checkAlt();
+  if (skipChecks.includes(GENERAL_CHK)) {
+    window.contentInsights.general = runGeneralChecks();
+  }
+
+  if (skipChecks.includes(SEO_CHK)) {
+    const seoChecks = [
+        checkH1s(document),
+        checkTitle(document),
+        checkCanon(document),
+        checkDescription(document),
+        checkBody(document),
+        checkLorem(document),
+    ];
+    const keys = ['h1', 'title', 'canon', 'description', 'body', 'lorem'];
+    const results = await Promise.all(seoChecks);
+    window.contentInsights.seo = keys.reduce((acc, key, idx) => {
+        acc[key] = results[idx];
+        return acc;
+    }, {});
+    window.contentInsights.accessibility = await checkAlt();
+  }
   
-  await openAllModals(document);
-  const {
-    assetsWithMismatch: imagesWithMismatch = [],
-    assetsWithMatch: imagesWithMatch = [],
-  } = (await checkImageDimensions(window.location.href, document))?.details ?? {};
-  
-  window.contentInsights.assets = {
-    imagesWithMismatch,
-    imagesWithMatch,
-  };
+  if (skipChecks.includes(ASSETS_CHK)) {
+    await openAllModals(document);
+    const {
+      assetsWithMismatch: imagesWithMismatch = [],
+      assetsWithMatch: imagesWithMatch = [],
+    } = (await checkImageDimensions(window.location.href, document))?.details ?? {};
+    
+    window.contentInsights.assets = {
+      imagesWithMismatch,
+      imagesWithMatch,
+    };
+  }
   
   window.dispatchEvent(new CustomEvent('content-insights-complete', { detail: window.contentInsights }));
 }
