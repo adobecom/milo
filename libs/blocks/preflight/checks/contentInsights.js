acdba7e174a9278b7f3424d3ed86ad17fa961e84
Raghu A <125877471+raga-adbe-gh@users.noreply.github.com>
2025-09-16 20:37:41 +0530

    Update contentInsights.js
    Updated from skipChecks to excludes

--- libs/blocks/preflight/checks/contentInsights.js
+++ libs/blocks/preflight/checks/contentInsights.js
@@ -18,7 +18,7 @@ const [ PERFORMANANCE_CHK, GENERAL_CHK, SEO_CHK, ACCESSIBILITY_CHK, ASSETS_CHK ]
 export default async function executeCheck(options) {
   const { excludes = [] } = options || {};
   window.contentInsights = {};
-  if (!skipChecks.includes(PERFORMANANCE_CHK)) {
+  if (!excludes.includes(PERFORMANANCE_CHK)) {
     const perfChecks = [
         checkLcpEl(window.location.href, document),
         checkSingleBlock(document),
@@ -37,11 +37,11 @@ export default async function executeCheck(options) {
     }, {});
   }
   
-  if (!skipChecks.includes(GENERAL_CHK)) {
+  if (!excludes.includes(GENERAL_CHK)) {
     window.contentInsights.general = runGeneralChecks();
   }
 
-  if (!skipChecks.includes(SEO_CHK)) {
+  if (!excludes.includes(SEO_CHK)) {
     const seoChecks = [
         checkH1s(document),
         checkTitle(document),
@@ -58,11 +58,11 @@ export default async function executeCheck(options) {
     }, {});
   }
 
-  if (!skipChecks.includes(ACCESSIBILITY_CHK)) {
+  if (!excludes.includes(ACCESSIBILITY_CHK)) {
     window.contentInsights.accessibility = await checkAlt();
   }
 
-  if (!skipChecks.includes(ASSETS_CHK)) {
+  if (!excludes.includes(ASSETS_CHK)) {
     await openAllModals(document);
     const {
       assetsWithMismatch: imagesWithMismatch = [],
