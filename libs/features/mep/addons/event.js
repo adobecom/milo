1aff17821d27b664cbd80037096e6412e54e9f98
Vivian A Goodrich <vgoodric@adobe.com>
2025-09-23 04:34:30 -0600

    Enhance error handling in fetchFromRainfocus function by adding try-catch block and logging for improved debugging and reliability.

--- libs/features/mep/addons/event.js
+++ libs/features/mep/addons/event.js
@@ -29,16 +29,19 @@ async function fetchFromRainfocus(eventId) {
 
   const domainSuffix = getConfig()?.env?.name === 'prod' ? '' : '.stage';
   const url = `https://www${domainSuffix}.adobe.com/events/api/rf-auth-seq-generic/${eventId}?user_id=${encodeURIComponent(userId)}`;
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: { Authorization: `Bearer ${accessToken}` },
-    credentials: 'same-origin',
-  });
-  if (!response.ok) {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${accessToken}` },
+      credentials: 'same-origin',
+    });
+    if (response.ok) return response.json();
     window.lana?.log(`Unable to fetch from Rainfocus: ${response.statusText}`);
     return {};
+  } catch (e) {
+    window.lana?.log(`Unable to fetch from Rainfocus: ${e.toString()}`);
+    return {};
   }
-  return response.json();
 }
 export default async function init(eventId) {
   if (eventId === true) return eventId;
