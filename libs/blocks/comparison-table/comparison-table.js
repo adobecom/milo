3c8c364e02cf7efba9eecba347e738788d61afc5
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-09-25 19:50:28 +0200

    update

--- libs/blocks/comparison-table/comparison-table.js
+++ libs/blocks/comparison-table/comparison-table.js
@@ -1,14 +1,30 @@
 import { createTag } from '../../utils/utils.js';
 import { decorateButtons } from '../../utils/decorate.js';
 
-function createSubHeaderContainer(childrenArray, startIndex, endIndex) {
+function createSubHeaderContainer(childrenArray, startIndex, endIndex, isLast = false) {
   const container = createTag('div', { class: 'sub-header-item-container' });
 
   for (let i = startIndex; i < endIndex; i += 1) {
     if (childrenArray[i] && childrenArray[i].textContent.trim() !== '-') {
       container.appendChild(childrenArray[i]);
+      if (isLast) decorateButtons(childrenArray[i]);
     }
   }
+
+  if (!isLast) return container;
+
+  const actionAreaElements = container.querySelectorAll('.action-area');
+  if (actionAreaElements.length > 0) {
+    const btnContainer = createTag('div', { class: 'btn-container' });
+    actionAreaElements.forEach((element) => {
+      btnContainer.appendChild(element);
+    });
+    container.appendChild(btnContainer);
+  }
+
+  const description = container.querySelector('p:not(:has(a))');
+  if (!description) container.prepend(createTag('p', { class: 'description' }));
+  else description.classList.add('description');
 
   return container;
 }
@@ -50,6 +66,7 @@ function decorateHeader(headerContent) {
         childrenArray,
         lastContainedIndex + 1,
         childrenArray.length,
+        true,
       );
 
       if (finalSubHeaderItemContainer.children.length > 0) {
