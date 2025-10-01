3d4327d6b230ab0bcfc42b6b890e22a3544647a8
Vivian A Goodrich <vgoodric@adobe.com>
2025-10-01 11:44:58 -0600

    Refactor AMCV_COOKIE handling in lob.js to use imported constant for improved maintainability. Update getECID logic to utilize encodeURIComponent for consistency.

--- libs/martech/helpers.js
+++ libs/martech/helpers.js
@@ -1,7 +1,7 @@
 import { getMepEnablement } from '../utils/utils.js';
 
 /* eslint-disable no-underscore-dangle */
-const AMCV_COOKIE = 'AMCV_9E1005A551ED61CA0A490D45@AdobeOrg';
+export const AMCV_COOKIE = 'AMCV_9E1005A551ED61CA0A490D45@AdobeOrg';
 const KNDCTR_COOKIE_KEYS = [
   'kndctr_9E1005A551ED61CA0A490D45_AdobeOrg_identity',
   'kndctr_9E1005A551ED61CA0A490D45_AdobeOrg_cluster',
