66f1cd04507b6f92ab2339160e95bc8c8e94962e
Bozo Jovicic <bozo@hitthecode.com>
2025-10-03 11:00:59 +0200

    MWPW-172104 [Kodiak][R2]: DOMXSS in [github.com/adobecom/cc-sandbox]

---
+++ libs/utils/sanitizeHtml.js
@@ -0,0 +1,38 @@
+function stringToHTML(str) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, 'text/html');
+  return doc.body || document.createElement('body');
+}
+
+function removeScripts(html) {
+  const scripts = html.querySelectorAll('script');
+  scripts.forEach((script) => script.remove());
+}
+
+function isPossiblyDangerous(name, value) {
+  const val = value.replace(/\s+/g, '').toLowerCase();
+  if (['src', 'href', 'xlink:href'].includes(name)) {
+    // eslint-disable-next-line no-script-url
+    if (val.includes('javascript:') || val.includes('data:text/html')) return true;
+  }
+  if (name.startsWith('on')) return true;
+  return false;
+}
+
+function removeAttributes(elem) {
+  [...elem.attributes].forEach((attr) => {
+    const { name, value } = attr;
+    if (isPossiblyDangerous(name, value)) {
+      elem.removeAttribute(name);
+    }
+  });
+}
+
+function sanitizeHtml(html) {
+  const htmlEl = stringToHTML(html);
+  removeScripts(htmlEl);
+  [htmlEl, ...htmlEl.querySelectorAll('*')].forEach(removeAttributes);
+  return htmlEl.firstChild;
+}
+
+export default sanitizeHtml;
