8887d533380a37ac7ecdc61ed32aedb5320df189
Mira <myrosvas@gmail.com>
2025-10-07 15:37:13 +0300

    updated Nala

--- nala/features/mas/acom/plans/plans.page.js
+++ nala/features/mas/acom/plans/plans.page.js
@@ -118,7 +118,7 @@ export default class MasPlans {
     };
 
     // Plans students card properties:
-    this.studentsCssProp = { card: { width: '486px' } };
+    this.studentsCssProp = { card: { width: '484px' } };
   }
 
   getCard(id) {
