15f37668cf70ef68594b15413a069d0cf0a5fc0d
Bandana Laishram <bandanalaishram@gmail.com>
2025-10-16 12:33:54 +0530

    Initial commit for wayfinding

---
+++ libs/mep/wayfinding/global-navigation/features/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,120 @@
+import { getMetadata, getConfig, getFederatedUrl } from '../../../../../blocks/utils/utils.js';
+import { toFragment, lanaLog } from '../../utilities/utilities.js';
+
+const metadata = {
+  seo: 'breadcrumbs-seo',
+  seoLegacy: 'breadcrumb-seo',
+  fromFile: 'breadcrumbs-from-file',
+  showCurrent: 'breadcrumbs-show-current-page',
+  hiddenEntries: 'breadcrumbs-hidden-entries',
+  pageTitle: 'breadcrumbs-page-title',
+  base: 'breadcrumbs-base',
+  fromUrl: 'breadcrumbs-from-url',
+};
+
+const setBreadcrumbSEO = (breadcrumbs) => {
+  const seoDisabled = (getMetadata(metadata.seo) || getMetadata(metadata.seoLegacy)) === 'off';
+  if (seoDisabled || !breadcrumbs) return;
+  const breadcrumbsSEO = {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    itemListElement: [],
+  };
+
+  breadcrumbs.querySelectorAll('ul > li').forEach((item, idx, list) => {
+    const link = item.querySelector('a');
+    const name = link ? link.innerText.trim() : [...item.childNodes].filter((node) => !node.matches?.('span[aria-hidden="true"]')).map((node) => node.textContent.trim()).join('');
+    let itemUrl = link?.href;
+    if (!itemUrl && idx === list.length - 1) {
+      itemUrl = window.location.href;
+    }
+    breadcrumbsSEO.itemListElement.push({
+      '@type': 'ListItem',
+      position: idx + 1,
+      name,
+      item: itemUrl,
+    });
+  });
+  const script = toFragment`<script type="application/ld+json">${JSON.stringify(
+    breadcrumbsSEO,
+  )}</script>`;
+  document.head.append(script);
+};
+
+const createBreadcrumbs = (element) => {
+  if (!element) return null;
+  const ul = element.querySelector('ul');
+  const pageTitle = getMetadata(metadata.pageTitle);
+  if (pageTitle || getMetadata(metadata.showCurrent) === 'on') {
+    ul.append(toFragment`
+      <li>
+        ${pageTitle || document.title}
+      </li>
+    `);
+  }
+
+  const hiddenEntries = getMetadata(metadata.hiddenEntries)
+    ?.toLowerCase()
+    .split(',')
+    .map((item) => item.trim()) || [];
+
+  ul.querySelectorAll('li').forEach((li, index) => {
+    if (hiddenEntries.includes(li.innerText?.toLowerCase().trim())) li.remove();
+    if (index > 0) li.insertAdjacentHTML('afterbegin', '<span aria-hidden="true">/</span>');
+  });
+
+  const noTransform = element.classList.contains('no-transform') ? ' no-transform' : '';
+
+  const breadcrumbs = toFragment`
+    <div class="feds-breadcrumbs-wrapper">
+      <nav class="feds-breadcrumbs${noTransform}" aria-label="Breadcrumb">${ul}</nav>
+    </div>
+  `;
+  ul.querySelector('li:last-of-type')?.setAttribute('aria-current', 'page');
+  return breadcrumbs;
+};
+
+const createWithBase = async (el) => {
+  const element = el || toFragment`<div><ul></ul></div>`;
+  const url = getFederatedUrl(getMetadata(metadata.base));
+  if (!url) return null;
+  try {
+    const resp = await fetch(`${url}.plain.html`);
+    const text = await resp.text();
+    const base = new DOMParser().parseFromString(text, 'text/html').body;
+    element.querySelector('ul')?.prepend(...base.querySelectorAll('li'));
+    return createBreadcrumbs(element);
+  } catch (e) {
+    lanaLog({ e, message: 'Breadcrumbs failed fetching base', tags: 'gnav-breadcrumbs', errorType: 'i' });
+    return null;
+  }
+};
+
+const fromUrl = () => {
+  if (getMetadata(metadata.fromUrl) !== 'on') return null;
+  const list = toFragment`<ul></ul>`;
+  const paths = document.location.pathname
+    .replace((getConfig().locale?.prefix || ''), '')
+    .split('/')
+    .filter((n) => n);
+
+  for (let i = 0; i < paths.length; i += 1) {
+    list.append(toFragment`
+      <li>
+        <a href="/${paths.slice(0, i + 1).join('/')}">${paths[i].replaceAll('-', ' ')}</a>
+      </li>
+    `);
+  }
+  return createBreadcrumbs(toFragment`<div>${list}</div>`);
+};
+
+export default async function init(el) {
+  try {
+    const breadcrumbsEl = await createWithBase(el) || createBreadcrumbs(el) || fromUrl();
+    setBreadcrumbSEO(breadcrumbsEl);
+    return breadcrumbsEl;
+  } catch (e) {
+    lanaLog({ e, message: 'Breadcrumbs failed rendering', tags: 'gnav-breadcrumbs', errorType: 'e' });
+    return null;
+  }
+}
---
+++ libs/mep/wayfinding/global-navigation/utilities/keyboard/mainNav.js
@@ -0,0 +1,187 @@
+/* eslint-disable class-methods-use-this */
+import { selectors, getNextVisibleItemPosition, getPreviousVisibleItemPosition } from './utils.js';
+import Popup from './popup.js';
+import MobilePopup from './mobilePopup.js';
+import { closeAllDropdowns, trigger, logErrorFor } from '../utilities.js';
+
+class MainNavItem {
+  constructor() {
+    this.desktop = window.matchMedia('(min-width: 900px)');
+    this.popup = new Popup({ mainNav: this });
+    this.mobilePopup = new MobilePopup({ mainNav: this });
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.querySelector(selectors.globalNavTag)?.addEventListener('keydown', (e) => logErrorFor(() => {
+      if (!e.target.closest(selectors.globalNav)) return;
+
+      if (!e.target.closest(selectors.fedsNav) || e.target.closest(selectors.popup)) {
+        return;
+      }
+
+      const newNav = !!document.querySelector('header.new-nav');
+
+      switch (e.code) {
+        case 'Tab': {
+          if (newNav) {
+            const activePopup = document.querySelector(selectors.activePopup);
+            if (!activePopup) e.preventDefault();
+            const items = [...document.querySelectorAll(`${selectors.mainMenuItems}, ${selectors.mainMenuLinks}, ${selectors.mainNavToggle}`)];
+            const current = items.findIndex((x) => x === e.target);
+            if (current > -1) {
+              const next = current < items.length - 1 ? current + 1 : 0;
+              const prev = current > 0 ? current - 1 : items.length - 1;
+              if (e.shiftKey) items[prev].focus();
+              else items[next].focus();
+            } else items?.[0]?.focus();
+            break;
+          }
+
+          if (e.shiftKey) {
+            const { prev, openTrigger } = this.getState();
+            if (openTrigger) {
+              if (prev === -1) {
+                closeAllDropdowns();
+              } else {
+                e.preventDefault();
+                this.focusPrev({ focus: 'last' });
+              }
+            }
+          }
+          break;
+        }
+        case 'Escape': {
+          closeAllDropdowns();
+          const activePopup = document.querySelector(selectors.activePopup);
+          if (newNav && !activePopup) {
+            const toggle = document.querySelector('header.new-nav .feds-toggle');
+            toggle?.click();
+            toggle?.focus();
+          }
+          break;
+        }
+        case 'ArrowLeft': {
+          if (newNav) break;
+          const { next, prev } = this.getState();
+          if (document.dir !== 'rtl') {
+            if (prev === -1) break;
+            this.focusPrev({ focus: null });
+          } else {
+            if (next === -1) break;
+            this.focusNext({ focus: null });
+          }
+          break;
+        }
+        case 'ArrowUp': {
+          if (newNav) break;
+          e.preventDefault();
+          e.stopPropagation();
+          this.focusPrev({ focus: 'last' });
+          break;
+        }
+        case 'ArrowRight': {
+          if (newNav) break;
+          const { next, prev, openTrigger } = this.getState();
+          if (document.dir !== 'rtl') {
+            if (next === -1) break;
+            this.focusNext();
+          } else {
+            if (prev === -1) break;
+            this.focusPrev({ focus: null });
+          }
+          if (openTrigger) {
+            this.open();
+          }
+          break;
+        }
+        case 'ArrowDown': {
+          if (newNav) break;
+          e.stopPropagation();
+          e.preventDefault();
+          const { items, curr } = this.getState();
+          if (items[curr] && items[curr].hasAttribute('aria-haspopup')) {
+            this.open({ focus: 'first' });
+            return;
+          }
+          this.focusNext();
+          break;
+        }
+        default:
+          break;
+      }
+    }, `mainNav key failed ${e.code}`, 'gnav-keyboard', 'e'));
+  }
+
+  getState = () => {
+    const items = [...document.querySelectorAll(selectors.mainNavItems)];
+    const openTrigger = document.querySelector(selectors.expandedPopupTrigger);
+    const currentEl = document.activeElement
+      .closest(selectors.navItem)
+      ?.querySelector(selectors.mainNavItems);
+    const curr = items.findIndex((el) => el === currentEl);
+    return {
+      items,
+      curr,
+      prev: getPreviousVisibleItemPosition(curr, items),
+      next: getNextVisibleItemPosition(curr, items),
+      openTrigger,
+    };
+  };
+
+  focusCurr = () => {
+    const { items, curr } = this.getState();
+    items[curr].focus();
+  };
+
+  focusPrev = ({ focus } = {}) => {
+    const { items, prev } = this.getState();
+    const open = document.querySelector(selectors.expandedPopupTrigger);
+    closeAllDropdowns();
+    if (prev === -1) return;
+    items[prev].focus();
+    if (open) {
+      this.open({ focus });
+    }
+  };
+
+  focusNext = () => {
+    const { items, next } = this.getState();
+    if (next === -1) return;
+    closeAllDropdowns();
+    items[next].focus();
+  };
+
+  open = ({ focus, triggerEl, e } = {}) => {
+    const { items, curr } = this.getState();
+    const triggerElement = triggerEl || items[curr];
+    if (!triggerElement || !triggerElement.hasAttribute('aria-haspopup')) return;
+    if (e) e.preventDefault();
+    if (triggerElement.getAttribute('aria-expanded') === 'false') {
+      trigger({ element: triggerElement });
+    }
+    const navItem = triggerElement.parentElement;
+    const popupEl = navItem.querySelector(selectors.popup);
+    if (popupEl) {
+      if (this.desktop.matches) {
+        this.popup.open({ focus });
+      } else {
+        this.mobilePopup.open({ focus });
+      }
+      return;
+    }
+
+    // We need to wait for the popup to be added to the DOM before we can open it.
+    const observer = new MutationObserver(() => {
+      observer.disconnect();
+      if (this.desktop.matches) {
+        this.popup.open({ focus });
+      } else {
+        this.mobilePopup.open({ focus });
+      }
+    });
+    observer.observe(navItem, { childList: true });
+  };
+}
+
+export default MainNavItem;
