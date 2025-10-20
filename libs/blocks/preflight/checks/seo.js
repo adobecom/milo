fdd193e645f141a852231624f371994e2779ea81
Aishwarya Mathuria <mathuria@adobe.com>
2025-10-20 16:21:07 +0530

    Enhance link validation checks in seo.js
    Added extra checks for URL schemes to prevent misidentification by Spidy.

--- libs/blocks/preflight/checks/seo.js
+++ libs/blocks/preflight/checks/seo.js
@@ -255,6 +255,7 @@ export async function validLinkFilter(area = document, envName = null) {
     .filter((link) => {
       if (
         link.href
+        // Added extra checks because Spidy misidentifies these URL schemes as faulty. Can be removed once we stop using Spidy.
         && !link.href.includes('tel:')
         && !link.href.includes('mailto:')
         && !link.href.startsWith('#')
