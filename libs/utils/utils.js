b528be8c1ece5ff8e257149c5282db0b7210f3af
Nishant Thakur <nishant.thakur.ece@gmail.com>
2025-10-24 18:24:48 +0530

    MWPW-182796: Fixed Icons in excluded blocks still being decorated (#5025)
    * MWPW-182796: improve icon filtering logic in decorateIcons
    
    * Moved updated logic within max25theme condition for isolation
    
    ---------
    
    Co-authored-by: Nishant Kumar Thakur <nthakur@adobe.com>

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1127,12 +1127,19 @@ async function decorateHeader() {
 }
 
 async function decorateIcons(area, config) {
-  const icons = area.querySelectorAll('span.icon');
+  let icons = area.querySelectorAll('span.icon');
   if (icons.length === 0) return;
   const { base, iconsExcludeBlocks } = config;
   if (iconsExcludeBlocks) {
-    const excludedIconsCount = [...icons].filter((icon) => iconsExcludeBlocks.some((block) => icon.closest(`div.${block}`))).length;
-    if (excludedIconsCount === icons.length) return;
+    if (getMetadata('theme') === 'max25') {
+      // TODO: Remove after correcting core logic
+      const includeIcons = [...icons].filter((icon) => !iconsExcludeBlocks.some((block) => icon.closest(`div.${block}`)));
+      if (!includeIcons.length) return;
+      icons = includeIcons;
+    } else {
+      const excludedIconsCount = [...icons].filter((icon) => iconsExcludeBlocks.some((block) => icon.closest(`div.${block}`))).length;
+      if (excludedIconsCount === icons.length) return;
+    }
   }
   loadStyle(`${base}/features/icons/icons.css`);
   const { default: loadIcons } = await import('../features/icons/icons.js');
