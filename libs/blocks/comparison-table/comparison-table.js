f29131bdd97d525b4d67d74fae7cf13d5194e0ce
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-10-27 15:47:32 +0100

    update

--- libs/blocks/comparison-table/comparison-table.js
+++ libs/blocks/comparison-table/comparison-table.js
@@ -143,7 +143,7 @@ function addLastContainerElements(container) {
     actionAreaElements.forEach((element) => btnContainer.appendChild(element));
     container.appendChild(btnContainer);
   }
-  const description = container.querySelector('p:not(:has(a))');
+  const description = container.querySelector('p:not(.action-area)');
   if (!description) container.prepend(createTag('p', { class: 'description' }));
   else description.classList.add('description');
 }
