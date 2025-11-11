0f319524795d68768762513da0dd02472a77e26a
Blaine Gunn <Blainegunn@gmail.com>
2025-11-11 13:48:03 -0700

    Delete libs/deps/htm-preact-debug.js

--- libs/deps/htm-preact-debug.js
+++
@@ -1,1 +0,0 @@
-import"preact/debug";import{h as e}from"preact";export{Component,createContext,createRef,h,render}from"preact";export{signal}from"@preact/signals";export{useCallback,useContext,useDebugValue,useEffect,useErrorBoundary,useId,useImperativeHandle,useLayoutEffect,useMemo,useReducer,useRef,useState}from"preact/hooks";import r from"htm";const t=r.bind(e);export{t as html};
