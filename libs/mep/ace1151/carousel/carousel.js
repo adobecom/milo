35c7cfbb18d16ae8a4b986356848c6c7fa9b8048
Denys Fedotov <dfedotov@Adobe.local>
2025-11-11 13:39:17 -0700

    fixed paths

--- libs/mep/ace1151/carousel/carousel.js
+++ libs/mep/ace1151/carousel/carousel.js
@@ -1,6 +1,6 @@
-import { createTag, getConfig, MILO_EVENTS } from '../../utils/utils.js';
-import { decorateAnchorVideo, syncPausePlayIcon } from '../../utils/decorate.js';
-import { debounce } from '../../utils/action.js';
+import { createTag, getConfig, MILO_EVENTS } from '../../../utils/utils.js';
+import { decorateAnchorVideo, syncPausePlayIcon } from '../../../utils/decorate.js';
+import { debounce } from '../../../utils/action.js';
 
 const { miloLibs, codeRoot } = getConfig();
 const isMobile = window.innerWidth < 900;
