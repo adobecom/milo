9fa07d12f0c9a1e3c613fcd25a21328128ccad99
Nicolas Peltier <1032754+npeltier@users.noreply.github.com>
2025-11-13 22:45:15 +0100

    MWPW-183666 don't seek tag labels in placeholders (#5116)
    * MWPW-183666 don't seek tag labels in placeholders
    
    added tagLabels section
    also if nothing is here,don't fail
    
    * final fix :)
    
    * upper case first letter
    
    * consider search might not be here

--- libs/features/mas/src/merch-card-collection.js
+++ libs/features/mas/src/merch-card-collection.js
@@ -344,7 +344,8 @@ export class MerchCardCollection extends LitElement {
                     // Example: "mas:types/web" -> "web"
                     // TODO: Get tag label from fragment instead of parsing the tag
                     const parsedTag = tag.split('/').pop();
-                    const tagLabel = fragment.placeholders.tags[parsedTag] || parsedTag;
+                    let tagLabel = fragment.settings?.tagLabels?.[parsedTag] || parsedTag;
+                    tagLabel = tagLabel.startsWith('coll-tag-filter') ? parsedTag.charAt(0).toUpperCase() + parsedTag.slice(1) : tagLabel;
                     return { name: parsedTag, label: tagLabel };
                   })
                 }
