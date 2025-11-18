d26343f76e2d4e70dc76a247070f0c88d783d8a0
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2025-11-18 18:41:16 +0100

    [MWPW-181657] - notification sticky closure

--- libs/blocks/notification/notification.js
+++ libs/blocks/notification/notification.js
@@ -385,8 +385,13 @@ function setStickyAccessibilityAttributes(el) {
   if (!section) return;
 
   const checkAndSetAttributes = () => {
-    const sticky = section.classList.contains('sticky-top') || section.classList.contains('sticky-bottom');
-    if (!sticky) return false;
+    const isSticky = section.classList.contains('sticky-top') || section.classList.contains('sticky-bottom');
+    if (!isSticky) return false;
+
+    if (el.classList.contains('no-closure') && el.matches(`:is(.${pill}, .${ribbon})`)) {
+      el.classList.remove('no-closure');
+      if (!el.querySelector('.close')) decorateClose(el);
+    }
 
     el.setAttribute('aria-label', getHeadingText(el)
        || (section.classList.contains('sticky-bottom') ? 'Promotional Banner Bottom' : 'Promotional Banner Top'));
