a5a6835406c5aa233982b5e948f2b74f91669be5
Bozo Jovicic <37440641+bozojovicic@users.noreply.github.com>
2025-12-02 15:57:11 +0100

    MWPW-174046 Plans Cards - adjust for long badge with mnemonics (#5114)
    * MWPW-174046 Plans Cards - adjust for long badge with mnemonics
    
    * Trigger Build
    
    * MWPW-174046 Plans Cards - adjust for long badge with mnemonics
    
    * Trigger Build
    
    * Trigger Build
    
    * Build mas
    
    * Build mas
    
    * Build mas
    
    * Build mas
    
    * Trigger Build
    
    ---------
    
    Co-authored-by: Bozo Jovicic <bozo@hitthecode.com>

--- libs/deps/mas/merch-card-collection.js
+++ libs/deps/mas/merch-card-collection.js
@@ -959,6 +959,10 @@ merch-card[variant^="plans"] {
     width: var(--consonant-merch-card-plans-width);
 }
 
+merch-card[variant^="plans"] merch-badge {
+    max-width: calc(var(--consonant-merch-card-plans-width) * var(--merch-badge-card-size) - var(--merch-badge-with-offset) * 40px - var(--merch-badge-offset) * 48px);
+}
+
 merch-card[variant="plans-students"] {
     width: var(--consonant-merch-card-plans-students-width);
 }
@@ -1288,6 +1292,10 @@ merch-card-collection:has([slot="subtitle"]) merch-card {
 
     merch-card[variant="plans-education"] .spacer {
         height: 0px;
+    }
+
+    merch-card[variant^="plans"] merch-badge {
+        max-width: calc(var(--consonant-merch-card-plans-width) - var(--merch-badge-with-offset) * 40px - var(--merch-badge-offset) * 48px);
     }
 }
 
