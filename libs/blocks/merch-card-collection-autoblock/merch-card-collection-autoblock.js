99823af80f793ccb087d037fb21b9ce6554bad72
Bozo Jovicic <bozo@hitthecode.com>
2025-12-02 14:45:01 +0100

    MWPW-184444 [M@S Catalog] Gnav link is not deep-linked to right Catalog filter

--- libs/blocks/merch-card-collection-autoblock/merch-card-collection-autoblock.js
+++ libs/blocks/merch-card-collection-autoblock/merch-card-collection-autoblock.js
@@ -131,7 +131,8 @@ function getSidenav(collection) {
   /* Filters */
   const spSidenav = createTag('sp-sidenav', { manageTabIndex: true });
   spSidenav.setAttribute('manageTabIndex', true);
-  const sidenavList = createTag('merch-sidenav-list', { deeplink: 'category' }, spSidenav);
+  const deeplink = collection.variant === 'catalog' ? 'category' : 'filter';
+  const sidenavList = createTag('merch-sidenav-list', { deeplink }, spSidenav);
 
   sidenavList.updateComplete.then(() => {
     sidenavList.querySelector('sp-sidenav')?.setAttribute('role', 'tablist');
