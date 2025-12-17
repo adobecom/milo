abfe00c207451f5d20f7e2162874714fea556451
Aishwarya Mathuria <mathuria@adobe.com>
2025-12-17 15:06:57 +0530

    Update stream-preflight.js

--- libs/templates/stream-preflight/stream-preflight.js
+++ libs/templates/stream-preflight/stream-preflight.js
@@ -1,8 +1,15 @@
 export default async function init() {
-  document.querySelector('iframe').classList.add('preflight-iframe');
+  const iframe = document.querySelector('iframe').classList.add('preflight-iframe');
   const url = new URL(window.location.href);
   const preflightUrl = url.searchParams.get('preflightUrl');
   if (preflightUrl) document.querySelector('iframe').src = preflightUrl;
+  setTimeout(() => {
+    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    const script = iframeDoc.createElement('script');
+    script.textContent = `
+    console.log('⚠️ mathuria: Injected JS running inside iframe');
+    `;
+  }, 1000);
 }
 
 (async () => {
