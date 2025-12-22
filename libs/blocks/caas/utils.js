58c1ca49cd18886a3e519fc700f707b5029c4af5
Carlos Miqueo <cmiqueo@adobe.com>
2025-12-22 09:31:38 -0800

    fixes linting errors

--- libs/blocks/caas/utils.js
+++ libs/blocks/caas/utils.js
@@ -843,11 +843,11 @@ export const getConfig = async (originalState, strs = {}) => {
 
   const langFirst = state.langFirst ? `&langFirst=${state.langFirst}` : '';
 
-  const navigationStyle = state.container === 'carousel' 
-    && state.paginationAnimationStyle.includes('Modern') 
-    && state.useLightControls 
-      ? `${state.paginationAnimationStyle}-light` 
-      : state.paginationAnimationStyle;
+  const navigationStyle = state.container === 'carousel'
+    && state.paginationAnimationStyle.includes('Modern')
+    && state.useLightControls
+    ? `${state.paginationAnimationStyle}-light`
+    : state.paginationAnimationStyle;
 
   const config = {
     collection: {
@@ -855,7 +855,7 @@ export const getConfig = async (originalState, strs = {}) => {
       layout: {
         type: state.layoutType,
         gutter: state.gutter,
-        container:  state.container,
+        container: state.container,
       },
       button: { style: state.collectionBtnStyle },
       resultsPerPage: state.resultsPerPage,
