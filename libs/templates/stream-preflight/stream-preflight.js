8e1e27f10232895739fabc36b323c5bfc1fe3b0b
Aishwarya Mathuria <mathuria@adobe.com>
2026-01-07 12:44:36 +0530

    Refactor iframe initialization and preflight trigger

--- libs/templates/stream-preflight/stream-preflight.js
+++ libs/templates/stream-preflight/stream-preflight.js
@@ -1,18 +1,25 @@
-export default async function init() {
+function initializeIframe() {
   const iframe = document.querySelector('iframe');
   iframe.classList.add('preflight-iframe');
   iframe.setAttribute('id', 'preflight-iframe')
   const preflightUrl = new URL(window.location.href).searchParams.get('url');
-  if (!preflightUrl) return;
   document.querySelector('iframe').src = preflightUrl;
-  setTimeout(() => {
+}
+
+async function triggerPreflight() {
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     const script = iframeDoc.createElement('script');
     script.textContent = `
-    console.log('⚠️ mathuria: Injected JS running inside iframe');
+      console.log('⚠️ mathuria: Injected JS running inside iframe');
     `;
     iframeDoc.head.appendChild(script);
-  }, 1000);
+}
+
+export default async function init() {
+  const preflightUrl = initializeIframe();
+  setTimeout(() => {
+    await triggerPreflight();
+  }, 6000);
 }
 
 (async () => {
