e36433ef6a536886c9f02c641f3b808f52f001a4
Sheridan Sunier <sunier@Sheridans-MacBook-Pro.local>
2026-01-07 14:21:29 -0800

    omg please let this be the last thing

--- test/blocks/caas/utils.test.js
+++ test/blocks/caas/utils.test.js
@@ -904,7 +904,7 @@ describe('getFloodgateCaasConfig', () => {
         collectionButtonStyle: 'primary',
         resultsPerPage: 5,
         endpoint:
-          'https://www.adobe.com/chimera-api/collection/myTargetActivity.json?originSelection=hawks&contentTypeTags=&secondSource=&secondaryTags=&collectionTags=&excludeContentWithTags=&language=en&country=us&complexQuery=((%22caas%3Aproducts%2Findesign%22%2BAND%2B%22caas%3Aproducts%2Freader%22)%2BAND%2B(%22caas%3Acountry%2Fbr%22%2BOR%2B%22caas%3Acountry%2Fca%22))%2BAND%2B((%22caas%3Acontent-type%2Fvideo%22%2BAND%2B%22caas%3Acontent-type%2Fblog%22))&excludeIds=&currentEntityId=&featuredCards=c94ec235-50c2-595e-9fa8-0b4602c08712%2Ce9d71f5e-e7c9-5d6d-89e9-2ffdad17b8bd&environment=&draft=true&size=10&flatFile=false',
+          'https://www.adobe.com/chimera-api/collection/myTargetActivity.json?originSelection=hawks&contentTypeTags=&secondSource=&secondaryTags=&collectionTags=&excludeContentWithTags=&language=en&country=us&complexQuery=((%22caas%3Aproducts%2Findesign%22%2BAND%2B%22caas%3Aproducts%2Freader%22)%2BAND%2B(%22caas%3Acountry%2Fbr%22%2BOR%2B%22caas%3Acountry%2Fca%22))%2BAND%2B((%22caas%3Acontent-type%2Fvideo%22%2BAND%2B%22caas%3Acontent-type%2Fblog%22))&excludeIds=&currentEntityId=&featuredCards=b6aa23a7-f6bf-51f4-a2b6-0a93fc31bd16%2Ce9d71f5e-e7c9-5d6d-89e9-2ffdad17b8bd&environment=&draft=true&size=10&flatFile=false',
         fallbackEndpoint: '',
         hideDateInterval: false,
         totalCardsToShow: 10,
@@ -943,7 +943,7 @@ describe('getFloodgateCaasConfig', () => {
         disableBanners: false,
         reservoir: { sample: 3, pool: 1000 },
       },
-      featuredCards: ['c94ec235-50c2-595e-9fa8-0b4602c08712', 'e9d71f5e-e7c9-5d6d-89e9-2ffdad17b8bd'],
+      featuredCards: ['b6aa23a7-f6bf-51f4-a2b6-0a93fc31bd16', 'e9d71f5e-e7c9-5d6d-89e9-2ffdad17b8bd'],
       headers: [['X-Adobe-Floodgate', 'pink']],
       hideCtaIds: [''],
       hideCtaTags: [],
