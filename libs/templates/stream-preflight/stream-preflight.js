fca68cb9dcf77932e428e0c9b38fd3864f0aaf1b
Aishwarya Mathuria <mathuria@adobe.com>
2026-01-07 13:13:03 +0530

    Update stream-preflight.js

--- libs/templates/stream-preflight/stream-preflight.js
+++ libs/templates/stream-preflight/stream-preflight.js
@@ -11,15 +11,15 @@ async function triggerPreflight() {
     const iframe = document.querySelector('iframe');
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     const script = iframeDoc.createElement('script');
+    const BRANCH = host.split('--')[0];
+    const SLD = host.includes('.aem.live') ? 'aem.live' : 'aem.page';
     script.textContent = `
       console.log('⚠️ mathuria: Injected JS running inside iframe');
       const { host } = window.location;
-      const branch = host.split('--')[0];
-      const sld = host.includes('.aem.live') ? 'aem.live' : 'aem.page';
-      const { getConfig, createTag, loadBlock } = await import(\`https://${branch}--milo--adobecom.${sld}/libs/utils/utils.js\`);
+      const { getConfig, createTag, loadBlock } = await import('https://${BRANCH}--milo--adobecom.${SLD}/libs/utils/utils.js');
       const preflight = createTag('div', {class: 'preflight'});
       const content = await loadBlock(preflight);
-      const { getModal } = await import(\`https://${branch}--milo--adobecom..${sld}/libs/blocks/modal/modal.js\`);
+      const { getModal } = await import('https://${BRANCH}--milo--adobecom..${SLD}/libs/blocks/modal/modal.js');
       getModal(null, { id: 'preflight', content, closeEvent: 'closeModal' });
     `;
     iframeDoc.head.appendChild(script);
