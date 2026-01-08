6bd0a13e8d6675ada4e295aea637fd1f5c05d448
Aishwarya Mathuria <mathuria@adobe.com>
2026-01-08 15:17:59 +0530

    Update stream-sidekick.js

--- libs/templates/stream-sidekick/stream-sidekick.js
+++ libs/templates/stream-sidekick/stream-sidekick.js
@@ -1,1 +1,18 @@
-//content
+export default async function init() {
+  if (window.opener) {
+  window.opener.postMessage(
+      {
+        status: "success",
+        data: "Sidekick login successful"
+      },
+      window.location.origin,
+    );
+  }
+  setTimeout( () => {
+    window.close()
+  }, 1000);
+}
+
+(async () => {
+  await init();
+})();
