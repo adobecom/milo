f323653da0a6bb49ed4b4f66cf155268b5a61685
Megan Thomas <methomas@adobe.com>
2026-01-07 17:44:34 -0800

    move section metadata in template, still issues

--- libs/templates/featured-story/featured-story.js
+++ libs/templates/featured-story/featured-story.js
@@ -9,20 +9,19 @@ export default function init() {
   const sections = document.querySelectorAll('body > main > div.section');
   if (!sections.length > i + 1) return;
   const section = sections[i];
-  const hasSectionMetadata = section.querySelector('.section-metadata') !== null;
+  const sectionMetadata = section.querySelector('.section-metadata');
   const nextSection = section.nextElementSibling;
   if (!nextSection) return;
   section.classList.add('section-1');
-  section.classList.remove('section');
   nextSection.classList.add('section-2');
-  nextSection.classList.remove('section');
   const featuredSection = createTag('div', {
-    class: `section featured-story-wrapper${hasSectionMetadata ? ' has-section-metadata' : ''}`,
+    class: `section featured-story-wrapper${sectionMetadata ? ' has-section-metadata' : ''}`,
     template: 'featured-story',
   });
   section.insertAdjacentElement('afterend', featuredSection);
   featuredSection.insertAdjacentElement('afterbegin', section);
   featuredSection.insertAdjacentElement('beforeend', nextSection);
+  if (sectionMetadata) featuredSection.append(sectionMetadata);
 }
 
 init();
