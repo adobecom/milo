469063cb05fbe59c5fa2afc4b1452d14f5f82330
cmiqueo <64917520+cmiqueo@users.noreply.github.com>
2026-01-13 17:28:43 +0100

    MWPW-185469: [Milo] UX update: Carousel Navigation Controls (#5272)
    * Adds option to caas configurator to center the video play button
    
    * removes console log message
    
    * removes console log message
    
    * updates tests
    
    * updates tests - part II
    
    * updates tests - part III
    
    * Initial test commit
    
    * updates carousel width
    
    * MWPW-185469: Adds new CasS caousel controls options
    
    * Updates css
    
    * Removes unused element
    
    * fixes linting errors

--- libs/blocks/caas/utils.js
+++ libs/blocks/caas/utils.js
@@ -861,6 +861,12 @@ export const getConfig = async (originalState, strs = {}) => {
   const isLingoActive = await getLingoActive();
   const langFirst = state.langFirst ? `&langFirst=${isLingoActive}` : '';
 
+  const navigationStyle = state.container === 'carousel'
+    && state.paginationAnimationStyle.includes('Modern')
+    && state.useLightControls
+    ? `${state.paginationAnimationStyle}-light`
+    : state.paginationAnimationStyle;
+
   const config = {
     collection: {
       mode: state.theme,
@@ -1004,7 +1010,7 @@ export const getConfig = async (originalState, strs = {}) => {
       options: getSortOptions(state, strs),
     },
     pagination: {
-      animationStyle: state.paginationAnimationStyle,
+      animationStyle: navigationStyle,
       enabled: state.paginationEnabled,
       resultsQuantityShown: state.paginationQuantityShown,
       loadMoreButton: {
@@ -1069,7 +1075,6 @@ export const getConfig = async (originalState, strs = {}) => {
     linkTransformer: pageConfig.caasLinkTransformer || stageMapToCaasTransforms(pageConfig),
     headers: caasRequestHeaders,
   };
-
   return config;
 };
 
