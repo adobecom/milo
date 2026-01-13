a502062d06d4b65dd7cd691eaf2d10d0b3381953
Nishant Thakur <nishant.thakur.ece@gmail.com>
2026-01-13 21:57:48 +0530

    MWPW-185835 Fix project-specific blocks loading in library with "aem" fallback (#5289)
    * MWPW-185835 Fix project-specific blocks loading in library
    
    * MWPW-185835 Fix UTs

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -169,7 +169,7 @@ const LANGUAGE_BASED_PATHS = [
   'news.adobe.com',
 ];
 const DEFAULT_LANG = 'en';
-export const SLD = PAGE_URL.hostname.includes('.aem.') ? 'aem' : 'hlx';
+export const SLD = PAGE_URL.hostname.includes('.hlx.') ? 'hlx' : 'aem';
 
 const PROMO_PARAM = 'promo';
 let isMartechLoaded = false;
