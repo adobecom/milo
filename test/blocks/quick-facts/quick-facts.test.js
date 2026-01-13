cdc09bd312fd8b889f25ff54c90adafba5494327
Megan Thomas <methomas@adobe.com>
2026-01-12 16:33:20 -0800

    cr - rewrite test

--- test/blocks/quick-facts/quick-facts.test.js
+++ test/blocks/quick-facts/quick-facts.test.js
@@ -14,48 +14,62 @@ const { default: init } = await import('../../../libs/blocks/quick-facts/quick-f
 describe('quick-facts', () => {
   const quickFactsBlocks = document.querySelectorAll('.quick-facts');
 
-  [...quickFactsBlocks].forEach((block, index) => {
+  describe('default', () => {
+    const block = quickFactsBlocks[0];
+
     before(() => init(block));
 
-    describe(`${index === 0 ? 'with logo' : 'without logo'}`, () => {
-      if (index === 0) {
-        it('has a logo', () => {
-          const logo = block.querySelector('.logo');
-          expect(logo).to.exist;
-        });
-      } else {
-        it('does not have a logo ', () => {
-          const logo = block.querySelector('.logo');
-          expect(logo).to.not.exist;
-        });
-      }
+    it('has a logo', () => {
+      const logo = quickFactsBlocks[0].querySelector('.logo');
+      expect(logo).to.exist;
+    });
 
-      it('has a fact list', () => {
-        const factList = block.querySelector('.fact-list');
-        expect(factList).to.exist;
-      });
+    it('has a fact list', () => {
+      const factList = block.querySelector('.fact-list');
+      expect(factList).to.exist;
+    });
 
-      it('has a product list', () => {
-        const productList = block.querySelector('.product-list');
-        expect(productList).to.exist;
-      });
+    it('has a product list', () => {
+      const productList = block.querySelector('.product-list');
+      expect(productList).to.exist;
+    });
 
-      it('decorates fact list headings', () => {
-        const headings = block.querySelector('.fact-list').querySelectorAll('h1, h2, h3, h4, h5, h6');
-        headings.forEach((heading) => {
-          expect(heading.className.includes('heading-')).to.be.true;
-        });
+    it('decorates fact list headings', () => {
+      const headings = block.querySelector('.fact-list').querySelectorAll('h1, h2, h3, h4, h5, h6');
+      headings.forEach((heading) => {
+        expect(heading.className.includes('heading-')).to.be.true;
       });
+    });
 
-      it('decorates product list subheading', () => {
-        const heading = block.querySelector('.product-list').querySelector('h1, h2, h3, h4, h5, h6');
-        expect(heading.className.includes('subheading-')).to.be.true;
-      });
+    it('decorates product list subheading', () => {
+      const heading = block.querySelector('.product-list').querySelector('h1, h2, h3, h4, h5, h6');
+      expect(heading.className.includes('subheading-')).to.be.true;
+    });
 
-      it('decorates body text', () => {
-        const bodyText = block.querySelector('[class*="body-"]');
-        expect(bodyText).to.exist;
-      });
+    it('decorates body text', () => {
+      const bodyText = block.querySelector('[class*="body-"]');
+      expect(bodyText).to.exist;
+    });
+  });
+
+  describe('without logo', () => {
+    const block = quickFactsBlocks[1];
+
+    before(() => init(block));
+
+    it('does not have a logo ', () => {
+      const logo = block.querySelector('.logo');
+      expect(logo).to.not.exist;
+    });
+
+    it('has a fact list', () => {
+      const factList = block.querySelector('.fact-list');
+      expect(factList).to.exist;
+    });
+
+    it('has a product list', () => {
+      const productList = block.querySelector('.product-list');
+      expect(productList).to.exist;
     });
   });
 });
