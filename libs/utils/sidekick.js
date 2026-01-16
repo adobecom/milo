0d6cb263cdbc727fdf245dfb93cb20f0978ec4af
Amit Bikram Sarangi <amitbtalk@gmail.com>
2026-01-16 22:05:12 +0530

    ne plugin button

--- libs/utils/sidekick.js
+++ libs/utils/sidekick.js
@@ -33,6 +33,7 @@ export default function init({ createTag, loadBlock, loadScript, loadStyle }) {
   sk.addEventListener('custom:send-to-caas', debounce(sendToCaasListener, 500));
   sk.addEventListener('custom:check-schema', checkSchemaListener);
   sk.addEventListener('custom:preflight', debounce(() => preflightListener(), 500));
+  sk.addEventListener('custom:milo-animate', debounce(() => {}, 500));
 
   // Color code publish button
   stylePublish(sk);
