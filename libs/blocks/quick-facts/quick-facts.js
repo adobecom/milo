b3dc79f2901c89558dcc3f2844f78609823717d5
Megan Thomas <methomas@adobe.com>
2026-01-22 14:06:36 -0800

    add vertical center, no inset, and bold body

--- libs/blocks/quick-facts/quick-facts.js
+++ libs/blocks/quick-facts/quick-facts.js
@@ -12,13 +12,23 @@ export default function init(el) {
   }
   const rows = el.querySelectorAll(':scope > div:not([class])');
   const facts = createTag('div', { class: 'fact-list' });
-  facts.append(...rows);
+  const isInset = !el.classList.contains('no-inset');
+  rows.forEach((row) => {
+    const heading = row.querySelector(':is(h1, h2, h3, h4, h5, h6)');
+    heading?.classList.add('fact-title');
+    if (isInset) heading?.classList.add('inset');
+    const boldEl = row.querySelector('strong');
+    boldEl?.parentElement.classList.add('bold-xl', 'fact-title');
+    if (isInset) boldEl?.parentElement.classList.add('inset');
+    facts.append(row);
+  });
   const productList = rows[rows.length - 1];
   productList.classList.add('product-list');
   foreground.append(facts, productList);
   decorateBlockText(el, ['m', 's'], 'merch');
   const subheading = el.querySelector('.product-list :is(h1, h2, h3, h4, h5, h6)');
   subheading?.classList.remove('heading-m');
-  subheading?.classList.add('subheading-xs');
-  decorateTextOverrides(el, ['-heading', '-body', '-subheading']);
+  subheading?.classList.add('subheading-xs', 'product-title');
+  if (isInset) subheading?.classList.add('inset');
+  decorateTextOverrides(el, ['-heading', '-body', '-subheading', '-bold']);
 }
