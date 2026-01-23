d31fb9c86249a0d692131c2ac91bb88ba6e68d10
AdobeLinhart <dlinhart@adobe.com>
2026-01-23 14:51:23 -0700

    Info tooltip

--- libs/features/personalization/preview.js
+++ libs/features/personalization/preview.js
@@ -480,6 +480,27 @@ export function getMepPopup(mepConfig, isMmm = false) {
 
   // Build Options : Toggles
   const mepToggleOptions = createTag('div', { class: 'mep-section' });
+  const infoSVG = `
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff" class="mep-info-icon">
+    <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+    </svg>`;
+  const mepToggleInfo = `
+    ${infoSVG}
+    <div class="mep-info-tooltip mep-section">
+      <h6 class="mep-section-header">Color Key</h6>
+      <div class="mep-section-data">
+        <span>Grey</span>
+        <span>Not a MEP swap</span>
+        <span>Green</span>
+        <span>MEP Lingo Swap</span>
+        <span>Yellow</span>
+        <span>MEP Lingo Fallback</span>
+        <span>Aqua</span>
+        <span>MEP Manifest Swap</span>
+      </div>
+    </div>
+    `;
+
   const showManifestsCheckbox = config.env?.name === 'prod' && !isMmm
     ? `<div>
         <input type="checkbox" name="mepHighlight${pageId}"
@@ -488,7 +509,10 @@ export function getMepPopup(mepConfig, isMmm = false) {
       </div>`
     : '';
   mepToggleOptions.innerHTML = `
-    <h6 class="mep-section-header">Toggles</h6>
+    <h6 class="mep-section-header">
+      Toggles
+      ${mepToggleInfo}
+    </h6>
     <div class="mep-manifest-variants">
       <div>
         <input type="checkbox" name="mepHighlight${pageId}"
