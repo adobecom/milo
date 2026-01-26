85280a6c80bee10ec59200b2adf5f8cd31bca23d
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-01-26 10:58:38 +0100

    update

--- test/utils/utils.test.js
+++ test/utils/utils.test.js
@@ -61,11 +61,11 @@ describe('Utils', () => {
     Object.keys(module).forEach((func) => {
       utils[func] = module[func];
     });
-    window.aem = { rum: { isSelected: false } };
+    window.hlx = { rum: { isSelected: false } };
   });
 
   after(() => {
-    delete window.aem;
+    delete window.hlx;
   });
 
   it('fetches with cache param', async () => {
