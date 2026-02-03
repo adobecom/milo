2270b8c855b78d2d535bba529e2fc00d0a6552ec
Narcis Radu <github@narcisradu.ro>
2026-02-03 11:48:54 +0200

    Implement feedback

--- libs/blocks/caas-config/caas-config.js
+++ libs/blocks/caas-config/caas-config.js
@@ -199,7 +199,7 @@ const defaultOptions = {
     workfront: 'Workfront',
     'bacom-blog': 'Bacom Blog',
     news: 'Newsroom',
-    blog: 'Blog',
+    'adobe-blog': 'Adobe Blog',
   },
   tagsUrl: 'https://www.adobe.com/chimera-api/tags',
   titleHeadingLevel: {
