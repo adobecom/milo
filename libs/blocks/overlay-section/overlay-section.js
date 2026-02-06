7b8a14109a238ad53d3296ac1bf52c2c57533ad7
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-02-06 12:03:59 +0100

    update

--- libs/blocks/overlay-section/overlay-section.js
+++ libs/blocks/overlay-section/overlay-section.js
@@ -1,4 +1,7 @@
+import { decorateButtons } from '../../utils/decorate.js';
+
 export default function init(el) {
+  decorateButtons(el.children[0].children[0]);
   el.children[0].children[0].classList.add('overlay-section-text');
   el.children[0].children[1].classList.add('overlay-section-video');
   const section = el.closest('.section');
