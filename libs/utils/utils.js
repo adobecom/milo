4d4fd553efcaebb069e48f9a6bf1ebf9e49b3442
Victor Hargrave <vhargrave@adobe.com>
2026-02-09 15:55:47 +0100

    fixes

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -649,7 +649,7 @@ async function loadQueryIndexes(prefix, onlyCurrentSite = false, links = []) {
   }
 
   if (config.queryIndexPath) {
-    const baseContentRoot = `${origin}${config.locale.base ? `/${config.locale.base}` : ''}${contentRoot}`;
+    const baseContentRoot = `${origin}${config.locale.base ? `/${config.locale.base !== undefined ? config.locale.base : config.locale.prefix.replace('/', '')}` : ''}${contentRoot}`;
     baseQueryIndex = processQueryIndexMap(
       `${baseContentRoot}${config.queryIndexPath}`,
       window.location.hostname,
@@ -679,7 +679,7 @@ async function loadQueryIndexes(prefix, onlyCurrentSite = false, links = []) {
       const domainInProdDomains = (d) => d.uniqueSiteId !== siteId
         && config.prodDomains?.includes(getDomainLingo(d.queryIndexWebPath));
 
-      const hasRegionalQueryIndex = (locale) => parseList(locale.regionalSites).includes(prefix);
+      const hasRegionalQueryIndex = (locale) => parseList(locale.regionalSites).some((e) => e.includes(prefix));
 
       siteQueryIndexMapLingo
         .filter(domainInProdDomains)
