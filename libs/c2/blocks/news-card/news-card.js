b6258522c40f51672f750b3942951a49ba4329b6
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-03-02 13:17:01 +0100

    update

--- libs/c2/blocks/news-card/news-card.js
+++
@@ -1,6 +0,0 @@
-import { decorateBlockText } from '../../../utils/decorate.js';
-
-export default function init(el) {
-  el.closest('.section').classList.add('news-card-section');
-  decorateBlockText(el);
-}
