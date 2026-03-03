a8de9680987a778db59b3987bec94cd4459181d5
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2026-03-03 15:24:06 +0530

    Adding market utility

---
+++ libs/utils/market.js
@@ -0,0 +1,46 @@
+import { getConfig, getMetadata, getCookie, getCountry, getFederatedContentRoot } from './utils.js';
+
+let marketConfig;
+const norm = (c) => (c?.toLowerCase() === 'uk' ? 'gb' : c?.toLowerCase()?.split('_')[0]);
+
+export async function getMarketConfig() {
+  if (marketConfig) return marketConfig;
+  const { contentRoot, marketSelector } = getConfig();
+  const sourceFromUrl = new URLSearchParams(window.location.search).get('marketSelector');
+  const marketSelectorKey = sourceFromUrl || getMetadata('marketselector') || marketSelector;
+  const marketsUrl = marketSelectorKey
+    ? `${contentRoot ?? ''}/market-selector/market-selector-${marketSelectorKey}.json`
+    : `${getFederatedContentRoot()}/federal/market-selector/market-selector.json`;
+  try {
+    const resp = await fetch(marketsUrl);
+    if (!resp.ok) throw new Error('Failed to load market config');
+    const json = await resp.json();
+    marketConfig = {
+      languages: json.languages?.data || [],
+      markets: json.markets?.data || [],
+    };
+    return marketConfig;
+  } catch (e) {
+    window.lana?.log(`Market Utils Error: ${e.message}`);
+    return null;
+  }
+}
+
+export async function getValidatedMarket() {
+  const config = await getMarketConfig();
+  const params = new URLSearchParams(window.location.search);
+  const countryParam = norm(params.get('country'));
+  const cookieMarket = getCookie('market');
+  let detectedMarket = countryParam || cookieMarket || norm(getCountry());
+  if (!detectedMarket) {
+    const { default: getAkamaiCode } = await import('./geo.js');
+    detectedMarket = norm(await getAkamaiCode());
+  }
+  if (!config) return detectedMarket || 'us';
+  const { locale } = getConfig();
+  const prefix = locale.prefix?.replace('/', '') || '';
+  const currLang = config.languages.find((l) => (l.prefix || '') === prefix) || config.languages[0];
+  const market = detectedMarket || currLang.defaultMarket || 'us';
+  const supported = currLang.markets?.split(',').map((m) => m.trim()) || [];
+  return supported.includes(market) ? market : currLang.defaultMarket;
+}
