a54c0079379150ed299d5cc762c34487d3484b76
H Parra <hgpa@adobe.com>
2026-03-12 15:36:08 -0700

    Localize sitemap-base links

--- libs/blocks/sitemap-base/sitemap-base.js
+++ libs/blocks/sitemap-base/sitemap-base.js
@@ -1,7 +1,11 @@
-export default function init(el) {
+import { decorateLinksAsync } from '../../utils/utils.js';
+
+export default async function init(el) {
   const items = el.querySelectorAll(':scope > div');
 
   items.forEach((item, index) => {
     item.classList.add('sitemap-base-item', `sitemap-base-item-${index + 1}`);
   });
+
+  await decorateLinksAsync(el);
 }
