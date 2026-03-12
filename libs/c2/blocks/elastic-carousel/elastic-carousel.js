aee2c9d9f0167d4f39b23ba378b70991a113d61a
Denys Fedotov <dfedotov@Adobe.local>
2026-03-12 17:24:40 -0600

    consuming the doc now

--- libs/c2/blocks/elastic-carousel/elastic-carousel.js
+++ libs/c2/blocks/elastic-carousel/elastic-carousel.js
@@ -30,46 +30,66 @@ const onCarouselHover = (event) => {
   if (slideIndex > 3) { carousel.classList.add('stick-right'); }
 };
 
-export default async function init(el) {
-  const section = el.closest('.section');
-  const slide = (index) => {
-    const content = `
-      <div class='elastic-carousel__item-container'>
-        <div class='elastic-carousel__item-header'>
-          <img src='https://www.adobe.com/cc-shared/assets/img/product-icons/svg/creative-cloud.svg'>
-          <p>Adobe Cloud</p>
-        </div>
-        <div class='elastic-carousel__item-media'>
-          <img src='https://www.adobe.com/cc-shared/fragments/creativecloud/pro/media_124da3b07bb29fe081c2748f8efbdadc8247b5c8c.jpg?width=2000&format=webply&optimize=medium'/>
-        </div>
-        <div class='elastic-carousel__item-footer'>
-          <h3>Content made easy</h3>
-          <p>Quickly create and edit images, video, and audio with creative AI.</p>
-        </div>
-      </div>
-    `;
-    const slideEl = createTag('div', {
-      class: 'elastic-carousel__item',
-      tabindex: 0,
-      'aria-label': 'number',
-      'data-index': index + 1,
-    }, content);
-    return slideEl;
+const buildSlide = ({ slide, index }) => {
+  const children = [...slide.children];
+  const slideObj = {
+    header: {
+      iconSrc: children[0].querySelector('img')?.src,
+      title: children[0].querySelector('p:not(:has(img))').innerText,
+    },
+    asset: {
+      imgSrc: children[1].querySelector('img')?.src,
+      videoSrc: children[1].querySelector('video')?.src
+    },
+    footer: {
+      title: children[2].querySelector('strong').innerText,
+      text: children[2].querySelector('p:not(:has(strong))').innerText,
+    },
   };
 
-  section.innerHTML = `
-  <div class='section before'>some content before</div>
-  <div class='elastic-carousel-container'>
-  <div class='elastic-carousel'></div>
-  </div>
-  <div class='section after'>some content after</div>`;
+  const content = `
+    <div class='elastic-carousel__item-container'>
+      <div class='elastic-carousel__item-header'>
+        <img src='${slideObj.header.iconSrc}'>
+        <p>${slideObj.header.title}</p>
+      </div>
+      <div class='elastic-carousel__item-media'>
+        ${slideObj.asset.imgSrc ? `<img src='${slideObj.asset.imgSrc}'/>` : ''}
+        ${slideObj.asset.videoSrc ? `<video src='${slideObj.asset.videoSrc}'/>` : ''}
+      </div>
+      <div class='elastic-carousel__item-footer'>
+        <h3>${slideObj.footer.title}</h3>
+        <p>${slideObj.footer.text}</p>
+      </div>
+    </div>
+  `;
+  const slideEl = createTag('div', {
+    class: 'elastic-carousel__item',
+    tabindex: 0,
+    'aria-label': 'slide X',
+    'data-index': index,
+    'ddl-lh': '',
+  }, content);
+  return slideEl;
+};
 
-  const carousel = section.querySelector('.elastic-carousel');
-  disableHoverOnScroll(carousel);
-  carousel.addEventListener('mouseleave', onCarouselLeave);
-  carousel.addEventListener('mouseover', onCarouselHover);
+const buildSlides = (carousel) => {
+  const slides = [...carousel.children];
+  const decoratedSlides = slides.map((slide, index) => buildSlide({ slide, index }));
+  // turn original div into carousel container
+  carousel.replaceChildren();
+  const newCarousel = createTag('div', {
+    class: carousel.className,
+  });
+  carousel.append(newCarousel);
+  carousel.className = 'elastic-carousel-container';
+  decoratedSlides.forEach((slide) => newCarousel.append(slide));
+  return newCarousel;
+};
 
-  for (let i = 0; i < 5; i += 1) {
-    carousel.append(slide(i));
-  }
+export default async function init(el) {
+  const decoratedCarousel = buildSlides(el);
+  disableHoverOnScroll(decoratedCarousel);
+  decoratedCarousel.addEventListener('mouseleave', onCarouselLeave);
+  decoratedCarousel.addEventListener('mouseover', onCarouselHover);
 }
