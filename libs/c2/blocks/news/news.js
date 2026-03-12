9a8fe17b72761b42b7fc3a6815fd9cc415890794
viloria <viloria@adobe.com>
2026-03-12 14:25:24 -0700

    move atag check logic to function

--- libs/c2/blocks/news/news.js
+++ libs/c2/blocks/news/news.js
@@ -15,6 +15,10 @@ const SECTION_CLASS_TRIGGERS = [
   // 'parallax-opacity',
 ];
 
+function isLinkOnlyContent(linkContainer, aTag) {
+  return aTag && aTag.textContent.trim() === linkContainer.textContent.trim();
+}
+
 function formatHeader(row) {
   row.classList.add('news-headline');
   const headlineText = row.textContent.trim();
@@ -30,25 +34,24 @@ function formatHeader(row) {
 }
 
 export default async function init(el) {
-  el.classList.add('news', ...BLOCK_CLASS_ADDITIONS);
+  el.classList.add(...BLOCK_CLASS_ADDITIONS);
   const sectionClasses = el.closest('.section').classList;
   const sectionMatches = SECTION_CLASS_TRIGGERS.filter((cls) => sectionClasses.contains(cls));
   sectionClasses.remove(...sectionMatches);
   const helperClasses = [];
   helperClasses.push(...sectionMatches);
-
   let rows = el.querySelectorAll(':scope > div');
   if (rows.length === 1) return;
   const [head, ...tail] = rows;
   formatHeader(head);
-  rows = tail || rows;
+  rows = tail;
   rows.forEach((row) => {
     row.classList.add('news-item');
     const pTags = row.querySelectorAll('p');
     pTags.forEach((p, indx) => {
       const linkEl = p.querySelector('a');
       if (indx === 0) p.classList.add('news-item-headline');
-      else if (linkEl && linkEl.innerText.trim() === p.innerText.trim()) {
+      else if (isLinkOnlyContent(p, linkEl)) {
         p.classList.add('news-item-link');
         linkEl.classList.add('standalone-link');
         if (el.classList.contains('quiet')) linkEl.classList.add('quiet');
