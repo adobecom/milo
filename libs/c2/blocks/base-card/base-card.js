5a5c66a5a29e8aa535370e4fa09b8f14ae960699
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-03-17 11:09:04 +0100

    update

--- libs/c2/blocks/base-card/base-card.js
+++ libs/c2/blocks/base-card/base-card.js
@@ -30,7 +30,7 @@ function decorateCard(wrapper) {
   media.classList.add('media');
   media.querySelector('picture').classList.add('parallax-scale-down');
   foreground.classList.add('foreground');
-  decorateBlockText(foreground, ['m', 'md']);
+  decorateBlockText(foreground);
   markStandaloneLinks(foreground);
   const firstCell = foreground.children[0];
   if (firstCell?.childElementCount !== 1 || firstCell?.firstElementChild?.tagName !== 'PICTURE') return;
