179fa867635bcb7006d4a1841fad7550904fd655
Bandana Laishram <bandanalaishram@gmail.com>
2026-03-24 15:32:03 +0530

    nit fix

--- libs/blocks/global-footer/global-footer.js
+++ libs/blocks/global-footer/global-footer.js
@@ -568,9 +568,8 @@ const footerInstances = new WeakMap();
 
 export default function init(block) {
   try {
-    if (footerInstances.has(block)) {
-      return footerInstances.get(block);
-    }
+    if (footerInstances.has(block)) return footerInstances.get(block);
+
     const footer = new Footer({ block });
     footerInstances.set(block, footer);
     if (isDarkMode()) block.classList.add('feds--dark');
