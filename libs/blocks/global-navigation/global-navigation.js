4932e8cd7235a81cefdf49ca65e5ffa0be82c4c3
Ketaki Deshwandikar <ketaki@youremail.com>
2026-04-01 15:57:18 +0530

    fix: move whatsnew placeholder before desktop apps CTA

--- libs/blocks/global-navigation/global-navigation.js
+++ libs/blocks/global-navigation/global-navigation.js
@@ -641,7 +641,6 @@ class Gnav {
         ${this.elements.navWrapper}
         ${getMetadata('product-entry-cta')?.toLowerCase() === 'on' ? toFragment`<div class="feds-product-entry-cta-placeholder"></div>` : ''}
         ${searchEnabled === 'on' && !isMiniGnav ? toFragment`<div class="feds-client-search"></div>` : ''}
-        ${whatsNew === 'on' ? toFragment`<div class="feds-client-whatsnew"></div>` : ''}
         ${isMiniGnav && desktopAppsCta ? toFragment`<div class="feds-client-desktop-apps"></div>` : ''}
         ${whatsNew === 'on' ? toFragment`<div class="feds-client-whatsnew"></div>` : ''}
         ${this.useUniversalNav ? this.blocks.universalNav : ''}
