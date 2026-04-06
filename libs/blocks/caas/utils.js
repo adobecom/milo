7ce2d2412e1dc8436234bd7b708cecf8673c989b
Sheridan Sunier <sunier@Sheridans-MacBook-Pro.local>
2026-04-06 09:50:44 -0700

    MWPW-191596: guardrails on bulk publisher

--- libs/blocks/caas/utils.js
+++ libs/blocks/caas/utils.js
@@ -685,6 +685,7 @@ async function getLingoSiteLocale(origin, path, fqdn = 'www.adobe.com') {
       tags: 'caas',
       severity: 'error',
     });
+    lingoSiteMapping.fromFallback = true;
   }
   return lingoSiteMapping;
 }
@@ -693,6 +694,7 @@ export const getLanguageFirstCountryAndLang = async (path, origin, fqdn) => {
   const localeArr = path.split('/');
   let langStr = 'en';
   let countryStr = 'xx';
+  let fromFallback = false;
   if (origin.toLowerCase() === 'news') {
     langStr = LANGS[localeArr[1]] ?? LANGS[''] ?? 'en';
     countryStr = LOCALES[localeArr[2]] ?? 'xx';
@@ -701,6 +703,7 @@ export const getLanguageFirstCountryAndLang = async (path, origin, fqdn) => {
     }
   } else {
     const mapping = await getLingoSiteLocale(origin, path, fqdn);
+    fromFallback = mapping.fromFallback === true;
     countryStr = LOCALES[mapping.country.toLowerCase()] ?? 'xx';
     if (typeof countryStr === 'object') {
       countryStr = countryStr.ietf?.split('-')[1] ?? 'xx';
@@ -710,6 +713,7 @@ export const getLanguageFirstCountryAndLang = async (path, origin, fqdn) => {
   return {
     country: countryStr.toLowerCase(),
     lang: langStr.toLowerCase(),
+    ...(fromFallback && { fromFallback: true }),
   };
 };
 
