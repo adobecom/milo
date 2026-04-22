d250427f8865c16c1ee632fc5224149b2cb7f7a6
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2026-04-22 19:00:19 +0530

    initial commit

--- libs/c2/blocks/marquee-test/marquee-test.js
+++ libs/c2/blocks/marquee-test/marquee-test.js
@@ -1,5 +1,5 @@
 export default function init(el) {
   console.log('el', el);
   el.classList.add('marquee-test');
-  el.innerHTML = 'Marquee test';
+  el.innerHTML = 'Work smarter with Acrobat. Edit and sign PDFs, understand your documents faster with AI, and share and collaborate with ease.';
 }
