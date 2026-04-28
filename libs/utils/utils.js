69ef2c4f409f768ec19670cb71b81cc9b3546554
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-04-28 14:54:08 +0200

    update

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -2225,30 +2225,31 @@ async function loadPostLCP(config) {
     import('../features/personalization/personalization.js')
       .then(({ addMepAnalytics }) => addMepAnalytics(config, header));
   }
-  if (getMetadata('foundation') === 'c2') {
-    // await Promise.all([
-    //   new Promise((resolve) => { loadStyle(`${config.base}/deps/lenis.min.css`, resolve); }),
-    //   loadScript(`${config.base}/deps/lenis.min.js`),
-    // ]);
-    // const lerp = parseFloat(PAGE_URL.searchParams.get('inertialFactor')) || 0.08;
-    // const fsThreshold = 110;
-    // const fsFactor = 0.11;
-    // const fsDelay = 700;
-    // const lenisPreventClasses = ['dialog-modal', 'ot-sdk-container', 'global-navigation'];
-    // window.lenis = new window.Lenis({
-    //   autoRaf: true,
-    //   lerp,
-    //   prevent: (node) => lenisPreventClasses.some((cls) => node.classList?.contains(cls)),
-    // });
-    // // Reduce inertia during fast scrolling to avoid sustained RAF CPU usage
-    // let fsScrollTimer;
-    // window.addEventListener('wheel', (e) => {
-    //   if (Math.abs(e.deltaY) > fsThreshold) {
-    //     window.lenis.options.lerp = fsFactor;
-    //     clearTimeout(fsScrollTimer);
-    //     fsScrollTimer = setTimeout(() => { window.lenis.options.lerp = lerp; }, fsDelay);
-    //   }
-    // }, { passive: true });
+  if (getMetadata('foundation') === 'c2' && !('ontouchstart' in window)) {
+    await Promise.all([
+      new Promise((resolve) => { loadStyle(`${config.base}/deps/lenis.min.css`, resolve); }),
+      loadScript(`${config.base}/deps/lenis.min.js`),
+    ]);
+    const lerp = parseFloat(PAGE_URL.searchParams.get('inertialFactor')) || 0.05;
+    const fsThreshold = 110;
+    const fsFactor = 0.11;
+    const fsDelay = 700;
+    const lenisPreventClasses = ['dialog-modal', 'ot-sdk-container', 'global-navigation'];
+    window.lenis = new window.Lenis({
+      autoRaf: true,
+      lerp,
+      syncTouch: false,
+      prevent: (node) => lenisPreventClasses.some((cls) => node.classList?.contains(cls)),
+    });
+    // Reduce inertia during fast scrolling to avoid sustained RAF CPU usage
+    let fsScrollTimer;
+    window.addEventListener('wheel', (e) => {
+      if (Math.abs(e.deltaY) > fsThreshold) {
+        window.lenis.options.lerp = fsFactor;
+        clearTimeout(fsScrollTimer);
+        fsScrollTimer = setTimeout(() => { window.lenis.options.lerp = lerp; }, fsDelay);
+      }
+    }, { passive: true });
   }
   // load privacy here if quick-link is present in first section
   const quickLink = document.querySelector('div.section')?.querySelector('.quick-link');
