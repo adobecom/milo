fdd2728b3026e80e28fb064a483938c42fdd1242
Snehal Sonawane <sonawane@Snehals-MacBook-Pro.local>
2026-05-05 11:44:40 +0530

    Considering base for babygeo check

--- libs/features/georoutingv2/georoutingv2.js
+++ libs/features/georoutingv2/georoutingv2.js
@@ -487,15 +487,15 @@ export default async function loadGeoRouting(
   if (storedLocale || storedLocale === '') {
     const urlLocaleGeo = urlLocale.split('_')[0];
     const storedLocaleGeo = storedLocale.split('_')[0];
-    // Show modal when url and cookie disagree (skip deprecated cookie rows)
+    // Show modal when url and cookie disagree. Skip if base matches and cookie geo is in locale row
     if (urlLocaleGeo !== storedLocaleGeo) {
       const localeMatches = json.georouting.data.filter(
         (d) => d.prefix === storedLocale,
       );
-      const storedLocaleData = localeMatches[0];
-      const isDeprecatedLocale = storedLocaleData?.akamaiCodes?.trim() === ''
+      const storedLocaleBase = config.locales?.[storedLocale]?.base;
+      const isSameBase = storedLocaleBase === urlLocale
         && getCodes(urlGeoData).includes(storedLocaleGeo);
-      if (isDeprecatedLocale) return;
+      if (isSameBase) return;
       const details = await getDetails(urlGeoData, localeMatches, json.geos.data);
       if (details) {
         handleOverflow(await showModal(details));
