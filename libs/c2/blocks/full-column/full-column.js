b89ec7afc8fecb5963d4d7dc86c2ec9660497233
Rares Munteanu <overmyheadandbody@gmail.com>
2026-05-06 19:49:18 +0200

    Wrap heading+body in text-content div, fix foreground layout

--- libs/c2/blocks/full-column/full-column.js
+++ libs/c2/blocks/full-column/full-column.js
@@ -1,4 +1,5 @@
 import { decorateBlockText, decorateViewportContent } from '../../../utils/decorate.js';
+import { createTag } from '../../../utils/utils.js';
 
 function decorate(block) {
   const row = block.children[0];
@@ -14,6 +15,14 @@ function decorate(block) {
     const iconWrapper = icon.closest('p');
     iconWrapper?.classList.add('icon');
     media?.prepend(iconWrapper);
+  }
+
+  const heading = foreground?.querySelector(':is(h1, h2, h3, h4, h5, h6)');
+  const body = heading?.nextElementSibling;
+  if (heading && body && !body.querySelector('a:only-child')) {
+    const textContent = createTag('div', { class: 'text-content' });
+    heading.before(textContent);
+    textContent.append(heading, body);
   }
 
   if (media) row.prepend(media);
