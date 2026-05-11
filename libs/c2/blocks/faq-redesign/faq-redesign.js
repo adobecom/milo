8ea383675816a74ff687796cad31a0472a16362f
Suhani <suhjain@Suhanis-MacBook-Pro.local>
2026-05-11 09:28:16 +0530

    faq-redesign

---
+++ libs/c2/blocks/faq-redesign/faq-redesign.js
@@ -0,0 +1,8 @@
+import { createTag, getFederatedUrl } from '../../../utils/utils.js';
+import { decorateBlockText, decorateViewportContent } from '../../../utils/decorate.js';
+
+
+export default function init(el) {
+  console.log('el', el);
+  // decorateViewportContent(el, decorate);
+}
