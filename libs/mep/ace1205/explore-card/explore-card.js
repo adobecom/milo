eb039448d62e6529f3ac0064ea80221443524b15
Ryan Clayton <rclayton@adobe.com>
2026-05-11 13:24:54 -0600

    Move files to mep ace1205. revert c2 files.

--- libs/mep/ace1205/explore-card/explore-card.js
+++ libs/mep/ace1205/explore-card/explore-card.js
@@ -1,5 +1,5 @@
 import { createTag, getFederatedUrl } from '../../../utils/utils.js';
-import { decorateBlockText, decorateViewportContent } from '../../../utils/decorate.js';
+import { decorateBlockText, decorateViewportContent, decorateBlockBg } from '../../../utils/decorate.js';
 
 function hasContent(node) {
   return node.textContent?.trim() !== '' || node.querySelector('img, picture, svg, a[href]');
@@ -27,19 +27,35 @@ function decorate(block, root) {
   const backgroundDiv = firstRow.querySelector(':scope > div:last-child');
   const isSvgUrl = (url) => /\.svg(\?.*)?$/i.test(url || '');
   const prodIcon = contentDiv?.querySelector('img');
-  const link = contentDiv?.querySelector('a');
+  const isVideoHref = (href) => /\.(mp4|webm|ogg|mov|m4v)(\?[^#]*)?(#.*)?$/i.test(href);
+  const link = [...(contentDiv?.querySelectorAll('a[href]:not([data-video-poster])') ?? [])]
+    .find((a) => !isVideoHref(a.href)) ?? null;
   const heading = contentDiv?.querySelector(':is(h1, h2, h3, h4, h5, h6)');
+  const contentAux = createTag('div', { class: 'content-aux' });
 
   if (prodIcon && isSvgUrl(prodIcon.src)) {
     prodIcon.src = getFederatedUrl(prodIcon.src);
+    prodIcon.closest('p').classList.add('icon');
   }
 
   backgroundDiv?.classList.add(`${blockName}-background`);
   contentDiv?.classList.add(`${blockName}-content`);
   firstRow.classList.add(`${blockName}-container`);
 
+  decorateBlockBg(block, backgroundDiv, { useHandleFocalpoint: true });
+
+  if (block !== root && block.style.background) {
+    const colorBg = createTag('div', {
+      class: `${blockName}-background`,
+      style: `background: ${block.style.background}`,
+    });
+    block.style.removeProperty('background');
+    firstRow.append(colorBg);
+  }
+
   getForegroundContent(foregroundRow, contentDiv, blockName);
   decorateBlockText(contentDiv, { heading: '5' });
+  contentDiv?.append(contentAux);
 
   if (!link) return;
   const linkContainer = createTag('a', { class: `${blockName}-link-container`, href: link.href, 'data-tracking-label': heading?.textContent });
