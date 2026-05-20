a03e8b48b5c73781123174ab9d70fe0503300e8d
AdobeLinhart <dlinhart@adobe.com>
2026-05-20 14:46:55 -0600

    Styling update. Content scaffolding.

--- libs/features/mep/overlay/mep-overlay.js
+++ libs/features/mep/overlay/mep-overlay.js
@@ -27,16 +27,51 @@ async function buildOverlay() {
   const marginOffset = 16;
   const gnavOffset = await getGnavOffset();
 
-  function generateFAB() {
-    const mepFabEl = createTag('div', { class: 'mep-fab', style: `top: ${gnavOffset + marginOffset}px` });
-    mepFabEl.innerHTML = svgData.svg['icon-mep-logo'];
+  function parseSvg(svgString) {
+    return new DOMParser().parseFromString(svgString, 'image/svg+xml').documentElement;
+  }
 
-    bodyEl.appendChild(mepFabEl);
+  function generateFAB() {
+    const mepFabDiv = createTag('div', { class: 'mep-fab', style: `top: ${gnavOffset + marginOffset}px` });
+    const mepIconLogoSvg = parseSvg(svgData.svg['icon-mep']);
+    mepFabDiv.appendChild(mepIconLogoSvg);
+    bodyEl.appendChild(mepFabDiv);
   }
 
   function generateDrawer() {
-    const mepOverlayEl = createTag('div', { class: 'mep-overlay', style: `top: ${gnavOffset}px` });
-    bodyEl.appendChild(mepOverlayEl);
+    // Header
+    const mepHeaderDiv = createTag('div', { class: 'mep-header' });
+    const mepLogoSvg = parseSvg(svgData.svg['logo-mep']);
+    const mepCloseSvg = parseSvg(svgData.svg['icon-close']);
+    mepHeaderDiv.append(mepLogoSvg, mepCloseSvg);
+
+    // Body
+    const mepContentDiv = createTag('div', { class: 'mep-body' });
+
+    const cards = [1, 2, 3];
+    cards.forEach((card) => {
+      const cardDiv = createTag('div', { class: 'mep-card' }, `Card ${card}`);
+      mepContentDiv.appendChild(cardDiv);
+    });
+
+    // Footer
+    const mepFooterDiv = createTag('div', { class: 'mep-footer' });
+    const previewBtn = createTag(
+      'a',
+      {
+        class: 'con-button button-l fill',
+        title: 'Preview',
+        href: '#',
+      },
+      'Preview',
+    );
+    mepFooterDiv.append(previewBtn);
+
+    // Drawer
+    const mepDrawerDiv = createTag('div', { class: 'mep-drawer', style: `top: ${gnavOffset}px; height: calc(100vh - ${gnavOffset}px)` });
+    mepDrawerDiv.append(mepHeaderDiv, mepContentDiv, mepFooterDiv);
+
+    bodyEl.appendChild(mepDrawerDiv);
   }
 
   generateFAB();
