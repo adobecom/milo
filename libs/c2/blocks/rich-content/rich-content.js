4e0eb2b7a0bc991ac324a6579dd1d755c095d191
Dusan Kosanovic <dusan.kosanovic@hitthecode.com>
2026-05-22 23:14:59 +0200

    init

--- libs/c2/blocks/rich-content/rich-content.js
+++ libs/c2/blocks/rich-content/rich-content.js
@@ -35,6 +35,60 @@ function decorate(block) {
   promoteParagraphHeading(content);
 }
 
+// --- video variant ---
+
+function decorateVideoMedia(mediaCell) {
+  const link = mediaCell.querySelector('a[href]');
+  if (!link) return;
+
+  const isVideoLink = /\.mp4($|\?)/i.test(link.href) || link.href.includes('/video/');
+  if (!isVideoLink) return;
+
+  const picture = mediaCell.querySelector('picture');
+  const video = createTag('video', { autoplay: '', muted: '', loop: '', playsinline: '' });
+  video.append(createTag('source', { src: link.href, type: 'video/mp4' }));
+
+  const wrap = createTag('div', { class: 'media-wrap' });
+  if (picture) wrap.append(picture);
+  wrap.append(video);
+  link.replaceWith(wrap);
+}
+
+function decorateVideoVariant(el) {
+  const [mediaRow, ctaRow] = [...el.children];
+
+  // Extract background image from second column and make it the block background
+  if (mediaRow?.children[1]) {
+    const bgPicture = mediaRow.children[1].querySelector('picture');
+    if (bgPicture) {
+      const bgDiv = createTag('div', { class: 'video-bg' });
+      bgDiv.append(bgPicture);
+      el.prepend(bgDiv);
+    }
+  }
+
+  // Promote media cell to direct block child
+  if (mediaRow?.children[0]) {
+    const mediaCell = mediaRow.children[0];
+    mediaCell.classList.add('media');
+    decorateVideoMedia(mediaCell);
+    el.append(mediaCell);
+  }
+  mediaRow?.remove();
+
+  // Promote CTA cell to direct block child
+  if (ctaRow?.children[0]) {
+    const ctaCell = ctaRow.children[0];
+    ctaCell.classList.add('cta-area');
+    el.append(ctaCell);
+  }
+  ctaRow?.remove();
+}
+
 export default function init(el) {
+  if (el.classList.contains('video')) {
+    decorateVideoVariant(el);
+    return;
+  }
   decorateViewportContent(el, decorate);
 }
