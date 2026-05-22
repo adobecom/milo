0e01dc46c4fe84e741d3e80773e78799ba53f9cc
markpadbe <markp@adobe.com>
2026-05-22 16:22:27 -0400

    Fix mep-overlay import path after mep-next rename

--- libs/features/personalization/personalization.js
+++ libs/features/personalization/personalization.js
@@ -1723,7 +1723,7 @@ export async function init(enablements = {}) {
   try {
     if (manifests?.length) await applyPers({ manifests });
     if (config.mep?.preview) await import('./preview.js').then(({ saveToMmm }) => saveToMmm());
-    if (config.mep?.preview) await import('../mep/overlay/mep-overlay.js');
+    if (config.mep?.preview) await import('../mep/mep-next/mep-overlay.js');
   } catch (e) {
     log(`MEP Error: ${e.toString()}`);
     window.lana?.log(`MEP Error: ${e.toString()}`);
