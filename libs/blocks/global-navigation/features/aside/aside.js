c7f8f6e74dd4aee218eef8a088b92a303023f8c5
Bandana Laishram <bandanalaishram@gmail.com>
2026-05-29 11:49:27 +0530

    Adding dismiss option to gnav promo-bar

--- libs/blocks/global-navigation/features/aside/aside.js
+++ libs/blocks/global-navigation/features/aside/aside.js
@@ -1,6 +1,42 @@
-import { loadBlock, decorateAutoBlock } from '../../../../utils/utils.js';
+import { loadBlock, decorateAutoBlock, loadStyle, getConfig } from '../../../../utils/utils.js';
 import { toFragment, lanaLog } from '../../utilities/utilities.js';
 import { processTrackingLabels } from '../../../../martech/attributes.js';
+
+const DISMISSED_KEY = (promoPath) => `gnav-promo-dismissed:${promoPath}`;
+
+const closeSvg = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+                    <g transform="translate(-10500 3403)">
+                      <circle cx="10" cy="10" r="10" transform="translate(10500 -3403)" fill="#707070"></circle>
+                      <line y1="8" x2="8" transform="translate(10506 -3397)" fill="none" stroke="#fff" stroke-width="2"></line>
+                      <line x1="8" y1="8" transform="translate(10506 -3397)" fill="none" stroke="#fff" stroke-width="2"></line>
+                    </g>
+                  </svg>`;
+
+function addCloseButton(aside, fedsPromoWrapper, promoPath, headerElem) {
+  const { miloLibs, codeRoot } = getConfig();
+  const base = miloLibs || codeRoot;
+  loadStyle(`${base}/blocks/global-navigation/features/aside/aside.css`);
+
+  const closeBtn = document.createElement('button');
+  closeBtn.className = 'promo-close';
+  closeBtn.setAttribute('aria-label', 'Close');
+  closeBtn.innerHTML = closeSvg;
+
+  aside.querySelector('.foreground').appendChild(closeBtn);
+
+  closeBtn.addEventListener('click', () => {
+    localStorage.setItem(DISMISSED_KEY(promoPath), '1');
+    fedsPromoWrapper.remove();
+    headerElem.classList.remove('has-promo');
+
+    const header = document.querySelector('header');
+    const localNav = document.querySelector('.feds-localnav');
+    if (header) header.style.top = '0';
+    if (localNav) localNav.style.top = '0';
+
+    document.dispatchEvent(new CustomEvent('milo:sticky:closed'));
+  });
+}
 
 export default async function decorateAside({ headerElem, fedsPromoWrapper, promoPath } = {}) {
   const onError = () => {
@@ -13,6 +49,12 @@ export default async function decorateAside({ headerElem, fedsPromoWrapper, prom
     });
     return '';
   };
+
+  if (localStorage.getItem(DISMISSED_KEY(promoPath))) {
+    fedsPromoWrapper?.remove();
+    headerElem?.classList.remove('has-promo');
+    return '';
+  }
 
   const fragLink = toFragment`<a href="${promoPath}">${promoPath}</a>`;
   const fragTemplate = toFragment`<div>${fragLink}</div>`;
@@ -28,6 +70,10 @@ export default async function decorateAside({ headerElem, fedsPromoWrapper, prom
   aside.querySelectorAll('a').forEach((link, index) => {
     link.setAttribute('daa-ll', `${processTrackingLabels(link.textContent)}--${index + 1}`);
   });
+
+  if (aside.classList.contains('dismissable')) {
+    addCloseButton(aside, fedsPromoWrapper, promoPath, headerElem);
+  }
 
   return aside;
 }
