a81e93ccee0f4542343a7a3a61396bf8fafa424e
Nishant Kaushik <nishantkaushikadobe@gmail.com>
2026-06-01 16:50:46 +0530

    MWPW-194269: update css as per the new assets + css update as per new figma

--- libs/mep/ace1205/quick-actions/quick-actions.js
+++ libs/mep/ace1205/quick-actions/quick-actions.js
@@ -1,7 +1,7 @@
 import { decorateViewportContent, decorateBlockText } from '../../../utils/decorate.js';
 import { createTag } from '../../../utils/utils.js';
 
-const CHEVRON_SVG = '<svg viewBox="0 0 4.5 7.5" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.75 6.75L3.75 3.75L0.75 0.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+const CHEVRON_SVG = '<svg aria-hidden="true" width="5" height="8" viewBox="0 0 5 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.75 6.75L3.75 3.75L0.75 0.75" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>';
 
 function decorateSectionHeader(block) {
   const firstRow = block.children[0];
