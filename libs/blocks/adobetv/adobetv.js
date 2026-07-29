ba0903783580927ea87f02401f51909319e5833c
Raghu A <125877471+raga-adbe-gh@users.noreply.github.com>
2026-07-29 16:30:39 +0530

    Update adobetv.js
    incorrect captions url

--- libs/blocks/adobetv/adobetv.js
+++ libs/blocks/adobetv/adobetv.js
@@ -103,7 +103,7 @@ export default function init(a) {
 
     if (geo && federalCR && url.searchParams.has('captions')) {
       if (!captionsLangMapPromise) {
-        const captionsUrl = `${federalCR}/federal/assets/data/adobetv-captions.json?sheet=${atvCaptionsKey}`;
+        const captionsUrl = `${federalCR}/federal/assets/data/adobetv-captions1.json?sheet=${atvCaptionsKey}`;
         captionsLangMapPromise = fetch(captionsUrl).then((res) => {
           if (!res.ok) {
             return new Promise(() => { throw new Error(`Failed to fetch ${captionsUrl}`); });
