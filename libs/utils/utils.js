9b395e12899545dce1b5ed5c93acc4419db47246
Ratko Zagorac <ratkozagorac@gmail.com>
2026-08-25 17:05:27 +0200

    Add a guard in decorateAutoBlock for merch links

--- libs/utils/utils.js
+++ libs/utils/utils.js
@@ -1536,6 +1536,7 @@ export function isTrustedAutoBlock(autoBlock, url) {
 }
 
 export function decorateAutoBlock(a) {
+  if (a.hasAttribute('data-wcs-osi')) return false;
   const config = getConfig();
   let url;
   try {
