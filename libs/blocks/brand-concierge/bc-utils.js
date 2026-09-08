ff8f7371e28446f56c7bc1055be5a8e484e7c684
Cody Lloyd <colloyd@adobe.com>
2026-09-08 13:42:56 -0600

    handle overlay top value for cases with breadcrumbs, or if elements are above the gnav

--- libs/blocks/brand-concierge/bc-utils.js
+++ libs/blocks/brand-concierge/bc-utils.js
@@ -39,20 +39,6 @@ export function hasChatCookie() {
   return false;
 }
 
-export function setCssGnavHeight() {
-  const gnav = document.querySelector('header.global-navigation');
-  const localGnav = document.querySelector('div.feds-localnav');
-  const localNavStyle = localGnav ? getComputedStyle(localGnav) : null;
-  const localNavOn = localGnav && localNavStyle ? localNavStyle.display !== 'none' : false;
-
-  if (!gnav) return;
-  const rootStyles = getComputedStyle(document.documentElement);
-  const gnavHeight = Number(rootStyles.getPropertyValue('--global-height-nav').trim().slice(0, -2));
-  const localNavHeight = Number(rootStyles.getPropertyValue('--feds-localnav-height').trim().slice(0, -2));
-  const newHeight = gnavHeight + (localGnav && localNavOn ? localNavHeight : 0);
-  document.documentElement.style.setProperty('--bc-gnav-height', `${newHeight}px`);
-}
-
 export function handleConsent(el) {
   if (!window.adobePrivacy) return;
   const cookieGrp = window.adobePrivacy.activeCookieGroups();
