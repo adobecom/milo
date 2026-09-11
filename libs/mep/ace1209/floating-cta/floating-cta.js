09acfcb1a800d873696c6a58c27b612944e7d7f6
Swarnim Negi <swarnimn@adobe.com>
2026-09-11 15:23:17 +0530

    Fixed minor issues

--- libs/mep/ace1209/floating-cta/floating-cta.js
+++ libs/mep/ace1209/floating-cta/floating-cta.js
@@ -322,9 +322,10 @@ function applyCustomHide(el, ctaEl, animation) {
   function getMarqueeBoundary() {
     const allCandidates = [...document.querySelectorAll(marqueeSelector)]
       .filter((candidate) => !candidate.closest('.floating-cta'));
-    const precedingCandidates = allCandidates.filter((candidate) => 
+    const precedingCandidates = allCandidates.filter((candidate) => {
       // eslint-disable-next-line no-bitwise
-      candidate.compareDocumentPosition(ctaBoundary) & Node.DOCUMENT_POSITION_FOLLOWING);
+      return candidate.compareDocumentPosition(ctaBoundary) & Node.DOCUMENT_POSITION_FOLLOWING
+    });
     const marquee = precedingCandidates[precedingCandidates.length - 1] || allCandidates[0];
     if (marquee) return getBoundary(marquee);
     let sibling = ctaBoundary?.previousElementSibling;
